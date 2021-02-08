35de0c0abeb2cd6bb3f8faeed3ebc40f4dd6da90
gisungchoi <chlrltjd159@naver.com>
2021-02-08 16:04:21 +0900

    pawnTest class 작성
    Junit을  pawn을 test할 클래스를 작성했다.

---
+++ src/main/java/test/pawnTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import net.coco.chess.Pawn;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class pawnTest {
+
+
+}

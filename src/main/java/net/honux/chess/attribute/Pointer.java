dd55e31e9bc08bf05150ac6239cfbb7e617222b2
tmdgusya <tmdgusya@naver.com>
2021-02-08 15:52:00 +0900

    Piece && Pointer 코드 오류 수정 및 개선
    - 필드가 private 하게 선언되어 있지 않던 부분 private 으로 변경
    - Pointer 의 경우 separator 필드를 추가하여 구분자로써 작용하게 함.
      - setPointer 함수의 경우 Pointer 를 받아와서 해당 함수의 Pointer 를 변경시키도록 함.
      - setter 함수는 내부 로직에서만 이용되므로 private 처리함

--- src/main/java/net/honux/chess/attribute/Pointer.java
+++ src/main/java/net/honux/chess/attribute/Pointer.java
@@ -2,17 +2,30 @@ package net.honux.chess.attribute;
 
 public class Pointer {
 
-    char x;
-    int y;
+    private char x;
+    private int y;
+    private final String separator = ",";
 
     public Pointer(char x, int y) {
         this.x = x;
         this.y = y;
     }
 
+    public void setPointer(Pointer pointer){
+        String pointer1 = pointer.getPointer();
+        this.x = pointer1.split(separator)[0].charAt(0);
+        this.y = Integer.parseInt(pointer1.split(separator)[1]);
+    }
+
+    private void setX(char x) {
+        this.x = x;
+    }
+
+    private void setY(int y) {
+        this.y = y;
+    }
+
     public String getPointer(){
-        StringBuilder sb = new StringBuilder(this.x);
-        sb.append(this.y);
-        return sb.toString();
+        return String.valueOf(this.x) + separator + this.y;
     }
 }
--- src/main/java/net/honux/chess/entity/pieces/Piece.java
+++ src/main/java/net/honux/chess/entity/pieces/Piece.java
@@ -4,10 +4,16 @@ import net.honux.chess.attribute.Color;
 import net.honux.chess.attribute.Pointer;
 
 public abstract class Piece {
-    String name;
-    Color color;
-    Pointer currentPointer;
-    boolean alive;
+    private String name;
+    private Color color;
+    private Pointer currentPointer;
+    private boolean alive = true;
     abstract void move();
     abstract void kill();
+
+    public Piece(String name, Color color, Pointer currentPointer) {
+        this.name = name;
+        this.color = color;
+        this.currentPointer = currentPointer;
+    }
 }

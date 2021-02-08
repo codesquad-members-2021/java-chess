b07e14ca179a4c68e074da373eca4c7110d28817
K <PizzaCola.K@gmail.com>
2021-02-08 14:51:44 +0900

    test: "black" color test
    - Add a test case that the color of Pawn is "black".

--- src/test/java/pieces/PawnTest.java
+++ src/test/java/pieces/PawnTest.java
@@ -7,9 +7,12 @@ import org.junit.jupiter.api.Test;
 public class PawnTest {
 
     @Test
-    @DisplayName("흰색 폰이 생성되어야 한다")
+    @DisplayName("흰색과 검은색 폰이 생성되어야 한다")
     public void create() {
-        Pawn pawn = new Pawn("white");
-        assertThat(pawn.getColor()).isEqualTo("white");
+        Pawn whitePawn = new Pawn("white");
+        assertThat(whitePawn.getColor()).isEqualTo("white");
+
+        Pawn blackPawn = new Pawn("black");
+        assertThat(blackPawn.getColor()).isEqualTo("black");
     }
 }

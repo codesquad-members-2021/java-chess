d016b03298ccb38829b1c52e3f8c6315a28670ae
jeong-inho <derosatam76@gmail.com>
2021-02-08 18:24:34 +0900

    Create default constructor in Pawn class Pawn class에 호출 시 흰색 폰이 만들어지는 기본 생성자 생성

--- src/main/java/pieces/Pawn.java
+++ src/main/java/pieces/Pawn.java
@@ -4,6 +4,10 @@ public class Pawn {
 
     private final String color;
 
+    public Pawn() {
+        this.color = "white";
+    }
+
     public Pawn(String color) {
         this.color = color;
     }

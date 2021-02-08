771c0618d7fc11a54556081f389062341624246c
jeong-inho <derosatam76@gmail.com>
2021-02-09 00:03:47 +0900

    Change method name in PawnTest class create_defaultConstructor 메서드의 이름을 createDefaultConstructor로 변경

--- src/test/java/pieces/PawnTest.java
+++ src/test/java/pieces/PawnTest.java
@@ -20,7 +20,7 @@ public class PawnTest {
 
     @Test
     @DisplayName("기본 생성자 호출 시 흰색 폰이 생성되어야 한다")
-    public void create_defaultConstructor() {
+    public void createDefaultConstructor() {
         Pawn pawn = new Pawn();
         assertEquals(Color.WHITE, pawn.getColor());
     }

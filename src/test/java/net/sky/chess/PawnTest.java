9ed6e53da7285672a866763679f16cf87458d2e1
aksmf1442 <64648893+aksmf1442@users.noreply.github.com>
2021-02-08 16:47:53 +0900

    Simplify PawnTest
    중복된 코드를 제거하기 위해 verifyPawn(final String color)메소드를 추가하여
    가독성을 높였다.

--- src/test/java/net/sky/chess/PawnTest.java
+++ src/test/java/net/sky/chess/PawnTest.java
@@ -11,10 +11,13 @@ class PawnTest {
   @Test
   @DisplayName("흰색 폰이 생성되어야 한다.")
   public void create(){
-    Pawn pawn = new Pawn("white");
-    assertThat(pawn.getColor()).isEqualTo("white");
-
-    Pawn pawn1 = new Pawn("black");
-    assertThat(pawn1.getColor()).isEqualTo("black");
+    verifyPawn("white");
+    verifyPawn("black");
   }
+
+  private void verifyPawn(final String color) {
+    Pawn pawn = new Pawn(color);
+    assertThat(pawn.getColor()).isEqualTo(color);
+  }
+
 }

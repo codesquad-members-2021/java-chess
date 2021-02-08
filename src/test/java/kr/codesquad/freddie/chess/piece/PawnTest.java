2f83ddb25be13825f02cf9edca2ecb345d849a6a
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-02-08 17:14:40 +0900

    Improve create in PawnTest
    checkColor 추가하여 중복 제거

--- src/test/java/kr/codesquad/freddie/chess/piece/PawnTest.java
+++ src/test/java/kr/codesquad/freddie/chess/piece/PawnTest.java
@@ -1,23 +1,18 @@
 package kr.codesquad.freddie.chess.piece;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
 class PawnTest {
-
     @Test
-    @DisplayName("흰색 생성")
-    void create_white() {
-        assertThat(Pawn.create("white"))
-                .isEqualTo(new Pawn(Color.WHITE));
+    void create() {
+        checkColor(Pawn.create("white"), Color.WHITE);
+        checkColor(Pawn.create("black"), Color.BLACK);
     }
 
-    @Test
-    @DisplayName("검은색 생성")
-    void create_black() {
-        assertThat(Pawn.create("black"))
-                .isEqualTo(new Pawn(Color.BLACK));
+    private void checkColor(Pawn pawn, Color expected) {
+        assertThat(pawn.getColor())
+                .isEqualTo(expected);
     }
 }

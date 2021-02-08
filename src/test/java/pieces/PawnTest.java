39719040f101d9f8fb4b508f480722bc85cd9aff
jeong-inho <derosatam76@gmail.com>
2021-02-08 17:18:00 +0900

    Create verifyPawn method in PawnTest.java create 메서드 안에 중복코드를 제거하기 위한 verifyPawn 메서드 생성

--- src/test/java/pieces/PawnTest.java
+++ src/test/java/pieces/PawnTest.java
@@ -6,9 +6,14 @@ import static org.assertj.core.api.Assertions.*;
 public class PawnTest {
 
     @Test
-    @DisplayName("흰색 폰이 생성되어야 한다")
+    @DisplayName("흰색/검은색 폰이 생성되어야 한다")
     public void create() {
-        Pawn pawn = new Pawn("white");
-        assertThat(pawn.getColor()).isEqualTo("white");
+        verifyPawn("white");
+        verifyPawn("black");
+    }
+
+    public void verifyPawn(final String color) {
+        Pawn pawn = new Pawn(color);
+        assertThat(pawn.getColor()).isEqualTo(color);
     }
 }

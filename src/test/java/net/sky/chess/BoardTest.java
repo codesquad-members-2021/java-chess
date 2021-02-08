ab4d42ba32e5ebadfef65645eb6a60adbae89cde
aksmf1442 <64648893+aksmf1442@users.noreply.github.com>
2021-02-08 17:46:27 +0900

    Add BoardTest for Board, Update Board
    BoardTest(테스트코드)를 먼저 작성하고 이를 기반으로 Board에 필요한 기능들을 구현했다.

---
+++ src/test/java/net/sky/chess/BoardTest.java
@@ -0,0 +1,24 @@
+package net.sky.chess;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BoardTest {
+  @Test
+  public void create() throws Exception{
+    Board board = new Board();
+
+    Pawn white = new Pawn(Pawn.WHITE);
+    board.add(white);
+    assertThat(board.size()).isEqualTo(1);
+    assertThat(board.findPawn(0)).isEqualTo(white);
+
+    Pawn black = new Pawn(Pawn.BLACK);
+    board.add(black);
+    assertThat(board.size()).isEqualTo(2);
+    assertThat(board.findPawn(1)).isEqualTo(black);
+
+  }
+}

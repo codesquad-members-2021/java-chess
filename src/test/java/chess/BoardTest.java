ca9eff3e4ccbb6cbd15ee09794ea95e728572a4f
K <PizzaCola.K@gmail.com>
2021-02-08 19:42:33 +0900

    test: Refactor BoardTest
    - Add addPawnToBoard() method to remove duplicate code.
    - Board board : Pawn will be added to the board.
    - Pawn pawn : it will be added.
    - int size : the number of Pawn pieces for test.

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -12,16 +12,18 @@ public class BoardTest {
     @DisplayName("게임 보드 생성 테스트")
     public void create() throws Exception {
         Board board = new Board();
-
         Pawn white = new Pawn(Pawn.WHITE_COLOR);
-        board.add(white);
-        assertThat(board.size()).isEqualTo(1);
-        assertThat(board.findPawn(0)).isEqualTo(white);
-
         Pawn black = new Pawn(Pawn.BLACK_COLOR);
-        board.add(black);
-        assertThat(board.size()).isEqualTo(2);
-        assertThat(board.findPawn(1)).isEqualTo(black);
+
+        addPawnToBoard(board, white, 1);
+        addPawnToBoard(board, black, 2);
+
+    }
+
+    public void addPawnToBoard(Board board, Pawn pawn, int size) {
+        board.add(pawn);
+        assertThat(board.size()).isEqualTo(size);
+        assertThat(board.findPawn(size - 1)).isEqualTo(pawn);
     }
 
 }

d6f3741b58a8015d07e6efca3344ca33ab1bd9cb
MJbae <gently.gogo@gmail.com>
2021-02-08 17:37:00 +0900

    Add: Pawn class 초기 정의

--- src/main/java/mj/chess/Pawn.java
+++ src/main/java/mj/chess/Pawn.java
@@ -1,4 +1,13 @@
 package mj.chess;
 
 public class Pawn {
+    private String color;
+
+    Pawn(String color){
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
 }

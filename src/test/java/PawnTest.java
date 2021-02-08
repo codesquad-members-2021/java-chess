7c98f4e4ec30fb7061b04c062bd77e4150a2ee01
Shion <ehdrhelr@gmail.com>
2021-02-08 16:46:19 +0900

    feat : add the Pawn and PawnTest classes
    The Pawn class has a String-typed variable named 'color' and it is initialized by a parameter when an instance of the Pawn class is generated.
    
    The PawnTest class checks the above process works well.

---
+++ src/test/java/PawnTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class) // 테스트 메서드 이름의 언더바(_)를 지워준다
+public class PawnTest {
+
+    @Test
+    @DisplayName("흰색과 검은색 폰이 생성되어야 한다")  // @DisplayNameGeneration보다 우선이다.
+    void create_white_and_black_pawns() {      // JUnit5부터 public 생략 가능하다(리플렉션? 때문)
+        final String white = "white";
+        final String black = "black";
+
+        assertAll(                             // 여러 개의 assert문을 모두 실행해서 실패한 모든 assert문을 알려준다.
+                () -> verifyPawn(white),
+                () -> verifyPawn(black)
+        );
+    }
+
+    void verifyPawn(final String color) {
+        Pawn pawn = new Pawn(color);
+        assertEquals(color           // 기대값
+                , pawn.getColor()    // 실제값
+                , () -> color + "를 인자로 생성된 Pawn인스턴스는 color속성의 값으로 " + color + "를 가지고 있어야 한다"); // 마지막 인자값을 람다식으로 작성했을 경우, 테스트 실패시에만 연산한다.
+    }
+}

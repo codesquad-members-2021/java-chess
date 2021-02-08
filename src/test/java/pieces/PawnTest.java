332575d89e5fbe5bee0b2e999bddcac507f044aa
jeong-inho <derosatam76@gmail.com>
2021-02-08 23:16:05 +0900

    Fix string color to enum color 문자열 흰색과 검은색을 사용한 부분을 color enum의 상수로 수정

--- src/test/java/pieces/PawnTest.java
+++ src/test/java/pieces/PawnTest.java
@@ -9,20 +9,20 @@ public class PawnTest {
     @Test
     @DisplayName("흰색과 검은색 폰이 생성되어야 한다")
     public void create() {
-        verifyPawn("white");
-        verifyPawn("black");
+        verifyPawn(Color.WHITE);
+        verifyPawn(Color.BLACK);
     }
 
-    public void verifyPawn(final String color) {
+    public void verifyPawn(final Color color) {
         Pawn pawn = new Pawn(color);
         assertThat(pawn.getColor()).isEqualTo(color);
     }
 
     @Test
     @DisplayName("기본 생성자 호출 시 흰색 폰이 생성되어야 한다")
-    public void create_defaultConstructor() throws Exception {
+    public void create_defaultConstructor() {
         Pawn pawn = new Pawn();
-        assertEquals("white", pawn.getColor());
+        assertEquals(Color.WHITE, pawn.getColor());
     }
 
 }

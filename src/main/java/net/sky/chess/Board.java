0143573351b1ecfb0b44d890020a10dbd15a8ae7
aksmf1442 <64648893+aksmf1442@users.noreply.github.com>
2021-02-08 17:46:27 +0900

    Add BoardTest for Board, Update Board
    BoardTest(테스트코드)를 먼저 작성하고 이를 기반으로 Board에 필요한 기능들을 구현했다.

--- src/main/java/net/sky/chess/Board.java
+++ src/main/java/net/sky/chess/Board.java
@@ -1,5 +1,25 @@
 package net.sky.chess;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Board {
 
+  List<Pawn> pawns;
+
+  public Board() {
+    pawns = new ArrayList<>();
+  }
+
+  public void add(Pawn pawn) {
+    pawns.add(pawn);
+  }
+
+  public int size(){
+    return pawns.size();
+  }
+
+  public Pawn findPawn(int idx){
+    return pawns.get(idx);
+  }
 }

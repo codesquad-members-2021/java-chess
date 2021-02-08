71cdef9aa823890e85af8f899781dbae791d2753
gisungchoi <chlrltjd159@naver.com>
2021-02-08 16:05:52 +0900

    create 메소드 작성
    폰을 생성할 때 요청한 색상대로 만들어 졌는지
    확인하는 메소드 작성

--- src/main/java/test/pawnTest.java
+++ src/main/java/test/pawnTest.java
@@ -9,5 +9,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class pawnTest {
 
+    @Test
+    @DisplayName("Pawn Color Test")
+    public void create(){
+        verifyPawn("white");
+        verifyPawn("black");
+    }
 
+    public void verifyPawn(final String color){
+        Pawn pawn=new Pawn(color);
+        Assertions.assertEquals(color,pawn.getColor());
+    }
 }

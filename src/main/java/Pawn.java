c8e6f40cffbdd65911e4165f6aa08b58b6981587
Shion <ehdrhelr@gmail.com>
2021-02-08 20:58:17 +0900

    feat : add the default constructor in the Pawn class and a test method for it in the PawnTest class
    And also add String literals for color, black and white, in the Pawn class.

--- src/main/java/Pawn.java
+++ src/main/java/Pawn.java
@@ -1,8 +1,15 @@
 public class Pawn {
+    public static final String BLACK = "black";
+    public static final String WHITE = "white";
+
     private String color;
 
     Pawn(String color) {
         this.color = color;
+    }
+
+    Pawn() {
+        this(WHITE); // 생성자에서 같은 클래스의 다른 생성자를 호출한다.
     }
 
     public String getColor() {
--- src/test/java/PawnTest.java
+++ src/test/java/PawnTest.java
@@ -5,8 +5,9 @@ import static org.junit.jupiter.api.Assertions.*;
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class) // 테스트 메서드 이름의 언더바(_)를 지워준다
 public class PawnTest {
 
+    @Disabled
     @Test
-    @DisplayName("흰색과 검은색 폰이 생성되어야 한다")  // @DisplayNameGeneration보다 우선이다.
+    @DisplayName("흰색과 검은색 폰이 생성되어야 한다") // @DisplayNameGeneration보다 우선이다.
     void create_white_and_black_pawns() {      // JUnit5부터 public 생략 가능하다(리플렉션? 때문)
         final String white = "white";
         final String black = "black";
@@ -22,5 +23,12 @@ public class PawnTest {
         assertEquals(color           // 기대값
                 , pawn.getColor()    // 실제값
                 , () -> color + "를 인자로 생성된 Pawn인스턴스는 color속성의 값으로 " + color + "를 가지고 있어야 한다"); // 마지막 인자값을 람다식으로 작성했을 경우, 테스트 실패시에만 연산한다.
+    }
+
+    @Test
+    @DisplayName("기본생성자로 흰색 폰이 생성되어야 한다.")
+    void create_default_constructor() throws Exception {
+        Pawn pawn = new Pawn();
+        assertEquals(Pawn.WHITE, pawn.getColor());
     }
 }

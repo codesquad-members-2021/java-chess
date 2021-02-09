6d675132aa30f46793b5deee4685dec2e6e7e4b8
Shion <ehdrhelr@gmail.com>
2021-02-09 15:15:24 +0900

    feat : add a String variable, representation, in the Pawn class
    Add literals for it, a constructor using it and the getter.
    
    The PawnTest class uses an added parameter.

--- src/main/java/chess/pieces/Pawn.java
+++ src/main/java/chess/pieces/Pawn.java
@@ -1,20 +1,32 @@
 package chess.pieces;
 
 public class Pawn {
-    public static final String BLACK = "black";
-    public static final String WHITE = "white";
+    public static final String BLACK_COLOR = "black";
+    public static final String WHITE_COLOR = "white";
+    public static final String BLACK_REPRESENTATION = "P";
+    public static final String WHITE_REPRESENTATION = "p";
 
     private String color;
+    private String representation;
+
+    public Pawn(String color, String representation) {
+        this.color = color;
+        this.representation = representation;
+    }
 
     public Pawn(String color) {
         this.color = color;
     }
 
     Pawn() {
-        this(Pawn.WHITE); // 생성자에서 같은 클래스의 다른 생성자를 호출한다.
+        this(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION); // 생성자에서 같은 클래스의 다른 생성자를 호출한다.
     }
 
     public String getColor() {
         return color;
+    }
+
+    public String getRepresentation() {
+        return representation;
     }
 }
--- src/test/java/chess/pieces/PawnTest.java
+++ src/test/java/chess/pieces/PawnTest.java
@@ -4,29 +4,34 @@ import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class) // 테스트 메서드 이름의 언더바(_)를 지워준다
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class PawnTest {
 
     @Test
-    @DisplayName("흰색과 검은색 폰이 생성되어야 한다") // @DisplayNameGeneration보다 우선이다.
-    void create_white_and_black_pawns() {      // JUnit5부터 public 생략 가능하다(리플렉션? 때문)
-        assertAll(                             // 여러 개의 assert문을 모두 실행해서 실패한 모든 assert문을 알려준다.
-                () -> verifyPawn(Pawn.WHITE),
-                () -> verifyPawn(Pawn.BLACK)
+    @DisplayName("흰색과 검은색 폰이 생성되어야 하며 올바른 출력값을 가져야 한다.")
+    void create_white_and_black_pawns() {
+        assertAll(
+                () -> verifyPawn(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION),
+                () -> verifyPawn(Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION)
         );
     }
 
-    void verifyPawn(final String color) {
-        Pawn pawn = new Pawn(color);
-        assertEquals(color           // 기대값
-                , pawn.getColor()    // 실제값
-                , () -> color + "를 인자로 생성된 Pawn인스턴스는 color속성의 값으로 " + color + "를 가지고 있어야 한다"); // 마지막 인자값을 람다식으로 작성했을 경우, 테스트 실패시에만 연산한다.
+    void verifyPawn(final String color, final String representation) {
+        Pawn pawn = new Pawn(color, representation);
+        assertAll(
+                () -> assertEquals(color, pawn.getColor()),
+                () -> assertEquals(representation, pawn.getRepresentation())
+        );
     }
 
     @Test
-    @DisplayName("기본생성자로 흰색 폰이 생성되어야 한다.")
+    @DisplayName("기본생성자로 흰색 폰이 생성되고 출력값으로 'p' 를 가져야한다.")
     void create_default_constructor() throws Exception {
         Pawn pawn = new Pawn();
-        assertEquals(Pawn.WHITE, pawn.getColor());
+        assertAll(
+                () -> assertEquals(Pawn.WHITE_COLOR, pawn.getColor()),
+                () -> assertEquals(Pawn.WHITE_REPRESENTATION, pawn.getRepresentation())
+        );
     }
+
 }

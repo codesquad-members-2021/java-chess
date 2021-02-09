2e0e79c815792f4a815c84e6c956060dfb8239da
aksmf1442 <64648893+aksmf1442@users.noreply.github.com>
2021-02-09 16:10:11 +0900

    충돌 제거

--- src/main/java/net/sky/pieces/Pawn.java
+++ src/main/java/net/sky/pieces/Pawn.java
@@ -1,7 +1,5 @@
 package net.sky.pieces;
 
-<<<<<<< HEAD
-
 public class Pawn {
 
   private final Color color;
@@ -17,23 +15,6 @@ public class Pawn {
   }
 
   public Color getColor() {
-=======
-public class Pawn {
-
-  private final String color;
-  public final static String WHITE = "white";
-  public final static String BLACK = "black";
-
-  public Pawn() {
-    this.color = WHITE;
-  }
-
-  public Pawn(String color) {
-    this.color = color;
-  }
-
-  public String getColor() {
->>>>>>> d97d3ddf7e34cfeb0e80af61cfb04328fb978349
     return color;
   }
 }

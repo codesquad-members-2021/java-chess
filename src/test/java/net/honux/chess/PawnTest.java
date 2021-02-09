8e9b0938452029d5eb75e245f57aca62171224a8
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2021-02-10 06:21:49 +1300

    Add default constructor
    요구사항에 따라 매개 변수가 없는 생성자를 생성했습니다. 매개 변수가 없는 기본 생성자에는 색을 white 로 지정했습니다.

--- src/test/java/net/honux/chess/PawnTest.java
+++ src/test/java/net/honux/chess/PawnTest.java
@@ -1,6 +1,7 @@
 package net.honux.chess;
 
 import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
 
 class PawnTest {
@@ -15,5 +16,11 @@ class PawnTest {
     private void verifyPawn(final String color) {
         Pawn pawn = new Pawn(color);
         assertThat(pawn.getColor()).isEqualTo(color);
+    }
+
+    @Test
+    public void create_기본생성자() throws Exception {
+        Pawn pawn = new Pawn();
+        assertEquals("white", pawn.getColor());
     }
 }

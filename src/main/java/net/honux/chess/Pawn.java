01443dc9a1dc5a8cf63e2692f423a7238904f081
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2021-02-10 06:46:49 +1300

    Fix name of the pawn's color

--- src/main/java/net/honux/chess/Pawn.java
+++ src/main/java/net/honux/chess/Pawn.java
@@ -1,13 +1,13 @@
 package net.honux.chess;
 
 public class Pawn {
-    public static final String WHITE = "white";
-    public static final String BLACK = "black";
+    public static final String WHITE_COLOR = "white";
+    public static final String BLACK_COLOR = "black";
 
     private String color;
 
     public Pawn() {
-        this.color = WHITE;
+        this.color = WHITE_COLOR;
     }
     public Pawn(String color) {
         this.color = color;

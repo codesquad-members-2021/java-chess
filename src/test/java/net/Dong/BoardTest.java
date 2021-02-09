1fa9b4c44fa018b36c0331bdd680a154473769f4
DHKim <ultraraptor@naver.com>
2021-02-09 14:40:10 +0900

    BoardTEST  파일을 추가하고 루카스 샘플소스코드 추가

---
+++ src/test/java/net/Dong/BoardTest.java
@@ -0,0 +1,23 @@
+package net.Dong;
+import org.junit.jupiter.api.Test;
+//import org.junit.;
+import static org.junit.jupiter.api.Assertions.*;
+import net.Dong.*;
+
+
+class BoardTest {
+    @Test
+    public void create() throws Exception {
+        Board board = new Board();
+
+        Pawn white = new Pawn(Pawn.WHITE_COLOR);
+        board.add(white);
+        assertEquals(1, board.size());
+        assertEquals(white, board.findPawn(0));
+
+        Pawn black = new Pawn(Pawn.BLACK_COLOR);
+        board.add(black);
+        assertEquals(2, board.size());
+        assertEquals(black, board.findPawn(1));
+    }
+}

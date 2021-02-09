532fc94535875fc774469e73c31df2699cf513d1
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-02-09 16:21:56 +0900

    Add test for get in File
    누락된 테스트 추가

--- src/test/java/kr/codesquad/freddie/chess/board/FileTest.java
+++ src/test/java/kr/codesquad/freddie/chess/board/FileTest.java
@@ -1,5 +1,6 @@
 package kr.codesquad.freddie.chess.board;
 
+import kr.codesquad.freddie.chess.piece.Color;
 import kr.codesquad.freddie.chess.piece.Pawn;
 import org.junit.jupiter.api.Test;
 
@@ -32,5 +33,35 @@ class FileTest {
         assertThatThrownBy(() -> file.add(new Pawn()))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("파일 당 8개만 추가 가능합니다.");
+    }
+
+    @Test
+    void get() {
+        File file = new File();
+        Pawn pawn_a = new Pawn();
+        Pawn pawn_b = new Pawn(Color.BLACK);
+        file.add(pawn_a);
+        file.add(pawn_b);
+
+        assertThat(file.get('a')).isEqualTo(pawn_a);
+        assertThat(file.get('b')).isEqualTo(pawn_b);
+    }
+
+    @Test
+    void get_범위초과() {
+        File file = new File();
+
+        for (int i = 0; i < 8; i++) {
+            file.add(new Pawn());
+        }
+
+        assertThatThrownBy(() -> file.get('i'))
+                .isInstanceOf(IndexOutOfBoundsException.class);
+        assertThatThrownBy(() -> file.get((char) 96))
+                .isInstanceOf(ArrayIndexOutOfBoundsException.class);
+        assertThatThrownBy(() -> file.get('0'))
+                .isInstanceOf(ArrayIndexOutOfBoundsException.class);
+        assertThatThrownBy(() -> file.get((char) 0))
+                .isInstanceOf(ArrayIndexOutOfBoundsException.class);
     }
 }

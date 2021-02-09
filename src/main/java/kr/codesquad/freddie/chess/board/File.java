8a5714d3ce897c59ae8db5b01897881be65854b2
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-02-09 16:22:11 +0900

    Add comments for get in File

--- src/main/java/kr/codesquad/freddie/chess/board/File.java
+++ src/main/java/kr/codesquad/freddie/chess/board/File.java
@@ -20,6 +20,14 @@ public class File {
         pawns.add(pawn);
     }
 
+    /**
+     * 체스판의 file은 column으로, 좌측부터 a에서 시작하여 h까지의 알파벳 인덱스로 매겨진다.
+     * 첫 번째 칸 a는 array의 인덱스로 생각했을 때 0과 같고, h는 7과 같다.
+     * 예를 들어 가장 좌측의 기물을 가져오려면 get('a') 와 같이 사용할 수 있다.
+     * @param fileIndex a~h 사이의 char
+     * @return 해당 칸에 존재하는 기물
+     * @see Board#findPawn(char, int)
+     */
     public Pawn get(char fileIndex) {
         return pawns.get(fileIndex - 'a');
     }

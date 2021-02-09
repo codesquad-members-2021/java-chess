a808c14c8a1fba8e1515c20f600c831451d91ce7
janeljs <jisunlim818@gmail.com>
2021-02-09 13:08:53 +0900

    Refactor PawnTest
    String으로 선언했던 색상을 Color 타입으로 변경

--- src/test/java/pieces/PawnTest.java
+++ src/test/java/pieces/PawnTest.java
@@ -1,6 +1,7 @@
 package pieces;
 
 import org.junit.jupiter.api.*;
+import pieces.Pawn.Color;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -8,19 +9,17 @@ public class PawnTest {
     @Test
     @DisplayName("지정한 색상의 폰이 생성되었는지 검증한다.")
     public void create() {
-        final String white = "white";
-        final String black = "black";
-        verifyPawn(white);
-        verifyPawn(black);
+        verifyPawn(Color.WHITE);
+        verifyPawn(Color.BLACK);
         verifyPawn();
     }
 
     public void verifyPawn() {
         Pawn pawn = new Pawn();
-        assertThat(pawn.getColor()).isEqualTo("white");
+        assertThat(pawn.getColor()).isEqualTo(Color.WHITE);
     }
 
-    public void verifyPawn(final String color) {
+    public void verifyPawn(final Color color) {
         Pawn pawn = new Pawn(color);
         assertThat(pawn.getColor()).isEqualTo(color);
     }

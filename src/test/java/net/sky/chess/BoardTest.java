a5e6670ccf1e5a97a0548c0e9299a6000502a0fb
aksmf1442 <64648893+aksmf1442@users.noreply.github.com>
2021-02-09 16:25:12 +0900

    BoardTest에 BeforeEach애노테이션 활용
    initBoard 메서드에 BeforeEach 애노테이션을 추가해서 board 객체를 각 테스트 메서드 실행전에 초기화

--- src/test/java/net/sky/chess/BoardTest.java
+++ src/test/java/net/sky/chess/BoardTest.java
@@ -4,18 +4,24 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import net.sky.pieces.Pawn;
 import net.sky.pieces.Pawn.Color;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class BoardTest {
+  Board board;
+
+  @BeforeEach
+  void initBoard(){
+    board = new Board();
+  }
 
   @Test
   public void create() throws Exception {
-    Board board = new Board();
-    verifyBoard(board, Color.WHITE, 1, 0);
-    verifyBoard(board, Color.BLACK, 2, 1);
+    verifyBoard(Color.WHITE, 1, 0);
+    verifyBoard(Color.BLACK, 2, 1);
   }
 
-  private void verifyBoard(Board board, final Color color, int size, int idx) {
+  private void verifyBoard(final Color color, int size, int idx) {
     Pawn pawn = new Pawn(color);
     board.add(pawn);
     assertThat(board.size()).isEqualTo(size);

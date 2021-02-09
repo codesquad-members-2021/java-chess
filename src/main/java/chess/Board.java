4353eb188c0b55f71dde48fddc2f38333646d4a9
janeljs <jisunlim818@gmail.com>
2021-02-09 13:29:13 +0900

    Create Board class
    - 폰들의 목록을 저장하는 ArrayList pawns 선언
    - 폰 추가, 인덱스를 이용한 검색, 추가된 폰의 수 확인이 가능한 메서드 정의

---
+++ src/main/java/chess/Board.java
@@ -0,0 +1,25 @@
+package chess;
+
+import pieces.Pawn;
+
+import java.util.ArrayList;
+
+public class Board {
+    private final ArrayList<Pawn> pawns;
+
+    public Board() {
+        pawns = new ArrayList<>();
+    }
+
+    public void add(Pawn pawn) {
+        pawns.add(pawn);
+    }
+
+    public Pawn findPawn(int index) {
+        return pawns.get(index);
+    }
+
+    public int size() {
+        return pawns.size();
+    }
+}

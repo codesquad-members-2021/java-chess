87fdf3a68185ddc23d978de82c064d8c8020e9d0
고정완 (Can) <jw.gho@ecubelabs.com>
2021-02-09 14:42:30 +0900

    [refactor] Enhance PawnTest.java
    - 까만 폰을 만드는 경우에 대한 테스트 케이스 추가
    - 존재하지 않는 색깔로 폰을 만드는 테스트 케이스 추가
    - verifyPawn 메소드로 중복 코드 제거

--- src/test/java/net/honux/chess/PawnTest.java
+++ src/test/java/net/honux/chess/PawnTest.java
@@ -4,13 +4,34 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 
 public class PawnTest {
 
     @Test
     @DisplayName("흰색 폰이 생성되어야 한다")
-    public void create() {
-        Pawn pawn = new Pawn("white");
-        assertThat(pawn.getColor()).isEqualTo("white");
+    public void create_white() {
+        verifyPawn("white");
+        verifyPawn("black");
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> verifyPawn("This is not a color."));
     }
-}
+
+    @Test
+    @DisplayName("까만 폰이 생성되어야 한다")
+    public void create_black() {
+        verifyPawn("black");
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 색깔로는 폰을 생성할 수 없다.")
+    public void create_illegal() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> verifyPawn("This is not a color."));
+    }
+
+    private void verifyPawn(String color) {
+        Pawn pawn = new Pawn(color);
+        assertThat(pawn.getColor()).isEqualTo(color);
+    }
+} 

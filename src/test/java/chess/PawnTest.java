42be7d4eda5812a3506c7f6ba2565bb29c5d5432
95degree <95degree@naver.com>
2021-02-09 15:20:36 +0900

    refactor compare()
    문자열 값 중복을 피하기이ㅜ해 color 지역변수를 만들어 compare()를 리팩토링했습니다.

--- src/test/java/chess/PawnTest.java
+++ src/test/java/chess/PawnTest.java
@@ -9,9 +9,9 @@ class PawnTest {
     @Test
     @DisplayName("Pawn 클래스 인스턴스의 String color를 비교한다.")
     public void compare(){
-        Pawn pawnWhite = new Pawn("white");
-        Pawn pawnBlack = new Pawn("black");
-        assertEquals(pawnWhite.getColor(),"white");
-        assertEquals(pawnBlack.getColor(), "black");
+        String[] colors = {"white,black"};
+        for(String color : colors){
+            assertEquals(new Pawn(color).getColor(),color);
+        }
     }
 }

1f6154091730bf743c3dde1ce82dd2373670512a
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2021-02-09 20:32:28 +0900

    Create Board class

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Board {
+import java.util.ArrayList;
+
+public class Board {
+
+    ArrayList<Pawn> pawns = new ArrayList<>();
+
+    public void add(Pawn pawn) {
+        pawns.add(pawn);
+    }
+
+    public int size() {
+        return pawns.size();
+    }
+
+    public Pawn findPawn(int index) {
+        return pawns.get(index);
+    }
 }

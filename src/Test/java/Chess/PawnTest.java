5aab2e58a289dd59ed7a8451cefeeaa367c7b482
rla36 <bhmarsd@naver.com>
2021-02-09 17:06:12 +0900

    폰테스트 커밋.

--- src/Test/java/Chess/PawnTest.java
+++ src/Test/java/Chess/PawnTest.java
@@ -1,19 +1,30 @@
+package Chess;
+
 import org.junit.jupiter.api.*;
+import org.junit.Test;      //-> 임포트했더니 테스트 에러나던 것 단번에 해결?!
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
 public class PawnTest {
 
     @Test
     @DisplayName("흰색 폰이 생성되어야 한다")
     public void create() {
-            Pawn pawn = new Pawn("white");
-            assertThat(pawn.getColor()).isEqualTo("white");
+        String w = "white";
+        String b = "black";
+//            Pawn wPawn = new Pawn(w);
+//            assertThat(wPawn.getColor()).isEqualTo(w);
+//
+//            Pawn bPawn = new Pawn(b);
+//            assertThat(bPawn.getColor()).isEqualTo(b);
+        verifyPawn(w);
+        verifyPawn(b);
     }
 
-//    @Test
-//    public void create_기본생성자() throws Exception {
-//        Pawn pawn = new Pawn();
-//        assertEquals("white", pawn.getColor());
-//    }
+
+    public void verifyPawn(String color){
+        Pawn p = new Pawn(color);
+        assertThat(p.getColor()).isEqualTo(color);
+    }
 }
 

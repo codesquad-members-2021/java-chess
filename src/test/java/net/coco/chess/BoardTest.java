ca4dc26c5cff9d334fdead2e3d214e6cb391842c
gisungchoi <chlrltjd159@naver.com>
2021-02-09 18:09:26 +0900

    verifyPawnToPawns 함수 작성
    원하는 색상과 찾고자 하는 index를 파라미터로 전달해 줍니다.
    해당 메소드는 사용자가 자신이 몇번째로 pawn을 넣는지 알고 있어야 test가 성공합니다.

--- src/test/java/net/coco/chess/BoardTest.java
+++ src/test/java/net/coco/chess/BoardTest.java
@@ -1,6 +1,13 @@
 package net.coco.chess;
 
+
+import net.coco.pieces.Pawn;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class BoardTest {
 
@@ -10,5 +17,11 @@ public class BoardTest {
     public void makeBoard() {
         board = new Board();
     }
-    
+
+    private void verifyPawnToPawns(final String color,int findPawnIndex) {
+        Pawn pawn = new Pawn(color);
+        board.addPawn(pawn);
+        assertThat(pawn).isEqualTo(board.findPawn(findPawnIndex));
+    }
+
 }

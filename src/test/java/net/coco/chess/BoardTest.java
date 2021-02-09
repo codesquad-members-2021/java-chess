4d28f91678df3f7c0b2344d6b4d7435ba1a93d05
gisungchoi <chlrltjd159@naver.com>
2021-02-09 18:00:03 +0900

    beforeEach로 board초기화
    각 테스트가 실행될 때 마다 초기화를 해주기 위해
    beforeEach를 사용했습니다.

--- src/test/java/net/coco/chess/BoardTest.java
+++ src/test/java/net/coco/chess/BoardTest.java
@@ -1,10 +1,14 @@
 package net.coco.chess;
 
+import org.junit.jupiter.api.BeforeEach;
 
 public class BoardTest {
 
     private Board board;
 
-
-
+    @BeforeEach
+    public void makeBoard() {
+        board = new Board();
+    }
+    
 }

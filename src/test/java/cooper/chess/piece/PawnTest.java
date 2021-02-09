79bd7397e0afb080a6553648166320727936ff37
pbg0205 <pbg0205@naver.com>
2021-02-09 23:38:49 +0900

    test : Pawn 클래스 예외 발생 테스트 코드 작성
    - Pawn 클래스에서 상수 외 입력값에 따른 예외를 처리하기 위한
    테스트 코드를 추가하였습니다.

--- src/test/java/cooper/chess/piece/PawnTest.java
+++ src/test/java/cooper/chess/piece/PawnTest.java
@@ -4,13 +4,19 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class PawnTest {
 
     @Test
-    @DisplayName("흰색 폰이 생성되어야 한다.")
+    @DisplayName("흰색폰이 생성되어야 한다.")
     public void create() {
-        Pawn pawn = new Pawn(Color.WHITE);
+        Pawn pawn = new Pawn("white");
         assertThat(pawn.getColor()).isEqualTo(Color.WHITE);
+    }
+
+    @Test()
+    public void checkOtherColorName() {
+        assertThatIllegalArgumentException().isThrownBy(() -> new Pawn("green"));
     }
 }

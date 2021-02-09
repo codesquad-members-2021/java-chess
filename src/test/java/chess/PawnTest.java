6e06bba1bc5e26b14f00e103b1fb7d368573c93f
95degree <95degree@naver.com>
2021-02-09 14:30:07 +0900

    make compare()
    Pawn클래스의 인스턴스의 color를 비교하는 compare 메소드를 추가했습니다.

--- src/test/java/chess/PawnTest.java
+++ src/test/java/chess/PawnTest.java
@@ -1,7 +1,15 @@
 package chess;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PawnTest {
-
+    @Test
+    @DisplayName("Pawn 클래스의 인스턴스의 String color를 비교한다.")
+    public void compare(){
+        Pawn pawn = new Pawn("white");
+        assertEquals(pawn.getColor(),"white");
+    }
 }

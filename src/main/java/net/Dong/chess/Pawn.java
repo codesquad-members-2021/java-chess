b405bf072ccdf0dc548c89e7b218ceb404493466
DHKim <ultraraptor@naver.com>
2021-02-09 21:48:02 +0900

    1.함수 시그니쳐 추가   - char represent 입력을 받는 생성자 추가
    2. 입력파라미터가 없는 생성자 변경
     - 이전버전에 비해 인덴트를 지정하고 주석을 추가
    
    3. final color 를 private 키워드를 추가해 접근제한자를 추가함

--- src/main/java/net/Dong/chess/Pawn.java
+++ src/main/java/net/Dong/chess/Pawn.java
@@ -2,23 +2,28 @@ package net.Dong.chess;
 
 public class Pawn {
 
+    private final Color color;
 
     public enum Color {
         WHITE,
         BALCK
     };
 
-    final Color color;
 
-    public Pawn(Color color) {
-        this.color = color;
-    }
 
     public Pawn() {
+        // defalut color : WHITE
         this.color = Color.WHITE;
     }
 
+    public Pawn(Color color) {
+        // insert color data constructor
+        this.color = color;
+    }
+
+
     public Pawn(String colorString) {
+        // insert String-type color constructor
         if(colorString.equals("BLACK")) {
             this.color = Color.BALCK;
         }else {
@@ -26,12 +31,17 @@ public class Pawn {
         }
     }
 
-    //public static Pawn create(String color) {
-    //    return new Pawn(color);
-    //}
+
+    public Pawn(Color color, char represent) {
+        this.color = color;
+    }
+
+
+
+
 
     public Color getColor() {
-
+        // return current Pawn's color
         return this.color;
     }
 

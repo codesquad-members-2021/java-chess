bc2883a89beda0ac328f44917b82292acfd67b37
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2021-02-09 20:52:31 +1300

    Add feature that returns color of Pawn
    요구사항에 따라 Pawn 클래스 구현 & 테스트 패스까지 했습니다.

---
+++ src/main/java/net/honux/chess/Pawn.java
@@ -0,0 +1,13 @@
+package net.honux.chess;
+
+public class Pawn {
+    private final String color;
+
+    public Pawn(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
---
+++ src/test/java/net/honux/chess/PawnTest.java
@@ -0,0 +1,18 @@
+package net.honux.chess;
+
+import org.junit.jupiter.api.*;
+import static org.assertj.core.api.Assertions.*;
+
+class PawnTest {
+
+    @Test
+    @DisplayName("흰색 폰이 생성되어야 한다")
+    public void create() {
+        Pawn pawn = new Pawn("white");
+        assertThat(pawn.getColor()).isEqualTo("white");
+
+        Pawn pawn2 = new Pawn("black");
+        assertThat(pawn2.getColor()).isEqualTo("black");
+    }
+
+}

3cb3ecaf90a363c7d81a73558838ccb558a27a1b
MJbae <gently.gogo@gmail.com>
2021-02-10 15:39:42 +0900

    Refactor: 테스트 케이스 간 중복코드에 따른 메소드 분할

--- src/test/java/mj/chess/PawnTest.java
+++ src/test/java/mj/chess/PawnTest.java
@@ -4,11 +4,19 @@ import org.junit.jupiter.api.*;
 import static org.assertj.core.api.Assertions.*;
 
 public class PawnTest {
+    @Test
+    @DisplayName("흰색 폰 생성")
+    public void create_pawn_check_color_white() {
+        verifyColorOfPawn(new Pawn(Color.WHITE), Color.WHITE);
+    }
 
     @Test
-    @DisplayName("흰색 폰이 생성되어야 한다")
-    public void create() {
-        Pawn pawn = new Pawn("white");
-        assertThat(pawn.getColor()).isEqualTo("white");
+    @DisplayName("검은색 폰 생성")
+    public void create_pawn_check_color_balck() {
+        verifyColorOfPawn(new Pawn(Color.BLACK), Color.BLACK);
+    }
+
+    public void verifyColorOfPawn(Pawn pawn, Color color){
+        assertThat(pawn.getColor()).isEqualTo(color);
     }
 }

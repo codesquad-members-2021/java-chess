17ab059aca589147420b1822239b73c57becf354
janeljs <jisunlim818@gmail.com>
2021-02-10 17:44:41 +0900

    Refactor BoardTest to remove duplicate code
    verifyAdd, verifySize, verifyFindPawn 메서드에서 Board board 매개변수 제거

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -17,21 +17,21 @@ class BoardTest {
     @Test
     @DisplayName("폰이 제대로 추가되었는지 검증한다.")
     public void create() {
-        verifyAdd(board, new Pawn(Color.WHITE), 1);
-        verifyAdd(board, new Pawn(Color.BLACK), 2);
+        verifyAdd(new Pawn(Color.WHITE), 1);
+        verifyAdd(new Pawn(Color.BLACK), 2);
     }
 
-    public void verifyAdd(Board board, Pawn pawn, int size) {
+    public void verifyAdd(Pawn pawn, int size) {
         board.add(pawn);
-        verifySize(board, size);
-        verifyFindPawn(board, pawn, size - 1);
+        verifySize(size);
+        verifyFindPawn(pawn, size - 1);
     }
 
-    public void verifySize(Board board, int size) {
+    public void verifySize(int size) {
         assertThat(board.size()).isEqualTo(size);
     }
 
-    public void verifyFindPawn(Board board, Pawn pawn, int index) {
+    public void verifyFindPawn(Pawn pawn, int index) {
         assertThat(board.findPawn(index)).isEqualTo(pawn);
     }
 

d6158bb523f5441542143147cae15c97d879b9a8
noel <68936@naver.com>
2021-02-10 18:28:31 +0900

    Pawn 클래스: 생성자 파라미터 조건추가
    약속되지 않은 문자열이 들어올 경우를 대비 생성자 함수 안에 조건문을 넣어두었고, 테스트를 완료했습니다.

--- src/main/java/net/sanhee/chess/Pawn.java
+++ src/main/java/net/sanhee/chess/Pawn.java
@@ -4,7 +4,14 @@ public class Pawn {
     private final String color;
 
     public Pawn(String color) {
-        this.color = color;
+
+        if (color.equalsIgnoreCase("white")
+                || color.equalsIgnoreCase("black")) {
+            this.color = color;
+        } else {
+            this.color = "white";
+        }
+
     }
 
     public String getColor() {

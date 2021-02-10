f2d14d8ecf6c84d669b2cd35890b132dfbffc43d
Sally Oh <min27604@hotmail.com>
2021-02-10 23:33:22 +0900

    refactor: Add setUp method and  verfyBoard method to cut duplicates of codes

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -5,19 +5,26 @@ import pieces.Pawn;
 
 public class BoardTest {
 
+    Board board;
+    Pawn whitePawn, blackPawn;
+
+    @BeforeEach
+    void setUp() {
+        board = new Board();
+        whitePawn = new Pawn(Pawn.WHITE);
+        blackPawn = new Pawn(Pawn.BLACK);
+    }
+
     @Test
     @DisplayName("폰이 보드에 정상적으로 등록")
     public void create() throws Exception {
-        Board board = new Board();
+        verifyBoard(board, whitePawn, 0);
+        verifyBoard(board, blackPawn, 1);
+    }
 
-        Pawn whitePawn = new Pawn(Pawn.WHITE);
-        board.addPawn(whitePawn);
-        assertThat(board.getPawnSize()).isEqualTo(1);
-        assertThat(board.findPawn(0)).isEqualTo(whitePawn);
-
-        Pawn blackPawn = new Pawn(Pawn.BLACK);
-        board.addPawn(blackPawn);
-        assertThat(board.getPawnSize()).isEqualTo(2);
-        assertThat(board.findPawn(1)).isEqualTo(blackPawn);
+    void verifyBoard(Board board, Pawn pawn, int index) {
+        board.addPawn(pawn);
+        assertThat(board.getPawnSize()).isEqualTo(index + 1);
+        assertThat(board.findPawn(index)).isEqualTo(pawn);
     }
 }

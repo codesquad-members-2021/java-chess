3f743fb2bc4091bf9abc2575bcfb5dd8d2e92706
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-02-10 16:11:07 +0900

    Add check() method, field in BoarTest to avoid duplicated code

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -1,23 +1,37 @@
 package chess;
 
-import chess.Board;
 import chess.pieces.Pawn;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class BoardTest {
+
+    Pawn white, black;
+    Board board;
+    int size, idx;
+
+    @BeforeEach
+    public void setUP() {
+        white = new Pawn(Pawn.WHITE);
+        black = new Pawn(Pawn.BLACK);
+        board = new Board();
+        size = 1;
+        idx = 0;
+    }
+
     @Test
     public void create() throws Exception {
-        Board board = new Board();
+        check(white);
+        check(black);
+    }
 
-        Pawn white = new Pawn(Pawn.WHITE);
-        board.add(white);
-        assertEquals(1, board.size());
-        assertEquals(white, board.findPawn(0));
-
-        Pawn black = new Pawn(Pawn.BLACK);
-        board.add(black);
-        assertEquals(2,board.size());
-        assertEquals(black, board.findPawn(1));
+    void check(Pawn pawn) {
+        board.add(pawn);
+        assertEquals(size, board.size());
+        assertEquals(pawn, board.findPawn(idx));
+        size++;
+        idx++;
     }
 }

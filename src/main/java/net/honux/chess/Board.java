136f22860b04ca4831ab7e9d56c3f9224d503dea
정이삭 <isaac56@naver.com>
2021-02-10 16:39:19 +0900

    Add add method to Board class
    체스판에 pawn을 추가하는 add 메소드 추가했습니다.

--- src/main/java/net/honux/chess/Board.java
+++ src/main/java/net/honux/chess/Board.java
@@ -1,4 +1,12 @@
 package net.honux.chess;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Board {
+    private List<Pawn> pawns = new ArrayList<>();
+
+    public void add(Pawn pawn){
+        pawns.add(pawn);
+    }
 }

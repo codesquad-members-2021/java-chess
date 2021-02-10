7655e33812dcbc5a3d7639ba897345d7b1d82f50
noel <68936@naver.com>
2021-02-10 23:33:02 +0900

    Pawn 클래스 수정: 중복 문자열 상수로 대체
    white, black 과 같은 중복 문자열을 enum 상수로 변경했습니다.

--- src/main/java/net/sanhee/chess/Pawn.java
+++ src/main/java/net/sanhee/chess/Pawn.java
@@ -1,19 +1,20 @@
 package net.sanhee.chess;
 
+
 public class Pawn {
     private final String color;
 
     public Pawn(){
-        this.color = "white";
+        this.color = UnitColor.WHITE.getColor();
     }
 
     public Pawn(String color) {
 
-        if (color.equalsIgnoreCase("white")
-                || color.equalsIgnoreCase("black")) {
+        if (color.equalsIgnoreCase(UnitColor.WHITE.getColor())
+                || color.equalsIgnoreCase(UnitColor.BLACK.getColor())) {
             this.color = color;
         } else {
-            this.color = "white";
+            this.color = UnitColor.WHITE.getColor();
         }
 
     }

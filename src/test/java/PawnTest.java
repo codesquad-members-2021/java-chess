9d51d84925d9ce6b91f48a6017fd8f7022f580e7
noel <68936@naver.com>
2021-02-10 17:43:21 +0900

    테스트: 검은색 폰 생성 완료
    create() 메소드에 두 번째 Pawn을 생성하고 black 문자열을 생성자로 전달했습니다.

--- src/test/java/PawnTest.java
+++ src/test/java/PawnTest.java
@@ -1,3 +1,4 @@
+import net.sanhee.chess.Pawn;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -5,9 +6,12 @@ import static org.assertj.core.api.Assertions.*;
 public class PawnTest {
 
     @Test
-    @DisplayName("흰색 폰이 생성되어야 한다.")
+    @DisplayName("흰색/검은색 폰이 생성되어야 한다.")
     public void create(){
         Pawn pawn = new Pawn("white");
         assertThat(pawn.getColor()).isEqualTo("white");
+        pawn = new Pawn("black");
+        assertThat(pawn.getColor()).isEqualTo("black");
     }
+
 }

4dba5ec963579f8b09f4c72b2908c75643e1d402
jeong-inho <derosatam76@gmail.com>
2021-02-10 16:12:49 +0900

    Fix BoardTest class Pawn 클래스의 두번째 인자로 representation를 받도록 수정

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -1,10 +1,10 @@
 package chess;
 
 import org.junit.jupiter.api.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.*;
 
-import pieces.Color;
 import pieces.Pawn;
+import pieces.Piece;
 
 public class BoardTest {
 
@@ -18,15 +18,15 @@ public class BoardTest {
     @Test
     @DisplayName("폰을 추가 시 전체 사이즈와 해당 인덱스의 폰이 일치해야 한다.")
     public void create() throws Exception {
-        verifyPawn(Color.WHITE, 1, 0);
-        verifyPawn(Color.BLACK, 2, 1);
+        verifyPawnSize(Piece.PAWN_WHITE.color, Piece.PAWN_WHITE.representation, 1, 0);
+        verifyPawnSize(Piece.PAWN_BLACK.color, Piece.PAWN_BLACK.representation, 2, 1);
     }
 
-    public void verifyPawn(Color color, int size, int index) {
-        Pawn pawn = new Pawn(color);
+    public void verifyPawnSize(final String color, final char representation ,int size, int index) {
+        Pawn pawn = new Pawn(color, representation);
         board.addPawn(pawn);
-        assertEquals(size, board.pawnSize());
-        assertEquals(pawn, board.findPawn(index));
+        assertThat(board.pawnSize()).isEqualTo(size);
+        assertThat(board.findPawn(index)).isEqualTo(pawn);
     }
 
 }

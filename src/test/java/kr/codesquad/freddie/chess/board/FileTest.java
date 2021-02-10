5d7e78233f4430de84f965aeaed27dacc5924ee6
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-02-10 16:11:18 +0900

    Refactor FileTest to use board as class field

--- src/test/java/kr/codesquad/freddie/chess/board/FileTest.java
+++ src/test/java/kr/codesquad/freddie/chess/board/FileTest.java
@@ -2,11 +2,19 @@ package kr.codesquad.freddie.chess.board;
 
 import kr.codesquad.freddie.chess.piece.Color;
 import kr.codesquad.freddie.chess.piece.Pawn;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
 class FileTest {
+    private File file;
+
+    @BeforeEach
+    void setFile() {
+        file = new File();
+    }
+
     @Test
     void add() {
         int size = 8;
@@ -24,8 +32,6 @@ class FileTest {
 
     @Test
     void add_8개초과() {
-        File file = new File();
-
         for (int i = 0; i < 8; i++) {
             file.add(new Pawn());
         }
@@ -37,7 +43,6 @@ class FileTest {
 
     @Test
     void get() {
-        File file = new File();
         Pawn pawn_a = new Pawn();
         Pawn pawn_b = new Pawn(Color.BLACK);
         file.add(pawn_a);
@@ -49,8 +54,6 @@ class FileTest {
 
     @Test
     void get_범위초과() {
-        File file = new File();
-
         for (int i = 0; i < 8; i++) {
             file.add(new Pawn());
         }

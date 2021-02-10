229f83503e1be8a4beedfbdec2f31a0ebda22586
Shion <ehdrhelr@gmail.com>
2021-02-10 15:57:19 +0900

    feat : add initializing chess board and printing it
    1. Board클래스에서 black pawn과 white pawn을 각각 다른 list에서 관리하기 시작했습니다.
    2. 2차원 문자열 배열로 체스판을 표현합니다.
    3. "."으로 빈 체스판을 표현하고 pawn도 위치에 맞게 배열에 넣어준 후 출력합니다.
    4. 이에 대한 테스트 코드를 작성했습니다.
    6. 하드코딩을 지양하기 위해 상수를 정의했습니다.
    5. BoardTest클래스에서 기존에 작성된 테스트 코드들은 주석처리해두었습니다. 코드 리뷰를 반영하여 수정할 예정입니다.

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -15,29 +15,48 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)  // 기본 원칙은 테스트 메서드마다 테스트 인스턴스를 새로 만드는데, 명시적으로 클래스당 인스턴스 하나만 만들어서 공유할 수 있다.
 public class BoardTest {
 
-    Board board = new Board();
+    Board board;
     int index = 0;
     int size = 0;
 
-    @DisplayName("체스판에 pawn을 넣어보기")
-    @ParameterizedTest(name = "{index} pawn color={0}")
-    @ValueSource(strings = {Pawn.WHITE, Pawn.BLACK})                       // 배열의 크기만큼, 배열의 값들을 파라미터로 테스트를 반복한다.
-    void add_pawn_to_board(@ConvertWith(PawnConverter.class) Pawn pawn) {  // 문자열 color로 Pawn 인스턴스를 생성한 후 인자로 받는다.
-        board.add(pawn);
-
-        assertAll(
-                () -> assertEquals(++size, board.size()),                  // board에 pawn이 추가되었으므로 size를 먼저 1 증가시킨 후에 연산한다.
-                () -> assertEquals(pawn, board.findPawn(index++))          // index는 0부터 시작하므로 먼저 연산한 후에 1 증가시킨다.
-        );
+    @BeforeEach
+    void initialize_board() {
+        board = new Board();
     }
 
-    /* 문자열 color로 Pawn 인스턴스로 변환해주는 클래스 */
-    static class PawnConverter extends SimpleArgumentConverter {
+//    @DisplayName("체스판에 pawn을 넣어보기")
+//    @ParameterizedTest(name = "{index} pawn color={0}")
+//    @ValueSource(strings = {Pawn.WHITE_COLOR, Pawn.BLACK_COLOR})           // 배열의 크기만큼, 배열의 값들을 파라미터로 테스트를 반복한다.
+//    void add_pawn_to_board(@ConvertWith(PawnConverter.class) Pawn pawn) {  // 문자열 color로 Pawn 인스턴스를 생성한 후 인자로 받는다.
+//        board.add(pawn);
+//
+//        assertAll(
+//                () -> assertEquals(++size, board.size()),                  // board에 pawn이 추가되었으므로 size를 먼저 1 증가시킨 후에 연산한다.
+//                () -> assertEquals(pawn, board.findPawn(index++))          // index는 0부터 시작하므로 먼저 연산한 후에 1 증가시킨다.
+//        );
+//    }
+//
+//    /* 문자열 color로 Pawn 인스턴스로 변환해주는 클래스 */
+//    static class PawnConverter extends SimpleArgumentConverter {
+//
+//        @Override
+//        protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
+//            assertEquals(Pawn.class, targetType, "Can only convert to Pawn");
+//            return new Pawn(source.toString());
+//        }
+//    }
 
-        @Override
-        protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
-            assertEquals(Pawn.class, targetType, "Can only convert to Pawn");
-            return new Pawn(source.toString());
-        }
+    @Test
+    void test_print() {
+        String[][] initializedBoard = board.initialize();
+        String chessBoard = board.print(initializedBoard);
+        System.out.println(chessBoard);
+    }
+
+    @Test
+    void initialize() throws Exception {
+        board.initialize();
+        assertEquals("pppppppp", board.getWhitePawnsResult());
+        assertEquals("PPPPPPPP", board.getBlackPawnsResult());
     }
 }

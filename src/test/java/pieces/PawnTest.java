4ba03e69c468f7b93669e950c37adef3417988ca
K <PizzaCola.K@gmail.com>
2021-02-10 21:49:07 +0900

    test: Edit PawnTest to use Color enum
    - Pawn.getColor() will return Color enum.
    
    - PawnTest에서 Color enum을 사용하도록 수정하였습니다.
    - Pawn.getColor()는 Color enum을 반환할 것입니다.

--- src/test/java/pieces/PawnTest.java
+++ src/test/java/pieces/PawnTest.java
@@ -9,30 +9,20 @@ public class PawnTest {
     @Test
     @DisplayName("흰색과 검은색 폰이 생성되어야 한다")
     public void create() {
-        final String white = "white";
-        final String black = "black";
-        verifyPawn(white);
-        verifyPawn(black);
+        verifyPawn(Color.WHITE);
+        verifyPawn(Color.BLACK);
     }
 
     @Test
     @DisplayName("기본 생성자는 흰색 폰을 생성")
     public void create_기본생성자() throws Exception {
         Pawn pawn = new Pawn();
-        assertThat(pawn.getColor()).isEqualTo("white");
+        assertThat(pawn.getColor()).isEqualTo(Color.WHITE);
     }
 
-    public void verifyPawn(String color) {
-        Pawn pawn = null;
-        if ("black".equalsIgnoreCase(color)) {
-            pawn = new Pawn(Pawn.BLACK_COLOR);
-        } else if ("white".equalsIgnoreCase(color)){
-            pawn = new Pawn(Pawn.WHITE_COLOR);
-        }
-        if (pawn != null) {
-            assertThat(pawn.getColor()).isEqualTo(color);
-        }
+    public void verifyPawn(Color color) {
+        Pawn pawn = new Pawn(color);
+        assertThat(pawn.getColor()).isEqualTo(color);
     }
-
 
 }

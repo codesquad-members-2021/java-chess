9f21130bb761261ca5379c3574d3c9932574911d
Shion <ehdrhelr@gmail.com>
2021-02-10 17:37:49 +0900

    test : refactor test
    미션 3의 변경사항과 미션 2의 코드 리뷰를 반영하여 테스트 코드를 작성하였습니다.

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -2,51 +2,41 @@ package chess;
 
 import chess.pieces.Pawn;
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.converter.ArgumentConversionException;
-import org.junit.jupiter.params.converter.ConvertWith;
-import org.junit.jupiter.params.converter.SimpleArgumentConverter;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)  // 기본 원칙은 테스트 메서드마다 테스트 인스턴스를 새로 만드는데, 명시적으로 클래스당 인스턴스 하나만 만들어서 공유할 수 있다.
 public class BoardTest {
 
     Board board;
-    int index = 0;
-    int size = 0;
 
     @BeforeEach
     void initialize_board() {
         board = new Board();
     }
 
-//    @DisplayName("체스판에 pawn을 넣어보기")
-//    @ParameterizedTest(name = "{index} pawn color={0}")
-//    @ValueSource(strings = {Pawn.WHITE_COLOR, Pawn.BLACK_COLOR})           // 배열의 크기만큼, 배열의 값들을 파라미터로 테스트를 반복한다.
-//    void add_pawn_to_board(@ConvertWith(PawnConverter.class) Pawn pawn) {  // 문자열 color로 Pawn 인스턴스를 생성한 후 인자로 받는다.
-//        board.add(pawn);
-//
-//        assertAll(
-//                () -> assertEquals(++size, board.size()),                  // board에 pawn이 추가되었으므로 size를 먼저 1 증가시킨 후에 연산한다.
-//                () -> assertEquals(pawn, board.findPawn(index++))          // index는 0부터 시작하므로 먼저 연산한 후에 1 증가시킨다.
-//        );
-//    }
-//
-//    /* 문자열 color로 Pawn 인스턴스로 변환해주는 클래스 */
-//    static class PawnConverter extends SimpleArgumentConverter {
-//
-//        @Override
-//        protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
-//            assertEquals(Pawn.class, targetType, "Can only convert to Pawn");
-//            return new Pawn(source.toString());
-//        }
-//    }
+    @Test
+    @DisplayName("체스판 생성")
+    void create() throws Exception {
+        Pawn white = new Pawn(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION);
+        board.addWhitePawn(white);
+
+        assertAll(
+                () -> assertEquals(1, board.size()),
+                () -> assertEquals(white, board.findWhitePawn(0))
+        );
+
+        Pawn black = new Pawn(Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION);
+        board.addBlackPawn(black);
+
+        assertAll(
+                () -> assertEquals(2, board.size()),
+                () -> assertEquals(black, board.findBlackPawn(0))
+        );
+    }
 
     @Test
+    @DisplayName("초기 체스판 출력")
     void test_print() {
         String[][] initializedBoard = board.initialize();
         String chessBoard = board.print(initializedBoard);
@@ -54,6 +44,7 @@ public class BoardTest {
     }
 
     @Test
+    @DisplayName("board에 흰색, 검은색 pawn 저장되어 있는지 확인")
     void initialize() throws Exception {
         board.initialize();
         assertEquals("pppppppp", board.getWhitePawnsResult());

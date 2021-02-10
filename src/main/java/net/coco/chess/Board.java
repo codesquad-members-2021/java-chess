1d56367886c8b47cd821150adc4d67af679cab8d
gisungchoi <chlrltjd159@naver.com>
2021-02-10 23:00:41 +0900

    pawns 자료구조 변경
    원래는 ArrayList타입이었지만 List타입으로 변경했다.
    현재는 생성자에서 ArrayList로 초기화하긴 하지만 만약 다른 요구사항이
    생겼을 때 유동적으로 타입을 변경할 수 있게 되었습니다.

--- src/main/java/net/coco/chess/Board.java
+++ src/main/java/net/coco/chess/Board.java
@@ -3,9 +3,14 @@ package net.coco.chess;
 import net.coco.pieces.Pawn;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Board {
-    private ArrayList<Pawn> pawns = new ArrayList<>();
+    private List<Pawn> pawns;
+
+    public Board() {
+        this.pawns = new ArrayList<>();
+    }
 
     public void addPawn(Pawn pawn){
         pawns.add(pawn);

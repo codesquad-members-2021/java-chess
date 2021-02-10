2196990275402719b1f2b65e6bfa191965f0bc7e
jeong-inho <derosatam76@gmail.com>
2021-02-10 17:28:04 +0900

    Add initialize and print test in BoardTest class 보드를 초기화하는 initialize와 보드를 출력하는 print 테스트 케이스 추가

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -11,22 +11,39 @@ public class BoardTest {
     private Board board;
 
     @BeforeEach
-    public void initialize() {
+    public void init() {
         board = new Board();
     }
 
     @Test
     @DisplayName("폰을 추가 시 전체 사이즈와 해당 인덱스의 폰이 일치해야 한다.")
-    public void create() throws Exception {
+    public void create()  {
         verifyPawnSize(Piece.PAWN_WHITE.color, Piece.PAWN_WHITE.representation, 1, 0);
+        verifyPawnSize(Piece.PAWN_WHITE.color, Piece.PAWN_WHITE.representation, 2, 1);
+        verifyPawnSize(Piece.PAWN_BLACK.color, Piece.PAWN_BLACK.representation, 1, 0);
         verifyPawnSize(Piece.PAWN_BLACK.color, Piece.PAWN_BLACK.representation, 2, 1);
     }
 
     public void verifyPawnSize(final String color, final char representation ,int size, int index) {
         Pawn pawn = new Pawn(color, representation);
-        board.addPawn(pawn);
-        assertThat(board.pawnSize()).isEqualTo(size);
-        assertThat(board.findPawn(index)).isEqualTo(pawn);
+        board.addPawn(color, pawn);
+        assertThat(board.pawnSize(color)).isEqualTo(size);
+        assertThat(board.findPawn(color, index)).isEqualTo(pawn);
+    }
+
+    @Test
+    @DisplayName("보드를 초기화한 결과 흰색 폰과 검은색 폰의 열이 각각 일치해 한다.")
+    public void initialize()  {
+        board.initialize();
+        assertThat(board.getPawnsResult(Piece.PAWN_WHITE.color)).isEqualTo("pppppppp");
+        assertThat(board.getPawnsResult(Piece.PAWN_BLACK.color)).isEqualTo("PPPPPPPP");
+    }
+
+    @Test
+    @DisplayName("초기화 된 보드가 출력되어야 한다.")
+    public void print() {
+        board.initialize();
+        System.out.println(board.print());
     }
 
 }

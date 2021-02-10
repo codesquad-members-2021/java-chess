6fc6ef069702e00f8767d06f24035ef5b67338ba
noel <68936@naver.com>
2021-02-11 04:42:10 +0900

    테스트: Pawn 멤버변수 타입 변경 후, Side Effect 테스트
    이상 없이 잘 작동 하는 것을 확인하였습니다.

--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -14,35 +14,38 @@ public class BoardTest {
     @Test
     @DisplayName("흰색 Pawn을 체스 판, 첫번째 칸에 추가")
     public void create() throws Exception {
-        spawnPawn(UnitColor.WHITE.getColor(), 1);
+        spawnPawn(UnitColor.WHITE, 1);
     }
+
     @Test
     @DisplayName("흑색 Pawn을 체스 판, 두번째 칸에 추가")
     public void create2() throws Exception {
-        spawnPawn(UnitColor.BLACK.getColor(), 2);
+        spawnPawn(UnitColor.BLACK, 2);
     }
 
-    @Test
-    @DisplayName("체스 판에 Pawn 이외의 객체가 추가되지 않도록 한다.")
-    public void otherUnitCheck(){
-        Integer testInteger = new Integer("7");
-        board.add(testInteger);
+    /*
+        @Deprecated
+        @DisplayName("체스 판에 Pawn 이외의 객체가 추가되지 않도록 한다.")
 
-        spawnCheck(testInteger,3);
-    }
+        public void otherUnitCheck(){
+            Integer testInteger = new Integer("7");
+            board.add(testInteger);
 
-    private void spawnPawn(String color, int order){
+            spawnCheck(testInteger,3);
+        }
+    */
+    private void spawnPawn(UnitColor color, int order) {
         Pawn pawn = new Pawn(color);
         board.add(pawn);
         spawnCheck(pawn, order);
     }
 
-    private void spawnCheck(Pawn object, int order){
+    private void spawnCheck(Pawn object, int order) {
         assertAll(
                 //A Case
                 () -> assertThat(board.size()).isEqualTo(order),
                 //B Case
-                () -> assertThat(board.findPawn(order-1)).isEqualTo(object)
+                () -> assertThat(board.findPawn(order - 1)).isEqualTo(object)
         );
     }
 

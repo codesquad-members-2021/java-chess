30978fb6f8ea69795e6a5f41d8acee32ede71357
noel <68936@naver.com>
2021-02-10 21:30:11 +0900

    추가: 매개 변수 없는 Pawn 생성 테스트

--- src/test/java/PawnTest.java
+++ src/test/java/PawnTest.java
@@ -24,6 +24,13 @@ public class PawnTest {
         assertThat(pawn.getColor()).isEqualTo("white");
     }
 
+    @Test
+    @DisplayName("색이 없는 Pawn을 생성할 경우, white를 지정한다.")
+    public void create3(){
+        Pawn pawn = new Pawn();
+        assertThat(pawn.getColor()).isEqualTo("white");
+    }
+
     private void verifyPawn(final String color) {
         Pawn pawn = new Pawn(color);
         assertThat(pawn.getColor()).isEqualTo(color);

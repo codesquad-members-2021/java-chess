95aa6bb20c93114101519639ca019210baead8f4
noel <68936@naver.com>
2021-02-10 23:31:13 +0900

    테스트 수정: 중복 문자열을 상수로 대체
    white, black 과 같은 중복 문자열을 enum의 상수로 변경했습니다.

--- src/test/java/PawnTest.java
+++ src/test/java/PawnTest.java
@@ -1,4 +1,4 @@
-import net.sanhee.chess.Pawn;
+import net.sanhee.chess.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,26 +9,26 @@ public class PawnTest {
     @Test
     @DisplayName("흰색 폰이 생성되어야 한다.")
     public void create(){
-        verifyPawn("white");
+        verifyPawn(UnitColor.WHITE.getColor());
     }
     @Test
     @DisplayName("검은색 폰이 생성되어야 한다.")
     public void create2(){
-        verifyPawn("black");
+        verifyPawn(UnitColor.BLACK.getColor());
     }
 
     @Test
     @DisplayName("약속되지 않은 문자일 경우, white를 지정한다.")
     public void checkColor(){
         Pawn pawn = new Pawn("blue");
-        assertThat(pawn.getColor()).isEqualTo("white");
+        assertThat(pawn.getColor()).isEqualTo(UnitColor.WHITE.getColor());
     }
 
     @Test
     @DisplayName("색이 없는 Pawn을 생성할 경우, white를 지정한다.")
     public void create3(){
         Pawn pawn = new Pawn();
-        assertThat(pawn.getColor()).isEqualTo("white");
+        assertThat(pawn.getColor()).isEqualTo(UnitColor.WHITE.getColor());
     }
 
     private void verifyPawn(final String color) {

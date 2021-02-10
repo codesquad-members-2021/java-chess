6ab94d99a7a8e4ea95358a5c0f35ee1ca18fc5b7
jeong-inho <derosatam76@gmail.com>
2021-02-10 16:05:57 +0900

    Update PawnTest class 체스판의 기물을 출력하는 기능을 위해 Pawn 클래스에 두번째 인자로 representation를 받도록 수정

--- src/test/java/pieces/PawnTest.java
+++ src/test/java/pieces/PawnTest.java
@@ -2,27 +2,28 @@ package pieces;
 
 import org.junit.jupiter.api.*;
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PawnTest {
 
     @Test
-    @DisplayName("흰색과 검은색 폰이 생성되어야 한다")
-    public void create() {
-        verifyPawn(Color.WHITE);
-        verifyPawn(Color.BLACK);
-    }
-
-    public void verifyPawn(final Color color) {
-        Pawn pawn = new Pawn(color);
-        assertThat(pawn.getColor()).isEqualTo(color);
+    @DisplayName("기본 생성자 호출 시 흰색 폰이 생성되어야 한다")
+    public void createDefaultConstructor() throws Exception {
+        Pawn pawn = new Pawn();
+        assertThat(pawn.getColor()).isEqualTo(Piece.PAWN_WHITE.color);
+        assertThat(pawn.getRepresentation()).isEqualTo(Piece.PAWN_WHITE.representation);
     }
 
     @Test
-    @DisplayName("기본 생성자 호출 시 흰색 폰이 생성되어야 한다")
-    public void createDefaultConstructor() {
-        Pawn pawn = new Pawn();
-        assertEquals(Color.WHITE, pawn.getColor());
+    @DisplayName("흰색과 검은색 폰이 생성되어야 한다")
+    public void create() {
+        verifyPawn(Piece.PAWN_WHITE.color, Piece.PAWN_WHITE.representation);
+        verifyPawn(Piece.PAWN_BLACK.color, Piece.PAWN_BLACK.representation);
+    }
+
+    public void verifyPawn(final String color, final char representation) {
+        Pawn pawn = new Pawn(color, representation);
+        assertThat(pawn.getColor()).isEqualTo(color);
+        assertThat(pawn.getRepresentation()).isEqualTo(representation);
     }
 
 }

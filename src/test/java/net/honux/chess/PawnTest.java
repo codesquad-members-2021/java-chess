bca65a87efb38421cfb53faa5b3d162a4587a81d
정이삭 <isaac56@naver.com>
2021-02-10 16:08:35 +0900

    Add createBasic method to PawnTest
    기본생성자를 테스트하는 createBasic 메소드 추가했습니다

--- src/test/java/net/honux/chess/PawnTest.java
+++ src/test/java/net/honux/chess/PawnTest.java
@@ -6,6 +6,13 @@ import static org.assertj.core.api.Assertions.*;
 class PawnTest {
 
     @Test
+    @DisplayName("폰 기본생성자 테스트")
+    public void createBasic() throws Exception{
+        Pawn pawn = new Pawn();
+        assertThat(pawn.getColor()).isEqualTo("White");
+    }
+
+    @Test
     @DisplayName("폰 생성자 테스트")
     public void create() {
         verifyPawn("White");

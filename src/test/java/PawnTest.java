d26858cbbf56ae6e219ef0f0fae059dd52cb1e07
noel <68936@naver.com>
2021-02-10 17:10:45 +0900

    create() 테스트 메소드 작성
    클래스를 구현하기 전 구현한 테스트코드 입니다.

--- src/test/java/PawnTest.java
+++ src/test/java/PawnTest.java
@@ -1,3 +1,13 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 public class PawnTest {
 
+    @Test
+    @DisplayName("흰색 폰이 생성되어야 한다.")
+    public void create(){
+        Pawn pawn = new Pawn("white");
+        assertThat(pawn.getColor()).isEqualTo("white");
+    }
 }

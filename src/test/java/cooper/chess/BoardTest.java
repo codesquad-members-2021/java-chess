650030fd262d722c8a069f1cb2882f951ac77196
pbg0205 <pbg0205@naver.com>
2021-02-11 01:19:27 +0900

    test : BoardTest 테스트 코드 리팩토링
    - mission01에 문자열 입력을 받는  Pawn 생성자로 변경하였습니다.
    - assertThrows 메서드를 사용해서 예외 발생을 확인하는 테스트 코드를
    작성했습니다.

--- src/test/java/cooper/chess/BoardTest.java
+++ src/test/java/cooper/chess/BoardTest.java
@@ -1,46 +1,49 @@
 package cooper.chess;
 
-import cooper.chess.piece.Color;
 import cooper.chess.piece.Pawn;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class BoardTest {
     @Test
     public void create() {
         Board board = new Board();
 
-        Pawn white = new Pawn(Color.WHITE);
+        Pawn white = new Pawn("white");
         board.add(white);
         assertEquals(1, board.size());
         assertEquals(white, board.findPawn(1));
 
-        Pawn black = new Pawn(Color.BLACK);
+        Pawn black = new Pawn("black");
         board.add(black);
         assertEquals(2, board.size());
         assertEquals(black, board.findPawn(2));
     }
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test
     public void testZeroSizeOfBoard() {
         Board board = new Board();
         Pawn pawn = new Pawn();
 
-        assertEquals(pawn, board.findPawn(0));
+        assertThrows(IllegalArgumentException.class,
+                () -> board.findPawn(0));
     }
 
-    @Test(expected = IndexOutOfBoundsException.class)
+    @Test
     public void testOutOfBoardSize() {
         Board board = new Board();
 
-        Pawn white = new Pawn(Color.WHITE);
+        Pawn white = new Pawn("white");
         board.add(white);
 
-        Pawn black = new Pawn(Color.BLACK);
+        Pawn black = new Pawn("black");
         board.add(black);
 
-        assertEquals(white, board.findPawn(-1));
-        assertEquals(black, board.findPawn(2));
+        assertThrows(IndexOutOfBoundsException.class,
+                () -> board.findPawn(-1));
+        assertThrows(IndexOutOfBoundsException.class,
+                () -> board.findPawn(2));
     }
 }

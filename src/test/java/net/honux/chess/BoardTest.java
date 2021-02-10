be580687adbb6d5bf3857dfd3f56bd2007fe83e1
정이삭 <isaac56@naver.com>
2021-02-10 18:26:50 +0900

    Add pawnAddTest() test method to BoardTest class
    Board에 Pawn을 추가하는 테스트 코드 작성

--- src/test/java/net/honux/chess/BoardTest.java
+++ src/test/java/net/honux/chess/BoardTest.java
@@ -1,7 +1,24 @@
 package net.honux.chess;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 
 class BoardTest {
+    @Test
+    @DisplayName("Board의 Pawn 등록 테스트")
+    public void pawnAddTest() throws Exception {
+        Board board = new Board();
 
+        Pawn white = new Pawn(Pawn.WHITE_COLOR);
+        board.add(white);
+        assertThat(board.size()).isEqualTo(1);
+        assertThat(board.findPawn(0)).isEqualTo(white);
+
+        Pawn black = new Pawn(Pawn.BLACK_COLOR);
+        board.add(black);
+        assertThat(board.size()).isEqualTo(2);
+        assertThat(board.findPawn(1)).isEqualTo(black);
+    }
 }

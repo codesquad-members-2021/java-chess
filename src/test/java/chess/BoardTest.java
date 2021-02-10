5c70201313b31cb430eb42ad5efe851be9ebbde7
Sally Oh <min27604@hotmail.com>
2021-02-10 15:39:23 +0900

    feat: Create test to see if Board class works properly
    - Create andn add pawn to the board and check if the methods in Board class works properly

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -1,2 +1,23 @@
-package chess;public class BoardTest {
+package chess;
+import org.junit.jupiter.api.*;
+import static org.assertj.core.api.Assertions.*;
+import pieces.Pawn;
+
+public class BoardTest {
+
+    @Test
+    @DisplayName("폰이 보드에 정상적으로 등록")
+    public void create() throws Exception {
+        Board board = new Board();
+
+        Pawn whitePawn = new Pawn(Pawn.WHITE);
+        board.addPawn(whitePawn);
+        assertThat(board.getPawnSize()).isEqualTo(1);
+        assertThat(board.findPawn(0)).isEqualTo(whitePawn);
+
+        Pawn blackPawn = new Pawn(Pawn.BLACK);
+        board.addPawn(blackPawn);
+        assertThat(board.getPawnSize()).isEqualTo(2);
+        assertThat(board.findPawn(1)).isEqualTo(blackPawn);
+    }
 }

d75af3f0cf85838059d7db328d5f349acdc740de
jeong-inho <derosatam76@gmail.com>
2021-02-10 17:32:12 +0900

    Add method in Board class Board 클래스에 흰색 폰과 검은색 폰을 초기화하는 initialize 메소드, 폰의 representation의 결과를 출력하는 getPawnsResult 메소드, 보드를 화면에 출력하는 print 메소드 추가 또한 기존에 폰을 저장하던 ArrayList를 흰색폰과 검은색폰을 따로 관리하기 위해 HashMap으로 변경

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -1,28 +1,66 @@
 package chess;
 
 import pieces.Pawn;
+import pieces.Piece;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 public class Board {
 
-    private final List<Pawn> pawns;
+    private final HashMap<String, LinkedList<Pawn>> pawns;
 
     public Board() {
-        this.pawns = new ArrayList<>();
+        this.pawns = new HashMap<>();
+        this.pawns.put(Piece.PAWN_WHITE.color, new LinkedList<>());
+        this.pawns.put(Piece.PAWN_BLACK.color, new LinkedList<>());
     }
 
-    public void addPawn(Pawn pawn) {
-        this.pawns.add(pawn);
+    public void addPawn(String color, Pawn pawn) {
+        this.pawns.get(color).add(pawn);
     }
 
-    public int pawnSize() {
-        return this.pawns.size();
+    public int pawnSize(String color) {
+        return this.pawns.get(color).size();
     }
 
-    public Pawn findPawn(int index) {
-        return this.pawns.get(index);
+    public Pawn findPawn(String color, int index) {
+        return this.pawns.get(color).get(index);
+    }
+
+    public void initialize() {
+        for (int i = 0; i < 8; i++) {
+            addPawn(Piece.PAWN_WHITE.color, new Pawn(Piece.PAWN_WHITE.color, Piece.PAWN_WHITE.representation));
+            addPawn(Piece.PAWN_BLACK.color, new Pawn(Piece.PAWN_BLACK.color, Piece.PAWN_BLACK.representation));
+        }
+    }
+
+    public String getPawnsResult(String color) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < pawnSize(color); i++) {
+            result.append(findPawn(color, i).getRepresentation());
+        }
+        return result.toString();
+    }
+
+    public String print() {
+        StringBuilder result = new StringBuilder();
+        result.append("........");
+        result.append("\n");
+        result.append(getPawnsResult(Piece.PAWN_BLACK.color));
+        result.append("\n");
+        result.append("........");
+        result.append("\n");
+        result.append("........");
+        result.append("\n");
+        result.append("........");
+        result.append("\n");
+        result.append("........");
+        result.append("\n");
+        result.append(getPawnsResult(Piece.PAWN_WHITE.color));
+        result.append("\n");
+        result.append("........");
+        return result.toString();
     }
 
 }

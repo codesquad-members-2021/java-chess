356126863f7246ec664a25cd5483bca807cc63ca
Jihye Woo <woojihye2339@gmail.com>
2021-02-11 18:52:53 +0900

    feat: BoardTest 클래스 생성
    > 빈 BoardTest 클래스 생성
      - 빈 create() 테스트 메소드 생성
      - 테스트 성공

---
+++ src/test/java/net/honux/chess/BoardTest.java
@@ -0,0 +1,13 @@
+package net.honux.chess;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+public class BoardTest {
+
+    @Test
+    public void create() throws Exception {
+
+    }
+}

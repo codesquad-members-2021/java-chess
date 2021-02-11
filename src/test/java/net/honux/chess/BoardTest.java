4b69e7aacd635b66dda20052e01529d8d1d888dd
Jihye Woo <woojihye2339@gmail.com>
2021-02-11 20:29:37 +0900

    refactor : BoardTest 중복 제거
    > BoardTest 중복 제거
     - addPawnAndCheckTheBoard()
       - 폰을 추가하고 업데이트 하는 중복 로직 분리
     - setup() @BeforeEach
       - 가장 먼저 보드를 생성하는 공통 부분을 BeforeEach를 사용해서 분리

--- src/test/java/net/honux/chess/BoardTest.java
+++ src/test/java/net/honux/chess/BoardTest.java
@@ -5,29 +5,35 @@ import org.junit.jupiter.api.*;
 
 public class BoardTest {
 
-    @Test
-    @DisplayName("생성된 보드 위에 폰 추가, 폰 갯수 확인, 특정 폰 찾기가 가능해야 한다.")
-    void create_보드생성과_폰추가() throws Exception {
-        Board board = new Board();
+    private Board board;
 
-        Pawn white = new Pawn(Pawn.WHITE_COLOR);
-        board.add(white);
-        Assertions.assertEquals(1, board.size());
-        Assertions.assertEquals(white, board.findPawn(0));
-
-        Pawn black = new Pawn(Pawn.BLACK_COLOR);
-        board.add(black);
-        Assertions.assertEquals(2, board.size());
-        Assertions.assertEquals(black, board.findPawn(1));
+    @BeforeEach
+    void setup() {
+        this.board = new Board();
     }
 
+    @Test
+    @DisplayName("보드 위에 폰 추가하고 추가된 폰이 보드에 정상적으로 반영되어야 한다.")
+    void add_폰이_보드에_정상적으로_추가되는지_확인() throws Exception {
+        Pawn white = new Pawn(Pawn.WHITE_COLOR);
+        addPawnAndCheckTheBoard(white);
+
+        Pawn black = new Pawn(Pawn.BLACK_COLOR);
+        addPawnAndCheckTheBoard(black);
+    }
 
     @Test
-    @DisplayName("폰이 아닌 객체를 Board에 추가할 시 에러가 발생해야 한다.")
+    @DisplayName("폰이 아닌 객체를 보드에 추가할 시 컴파일 에러가 발생해야 한다.")
     void add_보드위에_폰이_아닌_객체추가() {
-        Board board = new Board();
-
         Object notPawn = new Object();
         board.add(notPawn);
     }
+
+    private void addPawnAndCheckTheBoard(Pawn pawn) {
+        int sizeBeforeAddThePawn = board.size();
+        board.add(pawn);
+        Assertions.assertEquals(sizeBeforeAddThePawn + 1, board.size());
+        Assertions.assertEquals(pawn, board.findPawn(sizeBeforeAddThePawn));
+    }
+
 }

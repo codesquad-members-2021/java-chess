361b3d0861c0b083517c084a7a60f46b3b623518
Jihye Woo <woojihye2339@gmail.com>
2021-02-11 19:00:34 +0900

    feat: create() 테스트 메소드 생성
    > 빈 Board 클래스 생성
      - Board 클래스에 대한 테스트 메소드 구현을 위해
      - 빈 add(), size(), findPawn() 생성
    
    > 테스트 실행 후 test failed 확인

---
+++ src/main/java/net/honux/chess/Board.java
@@ -0,0 +1,15 @@
+package net.honux.chess;
+
+public class Board {
+
+    public void add(Pawn white) {
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public Pawn findPawn(int i) {
+        return new Pawn();
+    }
+}
--- src/test/java/net/honux/chess/BoardTest.java
+++ src/test/java/net/honux/chess/BoardTest.java
@@ -7,7 +7,18 @@ import org.junit.jupiter.api.*;
 public class BoardTest {
 
     @Test
+    @DisplayName("생성된 보드 위에 폰 추가, 폰 갯수 확인, 특정 폰 찾기가 가능해야 한다.")
     public void create() throws Exception {
+        Board board = new Board();
 
+        Pawn white = new Pawn(Pawn.WHITE_COLOR);
+        board.add(white);
+        Assertions.assertEquals(1, board.size());
+        Assertions.assertEquals(white, board.findPawn(0));
+
+        Pawn black = new Pawn(Pawn.BLACK_COLOR);
+        board.add(black);
+        Assertions.assertEquals(2, board.size());
+        Assertions.assertEquals(black, board.findPawn(1));
     }
 }

b76db82f19e84fed1fe5e68d6842b8109a412fde
bong6981 <kbk5190@naver.com>
2021-02-12 18:28:57 +0900

    Remove redundant codes
    - 흰, 검은 Pawn을 생성하고 확인하는 코드가 겹처 중복제거
    - Pawn을 생성하고 확인하는 부분을 createPawnAndVerify 메서드로 따로 뺌
    - board를 클래스 멤버로 선언
    - 추가한 Pawn의 수를 계산해주는 count 멤버변수를 추가해서
      assertThatt실행할 때 pawn의 개수로 값을 확인할 수 있게 함

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -1,5 +1,6 @@
 package chess;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pieces.Color;
@@ -8,19 +9,26 @@ import pieces.Pawn;
 import static org.assertj.core.api.Assertions.*;
 
 public class BoardTest {
+    private Board board;
+    private int count = 0;
+
+    @BeforeEach
+    public void createBoard() {
+        board = new Board();
+    }
+
+    public void createPawnAndVerify(Color color) {
+        Pawn pawn = new Pawn(color);
+        board.add(pawn);
+        count += 1;
+        assertThat(board.size()).isEqualTo(count);
+        assertThat(board.findPawn(count - 1)).isEqualTo(pawn);
+    }
+
     @Test
     @DisplayName("흰색, 검은색 Pawn을 생성해 board에 추가한다")
-    public void create() throws Exception {
-        Board board = new Board();
-
-        Pawn white = new Pawn(Color.WHITE);
-        board.add(white);
-        assertThat(board.size()).isEqualTo(1);
-        assertThat(board.findPawn(0)).isEqualTo(white);
-
-        Pawn black = new Pawn(Color.BLACK);
-        board.add(black);
-        assertThat(board.size()).isEqualTo(2);
-        assertThat(board.findPawn(1)).isEqualTo(black);
+    public void createPawnOnTheBoard() throws Exception {
+        createPawnAndVerify(Color.WHITE);
+        createPawnAndVerify(Color.BLACK);
     }
 }
--- src/test/java/pieces/PawnTest.java
+++ src/test/java/pieces/PawnTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.params.provider.EnumSource;
 import static org.assertj.core.api.Assertions.*;
 
 public class PawnTest {
+
     @Test
     @DisplayName("색 없이 Pawn을 생성하면 흰색으로 생성되어야 한다")
     public void createPawn() throws Exception {

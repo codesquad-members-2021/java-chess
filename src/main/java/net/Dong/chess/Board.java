c0ec4458d7435266c9b825a2aef0c35a9eaeb4c6
DHKim <ultraraptor@naver.com>
2021-02-13 02:35:29 +0900

    Pawnarr 로 구현하던 기존의 방식을 두개로 나눠서
    whitePawn과 balckPawn 으로 나눠서 구현함. 탐색속도 향상을 위해서

--- src/main/java/net/Dong/chess/Board.java
+++ src/main/java/net/Dong/chess/Board.java
@@ -1,17 +1,25 @@
 package net.Dong.chess;
 
 import net.Dong.chess.Pawn;
+import static net.Dong.chess.Pawn.Color;
+//import static net.Dong.chess.Pawn;
 
 import java.util.ArrayList;
 
 public class Board {
-    ArrayList<Pawn> arrPawn = new ArrayList<>();
+    ArrayList<Pawn> whitePawn = new ArrayList<>();
+    ArrayList<Pawn> blackPawn = new ArrayList<>();
 
 
-    public int add(Pawn pawn) {
+    public int whiteAdd(Pawn pawn) {
         //arrPawn.add()
-        arrPawn.add(pawn);
-        return arrPawn.size();
+        whitePawn.add(pawn);
+        return whitePawn.size();
+    }
+    public int blackAdd(Pawn pawn) {
+        //arrPawn.add()
+        blackPawn.add(pawn);
+        return blackPawn.size();
     }
 
     public void removePawn(Pawn remove) {
@@ -20,31 +28,76 @@ public class Board {
 
     public int size() {
         // return number of Mal
-        return arrPawn.size();
+        return whitePawn.size()+blackPawn.size();
+    }
+
+    private int whitePawnSize() {
+        return whitePawn.size();
+    }
+
+    public int blackPawnSize() {
+        return blackPawn.size();
     }
 
     public Pawn.Color findPawn(int index) {
         // Receives an index and returns it as an object color
-        return arrPawn.get(index).getColor();
+        return whitePawn.get(index).getColor();
+    }
+
+    public Pawn.Color findblackPawn(int index) {
+        // Receives an index and returns it as an object color
+        return whitePawn.get(index).getColor();
     }
 
     public void initialize() {
-        // white pawn 8, black pawn 8
 
+
+
+
+        // white pawn 8, black pawn 8
+        addPawn(Color.BALCK , 8);
+        addPawn(Color.WHITE , 8);
     }
 
-    public void print() {
+    private void addPawn(Pawn.Color color, int numOfPawn) {
+        for(int i=0 ; i<numOfPawn ; i++) {
+            arrPawn.add(new Pawn(color));
+        }
+        return;
+    }
+
+
+    private String print() {
         //print current state of chess board
+        // 화면에 출력할 결과를 StringBuilder에 저장한 후 String으로 반환하는 메소드를 구현한다
+        StringBuilder sb = new StringBuilder();
+
+
     }
 
 
     public String getWhitePawnsResult() {
-        return "";
+        StringBuilder sb = new StringBuilder();
+        int len = arrPawn.size();
+        for(int i=0 ; i<len ; i++) {
+            if(arrPawn.get(i).getColor() == Color.WHITE) {
+                sb.append("p");
+            }
+        }
+        return sb.toString();
     }
 
 
     public String getBlackPawnsResult() {
-        return "";
+        StringBuilder sb = new StringBuilder();
+        int len = arrPawn.size();
+        for(int i=0 ; i<len ; i++) {
+            if(arrPawn.get(i).getColor() == Color.BALCK) {
+                sb.append("P");
+            }
+        }
+        return sb.toString();
+
     }
 
 }

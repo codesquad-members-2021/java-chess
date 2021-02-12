565c3196aead2682f862a28edc228e6072397fc4
Haina <bibi6666667@kakao.com>
2021-02-12 15:13:39 +0900

    Pawn과 PawnTest클래스 생성 (미완성)

---
+++ src/main/java/Pawn.java
@@ -0,0 +1,11 @@
+public class Pawn {
+    private String color = "";
+
+    Pawn(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}

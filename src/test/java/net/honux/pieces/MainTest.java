b3275ad7f490d3f0ad3aab63a852b173d8256a94
Jihye Woo <woojihye2339@gmail.com>
2021-02-13 15:23:52 +0900

    refactor : MainTest 수정
      > 메소드명 영어로 변경
    
      > assertAll() 사용
        - white pawn 생성 여부와 관계없이 black pawn이 생성되는지 확인
    
      > 불필요한 변수 제거
    
      > assertThat → assertEquals로 변경
        - 이미 사용하고 있는 Assertion 구문으로 통일

--- src/test/java/net/honux/pieces/MainTest.java
+++ src/test/java/net/honux/pieces/MainTest.java
@@ -2,35 +2,34 @@ package net.honux.pieces;
 
 import org.junit.jupiter.api.*;
 
-import static org.assertj.core.api.Assertions.*;
-
 class PawnTest {
 
     @Test
     @DisplayName("흰색 폰과 검은색 폰이 생성되어야 한다")
-    void create_흰색폰과_검은색폰_생성() {
-        verifyPawn(Pawn.WHITE_COLOR);
-        verifyPawn(Pawn.BLACK_COLOR);
+    void createWhiteAndBlackPawns() {
+        Assertions.assertAll(
+                () -> verifyPawn(Pawn.WHITE_COLOR),
+                () -> verifyPawn(Pawn.BLACK_COLOR));
     }
 
     @Test
     @DisplayName("기본 생성자로 폰을 생성할 때, 흰색 폰이 생성되어야 한다")
-    void create_기본생성자로_폰생성() throws Exception {
+    void createWithDefaultPawn() {
         Pawn pawn = new Pawn();
         Assertions.assertEquals(Pawn.WHITE_COLOR, pawn.getColor());
     }
 
     @Test
     @DisplayName("다른 색 폰이 생성되면 안된다")
-    void create_지원하지않는_색의_폰생성() {
-        String unsupported_color = "red";
+    void createRedPawn() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            Pawn redPawn = new Pawn(unsupported_color);
+            Pawn redPawn = new Pawn("red");
         });
     }
 
     private void verifyPawn(String color) {
         Pawn pawn = new Pawn(color);
-        assertThat(pawn.getColor()).isEqualTo(color);
+        Assertions.assertEquals(pawn.getColor(), color);
     }
+
 }

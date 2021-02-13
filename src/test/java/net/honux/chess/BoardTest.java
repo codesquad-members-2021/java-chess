daefce64917945e15eab1caf1f0412abd461d0cc
Jihye Woo <woojihye2339@gmail.com>
2021-02-13 15:18:11 +0900

    refactor : BoardTest 수정
      > 메소드명 영어로 변경
    
      > 작은 기능 단위로 메소드 분할
        - addPawn()
        - checkTheBoard()
    
      > 테스트 클래스 접근 제어자
        - public → default 변경

--- src/test/java/net/honux/chess/BoardTest.java
+++ src/test/java/net/honux/chess/BoardTest.java
@@ -3,7 +3,9 @@ package net.honux.chess;
 import net.honux.pieces.Pawn;
 import org.junit.jupiter.api.*;
 
-public class BoardTest {
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class BoardTest {
 
     private Board board;
 
@@ -13,20 +15,33 @@ public class BoardTest {
     }
 
     @Test
-    @DisplayName("보드 위에 폰 추가하고 추가된 폰이 보드에 정상적으로 반영되어야 한다.")
-    void add_폰이_보드에_정상적으로_추가되는지_확인() throws Exception {
-        Pawn white = new Pawn(Pawn.WHITE_COLOR);
-        addPawnAndCheckTheBoard(white);
+    @DisplayName("보드에 있는 폰을 정상적으로 찾을 수 있어야 한다.")
+    void findPawnOnTheBoard() {
+        Pawn firstPawn = new Pawn();
+        addThePawnProperly(firstPawn, 0);
 
-        Pawn black = new Pawn(Pawn.BLACK_COLOR);
-        addPawnAndCheckTheBoard(black);
+        Pawn secondPawn = new Pawn();
+        addThePawnProperly(secondPawn, 1);
     }
 
-    private void addPawnAndCheckTheBoard(Pawn pawn) {
+    @Test
+    @DisplayName("보드에 없는 폰을 찾으려고 할 때, 에러가 발생해야 한다.")
+    void findPawnNotOnTheBoard() {
+        Assertions.assertThrows(IndexOutOfBoundsException.class, () -> board.findPawn(0));
+    }
+
+    private void addPawn(Pawn newPawn) {
         int sizeBeforeAddThePawn = board.size();
-        board.add(pawn);
+        board.add(newPawn);
         Assertions.assertEquals(sizeBeforeAddThePawn + 1, board.size());
-        Assertions.assertEquals(pawn, board.findPawn(sizeBeforeAddThePawn));
     }
 
+    private void checkTheBoard(Pawn newPawn, int newPawnIdx) {
+        Assertions.assertEquals(newPawn, board.findPawn(newPawnIdx));
+    }
+
+    private void addThePawnProperly(Pawn newPawn, int newPawnIdx) {
+        addPawn(newPawn);
+        checkTheBoard(newPawn, newPawnIdx);
+    }
 }

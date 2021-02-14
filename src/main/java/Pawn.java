dc4bff4de8ba283de6ec6214e1bd3d11f6373b12
MyungWanPark <mywanpark@gmail.com>
2021-02-14 22:06:35 +0900

    Adding PawnTest.java and Pawn.java

---
+++ src/main/java/Pawn.java
@@ -0,0 +1,8 @@
+public class Pawn {
+    public Pawn(String white) {
+    }
+
+    public String getColor() {
+        return null;
+    }
+}
---
+++ src/test/java/PawnTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.*;
+import static org.assertj.core.api.Assertions.*;
+
+public class PawnTest {
+
+    @Test
+    @DisplayName("흰색 폰이 생성되어야 한다")
+    public void create() {
+        Pawn pawn = new Pawn("white");
+        assertThat(pawn.getColor()).isEqualTo("white");
+    }
+}

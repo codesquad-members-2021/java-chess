2ccfc9a34d6a5dbabb814624256653ef332e059a
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2021-02-14 18:56:15 +1300

    Update diff

--- src/test/java/net/honux/chess/pieces/PawnTest.java
+++ src/test/java/net/honux/chess/pieces/PawnTest.java
@@ -8,8 +8,8 @@ import static org.assertj.core.api.Assertions.*;
 class PawnTest {
 
     @Test
-    @DisplayName("흰색 폰이 생성되어야 한다")
-    public void create() {
+    @DisplayName("흰색 폰과 검은색 폰이 생성되어야 한다")
+    void 폰을_만든다() {
         verifyPawn("white");
         verifyPawn("black");
     }
@@ -17,11 +17,5 @@ class PawnTest {
     private void verifyPawn(final String color) {
         Pawn pawn = new Pawn(color);
         assertThat(pawn.getColor()).isEqualTo(color);
-    }
-
-    @Test
-    public void create_기본생성자() throws Exception {
-        Pawn pawn = new Pawn();
-        assertThat(pawn.getColor()).isEqualTo("white");
     }
 }

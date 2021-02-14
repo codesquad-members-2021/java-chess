da1561386994c0fbeaf606e585e064ab36eca1ec
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2021-02-14 18:59:30 +1300

    Update diff_2

--- src/test/java/net/honux/chess/pieces/PawnTest.java
+++ src/test/java/net/honux/chess/pieces/PawnTest.java
@@ -18,4 +18,10 @@ class PawnTest {
         Pawn pawn = new Pawn(color);
         assertThat(pawn.getColor()).isEqualTo(color);
     }
+
+    @Test
+    public void create_기본생성자() throws Exception {
+        Pawn pawn = new Pawn();
+        assertThat(pawn.getColor()).isEqualTo("white");
+    }
 }

af8ded5aae72a02eb0ecae3bd2536f85f5646a01
rla36 <bhmarsd@naver.com>
2021-02-15 04:14:00 +0900

    size findPawn메서드 테스트 중복제거

--- src/Test/java/chess/BoardTest.java
+++ src/Test/java/chess/BoardTest.java
@@ -1,27 +1,41 @@
 package chess;
 
-import org.junit.jupiter.api.*;
 import pieces.Pawn;
 
+import org.junit.jupiter.api.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BoardTest {
 
-    @Test
-    @DisplayName("보드를 생성하고 흰색 검은색 폰을 추가한다.")
-    public void createBoardAddPawns() throws Exception{
-        Board board = new Board();
+    Board board;
+    Pawn whitePawn;
+    Pawn blackPawn;
 
-        Pawn whitePawn = new Pawn(Color.WHITE);
-        board.add(whitePawn);
-        assertEquals(1, board.size());
-        assertEquals(whitePawn, board.findPawn(0));
-
-        Pawn blackPawn = new Pawn(Color.BLACK);
-        board.add(blackPawn);
-        assertEquals(2, board.size());
-        assertEquals(blackPawn, board.findPawn(1));
+    @BeforeEach
+    void setUp(){
+        board = new Board();
+        whitePawn = new Pawn(Color.WHITE);
+        blackPawn = new Pawn(Color.BLACK);
 
     }
+    @Test
+    @DisplayName("보드를 생성하고 흰색 검은색 폰을 추가한 후 size, findPawn 메서드 검증한다.")
+    void createBoardAddPawns() throws Exception{
+
+        board.add(whitePawn);
+        assertAll("흰색 폰을 보드에 올린 후 size, findPawn메서드 검증한다.",
+                () -> assertThat(board.size()).isEqualTo(1),
+                () -> assertThat(board.findPawn(0)).isEqualTo(whitePawn));
+
+
+        board.add(blackPawn);
+        assertAll("검은색 폰을 보드에 올린 후 size, findPawn메서드 검증한다.",
+                () -> assertThat(board.size()).isEqualTo(2),
+                () -> assertThat(board.findPawn(1)).isEqualTo(blackPawn));
+
+    }
+
+
 
 }

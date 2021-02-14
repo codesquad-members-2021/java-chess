2508061fda64157a91a90a69e52c105e374c8aa8
MyungWanPark <mywanpark@gmail.com>
2021-02-14 22:25:58 +0900

    Adding local variable in create() in PawnTest.java

--- src/test/java/PawnTest.java
+++ src/test/java/PawnTest.java
@@ -6,9 +6,9 @@ public class PawnTest {
     @Test
     @DisplayName("흰색 폰이 생성되어야 한다")
     public void create() {
-        Pawn pawn = new Pawn("white");
-        Pawn pawn2 = new Pawn("black");
+        String color = "white";
+        Pawn pawn = new Pawn(color);
         assertThat(pawn.getColor()).isEqualTo("white");
-        assertThat(pawn2.getColor()).isEqualTo("black");
+//        assertThat(pawn2.getColor()).isEqualTo("black");
     }
 }

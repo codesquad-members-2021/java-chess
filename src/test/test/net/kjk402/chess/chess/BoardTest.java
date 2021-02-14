456458f5f93e089feb2013b231763c4af8ff2c94
JoonKi-Kim <joonki402@gmail.com>
2021-02-14 19:01:34 +0900

    assertAll 추가 및 메서드명, @DisplayName 수정

--- src/test/test/net/kjk402/chess/chess/BoardTest.java
+++ src/test/test/net/kjk402/chess/chess/BoardTest.java
@@ -11,21 +11,23 @@ class BoardTest {
     private Board board;
 
     @BeforeEach
-    void init(){
+    void init() {
         board = new Board();
     }
 
     @Test
-    @DisplayName("흰색폰 넣은 다음 검은색폰을 넣는다.")
-    public void create(){
+    @DisplayName("보드에 흰색폰과 검정폰을 차례로 넣고 보드의 사이즈와 폰의 위치를 확인.")
+    void createBoard() {
         verifyBoard(Pawn.WHITE, 1, 0);
-        verifyBoard(Pawn.BLACK,2,1);
+        verifyBoard(Pawn.BLACK, 2, 1);
     }
 
-    void verifyBoard(String color,int expectedSize ,int idx){
+    void verifyBoard(String color, int expectedSize, int idx) {
         Pawn pawn = new Pawn(color);
         board.add(pawn);
-        assertEquals(expectedSize, board.size());
-        assertEquals(pawn, board.findPwan(idx));
+        assertAll(
+                () -> assertEquals(expectedSize, board.size()),
+                () -> assertEquals(pawn, board.findPwan(idx))
+        );
     }
 }

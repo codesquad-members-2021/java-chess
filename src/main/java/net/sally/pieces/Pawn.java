0524f69e24a94efe6765d69a9e73bf22ae88576f
Sally Oh <min27604@hotmail.com>
2021-02-15 22:59:24 +0900

    feat: Create representation variable and change constructor
    - 출력 시 필요한 representation을 멤버변수로 지정해주었고 represenation을 리턴해주는 getRepresentation() 메소드를 추가해주었습니다.
    - 기존의 기본생성자와 color를 매개변수로 받는 생성자 모두 representation 값을 지정해줄 수 있도록 변경하였습니다.

--- src/main/java/net/sally/pieces/Pawn.java
+++ src/main/java/net/sally/pieces/Pawn.java
@@ -8,16 +8,23 @@ public class Pawn {
     public static final char BLACK_REPRESENTATION = 'P';
 
     private final String color;
+    private final char representation;
 
     public Pawn() {
         this.color = WHITE;
+        this.representation = WHITE_REPRESENTATION;
     }
 
-    public Pawn(String color) {
+    public Pawn(String color, char representation) {
         this.color = color;
+        this.representation = representation;
     }
 
     public String getColor() {
         return color;
+    }
+
+    public char getRepresentation() {
+        return representation;
     }
 }

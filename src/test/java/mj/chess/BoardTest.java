f18fc10d46fac83d851a084f2d86b0aa8d48dfa2
MJbae <gently.gogo@gmail.com>
2021-02-15 22:59:03 +0900

    Refactor: 테스트 클래스 및 메소드의 접근제어자 생략(public -> default)
    JUnit5에서부터 기본 접근제어자를 default로 사용할 것을 권장, 가독성을 향상할 수 있음

--- src/test/java/mj/chess/BoardTest.java
+++ src/test/java/mj/chess/BoardTest.java
@@ -7,22 +7,22 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-public class BoardTest {
+class BoardTest {
     Board board;
 
     @BeforeEach
-    public void setup(){
+    void setup(){
         board = new Board();
     }
 
     @Test
     @DisplayName("보드 객체 내 흰색 폰 1개, 검은색 폰 1개 생성")
-    public void createBoard() {
+    void createBoard() {
         verifyAddAndSearch(new Pawn(ColorOfPiece.WHITE), 1, 0);
         verifyAddAndSearch(new Pawn(ColorOfPiece.BLACK), 2, 1);
     }
 
-    public void verifyAddAndSearch(Pawn pawn, int size, int index){
+    void verifyAddAndSearch(Pawn pawn, int size, int index){
         board.add(pawn);
         assertThat(board.size()).isEqualTo(size);
         assertThat(pawn).isEqualTo(board.findPawn(index));
--- src/test/java/mj/chess/pieces/PawnTest.java
+++ src/test/java/mj/chess/pieces/PawnTest.java
@@ -5,27 +5,27 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
-public class PawnTest {
+class PawnTest {
 
     @Test
     @DisplayName("기본 생성자로 흰색 폰 생성")
-    public void createPawn() {
+    void createPawn() {
         verifyColorOfPawn(new Pawn(), ColorOfPiece.WHITE);
     }
 
     @Test
     @DisplayName("흰색 폰 생성")
-    public void createWhitePawn() {
+    void createWhitePawn() {
         verifyColorOfPawn(new Pawn(ColorOfPiece.WHITE), ColorOfPiece.WHITE);
     }
 
     @Test
     @DisplayName("검은색 폰 생성")
-    public void createBlackPawn() {
+    void createBlackPawn() {
         verifyColorOfPawn(new Pawn(ColorOfPiece.BLACK), ColorOfPiece.BLACK);
     }
 
-    public void verifyColorOfPawn(Pawn pawn, ColorOfPiece color) {
+    void verifyColorOfPawn(Pawn pawn, ColorOfPiece color) {
         assertThat(pawn.getColor()).isEqualTo(color);
     }
 }

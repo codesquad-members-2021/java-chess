4d0dabf1622397c47f6ae5debe31a01d7746ead6
noel <68936@naver.com>
2021-02-15 23:16:16 +0900

    [Feat] Pawn의 기호를 상수로 지정 및 기호 리턴 메소드 구현
    자주 사용되는 색상별 폰의 모양(p/P)를 static 상수로 지정하였습니다.
    Pawn 객체의 기호를 리턴하는, getRepresentation() 메소드를 구현하였습니다.

--- src/main/java/net/sanhee/pieces/Pawn.java
+++ src/main/java/net/sanhee/pieces/Pawn.java
@@ -3,8 +3,11 @@ package net.sanhee.pieces;
 
 public class Pawn {
     public static final int MAX_SPAWN_NUMBER = 8;
+    public static final char BLACK_REPRESENTATION = 'P';
+    public static final char WHITE_REPRESENTATION = 'p';
+
     private final UnitColor color;
-    private final String shape;
+    private final char shape;
 
     public Pawn() {
         this(UnitColor.WHITE);
@@ -13,17 +16,21 @@ public class Pawn {
     public Pawn(UnitColor color) {
         this.color = color;
 
-        switch (color){
+        switch (color) {
             case BLACK:
-                this.shape = "P";
+                this.shape = BLACK_REPRESENTATION;
                 break;
             case WHITE:
             default:
-                this.shape = "p";
+                this.shape = WHITE_REPRESENTATION;
         }
     }
 
     public UnitColor getColor() {
         return color;
+    }
+
+    public char getRepresentation() {
+        return this.shape;
     }
 }

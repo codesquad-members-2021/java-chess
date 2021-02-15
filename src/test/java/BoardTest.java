26a0906fcdc74e5adb3a0ba181a330f70c792f22
noel <68936@naver.com>
2021-02-15 21:01:12 +0900

    [Test] 리뷰를 반영하여, Board 객체를 BeforeEach를 통해 초기화
    기존에 각각의 폰 마다 모듈단위의 테스트를 하고자, Board 객체를 static으로 선언하여
    @Test 메소드간에 정보를 공유하였지만, 각 @Test가 순차적으로 실행되지 않고
    독립된 환경에서 이루어짐을 알게되었습니다.
    다만 현재 제 수준으로는 여러 개의 테스트를 모듈로 나눌 수 없어
    한 테스트에 분리 된 검사 메소드를 담았습니다.

--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -1,41 +1,50 @@
 import net.sanhee.chess.*;
 import net.sanhee.pieces.Pawn;
 import net.sanhee.pieces.UnitColor;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoardTest {
 
-    static public Board board = new Board();
+    Board board;
+    int totalPawnCnt;
 
-    @Test
-    @DisplayName("흰색 Pawn을 체스 판, 첫번째 칸에 추가")
-    void create() throws Exception {
-        spawnPawn(UnitColor.WHITE, 1);
+    @BeforeEach
+    void boardTestInit(){
+        board = new Board();
+        totalPawnCnt = 0;
     }
 
     @Test
-    @DisplayName("흑색 Pawn을 체스 판, 두번째 칸에 추가")
-    void create2() throws Exception {
-        spawnPawn(UnitColor.BLACK, 2);
+    @DisplayName("[테스트] 체스 판에 Pawn 추가 및 유효성 검사")
+    void createPawn() {
+        Pawn whitePawn = spawnPawn(UnitColor.WHITE);
+        Pawn blackPawn  = spawnPawn(UnitColor.BLACK);
+
+        //m 실제 스폰된 폰의 개수와 add 개수가 맞는지 검사하는 메소드
+        pawnSizeCheck();
+
+        //m 입력 idx에 pawn이 스폰되있는지 검사하는 메소드
+        pawnLocationCheck(whitePawn,0);
+        pawnLocationCheck(blackPawn,1);
     }
 
-    void spawnPawn(UnitColor color, int order) {
+    Pawn spawnPawn(UnitColor color) {
         Pawn pawn = new Pawn(color);
         board.add(pawn);
-        spawnCheck(pawn, order);
+        totalPawnCnt++;
+
+        return pawn;
     }
 
-    void spawnCheck(Pawn object, int order) {
-        assertAll(
-                //A Case
-                () -> assertThat(board.size()).isEqualTo(order),
-                //B Case
-                () -> assertThat(board.findPawn(order - 1)).isEqualTo(object)
-        );
+    void pawnSizeCheck() {
+        assertThat(board.size()).isEqualTo(totalPawnCnt);
+    }
+
+    void pawnLocationCheck(Pawn pawn, int idx){
+        assertThat(board.findPawn(idx)).isEqualTo(pawn);
     }
 
 }

a3d4c0bf3ad4a7fd6cb2d9713f8481d037e58f0a
tmdgusya <tmdgusya@naver.com>
2021-02-15 14:51:44 +0900

    feat 체스판 초기화
    Board.java
     - BOARD_SIZE = 8 , 반복문에서 사용할때 가독성을 위해 상수로 추가
     - 요구사항에 따른 색깔별 List 를 추가하고, initialize() 에서 그 구현체를 넣어주도록 했음.
     - initialize() 체스판을 초기화 하는 역할으로, 하얀색폰과 검은색폰을 8개씩 추가해줌.
     - add() 의 경우는 Color 로 구분할 수 있기에 Color 에 따른 List 를 골라서 add 하도록 함.
     - size() 의 경우 각 Color 에 따른 size() 를 리턴할 수 있도록 메소드 두개 생성. (findPawn 과 getPawnResult 도 마찬가지)
     - getPawnsResult() -> 문자열 연산이 많기에 StringBuilder 를 이용하였음

--- src/main/java/net/honux/chess/core/Board.java
+++ src/main/java/net/honux/chess/core/Board.java
@@ -1,5 +1,6 @@
 package net.honux.chess.core;
 
+import net.honux.chess.attribute.Color;
 import net.honux.chess.entity.pieces.Pawn;
 
 import java.util.ArrayList;
@@ -7,24 +8,70 @@ import java.util.List;
 import java.util.Optional;
 
 public class Board {
-    private List<Pawn> pawnList = new ArrayList<>();
+    private static final int BOARD_SIZE = 8;
+    private List<Pawn> whitePawnList;
+    private List<Pawn> blackPawnList;
+
+    public void initialize() {
+        whitePawnList = new ArrayList<>();
+        blackPawnList = new ArrayList<>();
+
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            whitePawnList.add(new Pawn(Color.WHITE));
+            blackPawnList.add(new Pawn(Color.BLACK));
+        }
+    }
 
     public void add(Pawn pawn) {
-        pawnList.add(pawn);
+        if (pawn.getColor() == Color.BLACK) {
+            blackPawnList.add(pawn);
+        } else {
+            whitePawnList.add(pawn);
+        }
     }
 
-    public int size() {
-        return pawnList.size();
+    public int whitePawnsize() {
+        return whitePawnList.size();
     }
 
-    public Optional<Pawn> findPawn(int index) {
+    public int blackPawnsize() {
+        return blackPawnList.size();
+    }
+
+    public Optional<Pawn> findWhitePawn(int index) {
         Optional<Pawn> pawn = Optional.empty();
         try {
-            pawn = Optional.ofNullable(pawnList.get(index));
-        }catch (IndexOutOfBoundsException e){
+            pawn = Optional.ofNullable(whitePawnList.get(index));
+        } catch (IndexOutOfBoundsException e) {
             System.out.println("해당 위치에 Pawn 이 존재하지 않습니다.");
         }
         return pawn;
+    }
+
+    public Optional<Pawn> findBlackPawn(int index) {
+        Optional<Pawn> pawn = Optional.empty();
+        try {
+            pawn = Optional.ofNullable(blackPawnList.get(index));
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("해당 위치에 Pawn 이 존재하지 않습니다.");
+        }
+        return pawn;
+    }
+
+    public String getWhitePawnsResult() {
+        StringBuilder sb = new StringBuilder();
+        for (Pawn pawn : whitePawnList) {
+            sb.append(pawn.getRepresentation());
+        }
+        return sb.toString();
+    }
+
+    public String getBlackPawnsResult() {
+        StringBuilder sb = new StringBuilder();
+        for (Pawn pawn : blackPawnList) {
+            sb.append(pawn.getRepresentation());
+        }
+        return sb.toString();
     }
 
 }

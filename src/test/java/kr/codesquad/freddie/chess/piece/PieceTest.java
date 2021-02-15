4772881f639cc6ba447493fa8cf22abd176e1bdb
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-02-15 17:57:46 +0900

    Update the test create in PieceTest to check kind

--- src/test/java/kr/codesquad/freddie/chess/piece/PieceTest.java
+++ src/test/java/kr/codesquad/freddie/chess/piece/PieceTest.java
@@ -9,11 +9,23 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 class PieceTest {
     @Test
     void create() {
+        checkPieceKind("pawn", Kind.PAWN);
+        checkPieceKind("knight", Kind.KNIGHT);
+        checkPieceKind("rook", Kind.ROOK);
+        checkPieceKind("bishop", Kind.BISHOP);
+        checkPieceKind("queen", Kind.QUEEN);
+        checkPieceKind("king", Kind.KING);
+    }
+
+    private void checkPieceKind(String kind, Kind expectedKind) {
+        Piece whitePiece = Piece.create("white", kind);
+        Piece blackPiece = Piece.create("black", kind);
+
         assertAll(
-                () -> assertThat(Piece.createBlackPawn().getColor()).isEqualTo(Color.BLACK),
-                () -> assertThat(Piece.createWhitePawn().getColor()).isEqualTo(Color.WHITE),
-                () -> assertThat(Piece.create("white", "pawn").getColor()).isEqualTo(Color.WHITE),
-                () -> assertThat(Piece.create("black", "pawn").getColor()).isEqualTo(Color.BLACK)
+                () -> assertThat(whitePiece.getColor()).isEqualTo(Color.WHITE),
+                () -> assertThat(whitePiece.getKind()).isEqualTo(expectedKind),
+                () -> assertThat(blackPiece.getColor()).isEqualTo(Color.BLACK),
+                () -> assertThat(blackPiece.getKind()).isEqualTo(expectedKind)
         );
     }
 

ce8329bd580ac83fa02b7e6e727298267ae5808e
noel <68936@naver.com>
2021-02-15 21:34:26 +0900

    [feat] Pawn을 구분하기 위해, 색상에 따라 문자 지정
    요구사항에 있는 검은색 Pawn은 대문자 P, 흰색 Pawn은 소문자 p를 반영하였습니다.
    기본 생성자는 this() 생성자 함수 호출로, 인자가 없을 경우를 생각했습니다.
    생성자 함수의 인자로 들어오는 enum Color 상수에 따라 모형을 Switch문의 조건에 따라 기물 모양을 지정해줍니다.

--- src/main/java/net/sanhee/pieces/Pawn.java
+++ src/main/java/net/sanhee/pieces/Pawn.java
@@ -3,13 +3,23 @@ package net.sanhee.pieces;
 
 public class Pawn {
     private final UnitColor color;
+    private final String shape;
 
     public Pawn() {
-        this.color = UnitColor.WHITE;
+        this(UnitColor.WHITE);
     }
 
     public Pawn(UnitColor color) {
         this.color = color;
+
+        switch (color){
+            case BLACK:
+                this.shape = "P";
+                break;
+            case WHITE:
+            default:
+                this.shape = "p";
+        }
     }
 
     public UnitColor getColor() {

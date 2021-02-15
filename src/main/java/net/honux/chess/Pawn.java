612d4d0a587aa0499c7d0074a35ad93800af0335
윤혜지 <yunhg95@gmail.com>
2021-02-15 17:04:32 +0900

    Pawn Enum 적용
    미션1에서 Pawn 컬러의 인자로 다른 컬러가 들어왔을때의 방어로직이 존재하지 않아, 미션 2에 적용함
    enum 활용에 미숙하여 제대로 잘 작성한 코드인지는 불분명하다
    enum 활용을 통해 Pawn의 컬러는 white 또는 black 두 가지만 가질 수 있다

--- src/main/java/net/honux/chess/Pawn.java
+++ src/main/java/net/honux/chess/Pawn.java
@@ -1,14 +1,33 @@
 package net.honux.chess;
 
 public class Pawn {
+    enum Color {
+        WHITE("white"),
+        BLACK("black");
 
-    private String color;
+        private String value;
+
+        Color(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return this.value;
+        }
+
+    }
+
+    private Color color;
 
     public Pawn(String color) {
-        this.color = color;
+        if (color.equals("WHITE") || color.equals("white"))
+            this.color = Color.WHITE;
+        else if (color.equals("BLACK") || color.equals("black"))
+            this.color = Color.BLACK;
     }
 
     public String getColor() {
-        return color;
+        return color.getValue();
     }
+
 }

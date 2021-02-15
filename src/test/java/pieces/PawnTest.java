626b5921f93d5c6b2cf9813690af6b77d5926ee4
janeljs <jisunlim818@gmail.com>
2021-02-15 16:25:11 +0900

    Remove public access modifier in PawnTest

--- src/test/java/pieces/PawnTest.java
+++ src/test/java/pieces/PawnTest.java
@@ -6,13 +6,13 @@ import pieces.Pawn.Color;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
-public class PawnTest {
+class PawnTest {
     private Pawn pawn;
     private Pawn whitePawn;
     private Pawn blackPawn;
 
     @BeforeEach
-    public void setup() {
+    void setup() {
         pawn = new Pawn();
         whitePawn = new Pawn(Color.WHITE);
         blackPawn = new Pawn(Color.BLACK);
@@ -20,13 +20,13 @@ public class PawnTest {
 
     @Test
     @DisplayName("색상과 출력 양식에 알맞게 폰이 생성되었는지 검증한다.")
-    public void create() {
+    void create() {
         assertAll(() -> verifyPawn(pawn, Color.WHITE, "p"),
                 () -> verifyPawn(whitePawn, Color.WHITE, "p"),
                 () -> verifyPawn(blackPawn, Color.BLACK, "P"));
     }
 
-    public void verifyPawn(Pawn pawn, Color color, String representation) {
+    void verifyPawn(Pawn pawn, Color color, String representation) {
         assertAll(() -> assertThat(pawn.getColor()).isEqualTo(color),
                 () -> assertThat(pawn.getColor().getRepresentation()).isEqualTo(representation));
     }

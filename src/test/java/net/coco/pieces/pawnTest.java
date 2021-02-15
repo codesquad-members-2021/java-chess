3b413b682ca1342c13aa4cc3ccd23d412f8b86ed
gisungchoi <chlrltjd159@naver.com>
2021-02-15 16:34:36 +0900

    PawnTest class 에서 기본생성자 테스트
    상수에서 enum으로 변경해서 상수 사용 부분을 enum으로 변경했다.
    전에 확인해보니 기본 생성자 test코드가 없어서 이번에 새로 작성했다.

--- src/test/java/net/coco/pieces/pawnTest.java
+++ src/test/java/net/coco/pieces/pawnTest.java
@@ -1,21 +1,22 @@
 package net.coco.pieces;
 
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 
 class pawnTest {
 
     @Test
-    @DisplayName("Pawn Color Test")
-    public void create(){
-        verifyPawn("white");
-        verifyPawn("black");
-    }
-
-    public void verifyPawn(final String color){
-        Pawn pawn=new Pawn(color);
-        Assertions.assertThat(color).isEqualTo(pawn.getColor());
+    @DisplayName("기본 생성자 테스트")
+    void create_defaultConstructor() {
+        Pawn pawn = new Pawn();
+        Assertions.assertAll(
+                () -> assertThat(PawnEnum.WHITE_COLOR).isEqualTo(pawn.getColor()),
+                () -> assertThat(PawnEnum.WHITE_COLOR.representation).isEqualTo(pawn.getRepresentation())
+        );
 
     }
+
 }

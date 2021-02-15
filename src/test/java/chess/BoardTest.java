3503d6513c2aa725065f9d1fb42719cc8eb810a0
고정완 (Can) <jw.gho@ecubelabs.com>
2021-02-15 12:03:39 +0900

    [test] BoardTest 를 메소드 단위의 unit 테스트로 변경
    흰색과 까만 Pawn 을 추가하는 기존 테스트 코드가 마음에 들지 않아서,
    메소드 단위로 독립적인 테스트가 이루어질 수 있도록 수정

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -2,29 +2,56 @@ package chess;
 
 import chess.pieces.Color;
 import chess.pieces.Pawn;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class BoardTest {
-    @Test
-    @DisplayName("흰색 Pawn 과 검은 색 Pawn 을 체스 판에 추가한다.")
-    public void create() {
-        Board board = new Board();
+    private Board board;
 
-        Pawn whitePawn = new Pawn(Color.WHITE);
-        board.add(whitePawn);
+    @BeforeEach
+    void setup() {
+        board = new Board();
+    }
+
+    @Test
+    @DisplayName("체스 판에 1개 추가하면 size 가 1이다.")
+    public void size_1() {
+        board.add(new Pawn(Color.WHITE));
         assertThat(board.size())
                 .isEqualTo(1);
-        assertThat(board.findPawn(0))
-                .isEqualTo(whitePawn);
+    }
 
-        Pawn blackPawn = new Pawn(Color.BLACK);
-        board.add(blackPawn);
+    @Test
+    @DisplayName("체스 판에 2개 추가하면 size 가 2이다.")
+    public void size_2() {
+        board.add(new Pawn(Color.WHITE));
+        board.add(new Pawn(Color.BLACK));
         assertThat(board.size())
                 .isEqualTo(2);
-        assertThat(board.findPawn(1))
-                .isEqualTo(blackPawn);
+    }
+
+    @Test
+    @DisplayName("체스 판에 추가한 pawn 을 findPawn 으로 조회할 수 있다.")
+    public void findPawn() {
+        Pawn whitePawn = new Pawn(Color.WHITE);
+        Pawn blackPawn = new Pawn(Color.BLACK);
+
+        board.add(whitePawn);
+        board.add(blackPawn);
+        assertAll(
+                () -> assertThat(board.findPawn(0))
+                        .isEqualTo(whitePawn),
+                () -> assertThat(board.findPawn(1))
+                        .isEqualTo(blackPawn),
+                () -> assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                        .isThrownBy(() -> board.findPawn(-1)),
+                () -> assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                        .isThrownBy(() -> board.findPawn(2))
+        );
     }
 }

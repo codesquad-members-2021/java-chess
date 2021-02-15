348f5a21ee6225f5c2832912fb1a68f50501f3d1
고정완 (Can) <jw.gho@ecubelabs.com>
2021-02-15 10:31:17 +0900

    [feat] 1. Pawn 클래스 변경; 매개변수 없는 생성자
    색이 없는 Pawn을 생성하는 경우 기본으로 흰색의 말을 생성하도록
    Pawn 에 매개변수가 없는 생성자를 추가함

--- src/main/java/net/honux/chess/Pawn.java
+++ src/main/java/net/honux/chess/Pawn.java
@@ -3,6 +3,10 @@ package net.honux.chess;
 public class Pawn {
     private final Color color;
 
+    public Pawn() {
+        color = Color.WHITE;
+    }
+
     public Pawn(String color) {
         this.color = Color.valueOf(color.toUpperCase());
     }

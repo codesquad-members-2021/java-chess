6d5c1bae999a561162b827e8eb9fbe60bbff7b75
noel <68936@naver.com>
2021-02-15 23:50:06 +0900

    [Test] 생성된 흰색/검은색 Pawn 열의 결과 검증
    initialize() 메소드에서 흰색 Pawn 열의 결과가 pppppppp
    검은색 Pawn 열의 결과가 PPPPPPPP 인지를 검증하는 테스트 입니다.

--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -4,7 +4,6 @@ import net.sanhee.pieces.UnitColor;
 import org.junit.jupiter.api.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class BoardTest {
 
@@ -12,7 +11,7 @@ class BoardTest {
     int totalPawnCnt;
 
     @BeforeEach
-    void boardTestInit(){
+    void boardTestInit() {
         board = new Board();
         totalPawnCnt = 0;
     }
@@ -21,14 +20,14 @@ class BoardTest {
     @DisplayName("[테스트] 체스 판에 Pawn 추가 및 유효성 검사")
     void createPawn() {
         Pawn whitePawn = spawnPawn(UnitColor.WHITE);
-        Pawn blackPawn  = spawnPawn(UnitColor.BLACK);
+        Pawn blackPawn = spawnPawn(UnitColor.BLACK);
 
         //m 실제 스폰된 폰의 개수와 add 개수가 맞는지 검사하는 메소드
         pawnSizeCheck();
 
         //m 입력 idx에 pawn이 스폰되있는지 검사하는 메소드
-        pawnLocationCheck(whitePawn,0);
-        pawnLocationCheck(blackPawn,1);
+        pawnLocationCheck(whitePawn, 0);
+        pawnLocationCheck(blackPawn, 1);
     }
 
     Pawn spawnPawn(UnitColor color) {
@@ -43,8 +42,16 @@ class BoardTest {
         assertThat(board.size()).isEqualTo(totalPawnCnt);
     }
 
-    void pawnLocationCheck(Pawn pawn, int idx){
+    void pawnLocationCheck(Pawn pawn, int idx) {
         assertThat(board.findPawn(idx)).isEqualTo(pawn);
     }
 
+    @Test
+    @DisplayName("[테스트] 생성된 흰색/검은색 Pawn 열의 결과 검증")
+    void initialize() {
+        Board board = new Board();
+        board.initialize();
+        assertThat(board.getWhitePawnsResult()).isEqualTo(pppppppp);
+        assertThat(board.getBlackPawnsResult()).isEqualTo(PPPPPPPP);
+    }
 }

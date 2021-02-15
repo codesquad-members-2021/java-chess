42314d0ff1b410aa523e9f7458bc69adb6bcf851
Sally Oh <min27604@hotmail.com>
2021-02-15 22:56:51 +0900

    feat: Create static final values of representations
    - 출력 시 필요한 representation 값을 컬러의 상수값과 같이 상수를 만들어 주었습니다.

--- src/main/java/net/sally/pieces/Pawn.java
+++ src/main/java/net/sally/pieces/Pawn.java
@@ -4,6 +4,9 @@ public class Pawn {
     public static final String WHITE = "white";
     public static final String BLACK = "black";
 
+    public static final char WHITE_REPRESENTATION = 'p';
+    public static final char BLACK_REPRESENTATION = 'P';
+
     private final String color;
 
     public Pawn() {

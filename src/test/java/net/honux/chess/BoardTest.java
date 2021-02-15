a1daeaab2bedca7bf8487a8c010dc85e223fc38a
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2021-02-15 20:11:48 +1300

    Add class Board with basic functions
    요구사항에 따라 BoardTest 에 있는 Board 의 메소드들을 구현했습니다.

---
+++ src/test/java/net/honux/chess/BoardTest.java
@@ -0,0 +1,23 @@
+package net.honux.chess;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+class BoardTest {
+
+    @Test
+    public void create() throws Exception {
+        Board board = new Board();
+
+        Pawn white = new Pawn(Pawn.WHITE_COLOR);
+        board.add(white);
+        assertEquals(1, board.size());
+        assertEquals(white, board.findPawn(0));
+
+        Pawn black = new Pawn(Pawn.BLACK_COLOR);
+        board.add(black);
+        assertEquals(2, board.size());
+        assertEquals(black, board.findPawn(1));
+    }
+
+}

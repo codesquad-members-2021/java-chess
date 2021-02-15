975a88a1ec80414038657f5ec7b4559e438080b1
tmdgusya <tmdgusya@naver.com>
2021-02-15 14:28:42 +0900

    feat Pawn 색에 따른 출력 문자 부여
    Pawn.java
    
      - Representation 을 char 가 아닌 따로 Enum Class 를 이용한 이유는 타입의 유효성을 위해 설정하였음. 예를 들면 'c' 가 들어온다해도 현재 요구사항에서는 막을 방법이 if 를 이용한 validate 말고는 없기에, Enum 을 이용하여 타입에 대한 제한을 걸어둠.
    
      - 생성자중 representation 을 받는 생성자에 this(color) 로 한 이유는 Client 의 실수로, representation 이 Black 인데도 색깔은 white 일수도 있기에, 이를 방지하기 위해 색깔에 맞는 값을 넣어주는 이전 생성자를 이용함.
    
      - getRepresentation() 에서 name() 을 리턴하는 이유는 후에 사용될 Board 에서의 이름 출력을 고려하여 아래와 같이 설계함.
    
     PawnName.java
    
      - 각 색깔 속성에 맞는 representation 값을 가지도록 하였음.
      - getRepresentation 을 통해서 해당 representation 값을 얻을 수 있음.

--- src/main/java/net/honux/chess/entity/pieces/Pawn.java
+++ src/main/java/net/honux/chess/entity/pieces/Pawn.java
@@ -1,20 +1,35 @@
 package net.honux.chess.entity.pieces;
 
 import net.honux.chess.attribute.Color;
+import net.honux.chess.attribute.PawnName;
 
-public class Pawn{
+public class Pawn {
 
     private final Color color;
+    private final PawnName representation;
 
-    public Pawn(){
+    public Pawn() {
         this.color = Color.WHITE;
+        this.representation = PawnName.WHITE_REPRESENTATION;
     }
 
     public Pawn(Color color) {
         this.color = color;
+        if (color == Color.BLACK) {
+            this.representation = PawnName.BLACK_REPRESENTATION;
+        } else
+            this.representation = PawnName.WHITE_REPRESENTATION;
+    }
+
+    public Pawn(Color color, PawnName representation) {
+        this(color);
     }
 
     public Color getColor() {
         return color;
+    }
+
+    public char getRepresentation() {
+        return representation.getRepresentation();
     }
 }

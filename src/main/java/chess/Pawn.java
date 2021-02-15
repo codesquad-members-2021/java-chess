c9ccf025261b4e9a0ca14013e8f770d98339a21c
95degree <95degree@naver.com>
2021-02-15 16:02:26 +0900

    상수(WHITE_COLOR,BLACK_COLOR) 추가
    "white" , "black" 문자열의 사용 중복을 피하기 위해 상수를 추가하였습니다.

--- src/main/java/chess/Pawn.java
+++ src/main/java/chess/Pawn.java
@@ -2,15 +2,16 @@ package chess;
 
 public class Pawn {
     private String color;
+    public static final String WHITE_COLOR = "white";
+    public static final String BLACK_COLOR = "black";
 
-    Pawn(){
-        this.color = "white";
+    public Pawn(){
+        this.color = WHITE_COLOR;
     }
 
     public Pawn(String color){
         this.color = color;
     }
-
 
     public String getColor(){
         return this.color;

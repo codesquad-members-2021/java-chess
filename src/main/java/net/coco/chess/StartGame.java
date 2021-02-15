3ac365608e37875e38587634f643800a3f620ea8
gisungchoi <chlrltjd159@naver.com>
2021-02-15 17:44:48 +0900

    commandSwitch 메소드 작성
    input값을 switch로 판단해서 상황에 맞는 함수를 호출한다.

--- src/main/java/net/coco/chess/StartGame.java
+++ src/main/java/net/coco/chess/StartGame.java
@@ -5,14 +5,29 @@ import java.util.Scanner;
 public class StartGame {
 
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        Board board=new Board();
-        while (true){
+        Scanner sc = new Scanner(System.in);
+        Board board = new Board();
+        while (true) {
             System.out.println("input > ");
             String input = sc.next();
 
             if (commandSwitch(board, input)) return;
         }
+    }
+
+    private static boolean commandSwitch(Board board, String input) {
+        switch (input) {
+            case "start":
+                board.initialize();
+                System.out.println(board.print());
+                break;
+            case "end":
+                System.out.println("게임을 종료");
+                return true;
+            default:
+                throw new IllegalArgumentException("알 수 없는 명령어");
+        }
+        return false;
     }
 
 }

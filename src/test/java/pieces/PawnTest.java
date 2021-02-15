e0495c85a6fbde17c80e3c47748336234d4e0ea3
janeljs <jisunlim818@gmail.com>
2021-02-15 21:30:09 +0900

    Edit test method description in PawnTest
    - DisplayName 내부의 테스트 메서드 설명 형식을 통일

--- src/test/java/pieces/PawnTest.java
+++ src/test/java/pieces/PawnTest.java
@@ -8,14 +8,14 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 class PawnTest {
     @Test
-    @DisplayName("기본 생성자에서 흰색 폰이 생성되었는지 검증")
+    @DisplayName("기본 생성자에서 흰색 폰이 생성되었는지 검증한다")
     void create_기본생성자() {
         Pawn pawn = new Pawn();
         assertThat(pawn.getColor()).isEqualTo(Color.WHITE);
     }
 
     @Test
-    @DisplayName("색상과 출력 양식에 알맞게 폰이 생성되었는지 검증")
+    @DisplayName("색상과 출력 양식에 알맞게 폰이 생성되었는지 검증한다")
     void create() {
         assertAll(() -> verifyPawn(Color.WHITE, "p"),
                 () -> verifyPawn(Color.BLACK, "P"));

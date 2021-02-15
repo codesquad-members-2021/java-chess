cb1f73ce016adbd2c14dbcdeb723ff0faf52ec64
Haina <bibi6666667@kakao.com>
2021-02-15 21:09:40 +0900

    [step2] 생성) BoardTest클래스와 테스트코드 생성
    [step2] 2.BoardTest추가 에 따라 클래스와 테스트코드를 생성했습니다.
    
    void putPawn()
    : 흰색 및 검은색 Pawn을 체스판에 추가하고,
    추가할 때마다 Pawn의 갯수와 추가된 Pawn을 확인하도록 했습니다.

--- src/test/java/bibi/BoardTest.java
+++ src/test/java/bibi/BoardTest.java
@@ -1,4 +1,24 @@
+package bibi;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
 class BoardTest {
-  
+    @Test
+    @DisplayName("체스판에 흰색, 검은색 폰을 추가하고 갯수와 목록을 확인한다.")
+    void putPawn(){
+        Board board = new Board();
+
+        Pawn white = new Pawn(Pawn.WHITE);
+        board.add(white);
+        assertEquals(1, board.size());
+        assertEquals(white, board.findPawn(0));
+
+        Pawn black = new Pawn(Pawn.BLACK);
+        board.add(black);
+        assertEquals(2, board.size());
+        assertEquals(black, board.findPawn(1));
+    }
 }

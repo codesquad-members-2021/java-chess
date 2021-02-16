90ee778d763613dd55c97859dd2a3cbc1cb2dd22
윤혜지 <yunhg95@gmail.com>
2021-02-16 15:56:07 +0900

    요구사항 PawnTest 추가 및 개선
    요구사항에 있는 PawnTest 추가하고 assertEquals로 작성되어있던 코드 assertThat으로 변경
    DisplayName어노테이션을 이용하여 해당 단위테스트 내용 표시

--- src/test/java/net/honux/chess/pieces/PawnTest.java
+++ src/test/java/net/honux/chess/pieces/PawnTest.java
@@ -8,22 +8,23 @@ import static org.junit.jupiter.api.Assertions.*;
 class PawnTest {
 
     @Test
-    @DisplayName("흰색과 검은색 폰이 생성되어야 한다")
-    void create() {
-        verifyPawn("white");
-        verifyPawn("black");
-
-    }
-
-    private void verifyPawn(final String color) {
-        Pawn pawn = new Pawn(color);
-        assertThat(pawn.getColor()).isEqualTo(color);
+    @DisplayName("기본 생성자는 흰색 말과 p로 표시한다")
+    void create_기본생성자() throws Exception {
+        Pawn pawn = new Pawn();
+        assertThat(pawn.getColor()).isEqualTo(Pawn.WHITE_COLOR);
+        assertThat(pawn.getRepresentation()).isEqualTo(Pawn.WHITE_REPRESENTATION);
     }
 
     @Test
-    @DisplayName("기본생성자일때 white 말을 생성하도록 한다")
-    void create_기본생성자() throws Exception {
-        Pawn pawn = new Pawn();
-        assertEquals("white", pawn.getColor());
+    @DisplayName("해당 색상과 대표문자를 잘 가져오는지 확인한다")
+    void create() {
+        verifyPawn(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION);
+        verifyPawn(Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION);
+    }
+
+    private void verifyPawn(final String color, final char representation) {
+        Pawn pawn = new Pawn(color, representation);
+        assertThat(pawn.getColor()).isEqualTo(color);
+        assertThat(pawn.getRepresentation()).isEqualTo(representation);
     }
 }

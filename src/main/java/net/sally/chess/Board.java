454c6d25c385325c86b3d135589bd429e246a827
Sally Oh <min27604@hotmail.com>
2021-02-16 14:22:38 +0900

    feat: Create initialize() method and getPawnsResult() method.
    - To make the test code work initailize() method and getWhitePawnsResult(), getBlackPawnsResult() was needed in Board class.
    - ArrayList whitePawns and blackPawns are more proper to differenciate colors instead of pawns(the one already exist).

--- src/main/java/net/sally/chess/Board.java
+++ src/main/java/net/sally/chess/Board.java
@@ -8,6 +8,37 @@ import java.util.List;
 public class Board {
     private List<Pawn> pawns = new ArrayList<>();
 
+    private List<Pawn> whitePawns = new ArrayList<>();
+    private List<Pawn> blackPawns = new ArrayList<>();
+
+    public void initialize() {
+
+        for (int i = 0; i < 8; i++) {
+            whitePawns.add(new Pawn());
+        }
+
+        for (int i = 0; i < 8; i++) {
+            blackPawns.add(new Pawn(Pawn.BLACK, Pawn.BLACK_REPRESENTATION));
+        }
+    }
+
+    public String getWhitePawnsResult() {
+        StringBuilder whitePawnsResult = new StringBuilder();
+        for (Pawn whitePawn : whitePawns) {
+            whitePawnsResult.append(whitePawn.getRepresentation());
+        }
+        return whitePawnsResult.toString();
+    }
+
+    public String getBlackPawnsResult() {
+        StringBuilder blackPawnsResult = new StringBuilder();
+        for (Pawn blackPawn : blackPawns) {
+            blackPawnsResult.append(blackPawn.getRepresentation());
+        }
+        return blackPawnsResult.toString();
+
+    }
+
     public void addPawn(Pawn pawn) {
         pawns.add(pawn);
     }

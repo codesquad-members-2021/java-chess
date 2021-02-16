690e29b47ebe21add1eba89d766c5840fc45593c
janeljs <jisunlim818@gmail.com>
2021-02-17 05:35:36 +0900

    Modify appendNewLine method in StringUtils
    - 매개변수로 받은 텍스트의 길이가 8보다 클 경우 줄바꿈하도록 수정

--- src/main/java/utils/StringUtils.java
+++ src/main/java/utils/StringUtils.java
@@ -7,6 +7,9 @@ public class StringUtils {
     }
 
     public static String appendNewLine(String text) {
+        if (text.length() > 8) {
+            return text.substring(0, 8) + NEWLINE + text.substring(8) + NEWLINE;
+        }
         return text + NEWLINE;
     }
 }

c03fb80e3ab4b55a72573bc59796099daa126c6d
pbg0205 <pbg0205@naver.com>
2021-02-16 11:23:46 +0900

    test : PawnTest 클래스 create 메서드 추가
    - verifyPawn 메서드를 추가햇습니다. 내부는 이전에 피드백을 받았던
    AssertAll을 사용했습니다. AssertAll을 사용할 경우, 테스트가 피해해도
    테스트를 멈추지 않고 모든 테스트를 확인할 수 있어 각각의 테스트 코드를
     확인할 때 용이하다고 판단했습니다.

--- src/test/java/cooper/chess/piece/PawnTest.java
+++ src/test/java/cooper/chess/piece/PawnTest.java
@@ -1,22 +1,36 @@
 package cooper.chess.piece;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class PawnTest {
 
     @Test
-    @DisplayName("흰색폰이 생성되어야 한다.")
+    public void create_defaultConstructor() {
+        Pawn pawn = new Pawn();
+        assertEquals(Color.WHITE.getColorName(), pawn.getColor().getColorName());
+        assertEquals(Color.WHITE.getRepresentation(), pawn.getRepresentation());
+    }
+
+    @Test
     public void create() {
-        Pawn pawn = new Pawn("white");
-        assertThat(pawn.getColor()).isEqualTo(Color.WHITE);
+        verifyPawn(Color.WHITE.getColorName(), Color.WHITE.getRepresentation());
+        verifyPawn(Color.BLACK.getColorName(), Color.BLACK.getRepresentation());
     }
 
     @Test()
     public void checkOtherColorName() {
-        assertThatIllegalArgumentException().isThrownBy(() -> new Pawn("green"));
+        assertThrows(IllegalArgumentException.class, () -> new Pawn("green"));
+    }
+
+    void verifyPawn(final String color, final char representation) {
+        Pawn pawn = new Pawn(color);
+
+        assertAll(
+                () -> assertEquals(color, pawn.getColor().getColorName()),
+                () -> assertEquals(representation, pawn.getRepresentation())
+        );
     }
 }

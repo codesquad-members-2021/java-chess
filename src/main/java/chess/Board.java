961cc30c09cc0b0d194f0ba7b0005913491265df
K <PizzaCola.K@gmail.com>
2021-02-16 18:02:14 +0900

    refactor: Rename piece list and methods in Board
    - 문법에 맞게 otherBlackPieceList, otherWhitePieceList로 수정
    - getPiecesResult(): 복수로 변경

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -10,9 +10,9 @@ import static utils.StringUtils.*;
 public class Board {
 
     private final List<Piece> blackPawnList = new ArrayList<>();
-    private final List<Piece> blackOtherPieceList = new ArrayList<>();
+    private final List<Piece> otherBlackPieceList = new ArrayList<>();
     private final List<Piece> whitePawnList = new ArrayList<>();
-    private final List<Piece> whiteOtherPieceList = new ArrayList<>();
+    private final List<Piece> otherWhitePieceList = new ArrayList<>();
 
     private final int FILES = 8;
 
@@ -29,28 +29,28 @@ public class Board {
     }
 
     private void addBlackPieces() {
-        blackOtherPieceList.add(Piece.createBlackRook());
-        blackOtherPieceList.add(Piece.createBlackKnight());
-        blackOtherPieceList.add(Piece.createBlackBishop());
-        blackOtherPieceList.add(Piece.createBlackQueen());
-        blackOtherPieceList.add(Piece.createBlackKing());
-        blackOtherPieceList.add(Piece.createBlackBishop());
-        blackOtherPieceList.add(Piece.createBlackKnight());
-        blackOtherPieceList.add(Piece.createBlackRook());
+        otherBlackPieceList.add(Piece.createBlackRook());
+        otherBlackPieceList.add(Piece.createBlackKnight());
+        otherBlackPieceList.add(Piece.createBlackBishop());
+        otherBlackPieceList.add(Piece.createBlackQueen());
+        otherBlackPieceList.add(Piece.createBlackKing());
+        otherBlackPieceList.add(Piece.createBlackBishop());
+        otherBlackPieceList.add(Piece.createBlackKnight());
+        otherBlackPieceList.add(Piece.createBlackRook());
     }
 
     private void addWhitePieces() {
-        whiteOtherPieceList.add(Piece.createWhiteRook());
-        whiteOtherPieceList.add(Piece.createWhiteKnight());
-        whiteOtherPieceList.add(Piece.createWhiteBishop());
-        whiteOtherPieceList.add(Piece.createWhiteQueen());
-        whiteOtherPieceList.add(Piece.createWhiteKing());
-        whiteOtherPieceList.add(Piece.createWhiteBishop());
-        whiteOtherPieceList.add(Piece.createWhiteKnight());
-        whiteOtherPieceList.add(Piece.createWhiteRook());
+        otherWhitePieceList.add(Piece.createWhiteRook());
+        otherWhitePieceList.add(Piece.createWhiteKnight());
+        otherWhitePieceList.add(Piece.createWhiteBishop());
+        otherWhitePieceList.add(Piece.createWhiteQueen());
+        otherWhitePieceList.add(Piece.createWhiteKing());
+        otherWhitePieceList.add(Piece.createWhiteBishop());
+        otherWhitePieceList.add(Piece.createWhiteKnight());
+        otherWhitePieceList.add(Piece.createWhiteRook());
     }
 
-    private String getPieceResult(List<Piece> pieceList) {
+    private String getPiecesResult(List<Piece> pieceList) {
         StringBuilder pieceResult = new StringBuilder();
         for (Piece piece : pieceList) {
             pieceResult.append(piece.getRepresentation());
@@ -68,17 +68,17 @@ public class Board {
     public String showBoard() {
         StringBuilder output = new StringBuilder();
         String blankRank = appendNewLine("........");
-        output.append(appendNewLine(getPieceResult(blackOtherPieceList)));
-        output.append(appendNewLine(getPieceResult(blackPawnList)));
+        output.append(appendNewLine(getPiecesResult(otherBlackPieceList)));
+        output.append(appendNewLine(getPiecesResult(blackPawnList)));
         output.append(blankRank).append(blankRank).append(blankRank).append(blankRank);
-        output.append(appendNewLine(getPieceResult(whitePawnList)));
-        output.append(appendNewLine(getPieceResult(whiteOtherPieceList)));
+        output.append(appendNewLine(getPiecesResult(whitePawnList)));
+        output.append(appendNewLine(getPiecesResult(otherWhitePieceList)));
         return output.toString();
     }
 
     public int pieceCount() {
         return blackPawnList.size() + whitePawnList.size()
-                + blackOtherPieceList.size() + whiteOtherPieceList.size();
+                + otherBlackPieceList.size() + otherWhitePieceList.size();
     }
 
 }

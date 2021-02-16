515418875c500eb7e5409f7ed5455b5e525adcda
95degree <95degree@naver.com>
2021-02-16 12:26:20 +0900

    PawnTest에 Pawn의 representation을 체크하는 테스트 추가
    Pawn의 출력을 위해 사용한 representation이 제대로 들어갔는지 체크하기 위한 새로운 메소드 및 원래 있던 메소드를 수정 하였습니다.

--- src/test/java/pieces/PawnTest.java
+++ src/test/java/pieces/PawnTest.java
@@ -12,17 +12,19 @@ class PawnTest {
     void create() {
         Pawn pawn = new Pawn();
         assertEquals(Pawn.WHITE_COLOR, pawn.getColor());
+        assertEquals(Pawn.WHITE_REPRESENTATION, pawn.getRepresentation());
     }
 
     @Test
     @DisplayName("Pawn 클래스 인스턴스의 String color를 비교한다.")
     void compare() {
-        verifyPawn(Pawn.WHITE_COLOR);
-        verifyPawn(Pawn.BLACK_COLOR);
+        verifyPawn(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION);
+        verifyPawn(Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION);
     }
 
-    void verifyPawn(final String color) {
+    void verifyPawn(final String color, final char representation) {
         Pawn pawn = new Pawn(color);
         assertEquals(pawn.getColor(), color);
+        assertEquals(representation, pawn.getRepresentation());
     }
 }

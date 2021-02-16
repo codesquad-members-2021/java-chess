31593e7415517391732de7565f4a155503a03895
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2021-02-16 13:46:26 +1300

    Fix test code to verify new constants before printing them out
    - 테스트 코드를 먼저 작성해서 쉽게 프로덕트 코드를 생성하도록 했습니다.
    
    - Representation 의 의미는 화면에 출력할 때 쓰이는 문자를 특정/대표한다는 의미에서 그렇게 변수를 정의하였습니다.
    
    - 일치하는 것을 확인하기 위해 AssertJ의 assertThat() 메소드를 사용하였습니다. 일관성을 주기위해 JUnit 의 Assert 는 사용하지 않았습니다.

--- src/test/java/net/honux/chess/pieces/PawnTest.java
+++ src/test/java/net/honux/chess/pieces/PawnTest.java
@@ -10,18 +10,20 @@ class PawnTest {
     @Test
     @DisplayName("흰색 폰이 생성되어야 한다")
     public void create() {
-        verifyPawn("white");
-        verifyPawn("black");
+        verifyPawn(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION);
+        verifyPawn(Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION);
     }
 
-    private void verifyPawn(final String color) {
+    private void verifyPawn(final String color, final char representation) {
         Pawn pawn = new Pawn(color);
         assertThat(pawn.getColor()).isEqualTo(color);
+        assertThat(pawn.getRepresentation()).isEqualTo(representation);
     }
 
     @Test
     public void create_기본생성자() throws Exception {
         Pawn pawn = new Pawn();
-        assertThat(pawn.getColor()).isEqualTo("white");
+        assertThat(pawn.getColor()).isEqualTo(pawn.WHITE_COLOR);
+        assertThat(pawn.getRepresentation()).isEqualTo(Pawn.WHITE_REPRESENTATION);
     }
 }

1cb2ab631b294b1cab85b12178afec5dd1011fc1
pbg0205 <pbg0205@naver.com>
2021-02-16 11:47:33 +0900

    feat : Board 클래스 자료구조 변경 밑 출력 기능 추가
    - 각각의 색상에 따른 리스트를 관리하기 위해 Color를 key를 가지는 HashMap
    으로 변경하였습니다. 각각의 List를 사용할 경우, 같은 기능의 메서드를
    각각 리스트에 맞게 메서드 작성이 필요해  중복 코드가 방생하였습니다.
    
    -  반면, Map을 사용할 경우, key 값을 통해서 리스트를 참조하기 때문에
    중복 코드를 작성하지 않고 메서드를 작성할 수 있는 장점있어  데이터 관리
    및  중복 코드 제거 면에서 적합하다고 판단했습니다.
    
    - print 메서드를  선언해 각 행의 결과를 출력할 수 있습니다. 리스트를 순회
    하면서 representation을 추가하기 때문에 StringBuilder를 사용하는 것이
    적합하다고 판단했고 각각의 결과는 요구사항에 맞게  String으로 반환하였
    습니다.
    
    - initialize 메서드를 작성해 초기 폰 상태를 초기화 하였습니다.

--- src/main/java/cooper/chess/Board.java
+++ src/main/java/cooper/chess/Board.java
@@ -1,24 +1,49 @@
 package cooper.chess;
+
+import cooper.chess.piece.Color;
 import cooper.chess.piece.Pawn;
 
 import java.util.*;
 
 public class Board {
-    private final List<Pawn> pawnList;
+    private static final int PAWN_MAX_SIZE = 8;
+    private static final int WHITE_INIT_INDEX = 1;
+    private static final int BLACK_INIT_INDEX = 6;
+
+    private final Map<Color, List<Pawn>> pawnListMap;
 
     public Board() {
-        pawnList = new ArrayList<>();
+        pawnListMap = new HashMap<>();
+        pawnListMap.put(Color.WHITE, new ArrayList<>());
+        pawnListMap.put(Color.BLACK, new ArrayList<>());
+    }
+
+    public void initialize() {
+        initPawnList(Color.WHITE);
+        initPawnList(Color.BLACK);
+    }
+
+    private void initPawnList(Color color) {
+        List<Pawn> pawnList = pawnListMap.get(color);
+
+        int pawnCount = 0;
+        while(pawnCount++ < PAWN_MAX_SIZE) {
+            pawnList.add(new Pawn(color.getColorName()));
+        }
     }
 
     public void add(Pawn pawn) {
+        List<Pawn> pawnList = pawnListMap.get(pawn.getColor());
         pawnList.add(pawn);
     }
 
-    public int size() {
-        return pawnList.size();
+    public int size(Color color) {
+        return pawnListMap.get(color).size();
     }
 
-    public Pawn findPawn(int index) {
+    public Pawn findPawn(int index, Color color) {
+        List<Pawn> pawnList = pawnListMap.get(color);
+
         if(pawnList.size() == 0) {
             throw new IllegalArgumentException("size가 0입니다.");
         }
@@ -28,5 +53,49 @@ public class Board {
         }
 
         return pawnList.get(index);
+    }
+
+    public String print () {
+        StringBuilder sb = new StringBuilder();
+        int index = 0;
+
+        while(index < PAWN_MAX_SIZE) {
+            sb.append(printEachRow(index++)).append("\n");
+        }
+
+        return sb.toString();
+    }
+
+    private String printEachRow(int index) {
+        StringBuilder sb = new StringBuilder();
+
+        if(index == WHITE_INIT_INDEX) {
+            return getPawnsResult(Color.WHITE);
+        }
+
+        if(index == BLACK_INIT_INDEX) {
+            return getPawnsResult(Color.BLACK);
+        }
+
+        return getBlank(sb);
+    }
+
+    public String getPawnsResult(Color color) {
+        StringBuilder sb = new StringBuilder();
+        List<Pawn> pawnList = pawnListMap.get(color);
+
+        for (int col = 0; col < PAWN_MAX_SIZE; col++) {
+            sb.append(pawnList.get(col).getRepresentation());
+        }
+
+        return sb.toString();
+    }
+
+    private String getBlank(StringBuilder sb) {
+        for (int i = 0; i < PAWN_MAX_SIZE; i++) {
+            sb.append('.');
+        }
+
+        return sb.toString();
     }
 }

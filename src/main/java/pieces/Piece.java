304c6f321476bf08129fede20fd1b17f911d6b40
janeljs <jisunlim818@gmail.com>
2021-02-17 01:05:33 +0900

    Rename Pawn class to Piece class
    - 요구사항 2에 따라 Pawn 클래스를 Piece 클래스로 리팩토링
    - enum Color와 enum Type을 Piece의 내부 클래스로 정의
    - Color와 Type을 매개변수로 받는 Piece 생성자 정의

---
+++ src/main/java/pieces/Piece.java
@@ -0,0 +1,54 @@
+package pieces;
+
+public class Piece {
+    private final Color color;
+    private final Type type;
+
+    public enum Color {
+        WHITE, BLACK;
+    }
+
+    public enum Type {
+        PAWN('p'),
+        KNIGHT('n'),
+        ROOK('r'),
+        BISHOP('b'),
+        QUEEN('q'),
+        KING('k');
+
+        private final char representation;
+
+        Type(final char representation) {
+            this.representation = representation;
+        }
+
+        public char getWhiteRepresentation() {
+            return this.representation;
+        }
+
+        public char getBlackRepresentation() {
+            return Character.toUpperCase(this.representation);
+        }
+    }
+
+    private Piece(Color color, Type type) {
+        this.color = color;
+        this.type = type;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public char getRepresentation() {
+        if (color == Color.WHITE) {
+            return type.getWhiteRepresentation();
+        }
+        return type.getBlackRepresentation();
+    }
+}
+

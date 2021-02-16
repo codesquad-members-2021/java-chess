6b21086ae6f98d78804b99d1e62ced77690f3f52
pbg0205 <pbg0205@naver.com>
2021-02-16 12:11:36 +0900

    feat : Main method 작성
    - 시작과 종료에 대한 표준 입력을 받기 위해 Scanner를 사용하였습니다.
    - 시작 명령을 받을 경우, board의 상태를 전체 출력할 수 있습니다.
    - 종료 명령을 확인할 경우, 프로그램이 정상 종료 됩니다.

--- src/main/java/cooper/chess/Main.java
+++ src/main/java/cooper/chess/Main.java
@@ -1,7 +1,35 @@
 package cooper.chess;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean isPlaying = true;
 
+        Board board = new Board();
+        board.initialize();
+
+        while(isPlaying) {
+            String command = scanner.nextLine();
+
+            if(isStartCommand(command)) {
+                System.out.println(board.print());
+            }
+
+            if(isEndCommand(command)) {
+                isPlaying = false;
+            }
+        }
+
+        scanner.close();
+    }
+
+    private static boolean isStartCommand(String command) {
+        return command.equalsIgnoreCase("start");
+    }
+
+    private static boolean isEndCommand(String command) {
+        return command.equalsIgnoreCase("end");
     }
 }

ef7543eb12442829762b0b8145a34b826de4d7b7
MJ <mj@MJui-MacBookPro.local>
2021-02-16 17:27:02 +0900

    Add: Board class에 initialize method 추가
    해당 메소드 호출에 따라 List pawns에 흰색과 검은색의 폰이 각각 8개씩 추가됨
    각각의 색의 폰의 표현문자에 대해 StringBuffer에 추가함

--- src/main/java/mj/chess/Board.java
+++ src/main/java/mj/chess/Board.java
@@ -1,12 +1,15 @@
 package mj.chess;
 
+import mj.chess.pieces.Color;
 import mj.chess.pieces.Pawn;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Board {
-    private List<Pawn> pawns = new ArrayList<>();
+    private final List<Pawn> pawns = new ArrayList<>();
+    private final StringBuffer rowOfBlackPawns = new StringBuffer();
+    private final StringBuffer rowOfWhitePawns = new StringBuffer();
 
     public Board() {
     }
@@ -21,5 +24,24 @@ public class Board {
 
     public Pawn findPawn(int index) {
         return pawns.get(index);
+    }
+
+    public void initialize() {
+        while (pawns.size() < 8) {
+            pawns.add(new Pawn(Color.BLACK, Pawn.BLACK_REPRESENTATION));
+            rowOfBlackPawns.append(Pawn.BLACK_REPRESENTATION);
+        }
+        while (pawns.size() < 16) {
+            pawns.add(new Pawn(Color.WHITE, Pawn.WHITE_REPRESENTATION));
+            rowOfWhitePawns.append(Pawn.WHITE_REPRESENTATION);
+        }
+    }
+
+    public String getBlackPawnsLocation() {
+        return rowOfBlackPawns.toString();
+    }
+
+    public String getWhitePawnsLocation() {
+        return rowOfWhitePawns.toString();
     }
 }

f1b084160ef5b599324e481768a58937ed573efd
윤혜지 <yunhg95@gmail.com>
2021-02-16 18:01:10 +0900

    Board Class initialize() getWhitePawnsResult() getBlackPawnsResult() 메소드 추가
    initialize() 메소드에 흰색 검은색 Pawn을 생성해 저장하는 List 추가
    initialize() 메소드에 흰색 검은색 리스트에 해당 Pawn 객체를 넣어주는 코드 추가
    StringBuilder에 각각 흰색 검정색에 대한 representation 문자열 내용 더하는 코드 추가
    getWhitePawnsResult() 흰색 Pawn 열의 값이 return 될 수 있도록 적용
    getBlackPawnsResult() 검정색 Pawn 열의 값이 return 될 수 있도록 적용

--- src/main/java/net/honux/chess/Board.java
+++ src/main/java/net/honux/chess/Board.java
@@ -8,6 +8,9 @@ public class Board {
 
     ArrayList<Pawn> pawns = new ArrayList<Pawn>();
 
+    StringBuilder whiteResult = new StringBuilder();
+    StringBuilder blackResult = new StringBuilder();
+
     public void add(Pawn pawn) {
         pawns.add(pawn);
     }
@@ -18,5 +21,30 @@ public class Board {
 
     public Pawn findPawn(int index) {
         return pawns.get(index);
+    }
+
+    public void initialize() {
+        ArrayList<Pawn> whitePawns = new ArrayList<Pawn>();
+        ArrayList<Pawn> blackPawns = new ArrayList<Pawn>();
+
+        for (int i = 0; i < 8; i++) {
+            whitePawns.add(new Pawn(Pawn.WHITE_COLOR,Pawn.WHITE_REPRESENTATION));
+            blackPawns.add(new Pawn(Pawn.BLACK_COLOR,Pawn.BLACK_REPRESENTATION));
+        }
+
+        for (int i = 0; i < whitePawns.size(); i++) {
+            whiteResult.append(whitePawns.get(i).getRepresentation());
+            blackResult.append(blackPawns.get(i).getRepresentation());
+        }
+
+    }
+
+    public String getWhitePawnsResult() {
+        return whiteResult.toString();
+
+    }
+
+    public String getBlackPawnsResult() {
+        return blackResult.toString();
     }
 }

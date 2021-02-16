03acb4df3644c278ccd7a8fca4b1644ae60801cd
janeljs <jisunlim818@gmail.com>
2021-02-17 02:19:40 +0900

    Add static factory methods to create pieces
    - 기물(pawn, knight, rook, bishop, queen, king)을 생성하기 위한 정적 팩터리 메서드 추가
    - 각 메서드는 private Piece(Color color, Type type) 생성자를 이용하여 생성한 인스턴스를 반환함

--- src/main/java/pieces/Piece.java
+++ src/main/java/pieces/Piece.java
@@ -50,5 +50,54 @@ public class Piece {
         }
         return type.getBlackRepresentation();
     }
+
+    public static Piece createWhitePawn() {
+        return new Piece(Color.WHITE, Type.PAWN);
+    }
+
+    public static Piece createBlackPawn() {
+        return new Piece(Color.BLACK, Type.PAWN);
+    }
+
+    public static Piece createWhiteKnight() {
+        return new Piece(Color.WHITE, Type.KNIGHT);
+    }
+
+    public static Piece createBlackKnight() {
+        return new Piece(Color.BLACK, Type.KNIGHT);
+    }
+
+    public static Piece createWhiteRook() {
+        return new Piece(Color.WHITE, Type.ROOK);
+    }
+
+    public static Piece createBlackRook() {
+        return new Piece(Color.BLACK, Type.ROOK);
+    }
+
+    public static Piece createWhiteBishop() {
+        return new Piece(Color.WHITE, Type.BISHOP);
+    }
+
+    public static Piece createBlackBishop() {
+        return new Piece(Color.BLACK, Type.BISHOP);
+    }
+
+    public static Piece createWhiteQueen() {
+        return new Piece(Color.WHITE, Type.QUEEN);
+    }
+
+    public static Piece createBlackQueen() {
+        return new Piece(Color.BLACK, Type.QUEEN);
+    }
+
+    public static Piece createWhiteKing() {
+        return new Piece(Color.WHITE, Type.KING);
+    }
+
+    public static Piece createBlackKing() {
+        return new Piece(Color.BLACK, Type.KING);
+    }
 }
+
 

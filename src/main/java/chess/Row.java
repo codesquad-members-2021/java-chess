cb4e6f17614ab2aded312ea69de7f3e82611eadc
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2021-02-16 23:56:02 +0900

    Character 타입을 받는 List를 생성하여, board 초기화를 진행 할 수 있는 메서드 추가

--- src/main/java/chess/Row.java
+++ src/main/java/chess/Row.java
@@ -1,2 +1,48 @@
-package chess;public class Row {
+package chess;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Row {
+    private final int BOARDSIZE = 8;
+    private List<Character> pices = new ArrayList<>(BOARDSIZE);
+
+    public Character getPices(int index) {
+        return pices.get(index);
+    }
+
+    public void addPices(char representation) {
+        pices.add(representation);
+    }
+
+    public int getPicesSize() {
+        return pices.size();
+    }
+
+    public Row initWhitePawns(Board board) {
+        Row row = new Row();
+        for (int i = 0; i < BOARDSIZE; i++) {
+            row.addPices(board.getWhitePawn(i).getRepresentation());
+        }
+        return row;
+    }
+
+    public Row initBlackPawns(Board board) {
+        Row row = new Row();
+        for (int i = 0; i < BOARDSIZE; i++) {
+            row.addPices(board.getBlackPawn(i).getRepresentation());
+        }
+        return row;
+    }
+
+
+    public Row initBlank() {
+        Row row = new Row();
+        for (int i = 0; i < BOARDSIZE; i++) {
+            row.addPices('.');
+        }
+        return row;
+    }
+
+
 }

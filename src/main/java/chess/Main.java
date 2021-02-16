c8448f17d98bc5b286613d7b8909e12f0db48695
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2021-02-16 17:39:03 +0900

    Main 클래스 생성

--- src/main/java/chess/Main.java
+++ src/main/java/chess/Main.java
@@ -1,2 +1,11 @@
-package chess;public class Main {
+package chess;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
 }

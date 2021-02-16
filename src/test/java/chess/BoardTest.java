2288e9000f0a5ddd5cc896320d22c0c5b47f5ebb
MyungWanPark <mywanpark@gmail.com>
2021-02-16 17:03:04 +0900

    feat: Create Pawn instance

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -2,12 +2,14 @@ package chess;
 
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import pieces.Pawn;
+import pieces.Color;
 
 public class BoardTest {
 
     @Test
     public void create() throws Exception {
         Board board = new Board();
-
+        Pawn white = new Pawn(Color.WHITE);
     }
 }

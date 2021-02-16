7ff8e66772a3f98833eaf4d036dbf39e023da143
Sally Oh <min27604@hotmail.com>
2021-02-16 21:11:35 +0900

    feat: Create Game class
    - Game class has main method that runs whole game.
    - The main method takes user's command with Scanner.

--- src/main/java/net/sally/chess/Game.java
+++ src/main/java/net/sally/chess/Game.java
@@ -1,2 +1,31 @@
-package net.sally.chess;public class Game {
+package net.sally.chess;
+
+import java.util.Scanner;
+
+public class Game {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        boolean running = true;
+        while(running) {
+            System.out.println("명령을 입력하세요.");
+            System.out.print("> ");
+
+            String command = sc.nextLine();
+
+            switch (command) {
+                case "start":
+                    Board board = new Board();
+                    board.initialize();
+                    board.print();
+                    break;
+
+                case "end":
+                    running = false;
+                    break;
+            }
+        }
+
+        sc.close();
+    }
 }

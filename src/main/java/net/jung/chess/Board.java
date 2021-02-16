baa26ab5b669e04ddbc94fb76544f97626a0ef0f
rla36 <bhmarsd@naver.com>
2021-02-16 17:11:55 +0900

    initialize메서드 추가해 보드에 흰/검 폰 8개씩 추가
    initialize()메서드로 보드의 whitePawnList와 blackPawnList에 각각 흰색,
    검은색 폰을 8개씩 올리도록 했습니다.
    getWhitePawnsResult(), getBlackPawnsResult로는 흰/검폰 ArrayList의 폰 개수만큼
    폰을 대표하는 알파벳을 나열해 String으로 리턴하도록 했습니다.

--- src/main/java/net/jung/chess/Board.java
+++ src/main/java/net/jung/chess/Board.java
@@ -7,25 +7,57 @@ import java.util.*;
 public class Board {
     private List<Pawn> blackPawnList = new ArrayList();
     private List<Pawn> whitePawnList = new ArrayList();
+    private final int MAX_PAWNS_SIZE = 8;
 
     public void add(Pawn pawn) {
-        if(pawn.getColor().equals(Color.WHITE)){
+        if (pawn.getColor()== (Color.WHITE)) {
             whitePawnList.add(pawn);
-        }else{
-            blackPawnList.add(pawn);
+            return;
+        }
+        blackPawnList.add(pawn);
+
+    }
+
+    public int size() {
+        return whitePawnList.size() + blackPawnList.size();
+    }
+
+    public Pawn findWhitePawn(int index) {
+        return whitePawnList.get(index);
+    }
+
+    public Pawn findBlackPawn(int index) {
+        return blackPawnList.get(index);
+    }
+
+    public void initialize() {
+        for (int i = 0; i < MAX_PAWNS_SIZE; i++) {
+            Pawn whitePawn = new Pawn(Color.WHITE);
+            Pawn blackPawn = new Pawn(Color.BLACK);
+            add(whitePawn);
+            add(blackPawn);
         }
     }
 
-    public int size(){
-        return whitePawnList.size()+blackPawnList.size();
+    public String getWhitePawnsResult() {
+        StringBuilder pawnReps = new StringBuilder();
+        int whiteSize = whitePawnList.size();
+
+        for (int i = 0; i < whiteSize; i++) {
+            pawnReps.append(whitePawnList.get(i).getRepresentation());
+        }
+        return pawnReps.toString();
     }
 
-    public Pawn findWhitePawn(int index){
-            return whitePawnList.get(index);
-        }
+    public String getBlackPawnsResult() {
+        StringBuilder pawnReps = new StringBuilder();
+        int blackSize = blackPawnList.size();
 
-    public Pawn findBlackPawn(int index){
-            return blackPawnList.get(index);
+        for (int i = 0; i < blackSize; i++) {
+            pawnReps.append(blackPawnList.get(i).getRepresentation());
         }
+        return pawnReps.toString();
+    }
+
 
 }

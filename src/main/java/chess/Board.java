6e799bf39061bed42e4a42f9bd29a7220e915eec
MyungWanPark <mywanpark@gmail.com>
2021-02-16 17:35:30 +0900

    feat: Add access modifier in Board.java

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class Board {
 
-    List<Pawn> pawnList;
+    private List<Pawn> pawnList;
 
     public Board(){
         pawnList = new ArrayList<>();
--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -8,12 +8,18 @@ import pieces.Color;
 public class BoardTest {
 
     @Test
-    void create() throws Exception {
+    @DisplayName("보드를 생성한 후 폰 테스트 한다.")
+    void createBoardAndTest() throws Exception {
         Board board = new Board();
 
         Pawn white = new Pawn(Color.WHITE);
         board.add(white);
         assertEquals(1, board.size());
         assertEquals(white, board.findPawn(0));
+
+        Pawn black = new Pawn(Color.BLACK);
+        board.add(black);
+        assertEquals(2, board.size());
+        assertEquals(black, board.findPawn(1));
     }
 }

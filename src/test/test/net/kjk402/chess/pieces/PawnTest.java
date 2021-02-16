e3dd2d91b4b3281f308d4c739275bded79e1843c
JoonKi-Kim <joonki402@gmail.com>
2021-02-16 18:02:20 +0900

    PawnTest class 수정
    Pawn 클래스의 color와 representation을 받아서 테스트하도록 수정.
    DisplayName 추가.

--- src/test/test/net/kjk402/chess/pieces/PawnTest.java
+++ src/test/test/net/kjk402/chess/pieces/PawnTest.java
@@ -10,19 +10,23 @@ import org.junit.jupiter.api.Test;
 
 class PawnTest {
     @Test
+    @DisplayName("기본 생성자 실행 시 흰색폰이 생성된다.")
     void create_기본생성자() throws Exception {
         Pawn pawn = new Pawn();
         assertEquals(Pawn.WHITE, pawn.getColor());
+        assertEquals(Pawn.WHITE_REPRESENTATION, pawn.getRepresentation());
     }
 
     @Test
     void createPawn() {
-        verifyPawn(Pawn.WHITE);
-        verifyPawn(Pawn.BLACK);
+        verifyPawn(Pawn.WHITE, Pawn.WHITE_REPRESENTATION);
+        verifyPawn(Pawn.BLACK, Pawn.BLACK_REPRESENTATION);
     }
 
-    private void verifyPawn(final String color) {
-        Pawn pawn = new Pawn(color);
-        assertThat(pawn.getColor()).isEqualTo(color);
+    private void verifyPawn(final String color, final char representation) {
+        Pawn pawn = new Pawn(color, representation);
+        assertEquals(color, pawn.getColor());
+        assertEquals(representation, pawn.getRepresentation());
     }
+
 }

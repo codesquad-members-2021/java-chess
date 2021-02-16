5a46ebe3cc5fbb2c3b8f9713579dea11bcc9959f
rla36 <bhmarsd@naver.com>
2021-02-16 11:05:11 +0900

    assertAll 하나만 쓰이도록 test  람다식으로 수정

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -23,16 +23,20 @@ public class BoardTest {
     @DisplayName("보드를 생성하고 흰색 검은색 폰을 추가한 후 size, findPawn 메서드 검증한다.")
     void createBoardAddPawns() throws Exception{
 
-        board.add(whitePawn);
-        assertAll("흰색 폰을 보드에 올린 후 size, findPawn메서드 검증한다.",
-                () -> assertThat(board.size()).isEqualTo(1),
-                () -> assertThat(board.findPawn(0)).isEqualTo(whitePawn));
-
-
-        board.add(blackPawn);
-        assertAll("검은색 폰을 보드에 올린 후 size, findPawn메서드 검증한다.",
-                () -> assertThat(board.size()).isEqualTo(2),
-                () -> assertThat(board.findPawn(1)).isEqualTo(blackPawn));
+        assertAll(
+                () -> {
+                    board.add(whitePawn);
+                    assertAll("흰색 폰을 보드에 올린 후 size, findPawn메서드 검증한다.",
+                            () -> assertThat(board.size()).isEqualTo(1),
+                            () -> assertThat(board.findPawn(0)).isEqualTo(whitePawn));
+                },
+                () -> {
+                    board.add(blackPawn);
+                    assertAll("검은색 폰을 보드에 올린 후 size, findPawn메서드 검증한다.",
+                            () -> assertThat(board.size()).isEqualTo(2),
+                            () -> assertThat(board.findPawn(1)).isEqualTo(blackPawn));
+                }
+        );
 
     }
 

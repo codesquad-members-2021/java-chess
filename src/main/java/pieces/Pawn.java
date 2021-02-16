346d9d3cdd756aa8d91aff2431f4d1407273da04
rla36 <bhmarsd@naver.com>
2021-02-16 11:07:04 +0900

    기본생성자 위치 수정
    기본생성자가 매개변수가 있는 생성자의 위로 오도록 위치 수정했습니다.

--- src/main/java/pieces/Pawn.java
+++ src/main/java/pieces/Pawn.java
@@ -5,12 +5,12 @@ import chess.Color;
 public class Pawn {
     private Color color;
 
-    public Pawn(Color color) {
-        this.color = color;
-    }
-
     public Pawn(){
         this(Color.WHITE);
+    }
+
+    public Pawn(Color color) {
+        this.color = color;
     }
 
     public Color getColor(){

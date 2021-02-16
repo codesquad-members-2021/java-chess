1f3dc911fc311301785510e7d7fe991a87371f81
MJ <mj@MJui-MacBookPro.local>
2021-02-16 16:12:53 +0900

    Test: 폰 클래스의 표현문자 정상 출력 테스트 추가
    폰 클래스 생성자의 두번째 매개변수로 표현문자 전달하고 해당 문자가 get method로 정상 출력되는지 확인

--- src/test/java/mj/chess/pieces/PawnTest.java
+++ src/test/java/mj/chess/pieces/PawnTest.java
@@ -19,7 +19,18 @@ class PawnTest {
         for (Color color : Color.values()) verifyColorOfPawn(new Pawn(color), color);
     }
 
+    @Test
+    @DisplayName("흰색과 검은색 폰의 표현문자 정상출력 확인")
+    void printRepresentationOfPawns() {
+        verifyRepresentationOfPawn(new Pawn(Color.WHITE, Pawn.WHITE_REPRESENTATION), Pawn.WHITE_REPRESENTATION);
+        verifyRepresentationOfPawn(new Pawn(Color.BLACK, Pawn.BLACK_REPRESENTATION), Pawn.BLACK_REPRESENTATION);
+    }
+
     void verifyColorOfPawn(Pawn pawn, Color color) {
         assertThat(pawn.getColor()).isEqualTo(color);
+    }
+
+    void verifyRepresentationOfPawn(Pawn pawn, final char representation) {
+        assertThat(pawn.getRepresentation()).isEqualTo(representation);
     }
 }

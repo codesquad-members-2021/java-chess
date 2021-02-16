41367182996c971f1ca3ae0721656b5ff77e26f2
JoonKi-Kim <joonki402@gmail.com>
2021-02-16 15:08:03 +0900

    main()메소드에 시작과 종료기능 추가
    while문 안에서 Scanner를 사용하여 입력 값을 받고 start일 때 콘솔에 출력, end면 break로 종료.

--- src/main/java/net/kjk402/chess/chess/Main.java
+++ src/main/java/net/kjk402/chess/chess/Main.java
@@ -1,10 +1,23 @@
 package net.kjk402.chess.chess;
 
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Board board = new Board();
-        board.initialize();
-        board.print();
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("> ");
+            String command = sc.nextLine();
+            if (command.equals("start")) {
+                Board board = new Board();
+                board.initialize();
+                board.print();
+            }
+            if (command.equals("end")) {
+                break;
+            }
+        }
     }
 }

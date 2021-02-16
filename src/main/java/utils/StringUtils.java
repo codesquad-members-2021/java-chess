0428d74b7412b5d72bfbdaddf70f3bb3aa7d28c0
janeljs <jisunlim818@gmail.com>
2021-02-16 23:52:25 +0900

    Create StringUtils class
    - 문자열에 관련된 메소드를 모아놓는 StringUtils class 생성
    - appendNewLine 메서드 호출 시 매개변수로 입력한 text에 개행문자를 더해 반환

---
+++ src/main/java/utils/StringUtils.java
@@ -0,0 +1,16 @@
+package utils;
+
+public class StringUtils {
+    public static final String NEWLINE = System.getProperty("line.separator");
+
+    private StringUtils() {
+    }
+
+    public static String appendNewLine(String text) {
+        return text + NEWLINE;
+    }
+}
+
+
+
+

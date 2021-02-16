37699bef924be6b9f27e505c9f18ac3a64a1bd06
K <PizzaCola.K@gmail.com>
2021-02-16 11:51:04 +0900

    feat: Piece class
    - Piece has Color and Type.
    - Color is White and Black. So, use Color Enum.
    - Type is Pawn, Knight, Bishop, Rook, Queen, King. So, use Type Enum.
    - Piece has getColor(), getRepresentation() method.
    - getColor() returns a Color of Piece.
    - getRepresentation returns a letter of Piece.
    - Black Piece is Upper Case letter and White Piece is Lower Case letter.
    - Piece has static factory methods such as create{Color}{Type}.
    
    - Piece는 Color와 Type을 가집니다.
    - Color는 흰색과 검은색을 가지므로, Color enum을 사용합니다.
    - Type은 Pawn, Knight, Bishop, Rook, Queen, King 입니다. 그래서 Type Enum
    을 이용하였습니다.
    - Piece는 getColor(), getRepresentation() 메소드를 가집니다.
    - getColor() 는 Piece의 Color를 반환합니다.
    - getRepresentation()은 Piece의 문자를 반환합니다.
    - 검은색 Piece는 대문자, 흰색 Piece는 소문자입니다.
    - Piece는 각 색상과 타입의 인스턴스를 반환하는 static 팩토리 메소드를
    가집니다.

--- src/main/java/pieces/Piece.java
+++ src/main/java/pieces/Piece.java
@@ -4,12 +4,10 @@ public class Piece {
 
     private final Color color;
     private final Type type;
-    private final char representation;
 
     private Piece(Color color, Type type) {
         this.color = color;
         this.type = type;
-        this.representation = 'p';
     }
 
     public static Piece createWhitePawn() {
@@ -17,51 +15,51 @@ public class Piece {
     }
 
     public static Piece createWhiteKnight() {
-        return new Piece(Color.WHITE, Type.PAWN);
+        return new Piece(Color.WHITE, Type.KNIGHT);
     }
 
     public static Piece createWhiteRook() {
-        return new Piece(Color.WHITE, Type.PAWN);
+        return new Piece(Color.WHITE, Type.ROOK);
     }
 
     public static Piece createWhiteBishop() {
-        return new Piece(Color.WHITE, Type.PAWN);
+        return new Piece(Color.WHITE, Type.BISHOP);
     }
 
     public static Piece createWhiteQueen() {
-        return new Piece(Color.WHITE, Type.PAWN);
+        return new Piece(Color.WHITE, Type.QUEEN);
     }
 
     public static Piece createWhiteKing() {
-        return new Piece(Color.WHITE, Type.PAWN);
+        return new Piece(Color.WHITE, Type.KING);
     }
 
     public static Piece createBlackPawn() {
-        return new Piece(Color.WHITE, Type.PAWN);
+        return new Piece(Color.BLACK, Type.PAWN);
     }
 
     public static Piece createBlackKnight() {
-        return new Piece(Color.WHITE, Type.PAWN);
+        return new Piece(Color.BLACK, Type.KNIGHT);
     }
 
     public static Piece createBlackRook() {
-        return new Piece(Color.WHITE, Type.PAWN);
+        return new Piece(Color.BLACK, Type.ROOK);
     }
 
     public static Piece createBlackBishop() {
-        return new Piece(Color.WHITE, Type.PAWN);
+        return new Piece(Color.BLACK, Type.BISHOP);
     }
 
     public static Piece createBlackQueen() {
-        return new Piece(Color.WHITE, Type.PAWN);
+        return new Piece(Color.BLACK, Type.QUEEN);
     }
 
     public static Piece createBlackKing() {
-        return new Piece(Color.WHITE, Type.PAWN);
+        return new Piece(Color.BLACK, Type.KING);
     }
 
     public char getRepresentation() {
-        return representation;
+        return type.getRepresentation(color);
     }
 
     public Color getColor() {

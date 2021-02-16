669a434df50b4d4b7f435af5aa69476517f1fce1
JoonKi-Kim <joonki402@gmail.com>
2021-02-16 12:12:20 +0900

    Create Main Class
    main() 메소드를 생성하여 Board에서의 initialize와  print가
    정상적으로 동작하는 지 확인.

---
+++ src/main/java/net/kjk402/chess/chess/Main.java
@@ -0,0 +1,10 @@
+package net.kjk402.chess.chess;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Board board = new Board();
+        board.initialize();
+        board.print();
+    }
+}

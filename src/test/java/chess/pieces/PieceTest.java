37616fc0ec3977924fca559390f4b9957b2a7f7f
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-02-17 14:24:01 +0900

    Add rep local variable and fix arguments of verifyPiece() method in create_piece() method

--- src/test/java/chess/pieces/PieceTest.java
+++ src/test/java/chess/pieces/PieceTest.java
@@ -8,18 +8,29 @@ public class PieceTest {
 
     @Test
     public void create_piece() {
-        verifyPiece(Piece.createWhitePawn(), Color.WHITE, 'p');
-        verifyPiece(Piece.createBlackPawn(), Color.BLACK, 'P');
-        verifyPiece(Piece.createWhiteKnight(), Color.WHITE, 'n');
-        verifyPiece(Piece.createBlackKnight(), Color.BLACK, 'N');
-        verifyPiece(Piece.createWhiteBishop(), Color.WHITE, 'b');
-        verifyPiece(Piece.createBlackBishop(), Color.BLACK, 'B');
-        verifyPiece(Piece.createWhiteRook(), Color.WHITE, 'r');
-        verifyPiece(Piece.createBlackRook(), Color.BLACK, 'R');
-        verifyPiece(Piece.createWhiteQueen(), Color.WHITE, 'q');
-        verifyPiece(Piece.createBlackQueen(), Color.BLACK, 'Q');
-        verifyPiece(Piece.createWhiteKing(), Color.WHITE, 'k');
-        verifyPiece(Piece.createBlackKing(), Color.BLACK, 'K');
+        char rep = PieceName.PAWN.getTempRepresentation();
+        verifyPiece(Piece.createWhitePawn(), Color.WHITE, rep);
+        verifyPiece(Piece.createBlackPawn(), Color.BLACK, Character.toUpperCase(rep));
+
+        rep = PieceName.KNIGHT.getTempRepresentation();
+        verifyPiece(Piece.createWhiteKnight(), Color.WHITE, rep);
+        verifyPiece(Piece.createBlackKnight(), Color.BLACK, Character.toUpperCase(rep));
+
+        rep = PieceName.BISHOP.getTempRepresentation();
+        verifyPiece(Piece.createWhiteBishop(), Color.WHITE, rep);
+        verifyPiece(Piece.createBlackBishop(), Color.BLACK, Character.toUpperCase(rep));
+
+        rep = PieceName.ROOK.getTempRepresentation();
+        verifyPiece(Piece.createWhiteRook(), Color.WHITE, rep);
+        verifyPiece(Piece.createBlackRook(), Color.BLACK, Character.toUpperCase(rep));
+
+        rep = PieceName.QUEEN.getTempRepresentation();
+        verifyPiece(Piece.createWhiteQueen(), Color.WHITE, rep);
+        verifyPiece(Piece.createBlackQueen(), Color.BLACK, Character.toUpperCase(rep));
+        
+        rep = PieceName.KING.getTempRepresentation();
+        verifyPiece(Piece.createWhiteKing(), Color.WHITE, rep);
+        verifyPiece(Piece.createBlackKing(), Color.BLACK, Character.toUpperCase(rep));
     }
 
     void verifyPiece(final Piece piece, final Color color, final char representation) {

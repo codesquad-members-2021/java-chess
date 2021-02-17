b6ef291491e814056cd98fd5c5f1b3e314343150
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-02-17 14:48:56 +0900

    Add getRepresentationBy in Color
    색깔에 따라 대소문자 구분하여 반환할 수 있도록 추가
    https://github.com/codesquad-members-2021/java-chess/pull/72#discussion_r576234029

--- src/main/java/kr/codesquad/freddie/chess/piece/Color.java
+++ src/main/java/kr/codesquad/freddie/chess/piece/Color.java
@@ -1,5 +1,17 @@
 package kr.codesquad.freddie.chess.piece;
 
 public enum Color {
-    WHITE, BLACK;
+    WHITE {
+        @Override
+        String getRepresentationBy(String representation) {
+            return representation.toLowerCase();
+        }
+    }, BLACK {
+        @Override
+        String getRepresentationBy(String representation) {
+            return representation.toUpperCase();
+        }
+    };
+
+    abstract String getRepresentationBy(String representation);
 }

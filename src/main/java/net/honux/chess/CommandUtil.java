ecebd8f533c161e5c06eb484483caf27bca05b3e
Jihye Woo <woojihye2339@gmail.com>
2021-02-17 14:13:00 +0900

    feat : CommandUtil 클래스 구현
      > 사용자 명령 처리 책임
       - printMenu() : 입력 가능한 명령어와 설명 출력
       - readCommand() : 입력받은 명령어를 해석

--- src/main/java/net/honux/chess/CommandUtil.java
+++ src/main/java/net/honux/chess/CommandUtil.java
@@ -3,10 +3,22 @@ package net.honux.chess;
 public class CommandUtil {
 
     public static void printMenu() {
-
+        System.out.println("< Command Menu >");
+        System.out.println(" 'start' : game start ");
+        System.out.println(" 'end'   : game end ");
+        System.out.println("( not case-sensitive 😊)");
+        System.out.print("> ");
     }
 
     public static boolean readCommand(String command) {
-        return true;
+        switch (command.toLowerCase()) {
+            case "start":
+                return true;
+            case "end":
+                return false;
+            default:
+                System.out.println("plz enter the command again, check the command menu!");
+                return true;
+        }
     }
 }

5797636d9fcb56e22e8d66fb785d929baad2cd3d
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-02-17 16:29:40 +0900

    Make PieceName class as a file to make it public seperate from Piece.java file

--- src/main/java/chess/pieces/Piece.java
+++ src/main/java/chess/pieces/Piece.java
@@ -75,23 +75,4 @@ public class Piece {
     }
 }
 
-enum PieceName {
-    PAWN('p'),
-    KNIGHT('n'),
-    ROOK('r'),
-    BISHOP('b'),
-    QUEEN('q'),
-    KING('k');
-
-    private final char tempRepresentation;
-
-    PieceName(char representation) {
-        this.tempRepresentation = representation;
-    }
-
-    public char getTempRepresentation() {
-        return tempRepresentation;
-    }
-}
-
 

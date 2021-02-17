c930d9216315954c8f4437eaed5876c48ddbac0d
rla36 <bhmarsd@naver.com>
2021-02-17 18:03:35 +0900

    try-with resources 추가
    Scanner은 AutoClosable을 구현한 클래스이므로 try- with resources를
    시도해보았다.

--- src/main/java/net/jung/chess/Main.java
+++ src/main/java/net/jung/chess/Main.java
@@ -4,31 +4,29 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        boolean play = true;
         String start = "start";
         String end = "end";
         Board board = new Board();
 
-        while(true){
-            System.out.print(">>");
-            String input = sc.nextLine();
-            if(end.equalsIgnoreCase(input)){
-                break;
-            }
+        try( Scanner sc = new Scanner(System.in)) {
+            while (true) {
+                System.out.print(">>");
+                String input = sc.nextLine();
+                if (end.equalsIgnoreCase(input)) {
+                    break;
+                }
 
-            if(start.equalsIgnoreCase(input)){
-                System.out.println("체스게임을 시작합니다.\n");
-                board.reset();
-                board.initialize();
-                System.out.print(board.print());
-                System.out.println();
-                continue;
+                if (start.equalsIgnoreCase(input)) {
+                    System.out.println("체스게임을 시작합니다.\n");
+                    board.reset();
+                    board.initialize();
+                    System.out.print(board.print());
+                    System.out.println();
+                    continue;
+                }
+                System.out.println("start/end 중에서 정확히 입력해주세요.");
             }
-
-            System.out.println("start/end 중에서 정확히 입력해주세요.");
+            System.out.println("체스게임이 종료되었습니다. 다음에 만나요~");
         }
-        System.out.println("체스게임이 종료되었습니다. 다음에 만나요~");
-        sc.close();
     }
 }

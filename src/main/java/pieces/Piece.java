9d282e07330f5187a29de3f6c357acb549dbf9b7
hiro <kiryuinh32@gmail.com>
2021-02-17 12:13:11 +0900

    팩토리 메서드 생성 각 Piece별로 색과, 이름을 입력받아서 각 Piece객체를 생성하는 메서드를 구현하고 테스트를 통해 확인.

--- src/main/java/pieces/Piece.java
+++ src/main/java/pieces/Piece.java
@@ -32,6 +32,50 @@ public class Piece {
         return new Piece(Color.WHITE, PieceRepresentation.p);
     }
 
+    public static Piece createBlackPawn(){
+        return new Piece(Color.BLACK, PieceRepresentation.P);
+    }
+
+    public static Piece createWhiteKnight(){
+        return new Piece(Color.WHITE, PieceRepresentation.n);
+    }
+
+    public static Piece createBlackKnight(){
+        return new Piece(Color.BLACK, PieceRepresentation.N);
+    }
+
+    public static Piece createWhiteRook(){
+        return new Piece(Color.WHITE, PieceRepresentation.r);
+    }
+
+    public static Piece createBlackRook(){
+        return new Piece(Color.BLACK, PieceRepresentation.R);
+    }
+
+    public static Piece createWhiteBishop(){
+        return new Piece(Color.WHITE, PieceRepresentation.b);
+    }
+
+    public static Piece createBlackBishop(){
+        return new Piece(Color.BLACK, PieceRepresentation.B);
+    }
+
+    public static Piece createWhiteQueen(){
+        return new Piece(Color.WHITE, PieceRepresentation.q);
+    }
+
+    public static Piece createBlackQueen(){
+        return new Piece(Color.BLACK, PieceRepresentation.Q);
+    }
+
+    public static Piece createWhiteKing(){
+        return new Piece(Color.WHITE, PieceRepresentation.k);
+    }
+
+    public static Piece createBlackKing(){
+        return new Piece(Color.BLACK, PieceRepresentation.K);
+    }
+
 
 
 }
--- src/test/java/piece/PieceTest.java
+++ src/test/java/piece/PieceTest.java
@@ -11,6 +11,23 @@ class PieceTest {
     @DisplayName("create Piece test")
     public void create() {
         verifyPawn(Piece.createWhitePawn(), Piece.createWhitePawn().getColor(), Piece.createWhitePawn().getRepresentation());
+        verifyPawn(Piece.createBlackPawn(), Piece.createBlackPawn().getColor(), Piece.createBlackPawn().getRepresentation());
+
+        verifyPawn(Piece.createWhiteKnight(), Piece.createWhiteKnight().getColor(), Piece.createWhiteKnight().getRepresentation());
+        verifyPawn(Piece.createBlackKnight(), Piece.createBlackKnight().getColor(), Piece.createBlackKnight().getRepresentation());
+
+        verifyPawn(Piece.createWhiteRook(), Piece.createWhiteRook().getColor(), Piece.createWhiteRook().getRepresentation());
+        verifyPawn(Piece.createBlackRook(), Piece.createBlackRook().getColor(), Piece.createBlackRook().getRepresentation());
+
+        verifyPawn(Piece.createWhiteBishop(), Piece.createWhiteBishop().getColor(), Piece.createWhiteBishop().getRepresentation());
+        verifyPawn(Piece.createBlackBishop(), Piece.createBlackBishop().getColor(), Piece.createBlackBishop().getRepresentation());
+
+        verifyPawn(Piece.createWhiteQueen(), Piece.createWhiteQueen().getColor(), Piece.createWhiteQueen().getRepresentation());
+        verifyPawn(Piece.createBlackQueen(), Piece.createBlackQueen().getColor(), Piece.createBlackQueen().getRepresentation());
+
+        verifyPawn(Piece.createWhiteKing(), Piece.createWhiteKing().getColor(), Piece.createWhiteKing().getRepresentation());
+        verifyPawn(Piece.createBlackKing(), Piece.createBlackKing().getColor(), Piece.createBlackKing().getRepresentation());
+
     }
 
     public void verifyPawn(Piece piece, Piece.Color color, Piece.PieceRepresentation representation){
@@ -18,6 +35,4 @@ class PieceTest {
         assertEquals(representation, piece.getRepresentation());
     }
 
-
-    
 }

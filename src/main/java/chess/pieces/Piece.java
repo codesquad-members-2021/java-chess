f8c8e8c02247f9b7b5299fc4d384bbce705f09da
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-02-17 11:40:14 +0900

    Fix Piece class Add enum PieceName Add createWhitePawn() and createBlackPawn() method

--- src/main/java/chess/pieces/Piece.java
+++ src/main/java/chess/pieces/Piece.java
@@ -2,9 +2,9 @@ package chess.pieces;
 
 public class Piece {
     private final Color color;
-    private final String name;
+    private final PieceName name;
 
-    private Piece(Color color, String name){
+    private Piece(Color color, PieceName name) {
         this.color = color;
         this.name = name;
     }
@@ -12,6 +12,18 @@ public class Piece {
     public Color getColor() {
         return this.color;
     }
+
+    public static Piece createWhitePawn() {
+        return new Piece(Color.WHITE, PieceName.PAWN);
+    }
+
+    public static Piece createBlackPawn() {
+        return new Piece(Color.BLACK, PieceName.PAWN);
+    }
+}
+
+enum PieceName {
+    PAWN, KNIGHT, ROOK, BISHOP, QUEEN, KING
 }
 
 

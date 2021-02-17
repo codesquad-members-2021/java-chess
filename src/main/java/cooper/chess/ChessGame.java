68a9de46495787bc0846630287e6ad5dbbe8363b
pbg0205 <pbg0205@naver.com>
2021-02-17 11:24:14 +0900

    feat : chessGame 클래스 생성
    - 체스게임이 작동하는 로직을 구분해서 관리하기 위해 ChessGame class를
    추가하여 구현하였습니다.
    
    - chessGame에서 메서드 분리를 체스판 세팅과 체스판 실행을 구분하기
    위해 init 메서드와 play 메서드를 분리하였습니다.

---
+++ src/main/java/cooper/chess/ChessGame.java
@@ -0,0 +1,41 @@
+package cooper.chess;
+
+import java.util.Scanner;
+
+public class ChessGame {
+    private static final String START_COMMAND = "start";
+    private static final String END_COMMAND = "end";
+
+    private final Scanner scanner = new Scanner(System.in);
+    private final Board board = new Board();
+
+    public void init() {
+        board.initialize();
+    }
+
+    public void play() {
+        boolean isPlaying = true;
+
+        while(isPlaying) {
+            String command = scanner.nextLine();
+
+            if(isStartCommand(command)) {
+                System.out.println(board.getBoardStatus());
+            }
+
+            if(isEndCommand(command)) {
+                isPlaying = false;
+            }
+        }
+
+        scanner.close();
+    }
+
+    private boolean isStartCommand(String command) {
+        return START_COMMAND.equalsIgnoreCase(command);
+    }
+
+    private boolean isEndCommand(String command) {
+        return END_COMMAND.equalsIgnoreCase(command);
+    }
+}
--- src/main/java/cooper/chess/Main.java
+++ src/main/java/cooper/chess/Main.java
@@ -1,35 +1,10 @@
 package cooper.chess;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        boolean isPlaying = true;
+        ChessGame chessGame = new ChessGame();
 
-        Board board = new Board();
-        board.initialize();
-
-        while(isPlaying) {
-            String command = scanner.nextLine();
-
-            if(isStartCommand(command)) {
-                System.out.println(board.getBoardStatus());
-            }
-
-            if(isEndCommand(command)) {
-                isPlaying = false;
-            }
-        }
-
-        scanner.close();
-    }
-
-    private static boolean isStartCommand(String command) {
-        return command.equalsIgnoreCase("start");
-    }
-
-    private static boolean isEndCommand(String command) {
-        return command.equalsIgnoreCase("end");
+        chessGame.init();
+        chessGame.play();
     }
 }

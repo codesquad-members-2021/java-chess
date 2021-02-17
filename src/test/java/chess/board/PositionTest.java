9273707e9cc31ae0679d203909378c4ecffdec8a
고정완 (Can) <jw.gho@ecubelabs.com>
2021-02-17 16:56:14 +0900

    [test] Add PositionTest, BoardViewTest
    보드를 출력하는 BoardView 와
    각 Piece 의 위치정보를 나타낼 Position의 테스트를 추가했다.
    Board 는 Position 과 Piece 의 관계를 나타내는 자료구조로 구현해 나가야한다.

---
+++ src/test/java/chess/board/PositionTest.java
@@ -0,0 +1,23 @@
+package chess.board;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class PositionTest {
+
+    @Test
+    @DisplayName("columnId 와 rowId 가 같다면, 같은 Position 이어야 한다.")
+    void testEquals() {
+        assertThat(new Position('c', 4))
+                .isEqualTo(new Position("c4"));
+    }
+
+    @Test
+    @DisplayName("columnId 와 rowId 가 같다면, hashCode 값도 같아야 한다.")
+    void testHashCode() {
+        assertThat(new Position('f', 2).hashCode())
+                .isEqualTo(new Position("f2").hashCode());
+    }
+}
---
+++ src/test/java/chess/view/BoardViewTest.java
@@ -0,0 +1,51 @@
+package chess.view;
+
+import chess.board.Board;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static chess.utils.StringUtils.NEWLINE;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class BoardViewTest {
+
+    private Board board;
+    private BoardView boardView;
+
+    @BeforeEach
+    void setup() {
+        board = new Board();
+        board.initialize();
+        boardView = new BoardView(board);
+    }
+
+    @Test
+    void getPositionRepresentation() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("a8 b8 c8 d8 e8 f8 g8 h8 " + NEWLINE);
+        sb.append("a7 b7 c7 d7 e7 f7 g7 h7 " + NEWLINE);
+        sb.append("a6 b6 c6 d6 e6 f6 g6 h6 " + NEWLINE);
+        sb.append("a5 b5 c5 d5 e5 f5 g5 h5 " + NEWLINE);
+        sb.append("a4 b4 c4 d4 e4 f4 g4 h4 " + NEWLINE);
+        sb.append("a3 b3 c3 d3 e3 f3 g3 h3 " + NEWLINE);
+        sb.append("a2 b2 c2 d2 e2 f2 g2 h2 " + NEWLINE);
+        sb.append("a1 b1 c1 d1 e1 f1 g1 h1 " + NEWLINE);
+        assertThat(boardView.getPositionRepresentation())
+                .isEqualTo(sb.toString());
+    }
+
+    @Test
+    void getPieceRepresentation() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(". . . . . . . . " + NEWLINE);
+        sb.append("P P P P P P P P " + NEWLINE);
+        sb.append(". . . . . . . . " + NEWLINE);
+        sb.append(". . . . . . . . " + NEWLINE);
+        sb.append(". . . . . . . . " + NEWLINE);
+        sb.append(". . . . . . . . " + NEWLINE);
+        sb.append("p p p p p p p p " + NEWLINE);
+        sb.append(". . . . . . . . " + NEWLINE);
+        assertThat(boardView.getPieceRepresentation())
+                .isEqualTo(sb.toString());
+    }
+}

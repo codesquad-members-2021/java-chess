0c012c1b9092f82f08ac2b77acd9b5ac40039ca5
jeong-inho <derosatam76@gmail.com>
2021-02-17 15:06:38 +0900

    Add another pieces test in PieceTest class 폰 이외의 다른 말들이 잘 생성되는지 확인을 위한 테스트 케이스 추가

--- src/test/java/net/eno/pieces/PieceTest.java
+++ src/test/java/net/eno/pieces/PieceTest.java
@@ -6,16 +6,26 @@ import static org.assertj.core.api.Assertions.*;
 public class PieceTest {
 
     @Test
-    @DisplayName("흰색과 검은색 폰이 생성되어야 한다")
+    @DisplayName("모든 피스의 흰색말과 검은색말이 생성되어야 한다.")
     public void create() {
-        verifyPawn(Color.WHITE, Representation.PAWN);
-        verifyPawn(Color.BLACK, Representation.PAWN);
+        verifyPiece(Color.WHITE, Representation.PAWN);
+        verifyPiece(Color.BLACK, Representation.PAWN);
+        verifyPiece(Color.WHITE, Representation.KNIGHT);
+        verifyPiece(Color.BLACK, Representation.KNIGHT);
+        verifyPiece(Color.WHITE, Representation.ROOK);
+        verifyPiece(Color.BLACK, Representation.ROOK);
+        verifyPiece(Color.WHITE, Representation.BISHOP);
+        verifyPiece(Color.BLACK, Representation.BISHOP);
+        verifyPiece(Color.WHITE, Representation.QUEEN);
+        verifyPiece(Color.BLACK, Representation.QUEEN);
+        verifyPiece(Color.WHITE, Representation.KING);
+        verifyPiece(Color.BLACK, Representation.KING);
     }
 
-    public void verifyPawn(Color color, Representation representation) {
-        Piece pawn = new Piece(color, representation);
-        assertThat(pawn.getColor()).isEqualTo(color.toString());
-        assertThat(pawn.getRepresentation()).isEqualTo(representation.get(color));
+    public void verifyPiece(Color color, Representation representation) {
+        Piece piece = Piece.createPiece(color, representation);
+        assertThat(piece.getColor()).isEqualTo(color.toString());
+        assertThat(piece.getRepresentation()).isEqualTo(representation.get(color));
     }
 
 }

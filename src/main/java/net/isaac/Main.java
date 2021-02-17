08e6e7dc8d689fcc98705a126ba2f5d5053f8909
정이삭 <isaac56@naver.com>
2021-02-17 11:55:40 +0900

    Replace "start","end" with constants in Main class
    코드 리뷰 반영
    Main 클래스에서 게임 시작 종료 명령을 판단하기 위해 사용하던 "start", "end" 문자열을 상수로 변경하였습니다.

--- src/main/java/net/isaac/Main.java
+++ src/main/java/net/isaac/Main.java
@@ -7,11 +7,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+    public static final String START = "start";
+    public static final String END = "end";
     public static void main(String[] args){
         String command;
         try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
-            while(!(command = reader.readLine()).equals("end")){
-                if(command.equals("start"))
+            while(!(command = reader.readLine()).equals(END)){
+                if(command.equals(START))
                     startChess();
             }
         }

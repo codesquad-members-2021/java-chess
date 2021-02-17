8363366e9d7b10d9da1cb55e08d2091e684939a1
MyungWanPark <mywanpark@gmail.com>
2021-02-17 16:26:47 +0900

    feat: Change the name of instance variable
    Change the name of instance variable pawn1 to whitePawn

--- src/test/java/pieces/PawnTest.java
+++ src/test/java/pieces/PawnTest.java
@@ -8,10 +8,10 @@ public class PawnTest {
     @Test
     @DisplayName("흰색과 검정색 폰이 생성되어야 한다")
      void createPawn() {
-        Pawn pawn = new Pawn(Pawn.WHITE);
-        assertThat(pawn.getColor()).isEqualTo(Pawn.WHITE);
-        Pawn pawn2 = new Pawn(Pawn.BLACK);
-        assertThat(pawn2.getColor()).isEqualTo(Pawn.BLACK);
+        Pawn whitePawn = new Pawn(Pawn.WHITE);
+        assertThat(whitePawn.getColor()).isEqualTo(Pawn.WHITE);
+        Pawn blackPawn= new Pawn(Pawn.BLACK);
+        assertThat(blackPawn.getColor()).isEqualTo(Pawn.BLACK);
     }
 
     @Test

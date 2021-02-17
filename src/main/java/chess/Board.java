e90d558374345ff92611629e0821a279a8d1f0cf
Haina <bibi6666667@kakao.com>
2021-02-17 11:07:12 +0900

    [step2] 수정) pawns 선언을 List<Pawn> pawns로 변경
    ArrayList를 선언해 사용하더라도 List pawns = new ArrayList 와 같은
    방식으로 선언하는 것이 유연성 면에서 더 좋다고 한다.

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -1,11 +1,12 @@
 package chess;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import pieces.Pawn;
 
 public class Board {
-    private ArrayList<Pawn> pawns = new ArrayList<>();
+    private List<Pawn> pawns = new ArrayList<>();
 
     public void add(Pawn pawn) {
         pawns.add(pawn);

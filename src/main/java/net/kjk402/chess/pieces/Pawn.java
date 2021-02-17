c9dc4cd53ecea7ded860704d43ef4aade59d0911
JoonKi-Kim <joonki402@gmail.com>
2021-02-17 12:23:56 +0900

    Pawn class 수정
    클래스변수와 인스턴스변수를 합쳐 멤버변수라 불리는데 멤버변수 생성
    시에 클래스변수가 인스턴스 변수 위에 위치해야한다.

--- src/main/java/net/kjk402/chess/pieces/Pawn.java
+++ src/main/java/net/kjk402/chess/pieces/Pawn.java
@@ -1,12 +1,12 @@
 package net.kjk402.chess.pieces;
 
 public class Pawn {
-    private final String color;
-    private final char representation;
     public static final String WHITE = "white";
     public static final String BLACK = "black";
     public static final char WHITE_REPRESENTATION = 'p';
     public static final char BLACK_REPRESENTATION = 'P';
+    private final String color;
+    private final char representation;
 
     public Pawn() {
         this.color = WHITE;

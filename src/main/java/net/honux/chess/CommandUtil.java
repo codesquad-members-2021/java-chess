2d2530617e53cff74a9edbe1fc5da75cb4f609d6
Jihye Woo <woojihye2339@gmail.com>
2021-02-17 14:09:32 +0900

    feat : 메인 클래스 Game 구현
      > 시작과 종료 기능
        - Scanner로 사용자 입력 값 받는다.
        - 사용자 입력 명령을 수행한다. (시작 혹은 종료)
        - 종료 명령을 받을 때, 프로그램이 종료된다.

---
+++ src/main/java/net/honux/chess/CommandUtil.java
@@ -0,0 +1,12 @@
+package net.honux.chess;
+
+public class CommandUtil {
+
+    public static void printMenu() {
+
+    }
+
+    public static boolean readCommand(String command) {
+        return true;
+    }
+}
---
+++ src/main/java/net/honux/chess/Game.java
@@ -0,0 +1,16 @@
+package net.honux.chess;
+
+import java.util.Scanner;
+
+public class Game {
+    public static void main(String[] args) {
+        boolean ongoing = true;
+        Scanner sc = new Scanner(System.in);
+        do {
+            CommandUtil.printMenu();
+            String command = sc.nextLine();
+            ongoing = CommandUtil.readCommand(command);
+        } while (ongoing);
+        sc.close();
+    }
+}

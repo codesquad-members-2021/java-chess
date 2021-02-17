5ed02f33350090b5952d1d781aaefbe27c594be9
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-02-17 17:38:58 +0900

    Remove useless field

--- src/main/java/chess/utils/StringUtils.java
+++ src/main/java/chess/utils/StringUtils.java
@@ -1,14 +1,13 @@
 package chess.utils;
 
 public class StringUtils {
-    private static String string = "";
 
-    private StringUtils() { }
+    private StringUtils() {
+    }
 
     public static final String NEWLINE = System.getProperty("line.separator");
 
-    public static String appendNewLine(String line){
-        string = string + line + NEWLINE;
-        return string;
+    public static String appendNewLine(String line) {
+        return line + NEWLINE;
     }
 }

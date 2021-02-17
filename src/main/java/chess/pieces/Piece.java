d4719b79aeecade6cb43329d1aaef9b8d15f0879
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-02-17 11:32:46 +0900

    Fix Piece class Remove constructor with no parameter Add name parameter in constructor

--- src/main/java/chess/pieces/Piece.java
+++ src/main/java/chess/pieces/Piece.java
@@ -1,16 +1,12 @@
 package chess.pieces;
 
 public class Piece {
+    private final Color color;
+    private final String name;
 
-    private String name;
-    private Color color;
-
-    public Piece() {
-        this(Color.WHITE);
-    }
-
-    public Piece(Color color){
+    private Piece(Color color, String name){
         this.color = color;
+        this.name = name;
     }
 
     public Color getColor() {

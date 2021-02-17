ed9d02b5184fda6778d020d0d8f46acb17c4379b
jeong-inho <derosatam76@gmail.com>
2021-02-17 17:18:05 +0900

    Add piece by piece test in PieceTest class 폰 이외의 다른 말들까지 테스트하기 위해 PieceTest class의 create 메소드에 테스트 추가

--- src/test/java/net/eno/pieces/PieceTest.java
+++ src/test/java/net/eno/pieces/PieceTest.java
@@ -8,18 +8,10 @@ public class PieceTest {
     @Test
     @DisplayName("모든 피스의 흰색말과 검은색말이 생성되어야 한다.")
     public void create() {
-        verifyPiece(Color.WHITE, Representation.PAWN);
-        verifyPiece(Color.BLACK, Representation.PAWN);
-        verifyPiece(Color.WHITE, Representation.KNIGHT);
-        verifyPiece(Color.BLACK, Representation.KNIGHT);
-        verifyPiece(Color.WHITE, Representation.ROOK);
-        verifyPiece(Color.BLACK, Representation.ROOK);
-        verifyPiece(Color.WHITE, Representation.BISHOP);
-        verifyPiece(Color.BLACK, Representation.BISHOP);
-        verifyPiece(Color.WHITE, Representation.QUEEN);
-        verifyPiece(Color.BLACK, Representation.QUEEN);
-        verifyPiece(Color.WHITE, Representation.KING);
-        verifyPiece(Color.BLACK, Representation.KING);
+        for (Representation representation : Representation.values()) {
+            verifyPiece(Color.WHITE, representation);
+            verifyPiece(Color.BLACK, representation);
+        }
     }
 
     public void verifyPiece(Color color, Representation representation) {

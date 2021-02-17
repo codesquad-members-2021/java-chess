b2e3d012ef5dfd08b9d7c687fd8d963ede225115
jeong-inho <derosatam76@gmail.com>
2021-02-17 17:22:40 +0900

    Create a test to test the overall health of the board 폰 이외의 다른 말들을 보드에 초기화 하기 위해 기존에 불필요한 테스트들은 삭제하고 초기화 된 보드의 개수와 배치와 출력을 테스트하는 메소드 생성

--- src/test/java/net/eno/chess/BoardTest.java
+++ src/test/java/net/eno/chess/BoardTest.java
@@ -3,9 +3,7 @@ package net.eno.chess;
 import org.junit.jupiter.api.*;
 import static org.assertj.core.api.Assertions.*;
 
-import net.eno.pieces.Piece;
-import net.eno.pieces.Color;
-import net.eno.pieces.Representation;
+import static net.eno.utils.StringUtils.appendNewLine;
 
 public class BoardTest {
 
@@ -14,37 +12,32 @@ public class BoardTest {
     @BeforeEach
     public void init() {
         board = new Board();
-    }
-
-    @Test
-    @DisplayName("폰을 추가 시 전체 사이즈와 해당 인덱스의 폰이 일치해야 한다.")
-    public void create() {
-        verifyPawnSize(Color.WHITE, Representation.PAWN, 1, 0);
-        verifyPawnSize(Color.WHITE, Representation.PAWN, 2, 1);
-        verifyPawnSize(Color.BLACK, Representation.PAWN, 1, 0);
-        verifyPawnSize(Color.BLACK, Representation.PAWN, 2, 1);
-    }
-
-    public void verifyPawnSize(Color color, Representation representation, int size, int index) {
-        Piece piece = Piece.createPiece(color, representation);
-        board.addPiece(color, piece);
-        assertThat(board.pieceSize(color)).isEqualTo(size);
-        assertThat(board.findPiece(color, index)).isEqualTo(piece);
-    }
-
-    @Test
-    @DisplayName("보드를 초기화한 결과 흰색 폰과 검은색 폰의 열이 각각 일치해 한다.")
-    public void initialize() {
         board.initialize();
-        assertThat(board.getPiecesResult(Color.WHITE)).isEqualTo("pppppppp");
-        assertThat(board.getPiecesResult(Color.BLACK)).isEqualTo("PPPPPPPP");
+    }
+
+    @Test
+    @DisplayName("초기화 된 보드의 말의 개수가 32개여야 한다.")
+    public void size() {
+        assertThat(board.pieceCount()).isEqualTo(32);
+    }
+
+    @Test
+    @DisplayName("초기화 된 보드의 말의 배치가 일치해야 한다.")
+    public void initialize() {
+        String blankRank = appendNewLine("........");
+        assertThat(board.showBoard()).isEqualTo(
+                appendNewLine("RNBQKBNR") +
+                appendNewLine("PPPPPPPP") +
+                blankRank + blankRank + blankRank + blankRank +
+                appendNewLine("pppppppp") +
+                appendNewLine("rnbqkbnr")
+        );
     }
 
     @Test
     @DisplayName("초기화 된 보드가 출력되어야 한다.")
     public void print() {
-        board.initialize();
-        board.print();
+        System.out.println(board.showBoard());
     }
 
 }

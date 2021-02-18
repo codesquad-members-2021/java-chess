662c4ddb444b684c977f7c61a0a7cea813b1c1df
tmdgusya <tmdgusya@naver.com>
2021-02-18 17:50:42 +0900

    test : BoardTest.java
    - Board 부분 코드 업데이트로 인한 수정

--- src/test/java/net/honux/chess/core/BoardTest.java
+++ src/test/java/net/honux/chess/core/BoardTest.java
@@ -1,7 +1,6 @@
 package net.honux.chess.core;
 
-import net.honux.chess.attribute.Color;
-import net.honux.chess.entity.pieces.Pawn;
+import net.honux.chess.entity.pieces.Piece;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -22,52 +21,55 @@ class BoardTest {
     @DisplayName("Pawn 이 보드에 정상적으로 등록되어야 한다.")
     void createPawnOnTheBoard() {
         board.initialize();
-        Pawn white = new Pawn(Color.WHITE);
-        Pawn black = new Pawn(Color.BLACK);
-        board.add(white);
-        board.add(black);
+        Piece whitePawn = Piece.createWhitePawn();
+        Piece blackPawn = Piece.createBlackPawn();
+        board.add(whitePawn);
+        board.add(blackPawn);
         Assertions.assertAll(
-                () -> assertThat(board.whitePawnSize()).isEqualTo(9),
-                () -> assertThat(board.findWhitePawn(8).get()).isEqualTo(white),
-                () -> assertThat(board.blackPawnSize()).isEqualTo(9),
-                () -> assertThat(board.findBlackPawn(8).get()).isEqualTo(black)
+                () -> assertThat(board.whitePawnSize()).isEqualTo(Board.BOARD_SIZE*2+1),
+                () -> assertThat(board.findWhitePawn(Board.BOARD_SIZE*2).get()).isEqualTo(whitePawn),
+                () -> assertThat(board.blackPawnSize()).isEqualTo(Board.BOARD_SIZE*2+1),
+                () -> assertThat(board.findBlackPawn(Board.BOARD_SIZE*2).get()).isEqualTo(blackPawn)
         );
     }
 
     @Test
-    @DisplayName("흰색폰 8개 블랙폰 8개가 정상적으로 initialize() 를 통해서 생성되는지 확인한다.")
+    @DisplayName("흰색 Pices 16개 블랙 Pices 16개가 정상적으로 initialize() 를 통해서 생성되는지 확인한다.")
     void checkPawnCount() {
         board.initialize();
         Assertions.assertAll(
-                () -> assertThat(board.whitePawnSize()).isEqualTo(8),
-                () -> assertThat(board.blackPawnSize()).isEqualTo(8));
+                () -> assertThat(board.whitePawnSize()).isEqualTo(Board.BOARD_SIZE*2),
+                () -> assertThat(board.blackPawnSize()).isEqualTo(Board.BOARD_SIZE*2));
     }
 
     @Test
-    @DisplayName("보드에 올려진 폰들의 Representation 이 정상적으로 출력되는지 확인한다.")
+    @DisplayName("보드에 올려진 조각들의 Representation 이 정상적으로 출력되는지 확인한다.")
     void checkPawnRepresentationOnTheBoard() {
         board.initialize();
         Assertions.assertAll(
+                () -> assertThat(board.getBlackPiecesExceptPawns()).isEqualTo("RNBQKBNR"),
+                ()->assertThat(board.getWhitePiecesExceptPawns()).isEqualTo("rnbqkbnr"),
                 () -> assertThat(board.getWhitePawnsResult()).isEqualTo("pppppppp"),
                 () -> assertThat(board.getBlackPawnsResult()).isEqualTo("PPPPPPPP")
         );
     }
+
 
     @Test
     @DisplayName("보드에 올려진 출력 메세지가 정상적으로 리턴되는지 확인한다.")
     void checkBoardPrintMessage() {
         board.initialize();
         String expectedMessage =
-                "********\n" +
+                "RNBQKBNR\n" +
                 "PPPPPPPP\n" +
                 "********\n" +
                 "********\n" +
                 "********\n" +
                 "********\n" +
                 "pppppppp\n" +
-                "********\n";
+                "rnbqkbnr\n";
         assertThat(board.getBoardStatusToString()).isEqualTo(expectedMessage);
         // TestCode 에서 출력 해달라는 요구사항 을 만족시키기 위해 작성
-        System.out.println(board.getBoardStatusToString());
+        board.print();
     }
 }

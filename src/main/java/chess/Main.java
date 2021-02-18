f8d0707e11accc9c5595856aa393c24b1af02481
95degree <95degree@naver.com>
2021-02-18 18:18:21 +0900

    init() 리팩토링
    가독성을 높이기 위해 init()를 리팩토링 했습니다.

--- src/main/java/chess/Main.java
+++ src/main/java/chess/Main.java
@@ -7,28 +7,27 @@ public class Main {
     private static final String END_COMMAND = "end";
 
     public static void main(String[] args) {
-
+        Main main = new Main();
+        main.init();
     }
 
     public void init(){
         try (Scanner scanner = new Scanner(System.in)) {
-            String notice = "명령어를 입력하세요 : start(게임 스타트), end(게임 끝내기)";
-            System.out.println(notice);
-            String command = scanner.nextLine();
-
-            while (!command.equals(END_COMMAND)) {
-                if (command.equals(START_COMMAND))
-                    start();
-                if (!command.equals(START_COMMAND)) {
-                    System.out.println("올바른 명령어를 입력하세요");
+            while(true) {
+                System.out.println("명령어를 입력하세요 : start(게임 스타트), end(게임 끝내기)");
+                String command = scanner.nextLine();
+                if(command.equals(END_COMMAND)) {
+                    System.out.println("게임을 종료합니다.");
+                    break;
                 }
-                System.out.println(notice);
-                command = scanner.nextLine();
+                if(command.equals(START_COMMAND)) {
+                    start();
+                    continue;
+                }
+
+                System.out.println("명령어를 다시 입력하세요");
             }
-
-            System.out.println("게임을 종료합니다.");
         }
-
     }
 
     private void start() {

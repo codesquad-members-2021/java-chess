f4d12c56122687e0eac6d57a0b7c9102166b258e
JoonKi-Kim <joonki402@gmail.com>
2021-02-18 17:20:23 +0900

    Add createWhitePawn method in Pices class
    Piece class 에서 pawn 의 색상별로 생성하는 팩토리 메소드 추가.

--- src/main/java/net/kjk402/chess/pieces/Piece.java
+++ src/main/java/net/kjk402/chess/pieces/Piece.java
@@ -3,14 +3,14 @@ package net.kjk402.chess.pieces;
 public class Piece {
     public static final String WHITE = "white";
     public static final String BLACK = "black";
-    public static final char WHITE_REPRESENTATION = 'p';
-    public static final char BLACK_REPRESENTATION = 'P';
+    public static final char WHITE_PAWN_REPRESENTATION = 'p';
+    public static final char BLACK_PAWN_REPRESENTATION = 'P';
     private final String color;
     private final char representation;
 
     public Piece() {
         this.color = WHITE;
-        this.representation = WHITE_REPRESENTATION;
+        this.representation = WHITE_PAWN_REPRESENTATION;
     }
 
     public Piece(String color, char representation) {
@@ -24,6 +24,14 @@ public class Piece {
 
     public char getRepresentation() {
         return representation;
+    }
+
+    public static Piece createWhitePawn() {
+        return new Piece(WHITE, WHITE_PAWN_REPRESENTATION);
+    }
+
+    public static Piece createBlackPawn() {
+        return new Piece(BLACK, BLACK_PAWN_REPRESENTATION);
     }
 
 }

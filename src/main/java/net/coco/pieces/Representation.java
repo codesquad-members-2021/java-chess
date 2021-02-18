91db0b612c4bf0dee036b1319cda6943e72a00a1
gisungchoi <chlrltjd159@naver.com>
2021-02-18 16:29:17 +0900

    Representation enum에 다른 기물들 추가
    다른 기물들을 추가했다. 구분문자는 서로 다르다.

--- src/main/java/net/coco/pieces/Representation.java
+++ src/main/java/net/coco/pieces/Representation.java
@@ -2,7 +2,17 @@ package net.coco.pieces;
 
 public enum Representation {
     WHITE_PAWN('p'),
-    BLACK_PAWN('P');
+    BLACK_PAWN('P'),
+    WHITE_KNIGHT('n'),
+    BLACK_KNIGHT('N'),
+    WHITE_ROOK('r'),
+    BLACK_ROOK('R'),
+    WHITE_BISHOP('b'),
+    BLACK_BISHOP('B'),
+    WHITE_QUEEN('q'),
+    BLACK_QUEEN('Q'),
+    WHITE_KING('k'),
+    BLACK_KING('K');
 
     private char representation;
 

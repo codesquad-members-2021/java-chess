6e45709458773d3f786b5cc9a6601f4fb6713e26
95degree <95degree@naver.com>
2021-02-18 14:54:08 +0900

    NEW_LINE, EMPTY 상수 추가
    체스판을 출력하는 메소드를 만들 때 반복해서 문자열을 부르는것을 방지하기 위해 미리 상수로 선언했습니다.

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -9,45 +9,52 @@ public class Board {
     private List<Pawn> whitePawns = new ArrayList<>();
     private List<Pawn> blackPawns = new ArrayList<>();
     private static final int BOARD_SIZE = 8;
+    private static final String NEW_LINE = System.lineSeparator();
+    private static final String EMPTY = "........" + NEW_LINE;
 
-    public void addWhitePawn(Pawn pawn){
+    public void addWhitePawn(Pawn pawn) {
         whitePawns.add(pawn);
     }
 
-    public void addBlackPawn(Pawn pawn){
+    public void addBlackPawn(Pawn pawn) {
         blackPawns.add(pawn);
     }
 
-    public Pawn findWhitePawn(int index){
+    public Pawn findWhitePawn(int index) {
         return whitePawns.get(index);
     }
 
-    public Pawn findBlackPawn(int index){
+    public Pawn findBlackPawn(int index) {
         return blackPawns.get(index);
     }
 
-    public void initialize(){
-        for(int i =0; i<BOARD_SIZE; i++){
+    public void initialize() {
+        for (int i = 0; i < BOARD_SIZE; i++) {
             addWhitePawn(new Pawn(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION));
             addBlackPawn(new Pawn(Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION));
         }
     }
 
-    public String getWhitePawnsResult(){
+    public void printBoard() {
+        StringBuilder result = new StringBuilder();
+
+    }
+
+    public String getWhitePawnsResult() {
         return getPawnsResult(whitePawns);
     }
 
-    public String getBlackPawnsResult(){
+    public String getBlackPawnsResult() {
         return getPawnsResult(blackPawns);
     }
 
-    public int size(){
-        return blackPawns.size()+ whitePawns.size();
+    public int size() {
+        return blackPawns.size() + whitePawns.size();
     }
 
-    private String getPawnsResult(List<Pawn> pawns){
+    private String getPawnsResult(List<Pawn> pawns) {
         StringBuilder pawnsResult = new StringBuilder();
-        for(Pawn pawn : pawns)
+        for (Pawn pawn : pawns)
             pawnsResult.append(pawn.getRepresentation());
         return pawnsResult.toString();
     }

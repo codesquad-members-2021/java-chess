acfb28289079e0fdacacdc2fc777afac60f91c70
95degree <95degree@naver.com>
2021-02-18 17:48:07 +0900

    main 메소드 생성
    게임을 시작하기 위해 명령어를 받고 그에 맞는 실행을 해주는 main 메소드를 추가했습니다.

--- src/main/java/chess/Main.java
+++ src/main/java/chess/Main.java
@@ -1,4 +1,34 @@
 package chess;
 
+import java.util.Scanner;
+
 public class Main {
+    private static final String START_COMMAND = "start";
+    private static final String END_COMMAND = "end";
+
+    public static void main(String[] args) {
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            String notice = "명령어를 입력하세요 : start(게임 스타트), end(게임 끝내기)";
+            System.out.println(notice);
+            String command = scanner.nextLine();
+
+            while (!command.equals(END_COMMAND)) {
+                if (command.equals(START_COMMAND))
+                    start();
+                if (!command.equals(START_COMMAND)) {
+                    System.out.println("올바른 명령어를 입력하세요");
+                }
+                System.out.println(notice);
+                command = scanner.nextLine();
+            }
+            System.out.println("게임을 종료합니다.");
+        }
+    }
+
+    private static void start() {
+        Board board = new Board();
+        board.initialize();
+        System.out.println(board.printBoard());
+    }
 }

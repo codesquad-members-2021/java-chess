004dfe69e798bf12cb6b3992d9559d52cbdebe4f
MJbae <gently.gogo@gmail.com>
2021-02-19 08:43:23 +0900

    Refactor: 맥토리 메소드 도입
    폰 타입에 대한 팩토리 메소드 추가
    팩토리 메소드에 필요한 생성자만 남기고 다른 생성자 불필요에 따라 제거

--- src/main/java/mj/chess/pieces/Piece.java
+++ src/main/java/mj/chess/pieces/Piece.java
@@ -1,33 +1,36 @@
 package mj.chess.pieces;
 
-public class Piece {
+final class Piece {
     public final static char WHITE_REPRESENTATION = 'p';
     public final static char BLACK_REPRESENTATION = 'P';
 
     private final Color color;
     private final char representation;
+    private final Type type;
 
-    public Piece() {
-        this.color = Color.WHITE;
-        this.representation = WHITE_REPRESENTATION;
-    }
-
-    public Piece(Color color) {
+    public Piece(Color color, Type type, char representation) {
         this.color = color;
-        this.representation = (color == Color.BLACK) ? BLACK_REPRESENTATION : WHITE_REPRESENTATION;
-    }
-
-    public Piece(Color color, char representation) {
-        this.color = color;
+        this.type = type;
         this.representation = representation;
+    }
+
+    public static Piece createWhitePawn() {
+        return new Piece(Color.WHITE, Type.PAWN, WHITE_REPRESENTATION);
+    }
+
+    public static Piece createBlackPawn() {
+        return new Piece(Color.BLACK, Type.PAWN, BLACK_REPRESENTATION);
     }
 
     public Color getColor() {
         return color;
     }
 
+    public Type getType() {
+        return type;
+    }
+
     public char getRepresentation() {
         return representation;
     }
-
 }

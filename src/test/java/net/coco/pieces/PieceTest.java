0415e95a7789c91426656b23e4e5ddcf687a0ac9
gisungchoi <chlrltjd159@naver.com>
2021-02-18 16:52:53 +0900

    기물 생성 test 작성
    기물들이 원하는데로 생성되는지 검사합니다.

--- src/test/java/net/coco/pieces/PieceTest.java
+++ src/test/java/net/coco/pieces/PieceTest.java
@@ -10,13 +10,33 @@ class PieceTest {
 
     @Test
     @DisplayName("폰이 요구대로 생성되는지 테스트")
-    void pawnColorTest() {
-        verifyPiece(Piece.WHITE);
-        verifyPiece(Piece.BLACK);
+    void piecesMake() {
+        Assertions.assertAll(
+                () -> verifyPiece(Piece.createWhitePawn(), Piece.WHITE, Representation.WHITE_PAWN.getRepresentation()),
+                () -> verifyPiece(Piece.createBlackPawn(), Piece.BLACK, Representation.BLACK_PAWN.getRepresentation()),
+
+                () -> verifyPiece(Piece.createWhiteRook(), Piece.WHITE, Representation.WHITE_ROOK.getRepresentation()),
+                () -> verifyPiece(Piece.createBlackRook(), Piece.BLACK, Representation.BLACK_ROOK.getRepresentation()),
+
+                () -> verifyPiece(Piece.createWhiteBishop(), Piece.WHITE, Representation.WHITE_BISHOP.getRepresentation()),
+                () -> verifyPiece(Piece.createBlackBishop(), Piece.BLACK, Representation.BLACK_BISHOP.getRepresentation()),
+
+                () -> verifyPiece(Piece.createWhiteKing(), Piece.WHITE, Representation.WHITE_KING.getRepresentation()),
+                () -> verifyPiece(Piece.createBlackKing(), Piece.BLACK, Representation.BLACK_KING.getRepresentation()),
+
+                () -> verifyPiece(Piece.createWhiteKnight(), Piece.WHITE, Representation.WHITE_KNIGHT.getRepresentation()),
+                () -> verifyPiece(Piece.createBlackKnight(), Piece.BLACK, Representation.BLACK_KNIGHT.getRepresentation()),
+
+                () -> verifyPiece(Piece.createWhiteQueen(), Piece.WHITE, Representation.WHITE_QUEEN.getRepresentation()),
+                () -> verifyPiece(Piece.createBlackQueen(), Piece.BLACK, Representation.BLACK_QUEEN.getRepresentation())
+
+        );
+
     }
 
-    void verifyPiece(String color) {
-        //팩토리 메소드 테스트예정
+    void verifyPiece(Piece piece, String color, char representation) {
+        assertThat(color).isEqualTo(piece.getColor());
+        assertThat(representation).isEqualTo(piece.getRepresentation());
     }
 
 }

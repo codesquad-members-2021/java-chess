8151d38d0985cf085321315f606e7db1f5048f7a
gisungchoi <chlrltjd159@naver.com>
2021-02-18 16:38:32 +0900

    각종 기물 팩토리 메소드 추가
    기본생성자를 사용하는 팩토리 메소드를 생성했다.

--- src/main/java/net/coco/pieces/Piece.java
+++ src/main/java/net/coco/pieces/Piece.java
@@ -1,8 +1,8 @@
 package net.coco.pieces;
 
 public class Piece {
-    public static final String WHITE="white";
-    public static final String BLACK="black";
+    public static final String WHITE = "white";
+    public static final String BLACK = "black";
 
     private final String color;
     private final char representation;
@@ -12,15 +12,65 @@ public class Piece {
         this.representation = representation;
     }
 
-    public static Piece createWhitePawn(){
+    public static Piece createWhitePawn() {
         return new Piece(WHITE
-                ,Representation.WHITE_PAWN.getRepresentation());
-    }
-    public static Piece createBlackPawn(){
-        return new Piece(BLACK
-                ,Representation.BLACK_PAWN.getRepresentation());
+                , Representation.WHITE_PAWN.getRepresentation());
     }
 
+    public static Piece createBlackPawn() {
+        return new Piece(BLACK
+                , Representation.BLACK_PAWN.getRepresentation());
+    }
+
+    public static Piece createWhiteKnight() {
+        return new Piece(WHITE
+                , Representation.WHITE_KNIGHT.getRepresentation());
+    }
+
+    public static Piece createBlackKnight() {
+        return new Piece(BLACK
+                , Representation.BLACK_KNIGHT.getRepresentation());
+    }
+
+    public static Piece createWhiteRook() {
+        return new Piece(WHITE
+                , Representation.WHITE_ROOK.getRepresentation());
+    }
+
+    public static Piece createBlackRook() {
+        return new Piece(BLACK
+                , Representation.BLACK_ROOK.getRepresentation());
+    }
+
+    public static Piece createWhiteBishop() {
+        return new Piece(WHITE
+                , Representation.WHITE_BISHOP.getRepresentation());
+    }
+
+    public static Piece createBlackBishop() {
+        return new Piece(BLACK
+                , Representation.BLACK_BISHOP.getRepresentation());
+    }
+
+    public static Piece createWhiteQueen() {
+        return new Piece(WHITE
+                , Representation.WHITE_QUEEN.getRepresentation());
+    }
+
+    public static Piece createBlackQueen() {
+        return new Piece(BLACK
+                , Representation.BLACK_QUEEN.getRepresentation());
+    }
+
+    public static Piece createWhiteKing() {
+        return new Piece(WHITE
+                , Representation.WHITE_KING.getRepresentation());
+    }
+
+    public static Piece createBlackKing() {
+        return new Piece(BLACK
+                , Representation.BLACK_KING.getRepresentation());
+    }
 
 
     public String getColor() {

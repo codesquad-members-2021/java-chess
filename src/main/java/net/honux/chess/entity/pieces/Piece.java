1b8d1757ee5d675c31aec11082500b98296f9fa7
tmdgusya <tmdgusya@naver.com>
2021-02-18 16:54:16 +0900

    feat : Piece 로 Rename 및 팩토리 메소드를 통한 Piece 생성
    Piece.java
    
    - static 변수로 각 Piece 마다 representation 을 설정해 주었음.
    - private 생성자로 두어 Piece 는 수정이 불가능하게 하여 값 객체로서 생성함.
    
    PieceTest.java
    
    - 각 말별로 테스트를 생성하였음 -> 후에 분리될 수도 있다고 생각하여 각 말별로 일단 테스트를 나눠하는 방식으로 진행하였음.

---
+++ src/main/java/net/honux/chess/entity/pieces/Piece.java
@@ -0,0 +1,83 @@
+package net.honux.chess.entity.pieces;
+
+import net.honux.chess.attribute.Color;
+
+public class Piece {
+
+    public static final char WHITE_PAWN_REPRESENTATION = 'p';
+    public static final char BLACK_PAWN_REPRESENTATION = 'P';
+    public static final char WHITE_KING_REPRESENTATION = 'k';
+    public static final char BLACK_KING_REPRESENTATION = 'K';
+    public static final char WHITE_KNIGHT_REPRESENTATION = 'n';
+    public static final char BLACK_KNIGHT_REPRESENTATION = 'N';
+    public static final char WHITE_QUEEN_REPRESENTATION = 'q';
+    public static final char BLACK_QUEEN_REPRESENTATION = 'Q';
+    public static final char WHITE_ROOK_REPRESENTATION = 'r';
+    public static final char BLACK_ROOK_REPRESENTATION = 'R';
+    public static final char WHITE_BISHOP_REPRESENTATION = 'b';
+    public static final char BLACK_BISHOP_REPRESENTATION = 'B';
+
+    private final Color color;
+    private final char representation;
+
+    private Piece(Color color, char representation) {
+        this.color = color;
+        this.representation = representation;
+    }
+
+    public static Piece createWhitePawn() {
+        return new Piece(Color.WHITE, Piece.WHITE_PAWN_REPRESENTATION);
+    }
+
+    public static Piece createBlackPawn() {
+        return new Piece(Color.BLACK, Piece.BLACK_PAWN_REPRESENTATION);
+    }
+
+    public static Piece createWhiteKnight() {
+        return new Piece(Color.WHITE, Piece.WHITE_KNIGHT_REPRESENTATION);
+    }
+
+    public static Piece createBlackKnight() {
+        return new Piece(Color.BLACK, Piece.BLACK_KNIGHT_REPRESENTATION);
+    }
+
+    public static Piece createWhiteBishop() {
+        return new Piece(Color.WHITE, Piece.WHITE_BISHOP_REPRESENTATION);
+    }
+
+    public static Piece createBlackBishop() {
+        return new Piece(Color.BLACK, Piece.BLACK_BISHOP_REPRESENTATION);
+    }
+
+    public static Piece createWhiteRook() {
+        return new Piece(Color.WHITE, Piece.WHITE_ROOK_REPRESENTATION);
+    }
+
+    public static Piece createBlackRook() {
+        return new Piece(Color.BLACK, Piece.BLACK_ROOK_REPRESENTATION);
+    }
+
+    public static Piece createWhiteQueen() {
+        return new Piece(Color.WHITE, Piece.WHITE_QUEEN_REPRESENTATION);
+    }
+
+    public static Piece createBlackQueen() {
+        return new Piece(Color.BLACK, Piece.BLACK_QUEEN_REPRESENTATION);
+    }
+
+    public static Piece createWhiteKing() {
+        return new Piece(Color.WHITE, Piece.WHITE_KING_REPRESENTATION);
+    }
+
+    public static Piece createBlackKing() {
+        return new Piece(Color.BLACK, Piece.BLACK_KING_REPRESENTATION);
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public char getRepresentation() {
+        return this.representation;
+    }
+}
---
+++ src/test/java/net/honux/chess/entity/pieces/PieceTest.java
@@ -0,0 +1,97 @@
+package net.honux.chess.entity.pieces;
+
+import net.honux.chess.attribute.Color;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static net.honux.chess.entity.pieces.Piece.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PieceTest {
+
+    @Test
+    @DisplayName("Pawn 이 정상적으로 생성되어야 한다.")
+    void createPawn() {
+        Piece whitePawn = createWhitePawn();
+        Piece blackPawn = createBlackPawn();
+        Assertions.assertAll(
+                () -> assertThat(whitePawn.getColor()).isEqualTo(Color.WHITE),
+                () -> assertThat(whitePawn.getRepresentation()).isEqualTo('p'),
+                () -> assertThat(blackPawn.getColor()).isEqualTo(Color.BLACK),
+                () -> assertThat(blackPawn.getRepresentation()).isEqualTo('P')
+        );
+
+    }
+
+    @Test
+    @DisplayName("Knight 가 정상적으로 생성되어야 한다.")
+    void createKnight() {
+        Piece whiteKnight = createWhiteKnight();
+        Piece blackKnight = createBlackKnight();
+        Assertions.assertAll(
+                () -> assertThat(whiteKnight.getColor()).isEqualTo(Color.WHITE),
+                () -> assertThat(whiteKnight.getRepresentation()).isEqualTo('n'),
+                () -> assertThat(blackKnight.getColor()).isEqualTo(Color.BLACK),
+                () -> assertThat(blackKnight.getRepresentation()).isEqualTo('N')
+        );
+
+    }
+
+    @Test
+    @DisplayName("Bishop 이 정상적으로 생성되어야 한다.")
+    void createBishop() {
+        Piece whiteBishop = createWhiteBishop();
+        Piece blackBishop = createBlackBishop();
+        Assertions.assertAll(
+                () -> assertThat(whiteBishop.getColor()).isEqualTo(Color.WHITE),
+                () -> assertThat(whiteBishop.getRepresentation()).isEqualTo('b'),
+                () -> assertThat(blackBishop.getColor()).isEqualTo(Color.BLACK),
+                () -> assertThat(blackBishop.getRepresentation()).isEqualTo('B')
+        );
+
+    }
+
+    @Test
+    @DisplayName("Rook 이 정상적으로 생성되어야 한다.")
+    void createRook() {
+        Piece whiteRook = createWhiteRook();
+        Piece blackRook = createBlackRook();
+        Assertions.assertAll(
+                () -> assertThat(whiteRook.getColor()).isEqualTo(Color.WHITE),
+                () -> assertThat(whiteRook.getRepresentation()).isEqualTo('r'),
+                () -> assertThat(blackRook.getColor()).isEqualTo(Color.BLACK),
+                () -> assertThat(blackRook.getRepresentation()).isEqualTo('R')
+        );
+
+    }
+
+    @Test
+    @DisplayName("Queen 이 정상적으로 실행되어야 한다.")
+    void createQueen() {
+        Piece whiteQueen = createWhiteQueen();
+        Piece blackQueen = createBlackQueen();
+        Assertions.assertAll(
+                () -> assertThat(whiteQueen.getColor()).isEqualTo(Color.WHITE),
+                () -> assertThat(whiteQueen.getRepresentation()).isEqualTo('q'),
+                () -> assertThat(blackQueen.getColor()).isEqualTo(Color.BLACK),
+                () -> assertThat(blackQueen.getRepresentation()).isEqualTo('Q')
+        );
+
+    }
+
+    @Test
+    @DisplayName("king 이 정상적으로 생성되어야 한다.")
+    void createKing() {
+        Piece whiteKing = createWhiteKing();
+        Piece blackKing = createBlackKing();
+        Assertions.assertAll(
+                () -> assertThat(whiteKing.getColor()).isEqualTo(Color.WHITE),
+                () -> assertThat(whiteKing.getRepresentation()).isEqualTo('k'),
+                () -> assertThat(blackKing.getColor()).isEqualTo(Color.BLACK),
+                () -> assertThat(blackKing.getRepresentation()).isEqualTo('K')
+        );
+
+    }
+
+}

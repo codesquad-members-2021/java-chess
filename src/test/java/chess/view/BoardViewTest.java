5433dec710cb6c2f6c9975c495903fe341ce86ed
고정완 (Can) <jw.gho@ecubelabs.com>
2021-02-18 16:39:16 +0900

    [refactor] 불필요한 String + 연산 제거

--- src/test/java/chess/view/BoardViewTest.java
+++ src/test/java/chess/view/BoardViewTest.java
@@ -24,14 +24,14 @@ class BoardViewTest {
     @DisplayName("Board 의 positionId 들을 출력")
     void getPositionRepresentation() {
         StringBuilder sb = new StringBuilder();
-        sb.append("a8 b8 c8 d8 e8 f8 g8 h8 " + NEWLINE);
-        sb.append("a7 b7 c7 d7 e7 f7 g7 h7 " + NEWLINE);
-        sb.append("a6 b6 c6 d6 e6 f6 g6 h6 " + NEWLINE);
-        sb.append("a5 b5 c5 d5 e5 f5 g5 h5 " + NEWLINE);
-        sb.append("a4 b4 c4 d4 e4 f4 g4 h4 " + NEWLINE);
-        sb.append("a3 b3 c3 d3 e3 f3 g3 h3 " + NEWLINE);
-        sb.append("a2 b2 c2 d2 e2 f2 g2 h2 " + NEWLINE);
-        sb.append("a1 b1 c1 d1 e1 f1 g1 h1 " + NEWLINE);
+        sb.append("a8 b8 c8 d8 e8 f8 g8 h8 ").append(NEWLINE);
+        sb.append("a7 b7 c7 d7 e7 f7 g7 h7 ").append(NEWLINE);
+        sb.append("a6 b6 c6 d6 e6 f6 g6 h6 ").append(NEWLINE);
+        sb.append("a5 b5 c5 d5 e5 f5 g5 h5 ").append(NEWLINE);
+        sb.append("a4 b4 c4 d4 e4 f4 g4 h4 ").append(NEWLINE);
+        sb.append("a3 b3 c3 d3 e3 f3 g3 h3 ").append(NEWLINE);
+        sb.append("a2 b2 c2 d2 e2 f2 g2 h2 ").append(NEWLINE);
+        sb.append("a1 b1 c1 d1 e1 f1 g1 h1 ").append(NEWLINE);
         assertThat(boardView.getPositionRepresentation())
                 .isEqualTo(sb.toString());
     }
@@ -40,14 +40,14 @@ class BoardViewTest {
     @DisplayName("Board 의 Piece 위치들을 출력")
     void getPieceRepresentation() {
         StringBuilder sb = new StringBuilder();
-        sb.append(". . . . . . . . " + NEWLINE);
-        sb.append("P P P P P P P P " + NEWLINE);
-        sb.append(". . . . . . . . " + NEWLINE);
-        sb.append(". . . . . . . . " + NEWLINE);
-        sb.append(". . . . . . . . " + NEWLINE);
-        sb.append(". . . . . . . . " + NEWLINE);
-        sb.append("p p p p p p p p " + NEWLINE);
-        sb.append(". . . . . . . . " + NEWLINE);
+        sb.append(". . . . . . . . ").append(NEWLINE);
+        sb.append("P P P P P P P P ").append(NEWLINE);
+        sb.append(". . . . . . . . ").append(NEWLINE);
+        sb.append(". . . . . . . . ").append(NEWLINE);
+        sb.append(". . . . . . . . ").append(NEWLINE);
+        sb.append(". . . . . . . . ").append(NEWLINE);
+        sb.append("p p p p p p p p ").append(NEWLINE);
+        sb.append(". . . . . . . . ").append(NEWLINE);
         assertThat(boardView.getPieceRepresentation())
                 .isEqualTo(sb.toString());
     }
@@ -56,15 +56,15 @@ class BoardViewTest {
     @DisplayName("main 함수에서 사용할 예쁜 모양을 출력")
     void getBoardRepresentation() {
         StringBuilder sb = new StringBuilder();
-        sb.append("  a b c d e f g h " + NEWLINE);
-        sb.append("8 . . . . . . . . " + NEWLINE);
-        sb.append("7 P P P P P P P P " + NEWLINE);
-        sb.append("6 . . . . . . . . " + NEWLINE);
-        sb.append("5 . . . . . . . . " + NEWLINE);
-        sb.append("4 . . . . . . . . " + NEWLINE);
-        sb.append("3 . . . . . . . . " + NEWLINE);
-        sb.append("2 p p p p p p p p " + NEWLINE);
-        sb.append("1 . . . . . . . . " + NEWLINE);
+        sb.append("  a b c d e f g h ").append(NEWLINE);
+        sb.append("8 . . . . . . . . ").append(NEWLINE);
+        sb.append("7 P P P P P P P P ").append(NEWLINE);
+        sb.append("6 . . . . . . . . ").append(NEWLINE);
+        sb.append("5 . . . . . . . . ").append(NEWLINE);
+        sb.append("4 . . . . . . . . ").append(NEWLINE);
+        sb.append("3 . . . . . . . . ").append(NEWLINE);
+        sb.append("2 p p p p p p p p ").append(NEWLINE);
+        sb.append("1 . . . . . . . . ").append(NEWLINE);
         assertThat(boardView.getBoardRepresentation())
                 .isEqualTo(sb.toString());
     }

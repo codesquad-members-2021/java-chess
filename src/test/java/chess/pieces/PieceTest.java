cf5487a2cadce1e401b391fcdbc578a8441e0454
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2021-02-19 17:27:39 +0900

    각 색상별 verifyPiece 메서드 부분의 Color 매개변수 삭제

--- src/test/java/chess/pieces/PieceTest.java
+++ src/test/java/chess/pieces/PieceTest.java
@@ -1,5 +1,6 @@
 package chess.pieces;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -7,36 +8,37 @@ import static org.assertj.core.api.Assertions.assertThat;
 class PieceTest {
 
     @Test
-    public void createPiece() {
-        verifyWhitePiece(Piece.createWhitePawn(), Color.WHITE, Type.PAWN);
-        verifyBlackPiece(Piece.createBlackPawn(), Color.BLACK, Type.PAWN);
+    @DisplayName("체스말이 정상적으로 생성되는지 확인한다.")
+    public void createPieces() {
+        verifyWhitePiece(Piece.createWhitePawn(), Type.PAWN);
+        verifyBlackPiece(Piece.createBlackPawn(), Type.PAWN);
 
-        verifyWhitePiece(Piece.createWhiteRook(), Color.WHITE, Type.ROOK);
-        verifyBlackPiece(Piece.createBlackRook(), Color.BLACK, Type.ROOK);
+        verifyWhitePiece(Piece.createWhiteRook(), Type.ROOK);
+        verifyBlackPiece(Piece.createBlackRook(), Type.ROOK);
 
-        verifyWhitePiece(Piece.createWhiteKnight(), Color.WHITE, Type.KNIGHT);
-        verifyBlackPiece(Piece.createBlackKnight(), Color.BLACK, Type.KNIGHT);
+        verifyWhitePiece(Piece.createWhiteKnight(), Type.KNIGHT);
+        verifyBlackPiece(Piece.createBlackKnight(), Type.KNIGHT);
 
-        verifyWhitePiece(Piece.createWhiteBishop(), Color.WHITE, Type.BISHOP);
-        verifyBlackPiece(Piece.createBlackBishop(), Color.BLACK, Type.BISHOP);
+        verifyWhitePiece(Piece.createWhiteBishop(), Type.BISHOP);
+        verifyBlackPiece(Piece.createBlackBishop(), Type.BISHOP);
 
-        verifyWhitePiece(Piece.createWhiteQueen(), Color.WHITE, Type.QUEEN);
-        verifyBlackPiece(Piece.createBlackQueen(), Color.BLACK, Type.QUEEN);
+        verifyWhitePiece(Piece.createWhiteQueen(), Type.QUEEN);
+        verifyBlackPiece(Piece.createBlackQueen(), Type.QUEEN);
 
-        verifyWhitePiece(Piece.createWhiteKing(), Color.WHITE, Type.KING);
-        verifyBlackPiece(Piece.createBlackKing(), Color.BLACK, Type.KING);
+        verifyWhitePiece(Piece.createWhiteKing(), Type.KING);
+        verifyBlackPiece(Piece.createBlackKing(), Type.KING);
     }
 
-    private void verifyWhitePiece(Piece piece, Color color, Type type) {
-        assertThat(Piece.createWhitePawn().isWhite()).isEqualTo(true);
-        assertThat(piece.getColorName()).isEqualTo(color.getColorName());
-        assertThat(piece.getWhiteRepresentation()).isEqualTo(type.getWhiteRepresentation());
+    private void verifyWhitePiece(Piece piece, Type type) {
+        assertThat(piece.isWhite()).isEqualTo(true);
+        assertThat(piece.getColorName()).isEqualTo(Color.WHITE.getColorName());
+        assertThat(piece.getRepresentation()).isEqualTo(type.getWhiteRepresentation());
     }
 
-    private void verifyBlackPiece(Piece piece, Color color, Type type) {
-        assertThat(Piece.createBlackPawn().isBlack()).isEqualTo(true);
-        assertThat(piece.getColorName()).isEqualTo(color.getColorName());
-        assertThat(piece.getBlackRepresentation()).isEqualTo(type.getBlackRepresentation());
+    private void verifyBlackPiece(Piece piece, Type type) {
+        assertThat(piece.isBlack()).isEqualTo(true);
+        assertThat(piece.getColorName()).isEqualTo(Color.BLACK.getColorName());
+        assertThat(piece.getRepresentation()).isEqualTo(type.getBlackRepresentation());
     }
 
 }

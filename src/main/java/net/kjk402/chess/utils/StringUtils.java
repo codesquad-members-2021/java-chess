2a280c47d914c9f085dc9c25b1ebdf9db1bd6fc0
JoonKi-Kim <joonki402@gmail.com>
2021-02-19 22:43:12 +0900

    Fix Access Modifier in StringUtils class
    StringUtils 의 NEWLINE을 private로 수정.

--- src/main/java/net/kjk402/chess/utils/StringUtils.java
+++ src/main/java/net/kjk402/chess/utils/StringUtils.java
@@ -1,7 +1,7 @@
 package net.kjk402.chess.utils;
 
 public class StringUtils {
-    public static final String NEWLINE = System.getProperty("line.separator");
+    private static final String NEWLINE = System.getProperty("line.separator");
 
     public static String appendNewLine(String str) {
         return str + NEWLINE;

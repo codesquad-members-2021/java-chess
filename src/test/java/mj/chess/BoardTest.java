aff9b7f4f12debe6cca65e6537df7b486c76e2a1
MJbae <gently.gogo@gmail.com>
2021-02-19 09:55:41 +0900

    Refactor: 지역변수에 final 설정

--- src/test/java/mj/chess/BoardTest.java
+++ src/test/java/mj/chess/BoardTest.java
@@ -1,11 +1,10 @@
 package mj.chess;
 
-import mj.chess.pieces.Color;
-import mj.chess.pieces.Piece;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static mj.chess.utils.StringUtils.appendNewLine;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
@@ -20,41 +19,27 @@ class BoardTest {
     @Test
     @DisplayName("Board class에 Pawn class 추가 및 인덱스로 탐색")
     void createBoard() {
-        Piece[] testPieces = {
-                new Piece(Color.BLACK),
-                new Piece(Color.WHITE)
-        };
-
-        for (int i = 0; i < testPieces.length; i++) {
-            final int index = i;
-            assertAll(
-                    () -> verifyAddition(testPieces[index], index),
-                    () -> verifySearchByIndex(testPieces[index], index)
-            );
-        }
-    }
-
-    private void verifyAddition(Piece piece, int index) {
-        int size = index + 1;
-        board.add(piece);
-        assertThat(board.size()).isEqualTo(size);
-    }
-
-    private void verifySearchByIndex(Piece piece, int index) {
-        assertThat(piece).isEqualTo(board.findPawn(index));
-    }
-
-    @Test
-    @DisplayName("initialize 메소드 호출에 따라 각 폰의 표현문자 정상출력")
-    void checkRepresentationOfInitialize() {
+        final int numOfAllPieces = 32;
         board.initialize();
+
         assertAll(
-                () -> verifyRepresentationOfPawns(Color.WHITE, "pppppppp"),
-                () -> verifyRepresentationOfPawns(Color.BLACK, "PPPPPPPP")
+                () -> verifyCounting(board, numOfAllPieces),
+                () -> verifyRepresentationOfPawns(board)
         );
     }
 
-    private void verifyRepresentationOfPawns(Color color, String representationOfExpected) {
-        assertThat(board.getRowOfPawns(color)).isEqualTo(representationOfExpected);
+    private void verifyCounting(Board board, int maxNum){
+        assertThat(board.pieceCount()).isEqualTo(maxNum);
+    }
+
+    private void verifyRepresentationOfPawns(Board board) {
+        String blankRank = appendNewLine("........");
+        assertThat(board.showBoard()).isEqualTo(
+                appendNewLine("RNBQKBNR") +
+                        appendNewLine("PPPPPPPP") +
+                        blankRank + blankRank + blankRank + blankRank +
+                        appendNewLine("pppppppp") +
+                        appendNewLine("rnbqkbnr")
+        );
     }
 }

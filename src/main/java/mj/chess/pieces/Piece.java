bf087d546a54770de75afb4c69fc2fcfe6892d7a
MJbae <gently.gogo@gmail.com>
2021-02-19 16:08:57 +0900

    Refactor: 상수로 관리하던 표현문자에 Map자료구조로 관리
    RepresentationMap 클래스를 생성하여 내부에 정의한 Map 자료 구조로 표현문자를 관리함

--- src/main/java/mj/chess/pieces/Piece.java
+++ src/main/java/mj/chess/pieces/Piece.java
@@ -5,24 +5,24 @@ public class Piece {
     public final static char BLACK_REPRESENTATION = 'P';
 
     private final Color color;
-    private final char representation;
     private final Type type;
+    private final char representation;
+    private final RepresentationMap representationMap;
 
-    //TODO: 해쉬 이용하면 color와 key를 key로 갖고
-    // representation을 value로 가질 수 있을거 같은데?
-    // 그리고 그 해쉬 자료형은 getRepresnetation()에서만 이용하
-    private Piece(Color color, Type type, char representation) {
+    private Piece(Color color, Type type) {
+        representationMap = new RepresentationMap();
+
         this.color = color;
         this.type = type;
-        this.representation = representation;
+        this.representation = representationMap.getRepresentation(color, type);
     }
 
     public static Piece createWhitePawn() {
-        return new Piece(Color.WHITE, Type.PAWN, WHITE_REPRESENTATION);
+        return new Piece(Color.WHITE, Type.PAWN);
     }
 
     public static Piece createBlackPawn() {
-        return new Piece(Color.BLACK, Type.PAWN, BLACK_REPRESENTATION);
+        return new Piece(Color.BLACK, Type.PAWN);
     }
 
     public Color getColor() {

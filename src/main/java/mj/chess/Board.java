ada9c20e93c310a8628e91e59a298fee36879923
MJbae <gently.gogo@gmail.com>
2021-02-19 12:46:06 +0900

    Add: showBoard 메소드 추가
    체스판의 모든 기물의 위치를 문자열로 반환함
    반환된 문자열은 Console::print에서 출력

--- src/main/java/mj/chess/Board.java
+++ src/main/java/mj/chess/Board.java
@@ -2,30 +2,25 @@ package mj.chess;
 
 import mj.chess.pieces.Color;
 import mj.chess.pieces.Piece;
+import mj.chess.pieces.Type;
+import mj.chess.utils.StringUtils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static java.util.stream.Collectors.joining;
-
+import static mj.chess.utils.StringUtils.appendNewLine;
 
 public class Board {
-    private static final int MAX_SIZE = 8;
+    private static final int NUM_OF_ALL_PIECES = 32;
     private static final int SECOND_ROW = 1;
     private static final int SEVENTH_ROW = 6;
+    private static final int SIZE_OF_ROW = 8;
     private final List<Piece> pieces = new ArrayList<>();
     private final StringBuilder rowOfBlackPawns = new StringBuilder();
     private final StringBuilder rowOfWhitePawns = new StringBuilder();
-    private final String[] cellsOfBoard = {
-            "********",
-            "********",
-            "********",
-            "********",
-            "********",
-            "********",
-            "********",
-            "********"
-    };
+    private final StringBuilder[] cellsOfBoard = new StringBuilder[SIZE_OF_ROW];
 
     public Board() {
     }
@@ -45,23 +40,50 @@ public class Board {
     public void initialize() {
         for (Color color : Color.values()) {
             initPawns(color);
-            initCellsOfBoard(color);
         }
+        initCellsOfBoard();
     }
 
     private void initPawns(Color color) {
-        for (int i = 0; i < MAX_SIZE; i++) {
-            Piece piece = new Piece(color);
-            pieces.add(piece);
+        for (int i = 0; i < SIZE_OF_ROW; i++) {
+            Piece pawn = (color == Color.WHITE) ? Piece.createWhitePawn() : Piece.createBlackPawn();
+            pieces.add(pawn);
         }
     }
 
-    private void initCellsOfBoard(Color color) {
-        if (color == Color.WHITE) {
-            cellsOfBoard[SECOND_ROW] = getRowOfPawns(color);
-        } else {
-            cellsOfBoard[SEVENTH_ROW] = getRowOfPawns(color);
+    // TODO: 스트림으로 바꿔보자
+    private void initCellsOfBoard() {
+        StringBuilder blankRank = appendNewLine("........");
+
+        for(int nthRow = 0; nthRow < SIZE_OF_ROW; nthRow++){
+                cellsOfBoard[nthRow] = blankRank;
         }
+
+        cellsOfBoard[SECOND_ROW] = StringUtils.appendNewLine(getRowOfPawns(Color.BLACK));
+        cellsOfBoard[SEVENTH_ROW] = StringUtils.appendNewLine(getRowOfPawns(Color.WHITE));
+    }
+
+    public StringBuilder[] getCellsOfBoard() {
+        return cellsOfBoard;
+    }
+
+    public int countPieces() {
+        return pieces.size();
+    }
+    public int countPawns() {
+        return (int) pieces.stream()
+                .filter(pawn -> pawn.getType() == Type.PAWN)
+                .count();
+    }
+
+    //TODO: 스트림으로 바꿔보자
+    public String showBoard() {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < SIZE_OF_ROW; i++) {
+            result.append(cellsOfBoard[i]);
+        }
+
+        return result.toString();
     }
 
     public String getRowOfPawns(Color color) {
@@ -70,9 +92,5 @@ public class Board {
                 .map(Piece::getRepresentation)
                 .map(Object::toString)
                 .collect(joining());
-    }
-
-    public String[] getCellsOfBoard() {
-        return cellsOfBoard;
     }
 }

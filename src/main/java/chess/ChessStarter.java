c7603b71ac28d00245c392442d96fac007864488
Haina <bibi6666667@kakao.com>
2021-02-19 23:55:29 +0900

    [step3] ChessStarter) while을 사용해 입력값을 받도록 수정
    기존 swith문과 재귀로 입력값을 반복적으로 받던 부분을
    while문을 사용하면서 메소드를 분리해 호출하는 방식으로
    수정했습니다.

--- src/main/java/chess/ChessStarter.java
+++ src/main/java/chess/ChessStarter.java
@@ -6,7 +6,7 @@ public class ChessStarter {
     public static void main(String[] args) {
         ChessStarter chessStarter = new ChessStarter();
         chessStarter.intro();
-        chessStarter.start();
+        chessStarter.ready();
     }
 
     void intro() {
@@ -14,30 +14,33 @@ public class ChessStarter {
         System.out.println("start : 게임 시작");
         System.out.println("end : 게임 종료");
     }
-    
-    void start() {
-        System.out.print("> ");
-        Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        goTo(input);
-    }
 
-    void goTo(String input) {
-        switch(input) {
-            case "start" :
+    void ready() {
+        while (true) {
+            System.out.print("> ");
+            Scanner sc = new Scanner(System.in);
+            String input = sc.nextLine();
+            if (input.equals("start")) {
                 System.out.println("게임을 시작합니다.");
-                Board board = new Board();
-                board.initialize();
-                board.print();
-                break;
-            case "end" :
-                System.out.println("게임을 종료합니다.");
-                System.exit(0);
-                break;
-            default :
-                System.out.println("지정되지 않은 명령어입니다. 다시 입력해 주세요.");
                 start();
                 break;
+            }
+            if (input.equals("end")) {
+                System.out.println("게임을 종료합니다.");
+                end();
+                break;
+            }
+            System.out.println("지정되지 않은 명령어입니다. 다시 입력해 주세요.");
         }
+    }
+
+    void start() {
+        Board board = new Board();
+        board.initialize();
+        board.print();
+    }
+
+    void end() {
+        System.exit(0);
     }
 }

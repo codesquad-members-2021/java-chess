b832cce7c6f7d67d71727bce8f3eb58a57186259
MJbae <gently.gogo@gmail.com>
2021-02-19 12:21:12 +0900

    Test: 체스판 초기화 시 폰의 생성 개수를 체크하는 테스트 생성

--- src/test/java/mj/chess/BoardTest.java
+++ src/test/java/mj/chess/BoardTest.java
@@ -1,8 +1,12 @@
 package mj.chess;
 
+import mj.chess.pieces.Color;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static mj.chess.utils.StringUtils.appendNewLine;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -17,7 +21,48 @@ class BoardTest {
     }
 
     @Test
-    @DisplayName("Board class에 Pawn class 추가 및 인덱스로 탐색")
+    @DisplayName("오직 pawns로 구성된 체스판을 생성")
+    void createBoardOnlyWithPawns() {
+        final int numOfAllPieces = 16;
+        board.initialize();
+
+        assertAll(
+                () -> verifyCounting(board, numOfAllPieces),
+                () -> verifyRepresentationOfPawns(board)
+        );
+    }
+
+    private void verifyCounting(Board board, int maxNum) {
+        assertThat(board.countPieces()).isEqualTo(maxNum);
+    }
+
+    private void verifyRepresentationOfPawns(Board board) {
+        StringBuilder blankRank = appendNewLine("........");
+        assertThat(board.showBoard()).isEqualTo(
+                new StringBuilder()
+                        .append(String.valueOf(blankRank))
+                        .append(appendNewLine("PPPPPPPP"))
+                        .append(blankRank).append(blankRank)
+                        .append(blankRank).append(blankRank)
+                        .append(appendNewLine("pppppppp"))
+                        .append(blankRank)
+                        .toString()
+        );
+    }
+
+    @Test
+    @DisplayName("폰의 생성 개수를 확인")
+    void checkInitPawns() {
+        verifySizeOfPawns();
+    }
+
+    private void verifySizeOfPawns(){
+        board.initialize();
+        assertThat(board.countPawns()).isEqualTo(16);
+    }
+
+    @Test
+    @DisplayName("Board class에 ")
     void createBoard() {
         final int numOfAllPieces = 32;
         board.initialize();
@@ -28,18 +73,16 @@ class BoardTest {
         );
     }
 
-    private void verifyCounting(Board board, int maxNum){
-        assertThat(board.pieceCount()).isEqualTo(maxNum);
-    }
-
-    private void verifyRepresentationOfPawns(Board board) {
-        String blankRank = appendNewLine("........");
+    private void verifyRepresentationOfPieces(Board board) {
+        StringBuilder blankRank = appendNewLine("........");
         assertThat(board.showBoard()).isEqualTo(
-                appendNewLine("RNBQKBNR") +
-                        appendNewLine("PPPPPPPP") +
-                        blankRank + blankRank + blankRank + blankRank +
-                        appendNewLine("pppppppp") +
-                        appendNewLine("rnbqkbnr")
+                new StringBuilder()
+                        .append(appendNewLine("RNBQKBNR"))
+                        .append(appendNewLine("PPPPPPPP"))
+                        .append(blankRank).append(blankRank).append(blankRank).append(blankRank)
+                        .append(appendNewLine("pppppppp"))
+                        .append(appendNewLine("rnbqkbnr"))
+                        .toString()
         );
     }
 }

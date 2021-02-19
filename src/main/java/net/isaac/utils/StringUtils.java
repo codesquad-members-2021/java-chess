59d17db4b8c00cb7b553687cae56a2404cec342c
정이삭 <isaac56@naver.com>
2021-02-19 16:08:41 +0900

    Create StringUtils class
    1. 생성자를 private으로 선언하여 외부에서 인스턴스 생성을 못하도록 막음
    2. 운영체제 별 개행문자를 갖는 NEWLINE 전역 상수 선언
    3. 개행문자를 추가하여 반환하는 appendNewLine 메소드 선언

---
+++ src/main/java/net/isaac/utils/StringUtils.java
@@ -0,0 +1,11 @@
+package net.isaac.utils;
+
+public class StringUtils {
+    private StringUtils(){}
+
+    public static final String NEWLINE = System.getProperty("line.separator");
+
+    public static String appendNewLine(String string){
+        return string + NEWLINE;
+    }
+}

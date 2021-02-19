8380bf8e9edeb72950919b849935172ec1d95a7e
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2021-02-19 17:51:29 +1300

    Add a bunch of constants of chess pieces
    - 생성이 필요한 체스말들을 모두 상수로 넣었습니다.

--- src/main/java/net/honux/chess/pieces/Piece.java
+++ src/main/java/net/honux/chess/pieces/Piece.java
@@ -3,8 +3,20 @@ package net.honux.chess.pieces;
 public class Piece {
     public static final String WHITE_COLOR = "white";
     public static final String BLACK_COLOR = "black";
+
     public static final char WHITE_PAWN_REPRESENTATION = 'p';
+    public static final char WHITE_KNIGHT_REPRESENTATION = 'n';
+    public static final char WHITE_ROOK_REPRESENTATION = 'r';
+    public static final char WHITE_BISHOP_REPRESENTATION = 'b';
+    public static final char WHITE_QUEEN_REPRESENTATION = 'q';
+    public static final char WHITE_KING_REPRESENTATION = 'k';
+
     public static final char BLACK_PAWN_REPRESENTATION = 'P';
+    public static final char BLACK_KNIGHT_REPRESENTATION = 'N';
+    public static final char BLACK_ROOK_REPRESENTATION = 'R';
+    public static final char BLACK_BISHOP_REPRESENTATION = 'B';
+    public static final char BLACK_QUEEN_REPRESENTATION = 'Q';
+    public static final char BLACK_KING_REPRESENTATION = 'K';
 
     private String color;
     private char representation;
@@ -25,9 +37,10 @@ public class Piece {
     }
 
     public static Piece createWhitePawn() {
-        return
+        return new Piece(WHITE_COLOR, WHITE_PAWN_REPRESENTATION);
     }
 
     public static Piece createBlackPawn() {
+        return new Piece(BLACK_COLOR, BLACK_PAWN_REPRESENTATION);
     }
 }

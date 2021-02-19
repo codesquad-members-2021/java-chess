08c7aea6f8ae627196830f8f8ee73795807ff7b6
MJbae <gently.gogo@gmail.com>
2021-02-19 21:14:39 +0900

    Add: 폰을 포함한 모든 기물의 초기위치를 문자열로 표현하는 기능 추가
    getRowOfPieces()에 초기 기물의 위치에 따라 표현문자를 삽입한 것을 반환함
    initCellsOfBoard()에 cellsOfBoard의 첫번째와 마지막 열에 폰을 제외한 기물의 표현문자의 값을 할당함
    흰색 기물과 검은색 기물 사이의 공백은 마침표로 표현함

--- src/main/java/mj/chess/Board.java
+++ src/main/java/mj/chess/Board.java
@@ -2,6 +2,7 @@ package mj.chess;
 
 import mj.chess.pieces.Color;
 import mj.chess.pieces.Piece;
+import mj.chess.pieces.RepresentationMap;
 import mj.chess.pieces.Type;
 import mj.chess.utils.StringUtils;
 
@@ -12,8 +13,14 @@ import static java.util.stream.Collectors.joining;
 import static mj.chess.utils.StringUtils.appendNewLine;
 
 public class Board {
+    private static final int FIRST_ROW = 0;
     private static final int SECOND_ROW = 1;
+    private static final int THIRD_ROW = 2;
+    private static final int FORTH_ROW = 3;
+    private static final int FIFTH_ROW = 4;
+    private static final int SIXTH_ROW = 5;
     private static final int SEVENTH_ROW = 6;
+    private static final int EIGHTH_ROW = 7;
     private static final int SIZE_OF_ROW = 8;
     private final List<Piece> pieces = new ArrayList<>();
     private final StringBuilder[] cellsOfBoard = new StringBuilder[SIZE_OF_ROW];
@@ -21,6 +28,7 @@ public class Board {
     public Board() {
     }
 
+    //TODO: 메소드의 역할을 잘 드러나도록 이름 더 직관적으로 바꿔보자
     public void add(Piece piece) {
         pieces.add(piece);
     }
@@ -30,8 +38,10 @@ public class Board {
     }
 
     public void initialize() {
-        for (Color color : Color.values()) {
-            initPawns(color);
+        for (Type type : Type.values()) {
+            for (Color color : Color.values()) {
+                initPieces(color, type);
+            }
         }
         initCellsOfBoard();
     }
@@ -43,21 +53,58 @@ public class Board {
         }
     }
 
+    private void initPieces(Color color, Type type) {
+        Piece piece;
+
+        switch (type) {
+            case PAWN:
+                piece = (color == Color.WHITE) ? Piece.createWhitePawn() : Piece.createBlackPawn();
+                for (int i = 0; i < 8; i++) this.add(piece);
+                break;
+            case ROOK:
+                piece = (color == Color.WHITE) ? Piece.createWhiteRook() : Piece.createBlackRook();
+                for (int i = 0; i < 2; i++) this.add(piece);
+                break;
+            case KNIGHT:
+                piece = (color == Color.WHITE) ? Piece.createWhiteKnight() : Piece.createBlackKnight();
+                for (int i = 0; i < 2; i++) this.add(piece);
+                break;
+            case BISHOP:
+                piece = (color == Color.WHITE) ? Piece.createWhiteBishop() : Piece.createBlackBishop();
+                for (int i = 0; i < 2; i++) this.add(piece);
+                break;
+            case QUEEN:
+                piece = (color == Color.WHITE) ? Piece.createWhiteQueen() : Piece.createBlackQueen();
+                for (int i = 0; i < 1; i++) this.add(piece);
+                break;
+            case KING:
+                piece = (color == Color.WHITE) ? Piece.createWhiteKing() : Piece.createBlackKing();
+                for (int i = 0; i < 1; i++) this.add(piece);
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + type);
+        }
+    }
+
     // TODO: 스트림으로 바꿔보자
     private void initCellsOfBoard() {
         StringBuilder blankRank = appendNewLine("........");
 
-        for(int nthRow = 0; nthRow < SIZE_OF_ROW; nthRow++){
+        for (int nthRow = 0; nthRow < SIZE_OF_ROW; nthRow++) {
+            if (nthRow < SEVENTH_ROW && nthRow > SECOND_ROW)
                 cellsOfBoard[nthRow] = blankRank;
         }
 
+        cellsOfBoard[FIRST_ROW] = StringUtils.appendNewLine(getRowOfPieces(Color.BLACK));
         cellsOfBoard[SECOND_ROW] = StringUtils.appendNewLine(getRowOfPawns(Color.BLACK));
         cellsOfBoard[SEVENTH_ROW] = StringUtils.appendNewLine(getRowOfPawns(Color.WHITE));
+        cellsOfBoard[EIGHTH_ROW] = StringUtils.appendNewLine(getRowOfPieces(Color.WHITE));
     }
 
     public int countPieces() {
         return pieces.size();
     }
+
     public int countPawns() {
         return (int) pieces.stream()
                 .filter(pawn -> pawn.getType() == Type.PAWN)
@@ -74,9 +121,25 @@ public class Board {
 
     public String getRowOfPawns(Color color) {
         return pieces.stream()
+                .filter(piece -> piece.getType() == Type.PAWN)
                 .filter(piece -> piece.getColor() == color)
                 .map(Piece::getRepresentation)
                 .map(Object::toString)
                 .collect(joining());
+    }
+
+    public String getRowOfPieces(Color color) {
+        RepresentationMap representationMap = new RepresentationMap();
+        StringBuilder stringBuilder = new StringBuilder();
+
+        return stringBuilder.append(representationMap.getRepresentation(color, Type.ROOK))
+                .append(representationMap.getRepresentation(color, Type.KNIGHT))
+                .append(representationMap.getRepresentation(color, Type.BISHOP))
+                .append(representationMap.getRepresentation(color, Type.QUEEN))
+                .append(representationMap.getRepresentation(color, Type.KING))
+                .append(representationMap.getRepresentation(color, Type.BISHOP))
+                .append(representationMap.getRepresentation(color, Type.KNIGHT))
+                .append(representationMap.getRepresentation(color, Type.ROOK))
+                .toString();
     }
 }

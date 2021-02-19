24693d7e81292256c4c0eccb8141511cd73eeae8
MJbae <gently.gogo@gmail.com>
2021-02-19 18:21:01 +0900

    Add: 모든 기물에 대한 생성 팩토리 메소드 추가

--- src/main/java/mj/chess/pieces/Piece.java
+++ src/main/java/mj/chess/pieces/Piece.java
@@ -14,22 +14,50 @@ public class Piece {
         this.representation = representationMap.getRepresentation(color, type);
     }
 
+    //TODO: 이거 반복되는 구조니까 개선해야 한다 어떻게?
     public static Piece createWhitePawn() {
         return new Piece(Color.WHITE, Type.PAWN);
     }
-
     public static Piece createBlackPawn() {
         return new Piece(Color.BLACK, Type.PAWN);
+    }
+    public static Piece createWhiteRook() {
+        return new Piece(Color.WHITE, Type.ROOK);
+    }
+    public static Piece createBlackRook() {
+        return new Piece(Color.BLACK, Type.ROOK);
+    }
+    public static Piece createWhiteBishop() {
+        return new Piece(Color.WHITE, Type.BISHOP);
+    }
+    public static Piece createBlackBishop() {
+        return new Piece(Color.BLACK, Type.BISHOP);
+    }
+    public static Piece createWhiteQueen() {
+        return new Piece(Color.WHITE, Type.QUEEN);
+    }
+    public static Piece createBlackQueen() {
+        return new Piece(Color.BLACK, Type.QUEEN);
+    }
+    public static Piece createWhiteKnight() {
+        return new Piece(Color.WHITE, Type.KNIGHT);
+    }
+    public static Piece createBlackKnight() {
+        return new Piece(Color.BLACK, Type.KNIGHT);
+    }
+    public static Piece createWhiteKing() {
+        return new Piece(Color.WHITE, Type.KING);
+    }
+    public static Piece createBlackKing() {
+        return new Piece(Color.BLACK, Type.KING);
     }
 
     public Color getColor() {
         return color;
     }
-
     public Type getType() {
         return type;
     }
-
     public char getRepresentation() {
         return representation;
     }

5f1db72df9912b4b2b6950fc7a23bb8f099e9aa4
MJbae <gently.gogo@gmail.com>
2021-02-19 16:07:45 +0900

    Add: 표현문자 처리 클래스 추가
    기존에는 Piece class 안에 상수로 표현문자를 처리함
    Color와 Piece만으로 표현문자를 처리할 수 있을 것이라 판단
    color + type을 키값으로 갖고 표현문자를 매칭 값으로 갖는 맵 자료구조 구현
    클래스 생성자에 init() 매소드 추가하여 모든 표현문자에 대한 키밸류 값을 초기화함

---
+++ src/main/java/mj/chess/pieces/RepresentationMap.java
@@ -0,0 +1,21 @@
+package mj.chess.pieces;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RepresentationMap {
+    private final Map<String, Character> representationMap = new HashMap<>();
+
+    RepresentationMap(){
+        init();
+    }
+
+    private void init(){
+        representationMap.put(Color.WHITE.name() + Type.PAWN.name(), 'p');
+        representationMap.put(Color.BLACK.name() + Type.PAWN.name(), 'P');
+    }
+
+    public char getRepresentation(Color color, Type type){
+        return representationMap.get(color.name() + type.name());
+    }
+}

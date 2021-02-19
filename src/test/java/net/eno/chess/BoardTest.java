f02c0a5ee429093b90cf6723e9d504b3090e6414
jeong-inho <derosatam76@gmail.com>
2021-02-19 18:00:33 +0900

    Add test case in BoardTest class BoardTest class에 전면이 화이트로 선택된 보드와 검은색으로 선택된 보드의 테스트 추가

--- src/test/java/net/eno/chess/BoardTest.java
+++ src/test/java/net/eno/chess/BoardTest.java
@@ -3,6 +3,7 @@ package net.eno.chess;
 import org.junit.jupiter.api.*;
 import static org.assertj.core.api.Assertions.*;
 
+import net.eno.pieces.Color;
 import static net.eno.utils.StringUtils.appendNewLine;
 
 public class BoardTest {
@@ -25,19 +26,29 @@ public class BoardTest {
     @DisplayName("초기화 된 보드의 말의 배치가 일치해야 한다.")
     public void initialize() {
         String blankRank = appendNewLine("........");
-        assertThat(board.showBoard()).isEqualTo(
+        assertThat(board.showBoard(Color.WHITE)).isEqualTo(
                 appendNewLine("RNBQKBNR") +
                 appendNewLine("PPPPPPPP") +
                 blankRank + blankRank + blankRank + blankRank +
                 appendNewLine("pppppppp") +
                 appendNewLine("rnbqkbnr")
         );
+
+        assertThat(board.showBoard(Color.BLACK)).isEqualTo(
+                appendNewLine("rnbkqbnr") +
+                appendNewLine("pppppppp") +
+                blankRank + blankRank + blankRank + blankRank +
+                appendNewLine("PPPPPPPP") +
+                appendNewLine("RNBKQBNR")
+        );
     }
 
     @Test
     @DisplayName("초기화 된 보드가 출력되어야 한다.")
     public void print() {
-        System.out.println(board.showBoard());
+        System.out.println(board.showBoard(Color.WHITE));
+        System.out.println();
+        System.out.println(board.showBoard(Color.BLACK));
     }
 
 }

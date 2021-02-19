b23b8afa597f3f3141cf529b15d5be64e95cf388
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2021-02-19 14:57:31 +1300

    Verify static factory method
    - Pawn 클래스가 Piece 클래스로 교체되었습니다. 교체되면서 각 체스말들을 생성하는 방법으로 Piece를 직접 생성하는게 아니라 정적 팩토리 메소드를 이용합니다.

--- src/test/java/net/honux/chess/pieces/PieceTest.java
+++ src/test/java/net/honux/chess/pieces/PieceTest.java
@@ -8,13 +8,12 @@ import static org.assertj.core.api.Assertions.*;
 class PieceTest {
 
     @Test
-    void 폰의_컬러와_화면에_출력할_Representation이_각_컬러에_맞는지_확인한다() {
-        verifyPawn(Piece.WHITE_COLOR, Piece.WHITE_REPRESENTATION);
-        verifyPawn(Piece.BLACK_COLOR, Piece.BLACK_REPRESENTATION);
+    void Piece의_컬러와_화면에_출력할_Representation이_각_컬러에_맞는지_확인한다() {
+        verifyPiece(Piece.createWhitePawn(), Piece.WHITE_COLOR, Piece.WHITE_PAWN_REPRESENTATION);
+        verifyPiece(Piece.createBlackPawn(), Piece.BLACK_COLOR, Piece.BLACK_PAWN_REPRESENTATION);
     }
 
-    private void verifyPawn(final String color, final char representation) {
-        Piece pawn = new Piece(color, representation);
+    private void verifyPiece(final Piece piece, final String color, final char representation) {
         assertThat(pawn.getColor()).isEqualTo(color);
         assertThat(pawn.getRepresentation()).isEqualTo(representation);
     }

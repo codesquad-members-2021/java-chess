217b41bc3b6bfbde697c3470d46d8a004fed5181
MJbae <gently.gogo@gmail.com>
2021-02-19 16:45:07 +0900

    Refactor: 생성자에 접근제어자 추가

--- src/main/java/mj/chess/pieces/RepresentationMap.java
+++ src/main/java/mj/chess/pieces/RepresentationMap.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class RepresentationMap {
     private final Map<String, Character> representationMap = new HashMap<>();
 
-    RepresentationMap(){
+    public RepresentationMap(){
         init();
     }
 

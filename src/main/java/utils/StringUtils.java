03cdb6ebcf87b453ed4e43e01bc493eea956a209
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2021-02-20 20:43:37 +0900

    보드 사이즈 체크하여 NEWLINE을 넣어주는 코드를 제외하고 NEWLINE을 리턴해주는 getNewLine메서드 추가

--- src/main/java/utils/StringUtils.java
+++ src/main/java/utils/StringUtils.java
@@ -3,16 +3,17 @@ package utils;
 import chess.Board;
 
 public class StringUtils {
-    public static final String NEWLINE = System.getProperty("line.separator");
+    private static final String NEWLINE = System.getProperty("line.separator");
 
     private StringUtils() {
 
     }
 
     public static String addNewLine(String text) {
-        if (text.length() > Board.BOARD_SIZE) {
-            return text.substring(0, 8) + NEWLINE + text.substring(8) + NEWLINE;
-        }
         return text + NEWLINE;
+    }
+
+    public static String getNewLine(){
+        return NEWLINE;
     }
 }

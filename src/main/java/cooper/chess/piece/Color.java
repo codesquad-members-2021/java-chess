f5e878a2b9b531b75dbd58db58a83ed718e97a16
pbg0205 <pbg0205@naver.com>
2021-02-20 17:34:16 +0900

    refactor : Color enum 상수 멤버 제거
    - Piece의 생성자의 매개인자를 Color enum 상수를 사용하기 때문에 불필요한
    상수 멤버의 문자열을 제거하였습니다.

--- src/main/java/cooper/chess/piece/Color.java
+++ src/main/java/cooper/chess/piece/Color.java
@@ -1,25 +1,6 @@
 package cooper.chess.piece;
 
-import java.util.Arrays;
-
 public enum Color {
-    WHITE("white"),
-    BLACK("black");
-
-    private final String colorName;
-
-    Color(String colorName) {
-        this.colorName = colorName;
-    }
-
-    public String getColorName() {
-        return colorName;
-    }
-
-    public static Color of(String colorName) {
-        return Arrays.stream(values())
-                .filter(color -> color.colorName.equals(colorName))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("해당 색상이 존재하지 않습니다."));
-    }
+    WHITE,
+    BLACK;
 }

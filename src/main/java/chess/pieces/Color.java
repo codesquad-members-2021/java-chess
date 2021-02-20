49f24ac610ec5edba6d81a7e91e427d00e6749b9
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2021-02-20 19:11:46 +0900

    String 변수 삭제

--- src/main/java/chess/pieces/Color.java
+++ src/main/java/chess/pieces/Color.java
@@ -1,16 +1,6 @@
 package chess.pieces;
 
 public enum Color {
-    WHITE("white"),
-    BLACK("black");
-
-    private final String colorName;
-
-    Color(String colorName) {
-        this.colorName = colorName;
-    }
-
-    public String getColorName() {
-        return colorName;
-    }
+    WHITE,
+    BLACK
 }

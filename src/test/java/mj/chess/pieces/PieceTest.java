e3a1df5b579525d20bd0d179bbacf2e98bdb75ae
MJbae <gently.gogo@gmail.com>
2021-02-20 14:38:59 +0900

    Test: 모든 기물에 대해 흑백 상태 확인 테스트 추가
    Piece::isBlack, isWhite 추가 전에 해당 메소드에 대한 테스트 케이스 작성
    switch문과 Type.values() 활용하여 모든 기물에 대해 isWhite or isBlack 메소드에 대한 테스트 코드 작성

--- src/test/java/mj/chess/pieces/PieceTest.java
+++ src/test/java/mj/chess/pieces/PieceTest.java
@@ -15,7 +15,9 @@ class PieceTest {
                 () -> verifyPiece(Piece.createWhitePawn(), Color.WHITE, Type.PAWN),
                 () -> verifyPiece(Piece.createBlackPawn(), Color.BLACK, Type.PAWN)
         );
-    };
+    }
+
+    ;
 
     private void verifyPiece(final Piece piece, Color color, Type type) {
         String representation = new RepresentationMap().getRepresentation(color, type);
@@ -25,17 +27,86 @@ class PieceTest {
                 () -> verifyTypeOfPiece(piece, type),
                 () -> verifyRepresentationOfPiece(piece, representation)
         );
-    };
+    }
+
+    ;
 
     private void verifyColorOfPiece(final Piece piece, Color color) {
         assertThat(piece.getColor()).isEqualTo(color);
-    };
+    }
+
+    ;
 
     private void verifyTypeOfPiece(final Piece piece, Type type) {
         assertThat(piece.getType()).isEqualTo(type);
-    };
+    }
+
+    ;
 
     private void verifyRepresentationOfPiece(final Piece piece, final String representation) {
         assertThat(piece.getRepresentation()).isEqualTo(representation);
-    };
+    }
+
+    ;
+
+    @Test
+    @DisplayName("모든 기물에 대해 흑백 상태 확인")
+    void checkIsWhiteOrBlack() {
+        for (Type eachType : Type.values()) {
+            assertAll(
+                    () -> verifyIsBlack(eachType),
+                    () -> verifyIsWhite(eachType)
+            );
+        }
+    }
+
+    private void verifyIsWhite(Type type) {
+        switch (type) {
+            case PAWN:
+                assertThat(Piece.createWhitePawn().isWhite()).isEqualTo(true);
+                break;
+            case KNIGHT:
+                assertThat(Piece.createWhiteKnight().isWhite()).isEqualTo(true);
+                break;
+            case ROOK:
+                assertThat(Piece.createWhiteRook().isWhite()).isEqualTo(true);
+                break;
+            case BISHOP:
+                assertThat(Piece.createWhiteBishop().isWhite()).isEqualTo(true);
+                break;
+            case QUEEN:
+                assertThat(Piece.createWhiteQueen().isWhite()).isEqualTo(true);
+                break;
+            case KING:
+                assertThat(Piece.createWhiteKing().isWhite()).isEqualTo(true);
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + type);
+        }
+    }
+
+    private void verifyIsBlack(Type type) {
+        switch (type) {
+            case PAWN:
+                assertThat(Piece.createBlackPawn().isBlack()).isEqualTo(true);
+                break;
+            case KNIGHT:
+                assertThat(Piece.createBlackKnight().isBlack()).isEqualTo(true);
+                break;
+            case ROOK:
+                assertThat(Piece.createBlackRook().isBlack()).isEqualTo(true);
+                break;
+            case BISHOP:
+                assertThat(Piece.createBlackBishop().isBlack()).isEqualTo(true);
+                break;
+            case QUEEN:
+                assertThat(Piece.createBlackQueen().isBlack()).isEqualTo(true);
+                break;
+            case KING:
+                assertThat(Piece.createBlackKing().isBlack()).isEqualTo(true);
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + type);
+        }
+    }
 }

5bde819dc408c88f483189586bfc673bac9f1c1c
gisungchoi <chlrltjd159@naver.com>
2021-02-20 18:16:43 +0900

    Representation enum의 이름 변경과 getter 변경
    원래 색상별로 돌려주다가 getRresentation() 메소드를 정의해서
    한번에 돌려주도록 재정의했다.
    이로인해 다른 부분에서 변경사항들이 발생해서 변경했다.

---
+++ src/main/java/net/coco/pieces/PieceType.java
@@ -0,0 +1,31 @@
+package net.coco.pieces;
+
+public enum PieceType {
+    PAWN('p'),
+    KNIGHT('n'),
+    ROOK('r'),
+    BISHOP('b'),
+    QUEEN('q'),
+    KING('k');
+
+    private char representation;
+
+    PieceType(char representation) {
+        this.representation = representation;
+    }
+
+    private char getWhiteRepresentation() {
+        return representation;
+    }
+
+    private char getBlackRepresentation() {
+        return Character.toUpperCase(representation);
+    }
+
+    public char getRepresentation(String color){
+        //색깔 체크 필요
+        if (color.equals(Piece.WHITE))
+            return getWhiteRepresentation();
+        return getBlackRepresentation();
+    }
+}

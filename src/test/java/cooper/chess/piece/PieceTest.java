6c4a93d05daef3e24a9ae46b07325833f60a565d
pbg0205 <pbg0205@naver.com>
2021-02-20 18:06:39 +0900

    test : 모든 기물 생성 확인 테스트
    - 모든 기물이 정확한 생성 여부와 내부 값을 확인하기 위해 테스트 코드를
    작성하였습니다.
    
    - 기존의 String과 char로 입력받아 유효성 검사하는 방식보보다 enum
    멤버에 한해서 정확한 값을 지정한 범위 내의 파라미터로 입력받는  것이
    효율적이라고 생각하여 enum 상수를 파라미터로 변경하였습니다.

--- src/test/java/cooper/chess/piece/PieceTest.java
+++ src/test/java/cooper/chess/piece/PieceTest.java
@@ -7,19 +7,33 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PieceTest {
-
     @Test
-    @DisplayName("흰색 폰과 검정색 폰이 각각 생성됨을 확인한다.")
+    @DisplayName("흰색 또는 검은색 기물이 각각 생성됨을 확인한다.")
     public void create() {
-        verifyPawn(Color.WHITE.getColorName(), Type.PAWN.getRepresentaion());
-        verifyPawn(Color.BLACK.getColorName(), Type.PAWN.getRepresentaion());
+        assertAll(
+                () -> verifyPiece(Piece.createWhitePawn(), Color.WHITE, 'P'),
+                () -> verifyPiece(Piece.createBlackPawn(), Color.BLACK, 'p'),
+
+                () -> verifyPiece(Piece.createWhiteKnight(), Color.WHITE, 'N'),
+                () -> verifyPiece(Piece.createBlackKnight(), Color.BLACK, 'n'),
+
+                () -> verifyPiece(Piece.createWhiteRook(), Color.WHITE, 'R'),
+                () -> verifyPiece(Piece.createBlackRook(), Color.BLACK, 'r'),
+
+                () -> verifyPiece(Piece.createWhiteBishop(), Color.WHITE, 'B'),
+                () -> verifyPiece(Piece.createBlackBishop(), Color.BLACK, 'b'),
+
+                () -> verifyPiece(Piece.createWhiteQueen(), Color.WHITE, 'Q'),
+                () -> verifyPiece(Piece.createBlackQueen(), Color.BLACK, 'q'),
+
+                () -> verifyPiece(Piece.createWhiteKing(), Color.WHITE, 'K'),
+                () -> verifyPiece(Piece.createBlackKing(), Color.BLACK, 'k')
+        );
     }
 
-    void verifyPawn(final String color, final char representation) {
-        Piece piece = new Piece(Color.of(color), Type.PAWN);
-
+    void verifyPiece(final Piece piece, final Color color, final char representation) {
         assertAll(
-                () -> assertEquals(color, piece.getColor().getColorName()),
+                () -> assertEquals(color, piece.getColor()),
                 () -> assertEquals(representation, piece.getRepresentation())
         );
     }

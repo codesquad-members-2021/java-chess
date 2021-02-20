71a9081bc9ddc1b801a04951efb5fb699f2d37a7
pbg0205 <pbg0205@naver.com>
2021-02-21 01:07:13 +0900

    test : BoardTest - showBoard 확인 테스트 추가
    - Board 테스트를 확인하는 클래스이므로 @BeforeEach를 선언해서 각각의
    테스트 이전에 Board 인스턴스를 생성하도록 변경하였습니다.
    
    - Board 내 전체 기물의 요소의 갯수를 확인하는 테스트 코드를 create 메서드
    내에 추가하였습니다.
    
    - Board 내 전체 기물 상태를 확인할 수 있도록 대조군을 StringBuilder로
    생성하고 StringUtils 내에 실제 Board 상태에 대한 정보를 추가 및 비교하
    도록 로직을 구현하였습니다.

--- src/test/java/cooper/chess/BoardTest.java
+++ src/test/java/cooper/chess/BoardTest.java
@@ -1,38 +1,46 @@
 package cooper.chess;
 
-import cooper.chess.piece.Color;
-import cooper.chess.piece.Piece;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import cooper.chess.piece.*;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoardTest {
+    private Board board;
+
+    @BeforeEach
+    public void setUp() {
+        board = new Board();
+    }
+
     @Test
-    @DisplayName("board에 흰색 폰과 검정색 폰이 추가됨을 확인한다.")
+    @DisplayName("모든 기물이 생성됨을 확인한다.")
     public void create() {
-        Board board = new Board();
+        board.initialize();
 
-        Piece white = Piece.createWhitePawn();
-        board.add(white);
+        assertEquals(32, board.pieceCount());
 
-        Piece black = Piece.createBlackPawn();
-        board.add(black);
+        StringBuilder sb = new StringBuilder();
+        String blankRank = "........";
 
-        assertAll(
-                () -> assertEquals(1, board.size(Color.WHITE)),
-                () -> {
-                    assertEquals(white, board.findPawn(0, Color.WHITE));
-                    assertEquals(black, board.findPawn(0, Color.BLACK));
-                }
+        sb.append("RNBQKBNR").append("\n");
+        sb.append("PPPPPPPP").append("\n");
+        sb.append(blankRank).append("\n");
+        sb.append(blankRank).append("\n");
+        sb.append(blankRank).append("\n");
+        sb.append(blankRank).append("\n");
+        sb.append("pppppppp").append("\n");
+        sb.append("rnbqkbnr").append("\n");
+
+        assertEquals(
+                sb.toString(),
+                board.showBoard()
         );
     }
 
     @Test
     @DisplayName("board 사이즈가 0일 때의 예외를 발생시킨다.")
     public void testZeroSizeOfBoard() {
-        Board board = new Board();
-
         assertThrows(IllegalArgumentException.class,
                 () -> board.findPawn(0, Color.WHITE));
     }
@@ -40,8 +48,6 @@ class BoardTest {
     @Test
     @DisplayName("index 외에 값을 입력이 예외를 발생시킨다.")
     public void testOutOfBoardSize() {
-        Board board = new Board();
-
         Piece white = Piece.createWhitePawn();
         board.add(white);
 
@@ -55,21 +61,9 @@ class BoardTest {
     }
 
     @Test
-    @DisplayName("board를 초기상태를 세팅한다.")
-    public void initialize() {
-        Board board = new Board();
-        board.initialize();
-        assertAll(
-                () -> assertEquals("PPPPPPPP", board.getPawnsResult(Color.WHITE)),
-                () -> assertEquals("pppppppp", board.getPawnsResult(Color.BLACK))
-        );
-    }
-
-    @Test
     @DisplayName("board의 상태를 확인한다.")
     public void getBoardStatusTest() {
-        Board board = new Board();
         board.initialize();
-        System.out.println(board.getBoardStatus());
+        System.out.println(board.showBoard());
     }
 }

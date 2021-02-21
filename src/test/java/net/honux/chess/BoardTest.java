f9ef32b7a16747241ec12151f285d89044155e8a
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2021-02-22 09:11:52 +1300

    Verify a chess board initialized
    - Pawn뿐만아니라 다른 체스말들도 체스판에 추가하여 출력이 잘되는지 확인하기위한 테스트를 구현하였습니다

--- src/test/java/net/honux/chess/BoardTest.java
+++ src/test/java/net/honux/chess/BoardTest.java
@@ -1,6 +1,8 @@
 package net.honux.chess;
 
+import static net.honux.chess.utils.StringUtils.appendNewLine;
 import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.*;
 
 import net.honux.chess.pieces.Piece;
@@ -32,10 +34,25 @@ class BoardTest {
     }
 
     @Test
-    void print_잘되는지_확인하자 () {
+    void print_잘되는지_확인하자() {
         board = new Board();
         board.initialize();
         board.print();
+    }
+
+    @Test
+    void 전체기물의_상태를_볼수있는_체스판구현_테스트() {
+        board.initialize();
+        assertThat(board.pieceCount()).isEqualTo(32);
+        String blankRank = appendNewLine("........");
+        assertThat(board.showBoard())
+                .isEqualTo(
+                        appendNewLine("RNBQKBNR") +
+                        appendNewLine("PPPPPPPP") +
+                        blankRank + blankRank + blankRank + blankRank +
+                        appendNewLine("pppppppp") +
+                        appendNewLine("rnbqkbnr")
+                );
     }
 
 }

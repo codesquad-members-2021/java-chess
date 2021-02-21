0b35bda50ba8ba48c6998ec1c39a0713b67812d7
윤혜지 <yunhg95@gmail.com>
2021-02-21 12:02:51 +0900

    리뷰 내용 반영
    - 과거 print()에서 출력 테스트를 출력을 통해 진행했던 테스트 메소드 변경

--- src/test/java/net/honux/chess/BoardTest.java
+++ src/test/java/net/honux/chess/BoardTest.java
@@ -18,26 +18,31 @@ class BoardTest {
 
     @Test
     @DisplayName("WhitePawns의 값 사이즈 index로 색상 값을 잘 가져오는지 확인")
-    void create_white(){
-        Pawn white = new Pawn(Pawn.WHITE_COLOR,Pawn.WHITE_REPRESENTATION);
-        board.whitePawnsAdd(white);
+    void create_white() {
+        Pawn white = new Pawn(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION);
+        board.addWhitePawns(white);
         assertThat(board.size()).isEqualTo(1);
+        assertThat(board.findWhitePawn(0)).isEqualTo(white);
         assertThat(board.findWhitePawn(0).getColor()).isEqualTo("white");
+        assertThat(board.findWhitePawn(0).getRepresentation()).isEqualTo('p');
     }
 
     @Test
     @DisplayName("BlackPawns의 값 사이즈 index로 색상 값을 잘 가져오는지 확인")
-    void create_black(){
-        Pawn black = new Pawn(Pawn.BLACK_COLOR,Pawn.BLACK_REPRESENTATION);
-        board.blackPawnsAdd(black);
+    void create_black() {
+        Pawn black = new Pawn(Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION);
+        board.addBlackPawns(black);
         assertThat(board.size()).isEqualTo(1);
+        assertThat(board.findBlackPawn(0)).isEqualTo(black);
         assertThat(board.findBlackPawn(0).getColor()).isEqualTo("black");
+        assertThat(board.findBlackPawn(0).getRepresentation()).isEqualTo('P');
     }
+
     @Test
     @DisplayName("Board에 추가되는 기물 size 값 검토")
-    void check_board_size (){
-        board.whitePawnsAdd(new Pawn(Pawn.WHITE_COLOR,Pawn.WHITE_REPRESENTATION));
-        board.blackPawnsAdd(new Pawn(Pawn.BLACK_COLOR,Pawn.BLACK_REPRESENTATION));
+    void check_board_size() {
+        board.addWhitePawns(new Pawn(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION));
+        board.addBlackPawns(new Pawn(Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION));
         assertThat(board.size()).isEqualTo(2);
     }
 
@@ -51,9 +56,16 @@ class BoardTest {
 
     @Test
     @DisplayName("보드 초기화가 잘 print 되는지 확인")
-    void print(){
+    void print() {
         board.initialize();
-        System.out.println(board.print());
+        assertThat(board.boardPrint()).isEqualTo("........" + System.getProperty("line.separator") +
+                "PPPPPPPP" + System.getProperty("line.separator") +
+                "........" + System.getProperty("line.separator") +
+                "........" + System.getProperty("line.separator") +
+                "........" + System.getProperty("line.separator") +
+                "........" + System.getProperty("line.separator") +
+                "pppppppp" + System.getProperty("line.separator") +
+                "........");
     }
 
 }

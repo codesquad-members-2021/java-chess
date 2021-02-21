35cad06664ee646e2c7f8b8bf78372f32b80981b
Haina <bibi6666667@kakao.com>
2021-02-21 22:25:58 +0900

    [step3]피드백) static변수, 기본 생성자 위치 변경
    static변수의 위치를 인스턴스 변수 윗 줄로 옮겼습니다.
    기본 생성자를 다른 생성자보다 위에 위치하도록 했습니다.

--- src/main/java/pieces/Pawn.java
+++ src/main/java/pieces/Pawn.java
@@ -1,20 +1,20 @@
 package pieces;
 
 public class Pawn {
-    private final String color;
-    private final String representation;
     public final static String WHITE = "white";
     public final static String BLACK = "black";
     public final static String WHITE_REPRESENTATION = "p";
     public final static String BLACK_REPRESENTATION = "P";
+    private final String representation;
+    private final String color;
+
+    public Pawn() {
+        this(WHITE, WHITE_REPRESENTATION);
+    }
 
     public Pawn(String color, String representation) {
         this.color = color;
         this.representation = representation;
-    }
-
-    public Pawn() {
-        this(WHITE, WHITE_REPRESENTATION);
     }
 
     public String getColor() {

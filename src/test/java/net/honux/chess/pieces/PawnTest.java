38ffb1a60f3932f00db7d54ca5f3149900c22fcd
윤혜지 <yunhg95@gmail.com>
2021-02-21 12:03:19 +0900

    리뷰 내용 반영

--- src/test/java/net/honux/chess/pieces/PawnTest.java
+++ src/test/java/net/honux/chess/pieces/PawnTest.java
@@ -9,7 +9,7 @@ class PawnTest {
 
     @Test
     @DisplayName("기본 생성자는 흰색 말과 p로 표시한다")
-    void create_기본생성자() throws Exception {
+    void create_default_constructor() throws Exception {
         Pawn pawn = new Pawn();
         assertThat(pawn.getColor()).isEqualTo(Pawn.WHITE_COLOR);
         assertThat(pawn.getRepresentation()).isEqualTo(Pawn.WHITE_REPRESENTATION);
@@ -17,7 +17,7 @@ class PawnTest {
 
     @Test
     @DisplayName("해당 색상과 대표문자를 잘 가져오는지 확인한다")
-    void create() {
+    void check_color_representation() {
         verifyPawn(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION);
         verifyPawn(Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION);
     }

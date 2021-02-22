c926f16eac64cc5890d7905c27a5bb7b813bcd94
gisungchoi <chlrltjd159@naver.com>
2021-02-22 15:59:33 +0900

    printBoard() 리팩토링
    Board가 RankList로 변함에 따라 rank에 따라
    print가 진행된다.
    test들도 사라진 함수에 맞게 삭제조치 했다.

--- src/test/java/net/coco/chess/BoardTest.java
+++ src/test/java/net/coco/chess/BoardTest.java
@@ -23,29 +23,8 @@ public class BoardTest {
     }
 
     @Test
-    @DisplayName("판에 폰이 잘 들어가는지 체크")
-    void boardAddPawnCheck() {
-
-        Assertions.assertAll(
-                () -> verifyPawnToPawns(Piece.WHITE, 0),
-                () -> verifyPawnsSize(1),
-
-                () -> verifyPawnToPawns(Piece.BLACK, 0),
-                () -> verifyPawnsSize(2)
-        );
-
-    }
-
-    @Test
     void initialize() {
         board.initialize();
-        Assertions.assertAll(
-                () -> assertThat(board.getPiecesSize()).isEqualTo(32),
-                () -> assertThat(board.getBlackPawnsResult(Piece.BLACK)).isEqualTo("PPPPPPPP"),
-                () -> assertThat(board.getOtherBlackPiecesResult(Piece.BLACK)).isEqualTo("RNBQKBNR"),
-                () -> assertThat(board.getOtherWhitePiecesResult(Piece.WHITE)).isEqualTo("rnbqkbnr"),
-                () -> assertThat(board.getWhitePawnsResult(Piece.WHITE)).isEqualTo("pppppppp")
-        );
         PrintChess.printBoard(board);
 
     }
@@ -53,7 +32,7 @@ public class BoardTest {
     @Test
     void printChessBoard() {
         board.initialize();
-        String blank = appendNewLine("********");
+        String blank = appendNewLine("........");
         assertThat(
                 PrintChess.printBoard(board)
         ).isEqualTo(
@@ -65,23 +44,6 @@ public class BoardTest {
         );
     }
 
-    @Test
-    @DisplayName("board에 piece를 넣을 때는 piece에 따른 알맞은 메소드를 사용해야한다.")
-    void addOtherPieces() {
-        Piece blackBishop = Piece.createBlackBishop();
-        Piece blackPawn = Piece.createBlackPawn();
-
-        Assertions.assertAll(
-                () -> board.addOtherBlackPiece(blackBishop),
-                () -> assertThat(board.getPiecesSize()).isEqualTo(1),
-
-                () -> board.addOtherBlackPiece(blackPawn),
-                () -> assertThat(board.getPiecesSize()).isEqualTo(1),
-
-                () -> board.addOtherWhitePiece(blackBishop),
-                () -> assertThat(board.getPiecesSize()).isEqualTo(1)
-        );
-    }
 
     @Test
     void checkBlackColor() {
@@ -91,30 +53,6 @@ public class BoardTest {
     @Test
     void checkWhiteColor() {
         assertThat(Piece.createWhiteBishop().isWhite()).isTrue();
-    }
-
-
-    void verifyPawnToPawns(String color, int findPawnIndex) {
-
-        if (color.equals(Piece.WHITE))
-            verifyWhitePawnsIndex(Piece.createWhitePawn(), findPawnIndex);
-        else
-            verifyBlackPawnsIndex(Piece.createBlackPawn(), findPawnIndex);
-
-    }
-
-    void verifyWhitePawnsIndex(Piece piece, int findPawnIndex) {
-        board.addWhitePawn(piece);
-        assertThat(piece).isEqualTo(board.findWhitePawn(findPawnIndex));
-    }
-
-    void verifyBlackPawnsIndex(Piece piece, int findPawnIndex) {
-        board.addBlackPawn(piece);
-        assertThat(piece).isEqualTo(board.findBlackPawn(findPawnIndex));
-    }
-
-    void verifyPawnsSize(int actualSize) {
-        assertThat(actualSize).isEqualTo(board.getPiecesSize());
     }
 
 

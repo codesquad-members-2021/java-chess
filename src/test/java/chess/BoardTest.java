05d5a5ff073d3b9e671d4c71171e12eef502a080
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2021-02-22 21:02:48 +0900

    printBoard 메서드 수정 - 띄어쓰기 고정 및 rank와 file의 인덱스 추가

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -1,5 +1,7 @@
 package chess;
 
+import chess.pieces.Color;
+import chess.pieces.Type;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -20,18 +22,22 @@ class BoardTest {
     @DisplayName("체스판의 초기화가 정상적으로 이루어졌는지 확인한다.")
     void printBoard() {
         board.initialize();
-        String blankRank = StringUtils.addNewLine("........");
+        String blankRank = ". . . . . . . .";
         final int TOTAL_PIECES_COUNT = 32;
 
         assertAll(
                 () -> assertThat(TOTAL_PIECES_COUNT).isEqualTo(board.totalPiecesCount()),
-                () -> assertThat(StringUtils.addNewLine("RNBQKBNR") +
-                StringUtils.addNewLine("PPPPPPPP") +
-                blankRank + blankRank + blankRank + blankRank +
-                StringUtils.addNewLine("pppppppp") +
-                StringUtils.addNewLine("rnbqkbnr"))
-                .isEqualTo(board.getChessBoard())
+                () -> assertThat(StringUtils.addNewLine("R N B Q K B N R  8") +
+                        StringUtils.addNewLine("P P P P P P P P  7") +
+                        StringUtils.addNewLine(blankRank + "  6") +
+                        StringUtils.addNewLine(blankRank + "  5") +
+                        StringUtils.addNewLine(blankRank + "  4") +
+                        StringUtils.addNewLine(blankRank + "  3") +
+                        StringUtils.addNewLine("p p p p p p p p  2") +
+                        StringUtils.addNewLine("r n b q k b n r  1") +
+                        StringUtils.getNewLine() +
+                        StringUtils.addNewLine("a b c d e f g h"))
+                        .isEqualTo(board.getChessBoard())
         );
-
     }
 }

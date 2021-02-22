0b894338fbf0815f06fb2c01947d628f5195bc9e
K <PizzaCola.K@gmail.com>
2021-02-22 16:58:06 +0900

    refactor: Color Enum
    - Color enum에서 사용하지 않는 color 필드를 삭제하였습니다.

--- src/main/java/pieces/Color.java
+++ src/main/java/pieces/Color.java
@@ -1,17 +1,7 @@
 package pieces;
 
 public enum Color {
-    BLACK("black"),
-    WHITE("white"),
-    NO_COLOR("noColor");
-
-    private final String color;
-    Color(String color) {
-        this.color = color;
-    }
-
-    @Override
-    public String toString() {
-        return color;
-    }
+    BLACK,
+    WHITE,
+    NO_COLOR
 }

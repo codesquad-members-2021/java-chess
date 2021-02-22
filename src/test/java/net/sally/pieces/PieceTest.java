987051f893974c7520085e0f5f2ff28ad1108a97
Sally Oh <min27604@hotmail.com>
2021-02-22 21:19:56 +0900

    feat: Add test codes in create_piece() test method.
    - 이전 커밋에서 빼먹은 피스들의 팩토리메소드를 테스트하는 코드를 추가하였습니다.

--- src/test/java/net/sally/pieces/PieceTest.java
+++ src/test/java/net/sally/pieces/PieceTest.java
@@ -13,6 +13,17 @@ public class PieceTest {
         verifyPiece(Piece.createWhitePawn(), Piece.WHITE, Piece.WHITE_PAWN_REPRESENTATION);
         verifyPiece(Piece.createBlackPawn(), Piece.BLACK, Piece.BLACK_PAWN_REPRESENTATION);
 
+        verifyPiece(Piece.createWhiteRook(), Piece.WHITE, Piece.WHITE_ROOK_REPRESENTATION);
+        verifyPiece(Piece.createBlackRook(), Piece.BLACK , Piece.BLACK_ROOK_REPRESENTATION);
+
+        verifyPiece(Piece.createWhiteBishop(), Piece.WHITE, Piece.WHITE_BISHOP_REPRESENTATION);
+        verifyPiece(Piece.createBlackBishop(), Piece.BLACK, Piece.BLACK_BISHOP_REPRESENTATION);
+
+        verifyPiece(Piece.createWhiteKing(), Piece.WHITE, Piece.WHITE_KING_REPRESENTATION);
+        verifyPiece(Piece.createBlackKing(), Piece.BLACK, Piece.BLACK_KING_REPRESENTATION);
+
+        verifyPiece(Piece.createWhiteQueen(), Piece.WHITE, Piece.WHITE_QUEEN_REPRESENTATION);
+        verifyPiece(Piece.createBlackQueen(), Piece.BLACK, Piece.BLACK_QUEEN_REPRESENTATION);
     }
 
     private void verifyPiece(final Piece piece, final String color, final char representation) {

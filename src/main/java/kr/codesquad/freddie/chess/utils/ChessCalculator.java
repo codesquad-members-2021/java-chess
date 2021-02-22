e16a39e4ee5b4d74781f6d94ff822cade1ccf507
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-02-22 17:08:49 +0900

    Remove unnecessary imports

--- src/main/java/kr/codesquad/freddie/chess/utils/ChessCalculator.java
+++ src/main/java/kr/codesquad/freddie/chess/utils/ChessCalculator.java
@@ -2,12 +2,8 @@ package kr.codesquad.freddie.chess.utils;
 
 import kr.codesquad.freddie.chess.board.File;
 import kr.codesquad.freddie.chess.piece.Color;
-import kr.codesquad.freddie.chess.piece.Piece;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 public interface ChessCalculator {
      double calculate(List<File> files, Color color);

930b3330388968af0923ba80070742f90d7c3d20
Shion <ehdrhelr@gmail.com>
2021-02-22 21:40:52 +0900

    test : add driving test codes checking count of pieces with specific color and type

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -1,5 +1,6 @@
 package chess;
 
+import chess.pieces.Piece;
 import org.junit.jupiter.api.*;
 
 import static chess.utils.StringUtils.appendNewLine;
@@ -27,5 +28,19 @@ public class BoardTest {
                         appendNewLine("pppppppp") +
                         appendNewLine("rnbqkbnr")
         );
+    }
+
+    @Test
+    @DisplayName("인자로 받은 color와 type을 동시에 만족하는 piece의 개수를 확인한다.")
+    void count_piece_with_color_and_type() {
+        board.initialize();
+
+        assertThat(board.pieceCountOf(Piece.Color.BLACK, Piece.Type.BISHOP)).isEqualTo(2);
+        assertThat(board.pieceCountOf(Piece.Color.BLACK, Piece.Type.QUEEN)).isEqualTo(1);
+        assertThat(board.pieceCountOf(Piece.Color.BLACK, Piece.Type.PAWN)).isEqualTo(8);
+
+        assertThat(board.pieceCountOf(Piece.Color.WHITE, Piece.Type.KNIGHT)).isEqualTo(2);
+        assertThat(board.pieceCountOf(Piece.Color.WHITE, Piece.Type.KING)).isEqualTo(1);
+        assertThat(board.pieceCountOf(Piece.Color.WHITE, Piece.Type.PAWN)).isEqualTo(8);
     }
 }

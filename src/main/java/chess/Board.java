996a4d458bb7807fab90ae3f8f4a5d242dfbfb84
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2021-02-22 21:14:19 +0900

    색상과 타입 별로 체스말의 개수를 세는 메서드를 추가하여, total 개수를 세는 메서드도 수정

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -13,5 +13,33 @@ public class Board {
 
     private final List<Rank> files = new ArrayList<>(BOARD_SIZE);
 
+    public int totalPiecesCount() {
+        int count = 0;
+
+        count += countPieceByColorAndType(Color.WHITE, Type.PAWN);
+        count += countPieceByColorAndType(Color.WHITE, Type.ROOK);
+        count += countPieceByColorAndType(Color.WHITE, Type.KNIGHT);
+        count += countPieceByColorAndType(Color.WHITE, Type.BISHOP);
+        count += countPieceByColorAndType(Color.WHITE, Type.QUEEN);
+        count += countPieceByColorAndType(Color.WHITE, Type.KING);
+
+        count += countPieceByColorAndType(Color.BLACK, Type.PAWN);
+        count += countPieceByColorAndType(Color.BLACK, Type.ROOK);
+        count += countPieceByColorAndType(Color.BLACK, Type.KNIGHT);
+        count += countPieceByColorAndType(Color.BLACK, Type.BISHOP);
+        count += countPieceByColorAndType(Color.BLACK, Type.QUEEN);
+        count += countPieceByColorAndType(Color.BLACK, Type.KING);
+
+        return count;
+    }
+
+    public int countPieceByColorAndType(Color color, Type type){
+        int countPiece = 0;
+        for(Rank rank : files) {
+            countPiece += rank.getCountPieceByColorAndType(color, type);
+        }
+
+        return countPiece;
+    }
 
 }

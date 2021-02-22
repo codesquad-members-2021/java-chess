d27247a43053e02da404431b838dd32ac98a7e5f
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2021-02-22 21:07:18 +0900

    Piece를 담는 pieces 리스트 생성

--- src/main/java/chess/Rank.java
+++ src/main/java/chess/Rank.java
@@ -1,4 +1,11 @@
 package chess;
 
+import chess.pieces.Piece;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Rank {
+
+    private List<Piece> pieces = new ArrayList<>(Board.BOARD_SIZE);
 }

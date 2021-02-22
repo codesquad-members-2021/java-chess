f694a159e1c54c90b57fdff223df431af4eb388d
95degree <95degree@naver.com>
2021-02-22 17:25:43 +0900

    appendNewLine() 추가
    import static을 통해 자주 사용되는 appendNewLine을 static으로 선언하고 사용되는 로직에서 가독성을 높이기 위해 appendNewLine 메소드를 추가했습니다.

--- src/main/java/utils/StringUtils.java
+++ src/main/java/utils/StringUtils.java
@@ -2,4 +2,12 @@ package utils;
 
 public class StringUtils {
 
+    public static final String NEW_LINE = System.getProperty("line.separator");
+
+    private StringUtils() {
+    }
+
+    public static String appendNewLine(String text) {
+        return NEW_LINE + text;
+    }
 }

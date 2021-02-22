7be7fb3d151cff7e6820d346182a03746d5ec9d9
95degree <95degree@naver.com>
2021-02-22 17:55:05 +0900

    createPiece()에 다른 기물 테스트 추가
    폰이외의 다른 기물도 테스트하기 위해 createPiece()를 수정했습니다.

--- src/test/java/pieces/PieceTest.java
+++ src/test/java/pieces/PieceTest.java
@@ -13,6 +13,21 @@ class PieceTest {
     void createPiece() {
         verifyPiece(Piece.createWhitePawn(),Piece.WHITE_COLOR, Piece.WHITE_PAWN_REPRESENTATION);
         verifyPiece(Piece.craeteBlakPawn(),Piece.BLACK_COLOR, Piece.BLACK_PAWN_REPRESENTATION);
+
+        verifyPiece(Piece.createWhiteKnight(),Piece.WHITE_COLOR, Piece.WHITE_KNIGHT_REPRESENTATION);
+        verifyPiece(Piece.createBlackKnight(),Piece.WHITE_COLOR, Piece.BLACK_KNIGHT_REPRESENTATION);
+
+        verifyPiece(Piece.createWhiteRook(),Piece.WHITE_COLOR, Piece.WHITE_ROOK_REPRESENTATION);
+        verifyPiece(Piece.createBlackRook(),Piece.WHITE_COLOR, Piece.BLACK_ROOK_REPRESENTATION);
+
+        verifyPiece(Piece.createWhiteBishop(),Piece.WHITE_COLOR, Piece.WHITE_BISHOP_REPRESENTATION);
+        verifyPiece(Piece.createBlackBishop(),Piece.WHITE_COLOR, Piece.BLACK_BISHOP_REPRESENTATION);
+
+        verifyPiece(Piece.createWhiteQueen(),Piece.WHITE_COLOR, Piece.WHITE_QUEEN_REPRESENTATION);
+        verifyPiece(Piece.createBlackQueen(),Piece.WHITE_COLOR, Piece.BLACK_QUEEN_REPRESENTATION);
+
+        verifyPiece(Piece.createWhiteKing(),Piece.WHITE_COLOR, Piece.WHITE_KING_REPRESENTATION);
+        verifyPiece(Piece.createBlackKing(),Piece.WHITE_COLOR, Piece.BLACK_KING_REPRESENTATION);
     }
 
     void verifyPiece(final Piece piece,final String color, final char representation) {

482b21c8383f46abe080e4afe93de18e9fec440f
Jihye Woo <woojihye2339@gmail.com>
2021-02-22 17:52:04 +0900

    feat : Piece 클래스, 나머지 체스말에 대한 팩토리 메소드 구현
      > Pawn 이외의 체스말에 대한 팩토리 메소드 구현
        - King, Queen, Knight, Rook, Bishop 객체를 리턴하는 팩토리 메소드 구현

--- src/main/java/net/woody/pieces/Piece.java
+++ src/main/java/net/woody/pieces/Piece.java
@@ -30,4 +30,43 @@ public class Piece {
         return new Piece(Color.WHITE, Representation.PAWN.white());
     }
 
+    public static Piece createBlackKnight() {
+        return new Piece(Color.WHITE, Representation.KNGINT.black());
+    }
+
+    public static Piece createWhiteKnight() {
+        return new Piece(Color.WHITE, Representation.KNGINT.white());
+    }
+
+    public static Piece createBlackRook() {
+        return new Piece(Color.WHITE, Representation.ROOK.black());
+    }
+
+    public static Piece createWhiteRook() {
+        return new Piece(Color.WHITE, Representation.ROOK.white());
+    }
+
+    public static Piece createBlackBishop() {
+        return new Piece(Color.WHITE, Representation.BISHOP.black());
+    }
+
+    public static Piece createWhiteBishop() {
+        return new Piece(Color.WHITE, Representation.BISHOP.white());
+    }
+
+    public static Piece createBlackQueen() {
+        return new Piece(Color.WHITE, Representation.QUEEN.black());
+    }
+
+    public static Piece createWhiteQueen() {
+        return new Piece(Color.WHITE, Representation.QUEEN.white());
+    }
+
+    public static Piece createBlackKing() {
+        return new Piece(Color.WHITE, Representation.KING.black());
+    }
+
+    public static Piece createWhiteKing() {
+        return new Piece(Color.WHITE, Representation.KING.white());
+    }
 }

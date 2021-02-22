365d2bf77e1549a8bdcd750329c5abbd080a05d5
jeong-inho <derosatam76@gmail.com>
2021-02-22 17:32:11 +0900

    Refactor Boarad class Board class에서 rank에 대한 정보와 메소드들을 Rank class로 분리 했기 때문에 그에 맞춰 리팩토링

--- src/main/java/net/eno/chess/Board.java
+++ src/main/java/net/eno/chess/Board.java
@@ -1,7 +1,5 @@
 package net.eno.chess;
 
-import net.eno.pieces.Piece;
-import net.eno.pieces.PieceType;
 import static net.eno.utils.StringUtils.appendNewLine;
 
 import java.security.InvalidParameterException;
@@ -11,99 +9,51 @@ import java.util.ArrayList;
 
 public class Board {
 
-    private final List<List<Piece>> board;
+    private final List<Rank> board;
 
     public Board() {
         this.board = new ArrayList<>();
-        for (int i = 0; i < 4; i++) {
-            this.board.add(new ArrayList<>());
-        }
     }
 
-    public void addPiece(int rank, Piece piece) {
-        this.board.get(rank).add(piece);
+    private void addRank(Rank rank) {
+        this.board.add(rank);
     }
 
     public int pieceCount() {
         int pieceCount = 0;
-        for (List<Piece> rank : this.board) {
-            pieceCount += rank.size();
+        for (Rank rank : this.board) {
+            pieceCount += rank.pieceCount();
         }
         return pieceCount;
     }
 
     public void initialize() {
-        List<PieceType> arrangeWhitePieceList = arrangeWhitePiece();
-        List<PieceType> arrangeBlackPieceList = arrangeBlackPiece();
-        for (int i = 0; i < 8; i++) {
-            addPiece(0, Piece.createPiece(arrangeBlackPieceList.get(i)));
-            addPiece(1, Piece.createPiece(PieceType.BLACK_PAWN));
-            addPiece(2, Piece.createPiece(PieceType.WHITE_PAWN));
-            addPiece(3, Piece.createPiece(arrangeWhitePieceList.get(i)));
-        }
-    }
-
-    private List<PieceType> arrangeWhitePiece() {
-        List<PieceType> arrangePieceList = new ArrayList<>();
-        arrangePieceList.add(PieceType.WHITE_ROOK);
-        arrangePieceList.add(PieceType.WHITE_KNIGHT);
-        arrangePieceList.add(PieceType.WHITE_BISHOP);
-        arrangePieceList.add(PieceType.WHITE_QUEEN);
-        arrangePieceList.add(PieceType.WHITE_KING);
-        arrangePieceList.add(PieceType.WHITE_BISHOP);
-        arrangePieceList.add(PieceType.WHITE_KNIGHT);
-        arrangePieceList.add(PieceType.WHITE_ROOK);
-        return arrangePieceList;
-    }
-
-    private List<PieceType> arrangeBlackPiece() {
-        List<PieceType> arrangePieceList = new ArrayList<>();
-        arrangePieceList.add(PieceType.BLACK_ROOK);
-        arrangePieceList.add(PieceType.BLACK_KNIGHT);
-        arrangePieceList.add(PieceType.BLACK_BISHOP);
-        arrangePieceList.add(PieceType.BLACK_QUEEN);
-        arrangePieceList.add(PieceType.BLACK_KING);
-        arrangePieceList.add(PieceType.BLACK_BISHOP);
-        arrangePieceList.add(PieceType.BLACK_KNIGHT);
-        arrangePieceList.add(PieceType.BLACK_ROOK);
-        return arrangePieceList;
+        addRank(Rank.createBlackPieces());
+        addRank(Rank.createBlackPawns());
+        addRank(Rank.createBlankLine());
+        addRank(Rank.createBlankLine());
+        addRank(Rank.createBlankLine());
+        addRank(Rank.createBlankLine());
+        addRank(Rank.createWhitePawns());
+        addRank(Rank.createWhitePieces());
     }
 
     public String showBoard(String color) {
-        checkColor(color);
         StringBuilder result = new StringBuilder();
-        int num = color.equals("white") ? 0 : 3;
-
-        result.append(appendNewLine(getPiecesResult(color, num)));
-        result.append(appendNewLine(getPiecesResult(color, Math.abs(1 - num))));
-        result.append(appendNewLine("........"));
-        result.append(appendNewLine("........"));
-        result.append(appendNewLine("........"));
-        result.append(appendNewLine("........"));
-        result.append(appendNewLine(getPiecesResult(color, Math.abs(2 - num))));
-        result.append(appendNewLine(getPiecesResult(color, Math.abs(3 - num))));
-        return result.toString();
-    }
-
-    private String getPiecesResult(String color, int rank) {
-        checkColor(color);
-        StringBuilder result = new StringBuilder();
-        int num = color.equals("white") ? 0 : this.board.get(rank).size() - 1;
-
-        for (int i = 0; i < this.board.get(rank).size(); i++) {
-            result.append(findPiece(rank, Math.abs(i - num)).getRepresentation());
+        int num = reverseOrder(color);
+        for (int i = 0; i < this.board.size(); i++) {
+            Rank rank = board.get(Math.abs(i - num));
+            result.append(appendNewLine(rank.getPiecesResult(num)));
         }
         return result.toString();
     }
 
-    private Piece findPiece(int rank, int file) {
-        return this.board.get(rank).get(file);
-    }
-
-    private void checkColor(String color) {
-        if (!color.equals("white") && !color.equals("black")) {
-            throw new InvalidParameterException("Color error");
-        }
+    private int reverseOrder(String color) {
+        if (color.equals("white"))
+            return 0;
+        if (color.equals("black"))
+            return 7;
+        throw new InvalidParameterException("Color error");
     }
 
 }

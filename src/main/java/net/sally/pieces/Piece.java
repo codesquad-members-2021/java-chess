15e2b2b729379a2cde1c353d62bf21b7c42921e6
Sally Oh <min27604@hotmail.com>
2021-02-22 21:32:27 +0900

    feat: Add factory methods and constant values to Piece
    - 미션 요구사항에 따라 기존에 Pawn이었던 클래스 이름을 Piece로 변경하였습니다.
    - Piece로 클래스를 변경하면서 pawn뿐만 아니라 다른 말들도 추가되어 그 말들의 Representation 값을 상수로 추가해주었습니다.
    - 각 피스별로 흰색 말과 검정색 말을 생성하는 팩토리 메소드를 추가하였습니다.
    - 해당 피스의 색을 확인해주는 isWhite(), isBlack() 메소드를 추가하였습니다.

--- src/main/java/net/sally/pieces/Piece.java
+++ src/main/java/net/sally/pieces/Piece.java
@@ -1,23 +1,48 @@
 package net.sally.pieces;
 
-public class Pawn {
+import java.time.Period;
+
+public class Piece {
     public static final String WHITE = "white";
     public static final String BLACK = "black";
-    public static final char WHITE_REPRESENTATION = 'p';
-    public static final char BLACK_REPRESENTATION = 'P';
+    public static final char WHITE_PAWN_REPRESENTATION = 'p';
+    public static final char BLACK_PAWN_REPRESENTATION = 'P';
+    public static final char WHITE_KNIGHT_REPRESENTATION = 'n';
+    public static final char BLACK_KNIGHT_REPRESENTATION = 'N';
+    public static final char WHITE_ROOK_REPRESENTATION = 'r';
+    public static final char BLACK_ROOK_REPRESENTATION = 'R';
+    public static final char WHITE_BISHOP_REPRESENTATION = 'b';
+    public static final char BLACK_BISHOP_REPRESENTATION = 'B';
+    public static final char WHITE_QUEEN_REPRESENTATION = 'q';
+    public static final char BLACK_QUEEN_REPRESENTATION = 'Q';
+    public static final char WHITE_KING_REPRESENTATION = 'k';
+    public static final char BLACK_KING_REPRESENTATION = 'K';
 
     private final String color;
     private final char representation;
 
-    public Pawn() {
-        this.color = WHITE;
-        this.representation = WHITE_REPRESENTATION;
-    }
-
-    public Pawn(String color, char representation) {
+    private Piece(String color, char representation) {
         this.color = color;
         this.representation = representation;
     }
+
+    public static Piece createWhitePawn() { return new Piece(WHITE, WHITE_PAWN_REPRESENTATION); }
+    public static Piece createBlackPawn() { return new Piece(BLACK, BLACK_PAWN_REPRESENTATION); }
+
+    public static Piece createWhiteKnight() { return new Piece(WHITE, WHITE_KNIGHT_REPRESENTATION); }
+    public static Piece createBlackKnight() { return new Piece(BLACK, BLACK_KNIGHT_REPRESENTATION); }
+
+    public static Piece createWhiteRook() { return new Piece(WHITE, WHITE_ROOK_REPRESENTATION); }
+    public static Piece createBlackRook() { return new Piece(BLACK, BLACK_ROOK_REPRESENTATION); }
+
+    public static Piece createWhiteBishop() { return new Piece(WHITE, WHITE_BISHOP_REPRESENTATION); }
+    public static Piece createBlackBishop() { return new Piece(BLACK, BLACK_BISHOP_REPRESENTATION); }
+
+    public static Piece createWhiteQueen() { return new Piece(WHITE, WHITE_QUEEN_REPRESENTATION); }
+    public static Piece createBlackQueen() { return new Piece(BLACK, BLACK_QUEEN_REPRESENTATION); }
+
+    public static Piece createWhiteKing() { return new Piece(WHITE, WHITE_KING_REPRESENTATION); }
+    public static Piece createBlackKing() { return new Piece(BLACK, BLACK_KING_REPRESENTATION); }
 
     public String getColor() {
         return color;
@@ -27,11 +52,11 @@ public class Pawn {
         return representation;
     }
 
-    public boolean isWhite(Pawn pawn) {
-        return pawn.getColor().equals(WHITE);
+    public boolean isWhite() {
+        return this.color.equals(WHITE);
     }
 
-    public boolean isBlack(Pawn pawn) {
-        return pawn.getColor().equals(BLACK);
+    public boolean isBlack() {
+        return this.color.equals(BLACK);
     }
 }

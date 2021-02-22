3c9c07bc969200458297460a7cf6eead20729a05
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2021-02-22 21:08:47 +0900

    기본적인 get메서드와 add 메서드 추가. - 리스트를 반환하는 getPieces, 체스말을 반환하는 getPiece, 체스말 과 공백 추가하는 add

--- src/main/java/chess/Rank.java
+++ src/main/java/chess/Rank.java
@@ -1,6 +1,8 @@
 package chess;
 
+import chess.pieces.Color;
 import chess.pieces.Piece;
+import chess.pieces.Type;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,4 +10,40 @@ import java.util.List;
 public class Rank {
 
     private List<Piece> pieces = new ArrayList<>(Board.BOARD_SIZE);
+
+    public List<Piece> getPieces() {
+        return pieces;
+    }
+
+    public Piece getPiece(int rankIndex) {
+        return pieces.get(rankIndex);
+    }
+
+    public void addWhitePieces(Piece piece) {
+        if (piece.isWhite()) {
+            pieces.add(piece);
+            return;
+        }
+        getAddErrorMessage();
+    }
+
+    public void addBlackPieces(Piece piece) {
+        if (piece.isBlack()) {
+            pieces.add(piece);
+            return;
+        }
+        getAddErrorMessage();
+    }
+
+    public void addBlank(Piece piece) {
+        if (piece.getType() == Type.BLANK) {
+            pieces.add(piece);
+            return;
+        }
+        getAddErrorMessage();
+    }
+    private void getAddErrorMessage(){
+        System.out.println("알 수 없는 color의 piece입니다.");
+        System.out.println("add에 실패하였습니다.");
+    }
 }

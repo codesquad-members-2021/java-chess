837f717b2e4bca547968f8111de2b5902873a0d6
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2021-02-22 21:09:42 +0900

    기본세팅을 담당하는 initialize 메서드들 추가

--- src/main/java/chess/Rank.java
+++ src/main/java/chess/Rank.java
@@ -46,4 +46,57 @@ public class Rank {
         System.out.println("알 수 없는 color의 piece입니다.");
         System.out.println("add에 실패하였습니다.");
     }
+
+    public static Rank initializeWhitePieces() {
+        Rank rank = new Rank();
+        rank.addWhitePieces(Piece.createWhiteRook());
+        rank.addWhitePieces(Piece.createWhiteKnight());
+        rank.addWhitePieces(Piece.createWhiteBishop());
+        rank.addWhitePieces(Piece.createWhiteQueen());
+        rank.addWhitePieces(Piece.createWhiteKing());
+        rank.addWhitePieces(Piece.createWhiteBishop());
+        rank.addWhitePieces(Piece.createWhiteKnight());
+        rank.addWhitePieces(Piece.createWhiteRook());
+
+        return rank;
+    }
+
+    public static Rank initializeWhitePawns() {
+        Rank rank = new Rank();
+        for (int i = 0; i < Board.BOARD_SIZE; i++) {
+            rank.addWhitePieces(Piece.createWhitePawn());
+        }
+        return rank;
+    }
+
+    public static Rank initializeBlackPieces() {
+        Rank rank = new Rank();
+        rank.addBlackPieces(Piece.createBlackRook());
+        rank.addBlackPieces(Piece.createBlackKnight());
+        rank.addBlackPieces(Piece.createBlackBishop());
+        rank.addBlackPieces(Piece.createBlackQueen());
+        rank.addBlackPieces(Piece.createBlackKing());
+        rank.addBlackPieces(Piece.createBlackBishop());
+        rank.addBlackPieces(Piece.createBlackKnight());
+        rank.addBlackPieces(Piece.createBlackRook());
+
+        return rank;
+    }
+
+    public static Rank initializeBlackPawns() {
+        Rank rank = new Rank();
+        for (int i = 0; i < Board.BOARD_SIZE; i++) {
+            rank.addBlackPieces(Piece.createBlackPawn());
+        }
+        return rank;
+    }
+
+    public static Rank initializeBlank() {
+        Rank rank = new Rank();
+        for (int i = 0; i < Board.BOARD_SIZE; i++) {
+            rank.addBlank(Piece.createBlank());
+        }
+        return rank;
+    }
+
 }

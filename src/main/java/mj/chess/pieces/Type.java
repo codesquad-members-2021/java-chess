6215d848ead36f00de78b36eba09f1291707f090
MJbae <gently.gogo@gmail.com>
2021-02-22 17:26:23 +0900

    Refactor: 표현문자를 얻는 로직을 RepresentationMap -> Type 이동
    다음의 설계적 요구, '특정 타입과 색깔에 해당하는 표현문자를 출력하라'에 대해 새로운 객체(RepresentationMap)가 책임을 졌음
    하지만 기존에 존재하는 객체, Type이 위의 설계적 요구에 대한 책임을 충분히 질 수 있었음
    기존 객체를 이용하는 것이 코드의 재사용에 따라 성능을 향상시킬 수 있으므로 Type 객체에서 표현문자에 대한 책임을 지는 것으로 수정

--- src/main/java/mj/chess/pieces/Type.java
+++ src/main/java/mj/chess/pieces/Type.java
@@ -1,5 +1,19 @@
 package mj.chess.pieces;
 
 public enum Type {
-    BISHOP, KING, KNIGHT, PAWN, QUEEN, ROOK
+    BISHOP("b"), KING("k"), KNIGHT("n"), PAWN("p"), QUEEN("q"), ROOK("r");
+
+    private final String representation;
+
+    Type(String representation) {
+        this.representation = representation;
+    }
+
+    public String getRepresentation(Color color) {
+        if (color == Color.BLACK) {
+            return representation.toUpperCase();
+        }
+
+        return representation;
+    }
 }

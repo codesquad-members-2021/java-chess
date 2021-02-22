f4cd3fd8f8289895e219e2401e8831d41b9bb257
gisungchoi <chlrltjd159@naver.com>
2021-02-22 21:22:43 +0900

    Point class 생성
    point를 전담하는 class를 생성
    pieceValid class에 있던 point 검증 메소드를 가져와서
    한 곳에서 처리하게 했다.

---
+++ src/main/java/net/coco/pieces/Point.java
@@ -0,0 +1,29 @@
+package net.coco.pieces;
+
+public class Point {
+    private int row;
+    private int column;
+
+    public Point(String point) {
+        this.row = point.charAt(0) - 'a';
+        this.column = Character.getNumericValue(point.charAt(1)) - 1;
+        pointValid(row, column);
+    }
+
+    private void pointValid(int row, int column) {
+        if (row >= 8 || row < 0) {
+            throw new IllegalArgumentException("올바른 point가 아닙니다.");
+        }
+        if (column >= 8 || column < 0) {
+            throw new IllegalArgumentException("올바른 point가 아닙니다.");
+        }
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+}

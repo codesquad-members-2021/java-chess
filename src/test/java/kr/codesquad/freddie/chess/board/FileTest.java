da7bc3a4b879c0f3d95ff0ec0fbfe8efb009e2a9
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-02-23 00:46:21 +0900

    Remove unnecessary imports

--- src/test/java/kr/codesquad/freddie/chess/board/FileTest.java
+++ src/test/java/kr/codesquad/freddie/chess/board/FileTest.java
@@ -8,9 +8,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertAll;
 

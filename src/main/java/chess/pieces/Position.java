9ba4457198bcf0145fb4360874eb4a79b91f5c55
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2021-02-23 18:23:50 +0900

    코드 정렬작업 진행

--- src/main/java/chess/pieces/Position.java
+++ src/main/java/chess/pieces/Position.java
@@ -6,17 +6,17 @@ import java.util.List;
 public class Position {
     private int rank, file;
 
-    public Position(String position){
+    public Position(String position) {
         char rankIndex = position.charAt(0);
         char fileIndex = position.charAt(1);
 
         this.rank = rankIndex - 'a';
-        this.file = (Character.getNumericValue(fileIndex))-1;
+        this.file = (Character.getNumericValue(fileIndex)) - 1;
     }
 
-    public Position(int rank, int file){
-        this.rank = rank-1;
-        this.file = file-1;
+    public Position(int rank, int file) {
+        this.rank = rank - 1;
+        this.file = file - 1;
     }
 
     public int getRank() {
@@ -27,11 +27,11 @@ public class Position {
         return file;
     }
 
-    public List<Position> getFileNeighborPosition(){
+    public List<Position> getFileNeighborPosition() {
         List<Position> neighborPosition = new ArrayList<>();
 
-        neighborPosition.add(new Position(this.rank, this.file +1));
-        neighborPosition.add(new Position(this.rank, this.file -1));
+        neighborPosition.add(new Position(this.rank, this.file + 1));
+        neighborPosition.add(new Position(this.rank, this.file - 1));
 
         return neighborPosition;
     }

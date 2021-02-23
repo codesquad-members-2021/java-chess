f92200070b59933e3f5f0e063f8d6b499828e9b5
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-02-23 21:04:48 +0900

    Add Rank class and fix Board class 체스판의 Rank를 담당하는 Rank 클래스 생성 Board 가 8개의 Rank 객체를 List에 담는 기능을 위해 Rank 클래스 생성함

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -1,8 +1,7 @@
 package chess;
 
 import chess.pieces.Piece;
-import chess.pieces.Color;
-import chess.pieces.PieceName;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,50 +10,32 @@ import static chess.utils.StringUtils.appendNewLine;
 
 public class Board {
     private static final int BOARD_RANK = 8;
-    private List<Piece> whitePawns = new ArrayList<>(8);
-    private List<Piece> blackPawns = new ArrayList<>(8);
-    private List<Piece> whitePieces = new ArrayList<>(8);
-    private List<Piece> blackPieces = new ArrayList<>(8);
+    private List<Rank> board = new ArrayList<>(BOARD_RANK);
 
-    public void add(Piece piece) {
-        getPieceList(piece).add(piece);
-    }
-
-    private List<Piece> getPieceList(Piece piece) {
-        if (piece.getColor() == Color.WHITE && piece.getName() == PieceName.PAWN) {
-            return whitePawns;
-        }
-        if (piece.getColor() == Color.BLACK && piece.getName() == PieceName.PAWN) {
-            return blackPawns;
-        }
-        if (piece.getColor() == Color.WHITE && piece.getName() != PieceName.PAWN) {
-            return whitePieces;
-        }
-        if (piece.getColor() == Color.BLACK && piece.getName() != PieceName.PAWN) {
-            return blackPieces;
-        }
-        return null;
-    }
 
     public void initialize() {
-        initBlackPiece();
-        initBlackPawn();
-        initWhitePawn();
-        initWhitePiece();
+        initBlackPieceRank();
+        initBlackPawnRank();
+        for(int i = 0; i < 4; i++) {
+            initBlankRank();
+        }
+        initWhitePawnRank();
+        initWhitePieceRank();
     }
 
-    private void initBlackPiece() {
-        add(Piece.createBlackRook());
-        add(Piece.createBlackKnight());
-        add(Piece.createBlackBishop());
-        add(Piece.createBlackQueen());
-        add(Piece.createBlackKing());
-        add(Piece.createBlackBishop());
-        add(Piece.createBlackKnight());
-        add(Piece.createBlackRook());
+    private void initBlackPieceRank() {
+        Rank rank1 = new Rank(1);
+        rank1.add(Piece.createBlackRook());
+        rank1.add(Piece.createBlackKnight());
+        rank1.add(Piece.createBlackBishop());
+        rank1.add(Piece.createBlackQueen());
+        rank1.add(Piece.createBlackKing());
+        rank1.add(Piece.createBlackBishop());
+        rank1.add(Piece.createBlackKnight());
+        rank1.add(Piece.createBlackRook());
     }
 
-    private void initWhitePiece() {
+    private void initWhitePieceRank() {
         add(Piece.createWhiteRook());
         add(Piece.createWhiteKnight());
         add(Piece.createWhiteBishop());
@@ -65,16 +46,20 @@ public class Board {
         add(Piece.createWhiteRook());
     }
 
-    private void initWhitePawn() {
+    private void initWhitePawnRank() {
         for (int i = 0; i < BOARD_RANK; i++) {
             add(Piece.createWhitePawn());
         }
     }
 
-    private void initBlackPawn() {
+    private void initBlackPawnRank() {
         for (int i = 0; i < BOARD_RANK; i++) {
             add(Piece.createBlackPawn());
         }
+    }
+
+    private void initBlankRank(){
+        
     }
 
     public int pieceCount() {
---
+++ src/main/java/chess/Rank.java
@@ -0,0 +1,23 @@
+package chess;
+
+import chess.pieces.Piece;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Rank {
+    private List<Piece> pieceList = new ArrayList<>(8);
+    private int rankNum;
+
+    public Rank(int rankNum){
+        this.rankNum = rankNum;
+    }
+
+    public void setPieceList(){
+
+    }
+
+    public void add(Piece piece){
+        pieceList.add(piece);
+    }
+}

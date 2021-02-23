50f2085e93567936181f4f8f6bad06d0e7853869
95degree <95degree@naver.com>
2021-02-23 10:46:01 +0900

    각 기물의 REPRESENTAION 상수 추가
    각 기물에 알맞은 REPRESENATAION을 추가하기 위해 상수를 추가했습니다.

--- src/main/java/pieces/Piece.java
+++ src/main/java/pieces/Piece.java
@@ -5,6 +5,17 @@ public class Piece {
     public static final String BLACK_COLOR = "black";
     public static final char WHITE_PAWN_REPRESENTATION = 'p';
     public static final char BLACK_PAWN_REPRESENTATION = 'P';
+    public static final char WHITE_KNIGHT_REPRESENTATION = 'k';
+    public static final char BLACK_KNIGHT_REPRESENTATION = 'K';
+    public static final char WHITE_ROOK_REPRESENTATION = 'r';
+    public static final char BLACK_ROOK_REPRESENTATION = 'R';
+    public static final char WHITE_BISHOP_REPRESENTATION = 'b';
+    public static final char BLACK_BISHOP_REPRESENTATION = 'B';
+    public static final char WHITE_QUEEN_REPRESENTATION = 'q';
+    public static final char BLACK_QUEEN_REPRESENTATION = 'Q';
+    public static final char WHITE_KING_REPRESENTATION = 'k';
+    public static final char BLACK_KING_REPRESENTATION = 'K';
+
     private String color;
     private char representation;
 

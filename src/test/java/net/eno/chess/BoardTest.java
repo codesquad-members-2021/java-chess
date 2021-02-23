6b8c51a035eec656ae06b1d95ea2466f4f2bc7b7
jeong-inho <derosatam76@gmail.com>
2021-02-23 10:54:38 +0900

    Create count test in BoardTest class BoardTest에 기물의 타입 별 보드 위에 기물의 개수가 일치하는지 확인하는 count 테스트 생성

--- src/test/java/net/eno/chess/BoardTest.java
+++ src/test/java/net/eno/chess/BoardTest.java
@@ -1,5 +1,6 @@
 package net.eno.chess;
 
+import net.eno.pieces.PieceType;
 import org.junit.jupiter.api.*;
 import static org.assertj.core.api.Assertions.*;
 
@@ -16,13 +17,34 @@ public class BoardTest {
     }
 
     @Test
-    @DisplayName("초기화 된 보드의 말의 개수가 32개여야 한다.")
+    @DisplayName("초기화 된 보드의 기물의 개수가 32개여야 한다.")
     public void size() {
         assertThat(board.pieceCount()).isEqualTo(32);
     }
 
     @Test
-    @DisplayName("초기화 된 보드의 말의 배치가 일치해야 한다.")
+    @DisplayName("타입 별 기물의 개수가 일치해야 한다.")
+    public void count() {
+        for (PieceType pieceType : PieceType.values()) {
+            assertThat(board.targetPieceCount(pieceType)).isEqualTo(pieceCount(pieceType));
+        }
+    }
+
+    private int pieceCount(PieceType pieceType) {
+        switch (pieceType) {
+            case NO_PIECE :
+                return 32;
+            case WHITE_PAWN : case BLACK_PAWN :
+                return 8;
+            case WHITE_KING : case BLACK_KING : case WHITE_QUEEN : case BLACK_QUEEN :
+                return 1;
+            default :
+                return 2;
+        }
+    }
+
+    @Test
+    @DisplayName("초기화 된 보드의 기물의 배치가 일치해야 한다.")
     public void initialize() {
         String blankRank = appendNewLine("........");
         assertThat(board.showBoard("white")).isEqualTo(

2547ea7436d8e4c44dea319598f75a949c9f80e7
95degree <95degree@naver.com>
2021-02-23 11:13:41 +0900

    createPawnPiece()추가
    흑폰,백폰이 제대로 생성되는지 확인하기 위해 createPawnPiece()추가

--- src/test/java/pieces/PieceTest.java
+++ src/test/java/pieces/PieceTest.java
@@ -9,25 +9,30 @@ import static org.junit.jupiter.api.Assertions.*;
 class PieceTest {
 
     @Test
-    @DisplayName("생성된 기물들의 색과 representation 이 올바른지 확인")
-    void createPiece() {
+    @DisplayName("흑폰 백폰이 제대로 생성되는지 확인")
+    void createPawnPiece(){
         verifyPiece(Piece.createWhitePawn(),Piece.WHITE_COLOR, Piece.WHITE_PAWN_REPRESENTATION);
         verifyPiece(Piece.createBlackPawn(),Piece.BLACK_COLOR, Piece.BLACK_PAWN_REPRESENTATION);
+    }
+
+    @Test
+    @DisplayName("생성된 기물들의 색과 representation 이 올바른지 확인")
+    void createPiece() {
 
         verifyPiece(Piece.createWhiteKnight(),Piece.WHITE_COLOR, Piece.WHITE_KNIGHT_REPRESENTATION);
-        verifyPiece(Piece.createBlackKnight(),Piece.WHITE_COLOR, Piece.BLACK_KNIGHT_REPRESENTATION);
+        verifyPiece(Piece.createBlackKnight(),Piece.BLACK_COLOR, Piece.BLACK_KNIGHT_REPRESENTATION);
 
         verifyPiece(Piece.createWhiteRook(),Piece.WHITE_COLOR, Piece.WHITE_ROOK_REPRESENTATION);
-        verifyPiece(Piece.createBlackRook(),Piece.WHITE_COLOR, Piece.BLACK_ROOK_REPRESENTATION);
+        verifyPiece(Piece.createBlackRook(),Piece.BLACK_COLOR, Piece.BLACK_ROOK_REPRESENTATION);
 
         verifyPiece(Piece.createWhiteBishop(),Piece.WHITE_COLOR, Piece.WHITE_BISHOP_REPRESENTATION);
-        verifyPiece(Piece.createBlackBishop(),Piece.WHITE_COLOR, Piece.BLACK_BISHOP_REPRESENTATION);
+        verifyPiece(Piece.createBlackBishop(),Piece.BLACK_COLOR, Piece.BLACK_BISHOP_REPRESENTATION);
 
         verifyPiece(Piece.createWhiteQueen(),Piece.WHITE_COLOR, Piece.WHITE_QUEEN_REPRESENTATION);
-        verifyPiece(Piece.createBlackQueen(),Piece.WHITE_COLOR, Piece.BLACK_QUEEN_REPRESENTATION);
+        verifyPiece(Piece.createBlackQueen(),Piece.BLACK_COLOR, Piece.BLACK_QUEEN_REPRESENTATION);
 
         verifyPiece(Piece.createWhiteKing(),Piece.WHITE_COLOR, Piece.WHITE_KING_REPRESENTATION);
-        verifyPiece(Piece.createBlackKing(),Piece.WHITE_COLOR, Piece.BLACK_KING_REPRESENTATION);
+        verifyPiece(Piece.createBlackKing(),Piece.BLACK_COLOR, Piece.BLACK_KING_REPRESENTATION);
     }
 
     void verifyPiece(final Piece piece,final String color, final char representation) {

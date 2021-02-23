bceb045d1803f87b557447473d4a445699b1b259
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-02-23 17:20:16 +0900

    Add getRepresentation() method 기물의 종류에 따른 representation이 잘 지정되는지 확인한다.

--- src/test/java/chess/pieces/PieceTest.java
+++ src/test/java/chess/pieces/PieceTest.java
@@ -1,46 +1,23 @@
 package chess.pieces;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class PieceTest {
 
+    @DisplayName("기물의 representation이 잘 반환되는지 확인한다.")
     @Test
-    public void create_piece() {
-        char rep = PieceName.PAWN.getTempRepresentation();
-        verifyPiece(Piece.createWhitePawn(), Color.WHITE, rep);
-        verifyPiece(Piece.createBlackPawn(), Color.BLACK, Character.toUpperCase(rep));
-
-        rep = PieceName.KNIGHT.getTempRepresentation();
-        verifyPiece(Piece.createWhiteKnight(), Color.WHITE, rep);
-        verifyPiece(Piece.createBlackKnight(), Color.BLACK, Character.toUpperCase(rep));
-
-        rep = PieceName.BISHOP.getTempRepresentation();
-        verifyPiece(Piece.createWhiteBishop(), Color.WHITE, rep);
-        verifyPiece(Piece.createBlackBishop(), Color.BLACK, Character.toUpperCase(rep));
-
-        rep = PieceName.ROOK.getTempRepresentation();
-        verifyPiece(Piece.createWhiteRook(), Color.WHITE, rep);
-        verifyPiece(Piece.createBlackRook(), Color.BLACK, Character.toUpperCase(rep));
-
-        rep = PieceName.QUEEN.getTempRepresentation();
-        verifyPiece(Piece.createWhiteQueen(), Color.WHITE, rep);
-        verifyPiece(Piece.createBlackQueen(), Color.BLACK, Character.toUpperCase(rep));
-
-        rep = PieceName.KING.getTempRepresentation();
-        verifyPiece(Piece.createWhiteKing(), Color.WHITE, rep);
-        verifyPiece(Piece.createBlackKing(), Color.BLACK, Character.toUpperCase(rep));
+    void getRepresentationPerPiece(){
+        assertThat(Piece.Type.PAWN.getWhiteRepresentation()).isEqualTo('p');
+        assertThat(Piece.Type.PAWN.getBlackRepresentation()).isEqualTo('P');
     }
 
+    @DisplayName("기물의 color가 잘 지정되었는지 확인한다.")
     @Test
     public void check_color() {
-        assertEquals(true, Piece.createBlackPawn().isBlack());
-        assertEquals(false, Piece.createBlackPawn().isWhite());
-    }
-
-    void verifyPiece(final Piece piece, final Color color, final char representation) {
-        assertEquals(color, piece.getColor());
-        assertEquals(representation, piece.getRepresentation());
+        assertThat(Piece.createBlackPawn().isBlack()).isTrue();
+        assertThat(Piece.createBlackPawn().isWhite()).isFalse();
     }
 }

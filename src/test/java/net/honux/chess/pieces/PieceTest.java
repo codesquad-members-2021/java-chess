0195ba3f35b853649e850112a4833467c79b7a40
윤혜지 <yunhg95@gmail.com>
2021-02-23 14:04:23 +0900

    PawnTest 요구사항에 맞춰 테스트 코드 작성

--- src/test/java/net/honux/chess/pieces/PieceTest.java
+++ src/test/java/net/honux/chess/pieces/PieceTest.java
@@ -3,26 +3,27 @@ package net.honux.chess.pieces;
 import org.junit.jupiter.api.*;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PieceTest {
 
     @Test
-    @DisplayName("기본 생성자는 흰색 말과 p로 표시한다")
-    void create_default_constructor() {
-        Piece piece = new Piece();
-        assertThat(piece.getColor()).isEqualTo(Piece.WHITE_COLOR);
-        assertThat(piece.getRepresentation()).isEqualTo(Piece.WHITE_REPRESENTATION);
+    void create_piece() {
+        verifyPiece(Piece.createWhitePawn(), Piece.WHITE_COLOR, Piece.WHITE_PAWN_REPRESENTATION);
+        verifyPiece(Piece.createBlackPawn(), Piece.BLACK_COLOR, Piece.BLACK_PAWN_REPRESENTATION);
+        verifyPiece(Piece.createWhiteKnight(), Piece.WHITE_COLOR, Piece.WHITE_KNIGHT_REPRESENTATION);
+        verifyPiece(Piece.createBlackKnight(), Piece.BLACK_COLOR, Piece.BLACK_KNIGHT_REPRESENTATION);
+        verifyPiece(Piece.createWhiteRook(), Piece.WHITE_COLOR, Piece.WHITE_ROOK_REPRESENTATION);
+        verifyPiece(Piece.createBlackRook(), Piece.BLACK_COLOR, Piece.BLACK_ROOK_REPRESENTATION);
+        verifyPiece(Piece.createWhiteBishop(), Piece.WHITE_COLOR, Piece.WHITE_BISHOP_REPRESENTATION);
+        verifyPiece(Piece.createBlackBishop(), Piece.BLACK_COLOR, Piece.BLACK_BISHOP_REPRESENTATION);
+        verifyPiece(Piece.createWhiteQueen(), Piece.WHITE_COLOR, Piece.WHITE_QUEEN_REPRESENTATION);
+        verifyPiece(Piece.createBlackQueen(), Piece.BLACK_COLOR, Piece.BLACK_QUEEN_REPRESENTATION);
+        verifyPiece(Piece.createWhiteKing(), Piece.WHITE_COLOR, Piece.WHITE_KING_REPRESENTATION);
+        verifyPiece(Piece.createBlackKing(), Piece.BLACK_COLOR, Piece.BLACK_KING_REPRESENTATION);
     }
 
-    @Test
-    @DisplayName("해당 색상과 대표문자를 잘 가져오는지 확인한다")
-    void check_color_representation() {
-        verifyPawn(Piece.WHITE_COLOR, Piece.WHITE_REPRESENTATION);
-        verifyPawn(Piece.BLACK_COLOR, Piece.BLACK_REPRESENTATION);
-    }
-
-    private void verifyPawn(final String color, final char representation) {
-        Piece piece = new Piece(color, representation);
+    private void verifyPiece(final Piece piece, final String color, final char representation) {
         assertThat(piece.getColor()).isEqualTo(color);
         assertThat(piece.getRepresentation()).isEqualTo(representation);
     }

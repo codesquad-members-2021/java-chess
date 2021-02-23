3f5eda2d7a770ff89618a3fc960cdb31798811a3
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2021-02-23 16:23:12 +0900

    체스말의 위치를 얻을 수 있는 Point 클래스 추가하여 기본 메서드 작성

--- src/main/java/chess/pieces/Position.java
+++ src/main/java/chess/pieces/Position.java
@@ -1,2 +1,27 @@
-package chess.pieces;public class Position {
+package chess.pieces;
+
+public class Position {
+    private int rank, file;
+
+    public Position(String position){
+        char rankIndex = position.charAt(0);
+        char fileIndex = position.charAt(1);
+
+        this.rank = rankIndex - 'a';
+        this.file = (Character.getNumericValue(fileIndex))-1;
+    }
+
+    public Position(int rank, int file){
+        this.rank = rank-1;
+        this.file = file-1;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public int getFile() {
+        return file;
+    }
+
 }

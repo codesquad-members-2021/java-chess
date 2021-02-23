3cd3a2dc290837e35434bd444a4a8456dc77ca2e
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2021-02-23 16:24:10 +0900

    Position 추가로인한 메서드 수정

--- src/main/java/chess/Rank.java
+++ src/main/java/chess/Rank.java
@@ -2,6 +2,7 @@ package chess;
 
 import chess.pieces.Color;
 import chess.pieces.Piece;
+import chess.pieces.Position;
 import chess.pieces.Type;
 
 import java.util.ArrayList;
@@ -52,54 +53,62 @@ public class Rank {
         System.out.println("add에 실패하였습니다.");
     }
 
-    public static Rank initializeWhitePieces() {
+    public static Rank initializeWhitePieces(int fileIndex) {
         Rank rank = new Rank();
-        rank.addWhitePieces(Piece.createWhiteRook());
-        rank.addWhitePieces(Piece.createWhiteKnight());
-        rank.addWhitePieces(Piece.createWhiteBishop());
-        rank.addWhitePieces(Piece.createWhiteQueen());
-        rank.addWhitePieces(Piece.createWhiteKing());
-        rank.addWhitePieces(Piece.createWhiteBishop());
-        rank.addWhitePieces(Piece.createWhiteKnight());
-        rank.addWhitePieces(Piece.createWhiteRook());
+        rank.addWhitePieces(Piece.createWhiteRook(new Position(8, fileIndex)));
+        rank.addWhitePieces(Piece.createWhiteKnight(new Position(7, fileIndex)));
+        rank.addWhitePieces(Piece.createWhiteBishop(new Position(6, fileIndex)));
+        rank.addWhitePieces(Piece.createWhiteQueen(new Position(5, fileIndex)));
+        rank.addWhitePieces(Piece.createWhiteKing(new Position(4, fileIndex)));
+        rank.addWhitePieces(Piece.createWhiteBishop(new Position(3, fileIndex)));
+        rank.addWhitePieces(Piece.createWhiteKnight(new Position(2, fileIndex)));
+        rank.addWhitePieces(Piece.createWhiteRook(new Position(1, fileIndex)));
 
         return rank;
     }
 
-    public static Rank initializeWhitePawns() {
+    public static Rank initializeWhitePawns(int fileIndex) {
         Rank rank = new Rank();
-        for (int i = 0; i < Board.BOARD_SIZE; i++) {
-            rank.addWhitePieces(Piece.createWhitePawn());
+        for (int rankIndex = 0; rankIndex < Board.BOARD_SIZE; rankIndex++) {
+            rank.addWhitePieces(Piece.createWhitePawn(new Position(rankIndex, fileIndex)));
         }
         return rank;
     }
 
-    public static Rank initializeBlackPieces() {
+    public static Rank initializeBlackPieces(int fileIndex) {
         Rank rank = new Rank();
-        rank.addBlackPieces(Piece.createBlackRook());
-        rank.addBlackPieces(Piece.createBlackKnight());
-        rank.addBlackPieces(Piece.createBlackBishop());
-        rank.addBlackPieces(Piece.createBlackQueen());
-        rank.addBlackPieces(Piece.createBlackKing());
-        rank.addBlackPieces(Piece.createBlackBishop());
-        rank.addBlackPieces(Piece.createBlackKnight());
-        rank.addBlackPieces(Piece.createBlackRook());
+        rank.addBlackPieces(Piece.createBlackRook(new Position(8, fileIndex)));
+        rank.addBlackPieces(Piece.createBlackKnight(new Position(7, fileIndex)));
+        rank.addBlackPieces(Piece.createBlackBishop(new Position(6, fileIndex)));
+        rank.addBlackPieces(Piece.createBlackQueen(new Position(5, fileIndex)));
+        rank.addBlackPieces(Piece.createBlackKing(new Position(4, fileIndex)));
+        rank.addBlackPieces(Piece.createBlackBishop(new Position(3, fileIndex)));
+        rank.addBlackPieces(Piece.createBlackKnight(new Position(2, fileIndex)));
+        rank.addBlackPieces(Piece.createBlackRook(new Position(1, fileIndex)));
 
         return rank;
     }
 
-    public static Rank initializeBlackPawns() {
+    public static Rank initializeBlackPawns(int fileIndex) {
         Rank rank = new Rank();
-        for (int i = 0; i < Board.BOARD_SIZE; i++) {
-            rank.addBlackPieces(Piece.createBlackPawn());
+        for (int rankIndex = 0; rankIndex < Board.BOARD_SIZE; rankIndex++) {
+            rank.addBlackPieces(Piece.createBlackPawn(new Position(rankIndex, fileIndex)));
         }
         return rank;
     }
 
-    public static Rank initializeBlank() {
+    public static Rank initializeBlank(int fileIndex) {
         Rank rank = new Rank();
-        for (int i = 0; i < Board.BOARD_SIZE; i++) {
-            rank.addBlank(Piece.createBlank());
+        for (int rankIndex = 0; rankIndex < Board.BOARD_SIZE; rankIndex++) {
+            rank.addBlank(Piece.createBlank(new Position(rankIndex, fileIndex)));
+        }
+        return rank;
+    }
+
+    public static Rank initializeAllBlank(int fileIndex) {
+        Rank rank = new Rank();
+        for (int rankIndex = 0; rankIndex < Board.BOARD_SIZE; rankIndex++) {
+            rank.addBlank(Piece.createBlank(new Position(rankIndex, fileIndex)));
         }
         return rank;
     }

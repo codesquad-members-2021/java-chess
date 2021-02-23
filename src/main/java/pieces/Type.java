57f3caed9f77ecc5d55e5ebd0f26bf81ed0c089d
K <PizzaCola.K@gmail.com>
2021-02-23 11:57:47 +0900

    feat: Add point field in Type
    - Type별로 기본 점수필드인 point를 추가하였습니다.
    - getPoint()로 가져올 수 있습니다.

--- src/main/java/pieces/Type.java
+++ src/main/java/pieces/Type.java
@@ -1,18 +1,20 @@
 package pieces;
 
 public enum Type {
-    PAWN  ('p'),
-    KNIGHT('n'),
-    ROOK  ('r'),
-    BISHOP('b'),
-    QUEEN ('q'),
-    KING  ('k'),
-    NO_PIECE('.');
+    PAWN    ('p', 1.0),
+    KNIGHT  ('n', 2.5),
+    ROOK    ('r', 5.0),
+    BISHOP  ('b', 3.0),
+    QUEEN   ('q', 9.0),
+    KING    ('k', 0.0),
+    NO_PIECE('.', 0.0);
 
     private final char representation;
+    private final double point;
 
-    Type(char representation) {
+    Type(char representation, double point) {
         this.representation = representation;
+        this.point = point;
     }
 
     public char getRepresentation(Color color) {
@@ -32,5 +34,9 @@ public enum Type {
 
     public char getBlackRepresentation() {
         return Character.toUpperCase(representation);
+    }
+
+    public double getPoint() {
+        return point;
     }
 }

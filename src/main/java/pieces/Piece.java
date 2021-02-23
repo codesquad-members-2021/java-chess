c85879f993af332913fe9cbd039add7c219f777f
janeljs <jisunlim818@gmail.com>
2021-02-23 15:48:59 +0900

    Modify Piece class
    - Piece 클래스를 추상 클래스로 변경
    - 팩터리 메서드를 PieceFactory 클래스로 분리
    - enum 클래스를 attribute 패키지 안으로 분리

--- src/main/java/pieces/Piece.java
+++ src/main/java/pieces/Piece.java
@@ -1,44 +1,26 @@
 package pieces;
 
-public class Piece {
+import pieces.attribute.*;
+
+public abstract class Piece {
     private final Color color;
     private final Type type;
 
-    public enum Color {
-        WHITE, BLACK, NO_COLOR;
-    }
-
-    public enum Type {
-        PAWN('p'),
-        KNIGHT('n'),
-        ROOK('r'),
-        BISHOP('b'),
-        QUEEN('q'),
-        KING('k'),
-        NO_PIECE('.');
-
-        private final char representation;
-
-        Type(final char representation) {
-            this.representation = representation;
-        }
-
-        public char getWhiteRepresentation() {
-            return this.representation;
-        }
-
-        public char getBlackRepresentation() {
-            return Character.toUpperCase(this.representation);
-        }
-    }
-
-    private Piece(Color color, Type type) {
+    protected Piece(Color color, Type type) {
         this.color = color;
         this.type = type;
     }
 
+    public Color getColor() {
+        return color;
+    }
+
     public Type getType() {
         return type;
+    }
+
+    public char getRepresentation() {
+        return isWhite() ? type.getWhiteRepresentation() : type.getBlackRepresentation();
     }
 
     public boolean isWhite() {
@@ -48,69 +30,6 @@ public class Piece {
     public boolean isBlack() {
         return color == Color.BLACK;
     }
-
-    public char getRepresentation() {
-        return isWhite() ? type.getWhiteRepresentation() : type.getBlackRepresentation();
-    }
-
-    public static Piece createBlank() {
-        return new Piece(Color.NO_COLOR, Type.NO_PIECE);
-    }
-
-    private static Piece createWhite(Type type) {
-        return new Piece(Color.WHITE, type);
-    }
-
-    private static Piece createBlack(Type type) {
-        return new Piece(Color.BLACK, type);
-    }
-
-    public static Piece createWhitePawn() {
-        return createWhite(Type.PAWN);
-    }
-
-    public static Piece createBlackPawn() {
-        return createBlack(Type.PAWN);
-    }
-
-    public static Piece createWhiteKnight() {
-        return createWhite(Type.KNIGHT);
-    }
-
-    public static Piece createBlackKnight() {
-        return createBlack(Type.KNIGHT);
-    }
-
-    public static Piece createWhiteRook() {
-        return createWhite(Type.ROOK);
-    }
-
-    public static Piece createBlackRook() {
-        return createBlack(Type.ROOK);
-    }
-
-    public static Piece createWhiteBishop() {
-        return createWhite(Type.BISHOP);
-    }
-
-    public static Piece createBlackBishop() {
-        return createBlack(Type.BISHOP);
-    }
-
-    public static Piece createWhiteQueen() {
-        return createWhite(Type.QUEEN);
-    }
-
-    public static Piece createBlackQueen() {
-        return createBlack(Type.QUEEN);
-    }
-
-    public static Piece createWhiteKing() {
-        return createWhite(Type.KING);
-    }
-
-    public static Piece createBlackKing() {
-        return createBlack(Type.KING);
-    }
 }
+
 

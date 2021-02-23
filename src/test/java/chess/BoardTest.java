73c9566e81d8f9c28ba46eef844eb15c60a386ad
janeljs <jisunlim818@gmail.com>
2021-02-23 22:30:27 +0900

    Refactor BoardTest to improve readability
    - 테스트의 가독성 증가를 위해 리팩토링

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -3,7 +3,6 @@ package chess;
 import org.junit.jupiter.api.*;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertAll;
 import static util.StringUtil.*;
 
 class BoardTest {
@@ -18,14 +17,18 @@ class BoardTest {
     @DisplayName("체스판이 제대로 초기화되었는지 검증한다")
     void verifyInitialize() {
         board.initialize();
-        String blankRank = appendNewLine("........");
-        assertAll(() -> assertThat(board.pieceNum()).isEqualTo(32),
-                () -> assertThat(board.getResultToPrint())
-                        .isEqualTo(appendNewLine("RNBQKBNR") +
-                                appendNewLine("PPPPPPPP") +
-                                blankRank + blankRank + blankRank + blankRank +
-                                appendNewLine("pppppppp") +
-                                appendNewLine("rnbqkbnr")));
+        String result = new StringBuilder()
+                .append("R N B Q K B N R   8").append(NEWLINE)
+                .append("P P P P P P P P   7").append(NEWLINE)
+                .append(". . . . . . . .   6").append(NEWLINE)
+                .append(". . . . . . . .   5").append(NEWLINE)
+                .append(". . . . . . . .   4").append(NEWLINE)
+                .append(". . . . . . . .   3").append(NEWLINE)
+                .append("3 . . . . . . . . ").append(NEWLINE)
+                .append("p p p p p p p p   2").append(NEWLINE)
+                .append("r n b q k b n r   1").toString();
+        assertThat(board.getResultToPrint())
+                .isEqualTo(result);
     }
 }
 

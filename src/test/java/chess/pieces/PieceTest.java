1e3bb4ac6a4a9998ac542511de780a2a74bb98c4
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-02-23 17:47:48 +0900

    Fix createPiece(), verifyPiece() method 기존에 create_piece() 메소드와 verify_piece() 메소드를 수정함 Piece 클래스의 팩토리메소드로 기물을 생성하고 기물의 타입과 색깔이 올바르게 지정되었는지 확인하는 역할을 한다.

--- src/test/java/chess/pieces/PieceTest.java
+++ src/test/java/chess/pieces/PieceTest.java
@@ -7,17 +7,29 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class PieceTest {
 
-    @DisplayName("기물의 representation이 잘 반환되는지 확인한다.")
+    @DisplayName("기물의 representation이 올바르게 지정되었는지 확인한다.")
     @Test
-    void getRepresentationPerPiece(){
+    void getRepresentationPerPiece() {
         assertThat(Piece.Type.PAWN.getWhiteRepresentation()).isEqualTo('p');
         assertThat(Piece.Type.PAWN.getBlackRepresentation()).isEqualTo('P');
     }
 
-    @DisplayName("기물의 color가 잘 지정되었는지 확인한다.")
+    @DisplayName("기물의 타입과 color가 올바르게 지정되었는지 확인한다.")
     @Test
-    public void check_color() {
-        assertThat(Piece.createBlackPawn().isBlack()).isTrue();
-        assertThat(Piece.createBlackPawn().isWhite()).isFalse();
+    void createPiece() {
+        verifyPiece(Piece.createWhitePawn(), Piece.createBlackPawn(), Piece.Type.PAWN);
+        verifyPiece(Piece.createWhiteKnight(), Piece.createBlackKnight(), Piece.Type.KNIGHT);
+        verifyPiece(Piece.createWhiteRook(), Piece.createBlackRook(), Piece.Type.ROOK);
+        verifyPiece(Piece.createWhiteBishop(), Piece.createBlackBishop(), Piece.Type.BISHOP);
+        verifyPiece(Piece.createWhiteQueen(), Piece.createBlackQueen(), Piece.Type.QUEEN);
+        verifyPiece(Piece.createWhiteKing(), Piece.createBlackKing(), Piece.Type.KING);
+    }
+
+    void verifyPiece(Piece whitePiece, Piece blackPiece, Piece.Type type) {
+        assertThat(whitePiece.isWhite()).isTrue();
+        assertThat(whitePiece.getType()).isEqualTo(type);
+
+        assertThat(blackPiece.isBlack()).isTrue();
+        assertThat(blackPiece.getType()).isEqualTo(type);
     }
 }

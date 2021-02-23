a08723f1c4a516af594517ca8fa9935fc863476e
K <PizzaCola.K@gmail.com>
2021-02-23 17:23:08 +0900

    feat: Reorder Type enum
    - Type enum의 순서를 point 순으로 수정하였습니다.

--- src/main/java/pieces/Type.java
+++ src/main/java/pieces/Type.java
@@ -1,13 +1,13 @@
 package pieces;
 
 public enum Type {
+    NO_PIECE('.', 0.0),
+    KING    ('k', 0.0),
     PAWN    ('p', 1.0),
     KNIGHT  ('n', 2.5),
-    ROOK    ('r', 5.0),
     BISHOP  ('b', 3.0),
-    QUEEN   ('q', 9.0),
-    KING    ('k', 0.0),
-    NO_PIECE('.', 0.0);
+    ROOK    ('r', 5.0),
+    QUEEN   ('q', 9.0);
 
     private final char representation;
     private final double point;

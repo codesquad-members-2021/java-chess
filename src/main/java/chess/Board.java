de72c0977129d8712954573a4e10d51fecebc1cf
MyungWanPark <mywanpark@gmail.com>
2021-02-23 19:31:20 +0900

    feat: Change initialize() in Board.java
    Change initialize() in Board.java to pass the test of pieceCount()

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -14,29 +14,49 @@ public class Board {
 
     public static final int PAWN_NUMBER = 8;
 
-    private void whitePawnAdd(Piece whitePawn) {
+    private void addWhitePawn(Piece whitePawn) {
         whitePawns.add(whitePawn);
     }
 
-    private void blackPawnAdd(Piece blackPawn) {
+    private void addBlackPawn(Piece blackPawn) {
         blackPawns.add(blackPawn);
     }
 
     public void initialize() {
         for(int i = 0; i < PAWN_NUMBER; i++){
-            whitePawnAdd(Piece.getWhitePawnInstance());
-            blackPawnAdd(Piece.createBlackPawnInstance());
+            addWhitePawn(Piece.createWhitePawnInstance());
+            addBlackPawn(Piece.createBlackPawnInstance());
         }
+            addWhitePieces();
+            addBlackPieces();
+    }
 
-        for(int i = 0; i < UNIT_NUMBER_WITHOUT_PAWN; i++){
+    private void addWhitePieces() {
+        whitePieces.add(Piece.createWhiteRookInstance());
+        whitePieces.add(Piece.createWhiteKnightInstance());
+        whitePieces.add(Piece.createWhiteBishopInstance());
+        whitePieces.add(Piece.createWhiteKingInstance());
+        whitePieces.add(Piece.createWhiteQueenInstance());
+        whitePieces.add(Piece.createWhiteBishopInstance());
+        whitePieces.add(Piece.createWhiteKnightInstance());
+        whitePieces.add(Piece.createWhiteRookInstance());
+    }
 
-        }
+    private void addBlackPieces() {
+        blackPieces.add(Piece.createBlackRookInstance());
+        blackPieces.add(Piece.createBlackKnightInstance());
+        blackPieces.add(Piece.createBlackBishopInstance());
+        blackPieces.add(Piece.createBlackKingInstance());
+        blackPieces.add(Piece.createBlackQueenInstance());
+        blackPieces.add(Piece.createBlackBishopInstance());
+        blackPieces.add(Piece.createBlackKnightInstance());
+        blackPieces.add(Piece.createBlackRookInstance());
     }
 
     String bringWhitePawnsRepresentation() {
         StringBuilder sb = new StringBuilder();
 
-        for(int i= 0; i < whitePawnSize(); i++){
+        for(int i = 0; i < whitePawns.size(); i++){
             sb.append(Piece.WHITE_PAWN_REPRESENTATION);
         }
         return sb.toString();
@@ -45,7 +65,7 @@ public class Board {
     String bringBlackPawnsRepresentation() {
         StringBuilder sb = new StringBuilder();
 
-        for(int i= 0; i < blackPawnSize(); i++){
+        for(int i= 0; i < blackPawns.size(); i++){
             sb.append(Piece.BLACK_PAWN_REPRESENTATION);
         }
         return sb.toString();
@@ -66,7 +86,11 @@ public class Board {
     }
 
     private int sizeOfPieces() {
-        return 0;
+        return whitePawns.size() +
+                whitePieces.size() +
+                blackPawns.size() +
+                blackPieces.size();
+
     }
 
     public String getBoard() {

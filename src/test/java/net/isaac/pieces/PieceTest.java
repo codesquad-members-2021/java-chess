94b63db7259e2966be395e3bf002d0a78820e19c
정이삭 <isaac56@naver.com>
2021-02-23 15:26:49 +0900

    Separate "create_piece" test into individual tests (PieceTest  class)
    create_piece 테스트를 각 체스말 별 테스트로 분리하였습니다.

--- src/test/java/net/isaac/pieces/PieceTest.java
+++ src/test/java/net/isaac/pieces/PieceTest.java
@@ -7,21 +7,63 @@ import static org.assertj.core.api.Assertions.*;
 
 class PieceTest {
     @Test
-    @DisplayName("체스말 생성 테스트")
-    void create_piece() {
+    @DisplayName("Pawn 생성 테스트")
+    void create_Pawn() {
         assertAll(
                 () -> verifyPiece(Piece.createPawn(Piece.Color.WHITE), Piece.Color.WHITE, Piece.PAWN_REPRESENTATION),
-                () -> verifyPiece(Piece.createPawn(Piece.Color.BLACK), Piece.Color.BLACK, Character.toUpperCase(Piece.PAWN_REPRESENTATION)),
+                () -> verifyPiece(Piece.createPawn(Piece.Color.BLACK), Piece.Color.BLACK, Character.toUpperCase(Piece.PAWN_REPRESENTATION))
+        );
+    }
+
+    @Test
+    @DisplayName("King 생성 테스트")
+    void create_King() {
+        assertAll(
                 () -> verifyPiece(Piece.createKing(Piece.Color.WHITE), Piece.Color.WHITE, Piece.KING_REPRESENTATION),
-                () -> verifyPiece(Piece.createKing(Piece.Color.BLACK), Piece.Color.BLACK, Character.toUpperCase(Piece.KING_REPRESENTATION)),
+                () -> verifyPiece(Piece.createKing(Piece.Color.BLACK), Piece.Color.BLACK, Character.toUpperCase(Piece.KING_REPRESENTATION))
+        );
+    }
+
+    @Test
+    @DisplayName("Queen 생성 테스트")
+    void create_Queen() {
+        assertAll(
                 () -> verifyPiece(Piece.createQueen(Piece.Color.WHITE), Piece.Color.WHITE, Piece.QUEEN_REPRESENTATION),
-                () -> verifyPiece(Piece.createQueen(Piece.Color.BLACK), Piece.Color.BLACK, Character.toUpperCase(Piece.QUEEN_REPRESENTATION)),
+                () -> verifyPiece(Piece.createQueen(Piece.Color.BLACK), Piece.Color.BLACK, Character.toUpperCase(Piece.QUEEN_REPRESENTATION))
+        );
+    }
+
+    @Test
+    @DisplayName("Rook 생성 테스트")
+    void create_Rook() {
+        assertAll(
                 () -> verifyPiece(Piece.createRook(Piece.Color.WHITE), Piece.Color.WHITE, Piece.ROOK_REPRESENTATION),
-                () -> verifyPiece(Piece.createRook(Piece.Color.BLACK), Piece.Color.BLACK, Character.toUpperCase(Piece.ROOK_REPRESENTATION)),
-                () -> verifyPiece(Piece.createBishop(Piece.Color.WHITE), Piece.Color.WHITE, Piece.BISHOP_REPRESENTATION),
-                () -> verifyPiece(Piece.createBishop(Piece.Color.BLACK), Piece.Color.BLACK, Character.toUpperCase(Piece.BISHOP_REPRESENTATION)),
+                () -> verifyPiece(Piece.createRook(Piece.Color.BLACK), Piece.Color.BLACK, Character.toUpperCase(Piece.ROOK_REPRESENTATION))
+        );
+    }
+
+    @Test
+    @DisplayName("Knight 생성 테스트")
+    void create_Knight() {
+        assertAll(
                 () -> verifyPiece(Piece.createKnight(Piece.Color.WHITE), Piece.Color.WHITE, Piece.KNIGHT_REPRESENTATION),
                 () -> verifyPiece(Piece.createKnight(Piece.Color.BLACK), Piece.Color.BLACK, Character.toUpperCase(Piece.KNIGHT_REPRESENTATION))
+        );
+    }
+
+    @Test
+    @DisplayName("Bishop 생성 테스트")
+    void create_Bishop() {
+        assertAll(
+                () -> verifyPiece(Piece.createBishop(Piece.Color.WHITE), Piece.Color.WHITE, Piece.BISHOP_REPRESENTATION),
+                () -> verifyPiece(Piece.createBishop(Piece.Color.BLACK), Piece.Color.BLACK, Character.toUpperCase(Piece.BISHOP_REPRESENTATION))
+        );
+    }
+
+    void verifyPiece(final Piece piece, final Piece.Color color, char representation) {
+        assertAll(
+                () -> assertThat(piece.getColor()).isEqualTo(color),
+                () -> assertThat(piece.getRepresentation()).isEqualTo(representation)
         );
     }
 
@@ -36,13 +78,6 @@ class PieceTest {
                 () -> assertThat(white.isBlack()).isEqualTo(false),
                 () -> assertThat(black.isWhite()).isEqualTo(false),
                 () -> assertThat(black.isBlack()).isEqualTo(true)
-        );
-    }
-
-    void verifyPiece(final Piece piece, final Piece.Color color, char representation) {
-        assertAll(
-                () -> assertThat(piece.getColor()).isEqualTo(color),
-                () -> assertThat(piece.getRepresentation()).isEqualTo(representation)
         );
     }
 }

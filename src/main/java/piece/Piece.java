364f6076996557d3f5280e75cacd117f98ceab76
janeljs <jisunlim818@gmail.com>
2021-02-24 12:25:02 +0900

    Modify Piece
    - getPoint() 메서드 추가
    - equals(obj), hashCode(), compareTo(piece), toString() 메서드 오버라이딩

--- src/main/java/piece/Piece.java
+++ src/main/java/piece/Piece.java
@@ -2,7 +2,10 @@ package piece;
 
 import piece.attribute.*;
 
-public abstract class Piece {
+import java.util.Comparator;
+import java.util.Objects;
+
+public abstract class Piece implements Comparable<Piece> {
     private final Color color;
     private final Type type;
 
@@ -30,6 +33,34 @@ public abstract class Piece {
     public boolean isBlack() {
         return color == Color.BLACK;
     }
+
+    public double getPoint() {
+        return type.getDefaultPoint();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Piece) {
+            return color == ((Piece) obj).color && type == ((Piece) obj).type;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(color, type);
+    }
+
+    @Override
+    public int compareTo(Piece piece) {
+        return (int) (piece.getPoint() - this.getPoint());
+    }
+
+    @Override
+    public String toString() {
+        return getRepresentation() + "";
+    }
 }
+
 
 

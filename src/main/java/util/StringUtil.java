a2e383dc7f467fb8b656835138016836e62c782a
janeljs <jisunlim818@gmail.com>
2021-02-24 12:27:39 +0900

    Remove appendNewLine(text) method in StringUtil
    - 사용하지 않는 메서드 제거

--- src/main/java/util/StringUtil.java
+++ src/main/java/util/StringUtil.java
@@ -5,12 +5,5 @@ public class StringUtil {
 
     private StringUtil() {
     }
-
-    public static String appendNewLine(String text) {
-        if (text.length() > 8) {
-            return text.substring(0, 8) + NEWLINE + text.substring(8) + NEWLINE;
-        }
-        return text + NEWLINE;
-    }
 }
 

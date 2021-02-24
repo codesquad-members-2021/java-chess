1244b743dcc87af1e73264e49a44b7323e322f23
Haina <bibi6666667@kakao.com>
2021-02-24 15:04:19 +0900

    [step4] 각 Piece별 흰색, 검은색 말 생성
    Piece클래스에 기존 Pawn에 더해 Knight, Rook, Bishop, Queen, King
    을 위한 클래스 변수와 팩토리 메서드를 만들었습니다.

--- src/main/java/pieces/Piece.java
+++ src/main/java/pieces/Piece.java
@@ -11,6 +11,16 @@ public class Piece {
     public final static String KING = "king";
     public final static String WHITE_PAWN_REPRESENTATION = "p";
     public final static String BLACK_PAWN_REPRESENTATION = "P";
+    public final static String WHITE_KNIGHT_REPRESENTATION = "n";
+    public final static String BLACK_KNIGHT_REPRESENTATION = "N";
+    public final static String WHITE_ROOK_REPRESENTATION = "r";
+    public final static String BLACK_ROOK_REPRESENTATION = "R";
+    public final static String WHITE_BISHOP_REPRESENTATION = "b";
+    public final static String BLACK_BISHOP_REPRESENTATION = "B";
+    public final static String WHITE_QUEEN_REPRESENTATION = "q";
+    public final static String BLACK_QUEEN_REPRESENTATION = "Q";
+    public final static String WHITE_KING_REPRESENTATION = "k";
+    public final static String BLACK_KING_REPRESENTATION = "K";
     private final String representation;
     private final String color;
     private final String name;
@@ -35,5 +45,45 @@ public class Piece {
 
     public static Piece createBlackPawn() {
         return new Piece(PAWN, BLACK, BLACK_PAWN_REPRESENTATION);
+    }
+
+    public static Piece createWhiteKnight() {
+        return new Piece(KNIGHT, WHITE, WHITE_KNIGHT_REPRESENTATION);
+    }
+
+    public static Piece createBlackKnight() {
+        return new Piece(KNIGHT, BLACK, BLACK_KNIGHT_REPRESENTATION);
+    }
+
+    public static Piece createWhiteRook() {
+        return new Piece(ROOK, WHITE, WHITE_ROOK_REPRESENTATION);
+    }
+
+    public static Piece createBlackRook() {
+        return new Piece(ROOK, BLACK, BLACK_ROOK_REPRESENTATION);
+    }
+
+    public static Piece createWhiteBishop() {
+        return new Piece(BISHOP, WHITE, WHITE_BISHOP_REPRESENTATION);
+    }
+
+    public static Piece createBlackBishop() {
+        return new Piece(BISHOP, BLACK, BLACK_BISHOP_REPRESENTATION);
+    }
+
+    public static Piece createWhiteQueen() {
+        return new Piece(QUEEN, WHITE, WHITE_QUEEN_REPRESENTATION);
+    }
+
+    public static Piece createBlackQueen() {
+        return new Piece(QUEEN, BLACK, BLACK_QUEEN_REPRESENTATION);
+    }
+
+    public static Piece createWhiteKing() {
+        return new Piece(KING, WHITE, WHITE_KING_REPRESENTATION);
+    }
+
+    public static Piece createBlackKing() {
+        return new Piece(KING, BLACK, BLACK_KING_REPRESENTATION);
     }
 }

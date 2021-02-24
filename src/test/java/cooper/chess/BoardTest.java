ccb263cb64d8b5de56c88e6cbe777af0582629b0
pbg0205 <pbg0205@naver.com>
2021-02-24 18:08:03 +0900

    test : getPieceCount 테스트 메서드 추가
    - 색생 및 기물 종류에 따른 기물의 갯수를 확인하는 테스트 코드를
    작성하였습니다.

--- src/test/java/cooper/chess/BoardTest.java
+++ src/test/java/cooper/chess/BoardTest.java
@@ -2,6 +2,8 @@ package cooper.chess;
 
 import cooper.chess.piece.Color;
 import cooper.chess.piece.Piece;
+import cooper.chess.piece.Type;
+import org.assertj.core.api.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -44,5 +46,29 @@ class BoardTest {
     @DisplayName("board의 상태를 확인한다.")
     public void getBoardStatusTest() {
         System.out.println(board.showBoard());
+    }
+
+    @Test
+    @DisplayName("원하는 색상과 기물의 수를 확인할 수 있다")
+    public void getPieCountTest() {
+        assertAll(
+                () -> verifyPieceCount(Color.WHITE, Type.PAWN, 8),
+                () -> verifyPieceCount(Color.WHITE, Type.KNIGHT, 2),
+                () -> verifyPieceCount(Color.WHITE, Type.ROOK, 2),
+                () -> verifyPieceCount(Color.WHITE, Type.BISHOP, 2),
+                () -> verifyPieceCount(Color.WHITE, Type.QUEEN, 1),
+                () -> verifyPieceCount(Color.WHITE, Type.KING, 1),
+
+                () -> verifyPieceCount(Color.BLACK, Type.PAWN, 8),
+                () -> verifyPieceCount(Color.BLACK, Type.KNIGHT, 2),
+                () -> verifyPieceCount(Color.BLACK, Type.ROOK, 2),
+                () -> verifyPieceCount(Color.BLACK, Type.BISHOP, 2),
+                () -> verifyPieceCount(Color.BLACK, Type.QUEEN, 1),
+                () -> verifyPieceCount(Color.BLACK, Type.KING, 1)
+        );
+    }
+
+    void verifyPieceCount(final Color color, final Type type, final int count) {
+        assertEquals(board.getPieceCount(color, type), count);
     }
 }

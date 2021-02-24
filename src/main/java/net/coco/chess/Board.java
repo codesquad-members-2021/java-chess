1c3bd89bef92c3774a608b1e8a252c8045455014
gisungchoi <chlrltjd159@naver.com>
2021-02-24 17:52:42 +0900

    findPieceCount() 메소드에 파라미터 추가
    색상을 확인해야 하기에 추가했다.

--- src/main/java/net/coco/chess/Board.java
+++ src/main/java/net/coco/chess/Board.java
@@ -37,7 +37,7 @@ public class Board {
     public int getPieceCount(Color color, PieceType pieceType) {
         int count = 0;
         for (Rank rank : ranks) {
-            count += rank.findPieceCount(pieceType);
+            count += rank.findPieceCount(pieceType, color);
         }
         return count;
     }

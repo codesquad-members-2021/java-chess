4869181df6e81d6a66ca444b243c9eec0a85bcbc
janeljs <jisunlim818@gmail.com>
2021-02-24 12:56:29 +0900

    Refactor ChessGame
    - try-with-resources문 추가
    - main 메서드 내 로직을 더 간단하게 리팩토링

--- src/main/java/chess/ChessGame.java
+++ src/main/java/chess/ChessGame.java
@@ -1,30 +1,40 @@
 package chess;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class ChessGame {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("시작: start, 종료: end");
+    private static boolean gameOver = false;
 
-        String command = "";
-        while (!command.equals("end")) {
-            System.out.print("> ");
-            command = sc.nextLine();
-            if (command.equals("start")) {
-                start();
+    public static void main(String[] args) {
+        System.out.println("시작: start, 종료: end");
+        try (Scanner sc = new Scanner(System.in)) {
+
+            while (!gameOver) {
+                System.out.print("> ");
+                String command = sc.nextLine().toLowerCase(Locale.ROOT);
+                if (command.equals("start")) {
+                    start();
+                    continue;
+                }
+                if (command.equals("end")) {
+                    end();
+                    continue;
+                }
+                System.out.println("명령어를 다시 입력해주세요.");
             }
         }
-
-        System.out.println("체스게임이 끝났습니다.");
-        sc.close();
     }
 
     private static void start() {
         System.out.println("체스 게임을 시작합니다.");
         Board board = new Board();
-        board.initialize();
         System.out.println(board.getResultToPrint());
+    }
+
+    private static void end() {
+        gameOver = true;
+        System.out.println("체스게임이 끝났습니다.");
     }
 }
 

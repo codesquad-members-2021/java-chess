026cf550142be79cc45dad18ec30e12a14a47735
rla36 <bhmarsd@naver.com>
2021-02-24 11:50:39 +0900

    test: 수정된 Board의 메서드 검증
    boardLayoutToString이 rank의 representation을 리턴하는지,
    boardPieceSize가 board의 모든 piece개수를 리턴하는지 검증하는 테스트를
    작성했고, 쓰이지 않는 테스트는 지웠습니다.

--- src/test/java/net/jung/chess/BoardTest.java
+++ src/test/java/net/jung/chess/BoardTest.java
@@ -11,48 +11,13 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 class BoardTest {
 
     private Board board;
-    private Piece whitePawn;
-    private Piece blackPawn;
 
     @BeforeEach
     void setUp() {
         board = new Board();
-        whitePawn = Piece.createWhitePawn();
-        blackPawn = Piece.createBlackPawn();
 
     }
 
-    @Test
-    @DisplayName("보드를 생성하고 흰색 검은색 폰을 추가한 후 size, findPawn 메서드 검증한다.")
-    void createBoardAddPawns() throws Exception {
-
-        assertAll(
-                () -> {
-                    board.addWhitePiece(whitePawn);
-                    assertAll("흰색 폰을 보드에 올린 후 size, findPawn메서드 검증한다.",
-                            () -> assertThat(board.size()).isEqualTo(1),
-                            () -> assertThat(board.findWhitePiece(0)).isEqualTo(whitePawn));
-                },
-                () -> {
-                    board.addBlackPiece(blackPawn);
-                    assertAll("검은색 폰을 보드에 올린 후 size, findPawn메서드 검증한다.",
-                            () -> assertThat(board.size()).isEqualTo(2),
-                            () -> assertThat(board.findBlackPiece(0)).isEqualTo(blackPawn));
-                }
-        );
-    }
-
-
-    @Test
-    @DisplayName("addWhitePawn(Piece pawn)의 매개변수로 blackPawn을 넘기면 whitePawnList에 더해지지 않고 반대도 마찬가지이다.")
-    void addBlackPawnToWhitePawnList() {
-        board.addWhitePiece(blackPawn);
-        board.addBlackPiece(whitePawn);
-        assertAll(
-                () -> assertThat(board.whitePieceListSize()).isEqualTo(0),
-                () -> assertThat(board.blackPieceListSize()).isEqualTo(0)
-        );
-    }
 
     @Test
     @DisplayName("initialize()는 32개 모든 말을 체스판 위에 올린다.")
@@ -60,7 +25,7 @@ class BoardTest {
         int maxNumOfPieces = 32;
         board.initialize();
         assertAll(
-                () -> assertThat(board.size()).isEqualTo(maxNumOfPieces),
+                () -> assertThat(board.boardPieceSize()).isEqualTo(maxNumOfPieces),
                 () -> assertThat(board.boardLayoutToString()).isEqualTo(
                     appendNewLine("RNBQKBNR")
                     +appendNewLine("PPPPPPPP")

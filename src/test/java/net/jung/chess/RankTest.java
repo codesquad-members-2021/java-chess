1a55dbfbf84def300e109c1921724518add45d52
rla36 <bhmarsd@naver.com>
2021-02-24 11:35:25 +0900

    test: getRankRepresentation을 검증하는 테스트 작성
    getRankRepresentation을 검증하는 테스트를 작성했습니다.
    blankRank의 경우 공백표시인 ********가 나오는지 확인했습니다.

--- src/test/java/net/jung/chess/RankTest.java
+++ src/test/java/net/jung/chess/RankTest.java
@@ -10,27 +10,44 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 class RankTest {
 
     Rank rank;
+    Rank whitePawnRank;
+    Rank blackPawnRank;
+    Rank whitePieceRank;
+    Rank blackPieceRank;
+    Rank blankRank;
 
     @BeforeEach
     void setter() {
         rank = new Rank();
+        whitePawnRank = rank.initializeWhitePawnRank();
+        blackPawnRank = rank.initializeBlackPawnRank();
+        whitePieceRank = rank.initializeWhitePieceRank();
+        blackPieceRank = rank.initializeBlackPieceRank();
+        blankRank = rank.initializeBlankRank();
+
     }
 
     @Test
     @DisplayName("initialize된 Rank 의 size는 8이어야 한다.")
     void checkRankSizeAfterInitialize() {
-        Rank whitePawnRank = rank.initializeWhitePawnRank();
-        Rank blackPawnRank = rank.initializeBlackPawnRank();
-        Rank whitePieceRank = rank.initializeWhitePieceRank();
-        Rank blackPieceRank = rank.initializeBlackPieceRank();
-        Rank blankRank = rank.initializeBlankRank();
-
         assertAll(
-                () -> assertThat(whitePawnRank.size()).isEqualTo(Rank.MAX_PIECES_IN_RANK),
-                () -> assertThat(blackPawnRank.size()).isEqualTo(Rank.MAX_PIECES_IN_RANK),
-                () -> assertThat(whitePieceRank.size()).isEqualTo(Rank.MAX_PIECES_IN_RANK),
-                () -> assertThat(blackPieceRank.size()).isEqualTo(Rank.MAX_PIECES_IN_RANK),
-                () -> assertThat(blankRank.size()).isEqualTo(0)
+                () -> assertThat(whitePawnRank.rankPieceSize()).isEqualTo(Rank.MAX_PIECES_IN_RANK),
+                () -> assertThat(blackPawnRank.rankPieceSize()).isEqualTo(Rank.MAX_PIECES_IN_RANK),
+                () -> assertThat(whitePieceRank.rankPieceSize()).isEqualTo(Rank.MAX_PIECES_IN_RANK),
+                () -> assertThat(blackPieceRank.rankPieceSize()).isEqualTo(Rank.MAX_PIECES_IN_RANK),
+                () -> assertThat(blankRank.rankPieceSize()).isEqualTo(0)
+        );
+    }
+
+    @Test
+    @DisplayName("getRepresentation으로 리턴되는 스트링을 검증한다.")
+    void checkGetRepresentation() {
+        assertAll(
+                () -> assertThat(whitePawnRank.getRankRepresentation()).isEqualTo("pppppppp"),
+                () -> assertThat(blackPawnRank.getRankRepresentation()).isEqualTo("PPPPPPPP"),
+                () -> assertThat(whitePieceRank.getRankRepresentation()).isEqualTo("rnbqkbnr"),
+                () -> assertThat(blackPieceRank.getRankRepresentation()).isEqualTo("RNBQKBNR"),
+                () -> assertThat(blankRank.getRankRepresentation()).isEqualTo("********")
         );
     }
 }

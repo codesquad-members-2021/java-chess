e02c2d4c02d0dc19854fbb987aa178a44c2911dd
DHKim <ultraraptor@naver.com>
2021-02-24 15:58:45 +0900

    [개선] Try-with-resources 사용으로 자원의 해제를 쉽게
    기존 : Scanner 변수를 프로그램 마지막에 해제
    
    수정 : try-catch 문으로 자원반납을 보다 더 자동적으로

--- src/main/java/net/Dong/chess/Game.java
+++ src/main/java/net/Dong/chess/Game.java
@@ -6,34 +6,35 @@ public class Game {
     private static final String PROMPT = "J-chess>> ";
     private static final String HELP =
             " Command  |  description\r\n" +
-            "-----------------------------\r\n"+
+            "-----------------------------\r\n" +
             " start   :  Java chess start\r\n" +
             " end     :  Java chess Terminate\r\n" +
             "-----------------------------";
 
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("==== Java chess started ===");
         System.out.println(HELP);
 
 
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                System.out.print(PROMPT);
+                String cmd = scanner.nextLine();
+                if (cmd.equals("start")) {
+                    Board board = new Board();
+                    board.initialize();
+                    System.out.println(board.print());
+                }
 
-        while (true) {
-            System.out.print(PROMPT);
-            String cmd = scanner.nextLine();
-            if (cmd.equals("start")) {
-                Board board = new Board();
-                board.initialize();
-                System.out.println(board.print());
+                if (cmd.equals("end")) {
+                    break;
+                }
             }
-
-            if (cmd.equals("end")) {
-                break;
-            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
         System.out.println("==== Java chess ended ===");
-        scanner.close();
     }
 }

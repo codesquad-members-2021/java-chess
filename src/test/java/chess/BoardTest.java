57b3e3bec3a48dd66d49ec124e89a9f5379480b2
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-02-24 11:13:36 +0900

    Add count() test method 기물의 개수를 올바르게 반환하는지 확인하는 테스트 코드 추가

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -1,5 +1,6 @@
 package chess;
 
+import chess.pieces.Piece;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,20 +12,32 @@ class BoardTest {
     private Board board;
 
     @BeforeEach
-    public void setUP() {
+    void setUP() {
         board = new Board();
     }
 
-    @DisplayName("보드의 초기화와 출력이 잘 되는지 확인한다.")
+    @DisplayName("보드의 초기화와 출력이 올바르게 되는지 확인한다.")
     @Test
-    public void check() throws Exception {
+    void check() {
         board.initializeBoard();
         String blankRank = appendNewLine("........");
         assertThat(board.showBoard())
                 .isEqualTo(appendNewLine("RNBQKBNR") +
-                appendNewLine("PPPPPPPP") +
-                blankRank + blankRank + blankRank + blankRank +
-                appendNewLine("pppppppp") +
-                appendNewLine("rnbqkbnr"));
+                        appendNewLine("PPPPPPPP") +
+                        blankRank + blankRank + blankRank + blankRank +
+                        appendNewLine("pppppppp") +
+                        appendNewLine("rnbqkbnr"));
+    }
+
+    @DisplayName("기물의 개수를 올바르게 반환하는지 확인한다.")
+    @Test
+    void count() {
+        board.initializeBoard();
+        assertThat(board.countPiece(Piece.Color.WHITE, Piece.Type.PAWN))
+                .isEqualTo(8);
+        assertThat(board.countPiece(Piece.Color.BLACK, Piece.Type.ROOK))
+                .isEqualTo(2);
+        assertThat(board.countPiece(Piece.Color.NOCOLOR, Piece.Type.NO_PIECE))
+                .isEqualTo(32);
     }
 }

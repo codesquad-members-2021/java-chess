a36a83febc9e8bbfe035a556278cf062c0012b4b
Haina <bibi6666667@kakao.com>
2021-02-24 16:04:25 +0900

    [step4] 각 Piece 테스트 메서드 추가
    Piece의 각 기물에 대한 테스트 메서드를 추가했습니다.
    (테스트 완료 - 성공)

--- src/test/java/pieces/PieceTest.java
+++ src/test/java/pieces/PieceTest.java
@@ -13,6 +13,42 @@ public class PieceTest {
         verifyPiece(Piece.createBlackPawn(), Piece.BLACK, Piece.BLACK_PAWN_REPRESENTATION);
     }
 
+    @Test
+    @DisplayName("흰색, 검은색 나이트 생성")
+    void createKnight() {
+        verifyPiece(Piece.createWhiteKnight(), Piece.WHITE, Piece.WHITE_KNIGHT_REPRESENTATION);
+        verifyPiece(Piece.createBlackKnight(), Piece.BLACK, Piece.BLACK_KNIGHT_REPRESENTATION);
+    }
+
+    @Test
+    @DisplayName("흰색, 검은색 룩 생성")
+    void createRook() {
+        verifyPiece(Piece.createWhiteRook(), Piece.WHITE, Piece.WHITE_ROOK_REPRESENTATION);
+        verifyPiece(Piece.createBlackRook(), Piece.BLACK, Piece.BLACK_ROOK_REPRESENTATION);
+    }
+
+    @Test
+    @DisplayName("흰색, 검은색 비숍 생성")
+    void createBishop() {
+        verifyPiece(Piece.createWhiteBishop(), Piece.WHITE, Piece.WHITE_BISHOP_REPRESENTATION);
+        verifyPiece(Piece.createBlackBishop(), Piece.BLACK, Piece.BLACK_BISHOP_REPRESENTATION);
+    }
+
+    @Test
+    @DisplayName("흰색, 검은색 퀸 생성")
+    void createQueen() {
+        verifyPiece(Piece.createWhiteQueen(), Piece.WHITE, Piece.WHITE_QUEEN_REPRESENTATION);
+        verifyPiece(Piece.createBlackQueen(), Piece.BLACK, Piece.BLACK_QUEEN_REPRESENTATION);
+    }
+
+    @Test
+    @DisplayName("흰색, 검은색 킹 생성")
+    void createKing() {
+        verifyPiece(Piece.createWhiteKing(), Piece.WHITE, Piece.WHITE_KING_REPRESENTATION);
+        verifyPiece(Piece.createBlackKing(), Piece.BLACK, Piece.BLACK_KING_REPRESENTATION);
+    }
+
+
     private void verifyPiece(final Piece piece, final String color, final String representation){
         assertThat(piece.getColor()).isEqualTo(color);
         assertThat(piece.getRepresentation()).isEqualTo(representation);

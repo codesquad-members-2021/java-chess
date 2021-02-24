75e0042fd89626b34981f6b69212598bb7ece044
noel <68936@naver.com>
2021-02-24 16:09:22 +0900

    [refactor] PieceFactory의 중복 메서드 제거
    - 가독성을 향상 시키고자 나눴던 메소드가 오히려 코드를 더 복잡하게 만들고, 기존 테스트 케이스도 재활용할 수 없어 다시 원래 메소드로 회귀 하였습니다.

--- src/main/java/net/sanhee/pieces/PieceFactory.java
+++ src/main/java/net/sanhee/pieces/PieceFactory.java
@@ -1,31 +1,14 @@
 package net.sanhee.pieces;
 
 public class PieceFactory {
-
-    //m  createWhite(), createBlack() 가독성을 위해 중복을 감안하고 분리하였습니다.
-
-    public Piece createWhite(UnitType unitType) {
-        Piece piece = null;
-        final UnitColor WHITE = UnitColor.WHITE;
-        switch (unitType) {
-            case KNIGHT: //m switch 문에서 열거형 타입 생략
-                piece = new Knight(UnitColor.WHITE);
-                break;
-            case PAWN:
-                piece = new Pawn(UnitColor.WHITE);
-                break;
-        }
-        return piece;
-    }
-
-    public Piece createBlack(UnitType unitType) {
+    public Piece create(UnitType unitType, UnitColor unitColor) {
         Piece piece = null;
         switch (unitType) {
             case KNIGHT: //m switch 문에서 열거형 타입 생략
-                piece = new Knight(UnitColor.BLACK);
+                piece = new Knight(unitColor);
                 break;
             case PAWN:
-                piece = new Pawn(UnitColor.BLACK);
+                piece = new Pawn(unitColor);
                 break;
         }
         return piece;

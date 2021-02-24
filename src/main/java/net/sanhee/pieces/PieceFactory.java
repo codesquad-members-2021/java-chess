8ffd4a30c2e886ef109259117fb4a315c5dd5fa8
noel <68936@naver.com>
2021-02-24 19:12:45 +0900

    [chore] PieceFactory의 팩토리 메소드 static 정의 및 스위치 조건 추가

--- src/main/java/net/sanhee/pieces/PieceFactory.java
+++ src/main/java/net/sanhee/pieces/PieceFactory.java
@@ -1,14 +1,29 @@
 package net.sanhee.pieces;
 
 public class PieceFactory {
-    public Piece create(UnitType unitType, UnitColor unitColor) {
+    public static Piece create(UnitType unitType, UnitColor unitColor) { //m 정적 팩토리 메서드
         Piece piece = null;
         switch (unitType) {
             case KNIGHT: //m switch 문에서 열거형 타입 생략
                 piece = new Knight(unitColor);
                 break;
+            case BISHOP:
+                piece = new Bishop(unitColor);
+                break;
+            case QUEEN:
+                piece = new Queen(unitColor);
+                break;
+            case KING:
+                piece = new King(unitColor);
+                break;
+            case ROOK:
+                piece = new Rook(unitColor);
+                break;
             case PAWN:
                 piece = new Pawn(unitColor);
+                break;
+            case NONE:
+                piece = new None(unitColor);
                 break;
         }
         return piece;

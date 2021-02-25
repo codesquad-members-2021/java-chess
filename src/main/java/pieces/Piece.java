041cec37f3397e3a573268c9566d8c759c7cc3ce
Haina <bibi6666667@kakao.com>
2021-02-25 22:17:03 +0900

    [step4] Piece 클래스 상수를 제어자 컨벤션에 따라 수정
    상수들의 선언부에서 public final static 타입 이 아닌
    public static final 타입 으로 변경했습니다.

--- src/main/java/pieces/Piece.java
+++ src/main/java/pieces/Piece.java
@@ -1,28 +1,28 @@
 package pieces;
 
 public class Piece {
-    public final static String WHITE = "white";
-    public final static String BLACK = "black";
+    public static final String WHITE = "white";
+    public static final String BLACK = "black";
 
-    public final static String PAWN = "pawn";
-    public final static String KNIGHT = "knight";
-    public final static String ROOK = "rook";
-    public final static String BISHOP = "bishop";
-    public final static String QUEEN = "queen";
-    public final static String KING = "king";
+    public static final String PAWN = "pawn";
+    public static final String KNIGHT = "knight";
+    public static final String ROOK = "rook";
+    public static final String BISHOP = "bishop";
+    public static final String QUEEN = "queen";
+    public static final String KING = "king";
 
-    public final static char WHITE_PAWN_SYMBOL = 'p';
-    public final static char WHITE_KNIGHT_SYMBOL = 'n';
-    public final static char WHITE_ROOK_SYMBOL = 'r';
-    public final static char WHITE_BISHOP_SYMBOL = 'b';
-    public final static char WHITE_QUEEN_SYMBOL = 'q';
-    public final static char WHITE_KING_SYMBOL = 'k';
-    public final static char BLACK_PAWN_SYMBOL = 'P';
-    public final static char BLACK_KNIGHT_SYMBOL = 'N';
-    public final static char BLACK_ROOK_SYMBOL = 'R';
-    public final static char BLACK_BISHOP_SYMBOL = 'B';
-    public final static char BLACK_QUEEN_SYMBOL = 'Q';
-    public final static char BLACK_KING_SYMBOL = 'K';
+    public static final char WHITE_PAWN_SYMBOL = 'p';
+    public static final char WHITE_KNIGHT_SYMBOL = 'n';
+    public static final char WHITE_ROOK_SYMBOL = 'r';
+    public static final char WHITE_BISHOP_SYMBOL = 'b';
+    public static final char WHITE_QUEEN_SYMBOL = 'q';
+    public static final char WHITE_KING_SYMBOL = 'k';
+    public static final char BLACK_PAWN_SYMBOL = 'P';
+    public static final char BLACK_KNIGHT_SYMBOL = 'N';
+    public static final char BLACK_ROOK_SYMBOL = 'R';
+    public static final char BLACK_BISHOP_SYMBOL = 'B';
+    public static final char BLACK_QUEEN_SYMBOL = 'Q';
+    public static final char BLACK_KING_SYMBOL = 'K';
 
     private final String name;
     private final String color;

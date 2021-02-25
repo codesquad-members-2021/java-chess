be0a9cd6ba2edb91182123c3b3c3ebc55839155c
JoonKi-Kim <joonki402@gmail.com>
2021-02-25 17:16:59 +0900

    기물의 point 부여

--- src/main/java/net/kjk402/chess/pieces/Piece.java
+++ src/main/java/net/kjk402/chess/pieces/Piece.java
@@ -8,18 +8,20 @@ public class Piece {
     }
 
     public enum Type {
-        PAWN('p'),
-        ROOK('r'),
-        KNIGHT('n'),
-        BISHOP('b'),
-        QUEEN('q'),
-        KING('k'),
-        NO_PIECE('.');
+        PAWN('p', 1.0),
+        ROOK('r', 5.0),
+        KNIGHT('n', 2.5),
+        BISHOP('b', 3.0),
+        QUEEN('q', 9.0),
+        KING('k',0.0),
+        NO_PIECE('.',0.0);
 
         private final char representation;
+        private final double defaultPoint;
 
-        Type(char representation) {
+        Type(char representation, double defaultPoint) {
             this.representation = representation;
+            this.defaultPoint = defaultPoint;
         }
 
         public char getWhiteRepresentation() {
@@ -28,6 +30,10 @@ public class Piece {
 
         public char getBlackRepresentation() {
             return Character.toUpperCase(this.representation);
+        }
+
+        public double getDefaultPoint() {
+            return defaultPoint;
         }
     }
 

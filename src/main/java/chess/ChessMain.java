6ade6d3d66be8024ccfa3c10c4a03cb9ca344fc2
MyungWanPark <mywanpark@gmail.com>
2021-02-25 19:37:05 +0900

    Refactor: Move initialize() for executing only once

--- src/main/java/chess/ChessMain.java
+++ src/main/java/chess/ChessMain.java
@@ -1,20 +1,21 @@
 package chess;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ChessMain {
     public static void main(String[] args) {
         Board board = new Board();
         System.out.println("게임을 시작합니다. ");
+
         try (Scanner scanner = new Scanner(System.in)) {
+            
+            board.initialize();
             while (true) {
                 System.out.println("명령어를 입력하세요:");
                 System.out.print("시작: start 종료: end  >>> ");
 
                 String command = scanner.nextLine();
                 if (command.equals("start")) {
-                    board.initialize();
                     System.out.println(board.bringBoard());
                 } else if (command.equals("end")) {
                     break;

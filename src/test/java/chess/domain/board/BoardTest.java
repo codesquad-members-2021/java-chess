4bab2339606eb273daaad9b29b8bd5d292e30efb
고정완 (Can) <jw.gho@ecubelabs.com>
2021-02-25 18:07:42 +0900

    [test] 주어진 위치의 기물을 조회
    이미 Board 에 getPiece 함수가 구현되어 있어서,
    그에 관한 테스트 코드를 작성

--- src/test/java/chess/domain/board/BoardTest.java
+++ src/test/java/chess/domain/board/BoardTest.java
@@ -1,5 +1,6 @@
 package chess.domain.board;
 
+import chess.domain.board.position.Position;
 import chess.domain.pieces.Pawn;
 import chess.domain.pieces.Rook;
 import chess.view.BoardView;
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class BoardTest {
     private Board board;
@@ -31,5 +33,20 @@ class BoardTest {
     void count_whiteRook() {
         assertThat(board.count(Rook.ofWhite()))
                 .isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("주어진 기물의 위치를 조회")
+    void getPiece() {
+        assertAll(
+                () -> assertThat(board.getPiece(Position.of("a8")))
+                        .isEqualTo(Rook.ofBlack()),
+                () -> assertThat(board.getPiece(Position.of("h8")))
+                        .isEqualTo(Rook.ofBlack()),
+                () -> assertThat(board.getPiece(Position.of("a1")))
+                        .isEqualTo(Rook.ofWhite()),
+                () -> assertThat(board.getPiece(Position.of("h1")))
+                        .isEqualTo(Rook.ofWhite())
+        );
     }
 }

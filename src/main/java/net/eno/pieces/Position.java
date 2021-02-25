8c863a5bd56c6e23ddf89e6afb25d49bb7a4bf4e
jeong-inho <derosatam76@gmail.com>
2021-02-25 18:22:14 +0900

    delete position validation in Position class Position class에 기존에 있던 exception과 유효성검사 부분을 불필요하다 판단하여 삭제

--- src/main/java/net/eno/pieces/Position.java
+++ src/main/java/net/eno/pieces/Position.java
@@ -2,28 +2,12 @@ package net.eno.pieces;
 
 public class Position {
 
-    private char file;
-    private int rank;
+    private final char file;
+    private final int rank;
 
     public Position(String position) {
-        setPosition(position);
-    }
-
-    private void setPosition(String position) {
-        try {
-            char file = position.charAt(0);
-            int rank = Character.getNumericValue(position.charAt(1));
-            if (position.length() == 2 &&
-                    (97 <= file && file <= 104) &&
-                    (1 <= rank && rank <= 8)) {
-                this.file = file;
-                this.rank = rank;
-            } else {
-                System.out.println("location range error");
-            }
-        } catch (Exception e) {
-            System.out.println("location parsing error");
-        }
+        this.file = position.charAt(0);
+        this.rank = Character.getNumericValue(position.charAt(1));
     }
 
     public int getFileIndex() {

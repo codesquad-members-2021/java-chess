cedbcfc4ea6d724dc622a3b61e14b0f3219d51d6
rla36 <bhmarsd@naver.com>
2021-02-25 20:50:55 +0900

    feat: Piece의 equals와 hashCode 오버라이드
    Piece가 동일한 파일, 랭크이면 동일한 값으로 인식되도록(VO) equals와
    hashCode을 오버라이드했습니다.

--- src/main/java/net/jung/chess/Position.java
+++ src/main/java/net/jung/chess/Position.java
@@ -4,17 +4,17 @@ public class Position {
     private int fileIndex;
     private int rankIndex;
 
-    private Position(String position) {
-            fileIndex = position.toCharArray()[0]-'a';
-            rankIndex = position.toCharArray()[1]-'0';
+    private Position(String location) {
+        fileIndex = location.toCharArray()[0] - 'a';
+        rankIndex = location.toCharArray()[1] - '0';
     }
 
     public static Position at(String location) throws IllegalArgumentException {
         char[] positionChar = location.toCharArray();
-        if( !(location.length()==2
+        if (!(location.length() == 2
                 && isValidFile(positionChar[0])
                 && isValidRank(positionChar[1]))) {
-            throw new IllegalArgumentException ("a1~h8 범위의 위치를 입력해주세요.");
+            throw new IllegalArgumentException("a1~h8 범위의 위치를 입력해주세요.");
         }
         return new Position(location);
     }
@@ -24,7 +24,7 @@ public class Position {
     }
 
     private static boolean isValidRank(char rankNum) {
-            return (rankNum >='1' && rankNum <= '8');
+        return (rankNum >= '1' && rankNum <= '8');
     }
 
     public int getFileIndex() {
@@ -34,4 +34,26 @@ public class Position {
     public int getRankIndex() {
         return rankIndex;
     }
+
+    public boolean isCertainFile(int index) {
+        return getFileIndex() == index ;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj != null & obj instanceof Position) {
+            Position positionObj = (Position) obj;
+            boolean compareFileIndex = fileIndex == positionObj.fileIndex;
+            boolean compareRankIndex = rankIndex == positionObj.rankIndex;
+            return compareFileIndex && compareRankIndex;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return fileIndex + rankIndex;
+    }
 }
+
+

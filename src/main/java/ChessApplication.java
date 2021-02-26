c899c0e17679bcd4b40edbd49bdc0e2aebb7e8dd
janeljs <jisunlim818@gmail.com>
2021-02-26 21:31:02 +0900

    Move parts of ChessApplication to ChessGame class

--- src/main/java/ChessApplication.java
+++ src/main/java/ChessApplication.java
@@ -1,47 +1,10 @@
 import chess.ChessGame;
-import chess.ChessView;
-
-import java.util.Locale;
-import java.util.Scanner;
 
 public class ChessApplication {
-    private static final String START = "start";
-    private static final String END = "end";
-    private static final String MOVE = "move";
-    private static final String PROMPT = "> ";
 
     public static void main(String[] args) {
-        System.out.println("체스 게임을 시작합니다.");
-        System.out.println("시작: start, 종료: end");
-
         ChessGame chessGame = new ChessGame();
-        ChessView chessView = new ChessView();
-
-        try (Scanner sc = new Scanner(System.in)) {
-            String command = "";
-            while (true) {
-                System.out.print(PROMPT);
-                command = sc.nextLine().toLowerCase(Locale.ROOT);
-                if (command.equals(START)) {
-                    chessGame.initialize();
-                    System.out.println(chessView.view(chessGame.getBoard()));
-                    continue;
-                }
-                if (command.startsWith(MOVE)) {
-                    String[] commands = command.split(" ");
-                    chessGame.move(commands[1], commands[2]);
-                    Thread.sleep(1000);
-                    System.out.println(chessView.view(chessGame.getBoard()));
-                    continue;
-                }
-                if (command.equals(END)) {
-                    System.out.println("체스게임이 끝났습니다.");
-                    break;
-                }
-                System.out.println("명령어를 다시 입력해주세요.");
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        chessGame.runPrompt();
     }
 }
+
--- src/main/java/chess/ChessGame.java
+++ src/main/java/chess/ChessGame.java
@@ -1,9 +1,17 @@
 package chess;
 
+import piece.PositionNotMovableException;
 import piece.attribute.Color;
+
+import java.util.Locale;
+import java.util.Scanner;
 
 
 public class ChessGame {
+    private final String START = "start";
+    private final String END = "end";
+    private final String MOVE = "move";
+    private final String PROMPT = "> ";
     private final Board board = new Board();
 
     public Board getBoard() {
@@ -20,6 +28,43 @@ public class ChessGame {
 
     public double getPoint(Color color) {
         return board.calculatePoint(color);
+    }
+
+    public void runPrompt() {
+        System.out.println("체스 게임을 시작합니다.");
+        System.out.println("시작: start, 종료: end");
+
+        ChessView chessView = new ChessView();
+
+        try (Scanner sc = new Scanner(System.in)) {
+            String command = "";
+            while (true) {
+                System.out.print(PROMPT);
+                command = sc.nextLine().toLowerCase(Locale.ROOT);
+                if (command.equals(START)) {
+                    initialize();
+                    System.out.println(chessView.view(getBoard()));
+                    continue;
+                }
+                if (command.startsWith(MOVE)) {
+                    String[] commands = command.split(" ");
+                    try {
+                        move(commands[1], commands[2]);
+                    } catch (PositionNotMovableException e) {
+                        System.out.println(e.getMessage());
+                    }
+                    System.out.println(chessView.view(getBoard()));
+                    continue;
+                }
+                if (command.equals(END)) {
+                    System.out.println("WHITE TEAM : " + getPoint(Color.WHITE));
+                    System.out.println("BLACK TEAM : " + getPoint(Color.BLACK));
+                    System.out.println("Game Over");
+                    break;
+                }
+                System.out.println("Please enter the command again.");
+            }
+        }
     }
 }
 

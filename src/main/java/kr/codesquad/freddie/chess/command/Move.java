7befaf6a6e6060fbe8dcbfc2c3b8c9e8b5209324
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-03-03 02:33:26 +0900

    Fix Move to throw err when argument size is wrong

--- src/main/java/kr/codesquad/freddie/chess/command/Move.java
+++ src/main/java/kr/codesquad/freddie/chess/command/Move.java
@@ -2,10 +2,15 @@ package kr.codesquad.freddie.chess.command;
 
 import kr.codesquad.freddie.chess.game.ChessGame;
 
+import java.util.Arrays;
+
 public class Move implements Command {
     private String[] command;
 
     public Move(String[] command) {
+        if (command.length != 3) {
+            throw new IllegalArgumentException("잘못된 입력입니다." + Arrays.toString(command));
+        }
         this.command = command;
     }
 
